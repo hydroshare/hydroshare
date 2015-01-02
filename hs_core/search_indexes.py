ad341d73517c85c7348835aaa70c5de533dfae66
Calvin Spealman <calvin@caktusgroup.com>
2015-01-02 16:00:19 -0500

    Remove a line included only for debugging purposes

--- hs_core/search_indexes.py
+++ hs_core/search_indexes.py
@@ -17,7 +17,6 @@ class GenericResourceIndex(indexes.SearchIndex, indexes.Indexable):
         return obj.description
 
     def prepare(self, obj):
-        1/0
         dublindata = obj.get_dublin_metadata()
         data = super(GenericResourceIndex, self).prepare(obj)
         data.update(dublindata)
