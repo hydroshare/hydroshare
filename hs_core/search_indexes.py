53e08c9a012745ab11acc4a10a46c7155d4cb26c
Calvin Spealman <calvin@caktusgroup.com>
2015-01-02 08:51:51 -0500

    Index Dublincore metadata and pull keywords properly form Mezzanine keywords for indexing

--- hs_core/search_indexes.py
+++ hs_core/search_indexes.py
@@ -1,16 +1,23 @@
 from haystack import indexes
 from hs_core.models import GenericResource
+from dublincore.models import AbstractQualifiedDublinCoreTerm
 
 
 class GenericResourceIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True)
     keywords = indexes.MultiValueField()
 
+    for code, name in AbstractQualifiedDublinCoreTerm.DCTERMS:
+        locals()[name] = indexes.CharField(null=True)
+
     def prepare_keywords(self, obj):
-        return obj.get_dublin_metadata().get('keywords', '').split(',')
+        return [k.keyword.title for k in obj.keywords.all()]
 
     def prepare_text(self, obj):
         return obj.description
+
+    def prepare(self, obj):
+        dublindata = obj.get_dublin_metadata()
 
     def get_model(self):
         return GenericResource
