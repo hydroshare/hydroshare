8d5869d38971ef6174e1b53aec3f1179cbe9f793
jsadler2 <jeffrey.sadler2@gmail.com>
2015-01-16 13:11:51 -0700

    raster makemigrations

--- hs_geo_raster_resource/migrations/0001_initial.py
+++ hs_geo_raster_resource/migrations/0001_initial.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 import hs_core.models
@@ -151,7 +150,7 @@ class Migration(SchemaMigration):
         u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
