9b07d27a7902869cd64c118bc76cd370d7fb202d
jamy <gantian127@gmail.com>
2015-02-09 12:15:25 -0700

    Iss: nc frontend view mode

--- hs_app_netCDF/page_processors.py
+++ hs_app_netCDF/page_processors.py
@@ -12,18 +12,25 @@ from django.forms.models import formset_factory
 # when the resource is created this page will be shown
 def landing_page(request, page):
     content_model = page.get_content_model()
-    edit_mode = False
-    if content_model.creator == request.user or request.user in content_model.owners.all() or request.user in content_model.edit_users.all():
-        edit_mode = True
-    if request.method == 'GET':
-        VariableFormSetEdit = formset_factory(wraps(VariableForm)(partial(VariableForm, allow_edit=edit_mode)), formset=BaseVariableFormSet, extra=0)
+    edit_resource = page_processors.check_resource_mode(request)
+
+    if not edit_resource:
+        # get the context from hs_core
+        context = page_processors.get_page_context(page, request.user, resource_edit=edit_resource, extended_metadata_layout=None)
+        extended_metadata_exists = False
+        if content_model.metadata.variables:
+            extended_metadata_exists = True
+
+        context['extended_metadata_exists'] = extended_metadata_exists
+        context['variables'] = content_model.metadata.variable #TODO not clear variable.all()?
+    else:
+        VariableFormSetEdit = formset_factory(wraps(VariableForm)(partial(VariableForm, allow_edit=edit_resource)), formset=BaseVariableFormSet, extra=0)
         variable_formset = VariableFormSetEdit(initial=content_model.metadata.variables.all().values(), prefix='variable')
-        add_variable_modal_form = VariableForm(allow_edit=edit_mode, res_short_id=content_model.short_id)
+        add_variable_modal_form = VariableForm(allow_edit=edit_resource, res_short_id=content_model.short_id)
         ext_md_layout = Layout(
                                 VariableLayoutEdit,
                                 ModalDialogLayoutAddVariable
                             )
-
         for form in variable_formset.forms:
             if len(form.initial) > 0:
                 form.delete_modal_form = MetaDataElementDeleteForm(content_model.short_id, 'variable', form.initial['id'])
