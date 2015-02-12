1f912f55ff7e7f43d4c785486f2b46845f0ec094
Brian Miles <hydrobuild@codefest.renci.org>
2015-02-12 14:27:52 -0500

    Finished initial implementation of run-model user interface

--- hs_rhessys_inst_resource/urls.py
+++ hs_rhessys_inst_resource/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 
 from hs_rhessys_inst_resource.views import RunModelView
-
+from hs_rhessys_inst_resource.views import GetRunInfoJSON
 
 urlpatterns = patterns('',
-                       (r'(?P<resource_short_id>[A-z0-9]+)/runmodel/$', RunModelView.as_view()),
+                       url(r'(?P<resource_short_id>[A-z0-9]+)/runmodel/$', RunModelView.as_view(),
+                           name='hs_rhessys_inst_resource-runmodel'),
+                       url(r'(?P<resource_short_id>[A-z0-9]+)/getruninfo(?:/(?P<token>[A-z0-9\-]+))?/$', 
+                           GetRunInfoJSON.as_view(), # Note: token element is optional
+                           name='hs_rhessys_inst_resource-getruninfo'),
                        )
--- hs_rhessys_inst_resource/views.py
+++ hs_rhessys_inst_resource/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
@@ -6,6 +8,42 @@ from django.core.urlresolvers import reverse
 
 from .models import *
 from .forms import RunModelForm
+
+def _getModelRunsForModel(request, model, token=None):
+    #import logging
+    #logger = logging.getLogger('django')
+    if token:
+        model_runs = ModelRun.objects.filter(model_instance=model, docker_process__token=token)
+    else:
+        model_runs = ModelRun.objects.filter(model_instance=model)
+        
+    run_info = []
+    for run in model_runs:
+        my_run = {}
+
+        my_run['token'] = run.docker_process.token
+        poll_url = reverse('hs_rhessys_inst_resource-getruninfo', kwargs={
+                'resource_short_id': model.short_id,
+                'token': run.docker_process.token
+                })
+        poll_url = request.build_absolute_uri(poll_url)
+        my_run['poll_url'] = poll_url
+        
+        my_run['title'] = run.title
+        my_run['description'] = run.description
+        my_run['model_command_line_parameters'] = run.model_command_line_parameters
+        my_run['status'] = run.status
+        my_run['date_started'] = str(run.date_started)
+        my_run['date_finished'] = str(run.date_finished)
+        
+        if run.model_results:
+            results_url = "/r/{0}/".format(run.model_results.short_id) # TODO: use reverse to build this URL
+            results_url = request.build_absolute_uri(results_url)
+            my_run['results_url'] = results_url
+            
+        run_info.append(my_run)
+    return run_info
+
 
 class RunModelView(View):
     template_name = 'pages/runmodel.html'
@@ -20,39 +58,26 @@ class RunModelView(View):
 
         kwargs['form'] = RunModelForm()
 
-        import logging
-        logger = logging.getLogger('django')
-        logger.info("Model runs for model: {0}".format(kwargs['resource_short_id']))
-
-        model_runs = ModelRun.objects.filter(model_instance=my_model)
-        run_info = []
-        for run in model_runs:
-            my_run = {}
-
-            docker_process_poll_url = reverse('docker-process-poll-status', kwargs={
-                    'profile_name': run.docker_process.profile.name,
-                    'token': run.docker_process.token
-                    })
-            docker_process_poll_url = request.build_absolute_uri(docker_process_poll_url)
-            my_run['docker_process_poll_url'] = docker_process_poll_url
-
-            my_run['title'] = run.title
-            my_run['description'] = run.description
-            my_run['model_command_line_parameters'] = run.model_command_line_parameters
-            my_run['status'] = run.status
-            my_run['date_started'] = str(run.date_started)
-            my_run['date_finished'] = str(run.date_finished)
-
-            if run.model_results:
-                results_url = "/r/{0}/".format(run.model_results.short_id)
-                results_url = request.build_absolute_uri(results_url)
-                my_run['results_url'] = results_url
-
-            run_info.append(my_run)
-
+        run_info = _getModelRunsForModel(request, my_model)
         kwargs['run_info'] = run_info
+
+        get_run_info_url = reverse('hs_rhessys_inst_resource-getruninfo', kwargs={
+                'resource_short_id': kwargs['resource_short_id']
+                })
+        get_run_info_url = request.build_absolute_uri(get_run_info_url)
+        kwargs['get_run_info_url'] = get_run_info_url
 
         return render(request, self.template_name, kwargs)
 
     def post(self, request, *args, **kwargs):
         return render(request, self.template_name, kwargs)
+
+
+class GetRunInfoJSON(View):
+    def get(self, request, *args, **kwargs):
+        my_model = get_object_or_404(InstResource, short_id=kwargs['resource_short_id'])
+        token = None
+        if kwargs.has_key('token'):
+            token = kwargs['token']
+        response_data = _getModelRunsForModel(request, my_model, token)
+        return HttpResponse(json.dumps(response_data), content_type='application/json')
