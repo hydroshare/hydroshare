6952647ba9134e051ea9a09c189ba905910f1c62
jamy <gantian127@gmail.com>
2015-03-03 17:41:37 -0700

    Iss: ncdump function (D)

--- hs_app_netCDF/receivers.py
+++ hs_app_netCDF/receivers.py
@@ -2,6 +2,7 @@ from django.dispatch import receiver
 from hs_core.signals import *
 from hs_app_netCDF.models import NetcdfResource, NetcdfMetaData, Variable
 from hs_app_netCDF.forms import *
+from hs_core import hydroshare
 
 
 # receiver used to extract metadata after user click on "create resource"
@@ -22,7 +23,6 @@ def netcdf_create_resource_trigger(sender, **kwargs):
                 res_dublin_core_meta = {}
                 res_type_specific_meta = {}
 
-            # For dict key name and structure refer to the hs_core/models.py
             # add title
             if res_dublin_core_meta.get('title'):
                 title = {'title': {'value': res_dublin_core_meta['title']}}
@@ -56,7 +56,29 @@ def netcdf_create_resource_trigger(sender, **kwargs):
                         meta_info[element] = value
                 metadata.append({'variable': meta_info})
 
-            return metadata
+@receiver(post_create_resource,sender=NetcdfResource)
+def netcdf_create_ncdump_file(sender, **kwargs):
+    if sender is NetcdfResource:
+        nc_res = kwargs['resource']
+        nc_file = nc_res.files.all()[0]
+        nc_file_name = nc_file.resource_file.path
+
+        # create InMemoryUploadedFile text file to store the dump info and add it to resource
+        import nc_functions.nc_dump as nc_dump
+        if nc_dump.get_nc_dump_string_by_ncdump(nc_file_name):
+            dump_str = nc_dump.get_nc_dump_string_by_ncdump(nc_file_name)
+        else:
+            dump_str = nc_dump.get_nc_dump_string(nc_file_name)
+
+        if dump_str:
+            from django.core.files.uploadedfile import InMemoryUploadedFile
+            import StringIO, os
+            io = StringIO.StringIO()
+            io.write(dump_str)
+            dump_file_name = '.'.join(os.path.basename(nc_file_name).split('.')[:-1])+'_header_info.txt'
+            dump_file = InMemoryUploadedFile(io, None, dump_file_name, 'text', io.len, None)
+            dump_file.seek(0)
+            hydroshare.add_resource_files(nc_res.short_id, dump_file)
 
 
 @receiver(pre_metadata_element_create, sender=NetcdfResource)
