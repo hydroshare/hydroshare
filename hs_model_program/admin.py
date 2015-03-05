c6d800266bee45f01ed841b5a61aab4eddbc765c
Tony Castronova <castronova.anthony@gmail.com>
2015-03-05 09:28:39 -0700

    cleaned up the layout and language used on the landing page.  Removed old/unnecessary code.

--- hs_model_program/admin.py
+++ hs_model_program/admin.py
@@ -1,10 +1,3 @@
-#from mezzanine.pages.admin import PageAdmin
-#from django.contrib.gis import admin
-#from .models import *
-
-#admin.site.register(HydroProgramResource, PageAdmin)
-
-
 from mezzanine.pages.admin import PageAdmin
 from django.contrib import admin
 from .models import *
