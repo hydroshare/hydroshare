3d43fcba976d299bf54c8c0bcc1a34aafa9cf442
gantian127 <gantianhejimao@yahoo.com.cn>
2015-03-04 19:09:05 -0600

    Iss: ncdump function (D): add pre_add_file and pre_delete_filereceiver functions

--- hs_app_netCDF/receivers.py
+++ hs_app_netCDF/receivers.py
@@ -3,7 +3,8 @@ from hs_core.signals import *
 from hs_app_netCDF.models import NetcdfResource, NetcdfMetaData, Variable
 from hs_app_netCDF.forms import *
 from hs_core import hydroshare
-
+from hs_core.hydroshare.resource import ResourceFile
+import os
 
 # receiver used to extract metadata after user click on "create resource"
 @receiver(pre_create_resource, sender=NetcdfResource)
@@ -56,6 +57,7 @@ def netcdf_create_resource_trigger(sender, **kwargs):
                         meta_info[element] = value
                 metadata.append({'variable': meta_info})
 
+# receiver used to create netcdf header text after user click on "create resource"
 @receiver(post_create_resource,sender=NetcdfResource)
 def netcdf_create_ncdump_file(sender, **kwargs):
     if sender is NetcdfResource:
@@ -80,6 +82,96 @@ def netcdf_create_ncdump_file(sender, **kwargs):
                 dump_file = InMemoryUploadedFile(io, None, dump_file_name, 'text', io.len, None)
                 dump_file.seek(0)
                 hydroshare.add_resource_files(nc_res.short_id, dump_file)
+
+# receiver used after user clicks on "delete file" for existing netcdf file
+@receiver(pre_delete_file_from_resource, sender=NetcdfResource)
+def netcdf_pre_delete_file_from_resource(sender, **kwargs):
+    if sender is NetcdfResource:
+        nc_res = kwargs['resource']
+        del_file = kwargs['file']
+        del_file_ext = os.path.splitext(del_file.resource_file.name)[-1]
+        if del_file_ext == '.nc':
+            # delete all the coverage info
+            cov_box = nc_res.metadata.coverages.all().filter(type='box').first()
+            cov_period = nc_res.metadata.coverages.all().filter(type='period').first()
+            if cov_box:
+                nc_res.metadata.delete_element('coverage', cov_box.id)
+            if cov_period:
+                nc_res.metadata.delete_element('coverage', cov_period.id)
+
+            # delete all the extended meta info
+            for variable in nc_res.metadta.variable:
+                variable.delete()
+
+            # delete the netcdf header text file
+            for f in ResourceFile.objects.filter(object_id=nc_res.id):
+                ext = os.path.splitext(f.resource_file.name)[-1]
+                if ext == '.txt':
+                    f.resource_file.delete()
+                    f.delete()
+                    break
+
+# receiver used after user clicks on "add file" for existing resource netcdf file
+@receiver(pre_add_files_to_resource, sender=NetcdfResource)
+def netcdf_pre_delete_file_from_resource(sender, **kwargs):
+    if sender is NetcdfResource:
+        nc_res = kwargs['resource']
+        files = kwargs['files']
+        if files:
+            infile = files[0]
+            if os.path.splitext(infile.file.name)[-1] == '.nc':
+                # delete all existing resource files and metadata related
+                for f in ResourceFile.objects.filter(object_id=nc_res.id):
+                    f.resource_file.delete()
+                    f.delete()
+
+                # extract metadata
+                import nc_functions.nc_meta as nc_meta
+                try:
+                    res_md_dict = nc_meta.get_nc_meta_dict(infile.file.name)
+                    res_dublin_core_meta = res_md_dict['dublin_core_meta']
+                    res_type_specific_meta = res_md_dict['type_specific_meta']
+                except:
+                    res_dublin_core_meta = {}
+                    res_type_specific_meta = {}
+
+                # update box info
+                cov_box = nc_res.metadata.coverages.all().filter(type='box').first()
+                if cov_box:
+                    nc_res.metadata.delete_element('coverage', cov_box.id)
+                if res_dublin_core_meta.get('box'):
+                    nc_res.metadata.create_element('coverage', type='box', value=res_dublin_core_meta['box'])
+
+                # update period info
+                cov_period = nc_res.metadata.coverages.all().filter(type='period').first()
+                if cov_period:
+                    nc_res.metadata.delete_element('coverage', cov_period.id)
+                if res_dublin_core_meta.get('period'):
+                    nc_res.metadata.create_element('coverage', type='period', value=res_dublin_core_meta['period'])
+
+                # update variable info
+                for variable in nc_res.metadta.variable:
+                    variable.delete()
+                for var_info in res_type_specific_meta.values():
+                    nc_res.metadta.create_element('variable', var_info)
+
+                # create a header text file and add to resource
+                nc_file_name = infile.file.name
+                # create InMemoryUploadedFile text file to store the dump info and add it to resource
+                import nc_functions.nc_dump as nc_dump
+                if nc_dump.get_nc_dump_string_by_ncdump(nc_file_name):
+                    dump_str = nc_dump.get_nc_dump_string_by_ncdump(nc_file_name)
+                else:
+                    dump_str = nc_dump.get_nc_dump_string(nc_file_name)
+                if dump_str:
+                    from django.core.files.uploadedfile import InMemoryUploadedFile
+                    import StringIO,os
+                    io = StringIO.StringIO()
+                    io.write(dump_str)
+                    dump_file_name = '.'.join(os.path.basename(nc_file_name).split('.')[:-1])+'_header_info.txt'
+                    dump_file = InMemoryUploadedFile(io, None, dump_file_name, 'text', io.len, None)
+                    dump_file.seek(0)
+                    hydroshare.add_resource_files(nc_res.short_id, dump_file)
 
 
 @receiver(pre_metadata_element_create, sender=NetcdfResource)
