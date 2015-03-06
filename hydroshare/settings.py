e51935dd4977c82ca3a142a53f932546595aefe3
Tony Castronova <castronova.anthony@gmail.com>
2015-03-06 13:51:26 -0700

    Added foreign key relationship

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -286,11 +286,11 @@ INSTALLED_APPS = (
     "hs_geo_raster_resource",
     "djcelery",
     "ref_ts",
-    "hs_modelinstance",
     "hs_app_timeseries",
     "widget_tweaks",
     "hs_app_netCDF",
     "hs_model_program",
+    "hs_modelinstance",
 )
 
 # List of processors used by RequestContext to populate the context.
