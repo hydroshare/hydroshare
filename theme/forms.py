f8eae13527b6f19ab9a1f1b58d3e90ab237a2811
hyi <hongyi@renci.org>
2015-03-09 18:04:52 -0400

    first phase of implementation to address issue #186

---
+++ theme/forms.py
@@ -0,0 +1,194 @@
+from mezzanine.conf import settings
+from django import forms
+from mezzanine.core.forms import Html5Mixin
+from mezzanine.generic.models import ThreadedComment
+from django.utils.translation import ugettext, ugettext_lazy as _
+from mezzanine.utils.views import ip_for_request
+from django.contrib.comments.signals import comment_was_posted
+from mezzanine.utils.email import split_addresses, send_mail_template
+from mezzanine.utils.cache import add_cache_bypass
+from django.contrib.comments.forms import CommentSecurityForm
+from django.contrib.comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
+from django.utils.encoding import force_text
+from django.utils import timezone
+from django.utils.text import get_text_list
+from django.utils.translation import ungettext
+
+COMMENT_MAX_LENGTH = getattr(settings,'COMMENT_MAX_LENGTH', 3000)
+
+# This form.py is added by Hong Yi for customizing comments in HydroShare
+# as part of effort to address issue https://github.com/hydroshare/hydroshare/issues/186
+# In particular, we want to remove name, email, and url fields and
+# want to link comments to user profile
+class CommentDetailsForm(CommentSecurityForm):
+    """
+    Handles the specific details of the comment (name, comment, etc.).
+    """
+    #name          = forms.CharField(label=_("Name"), max_length=50, widget=forms.HiddenInput())
+    #email         = forms.EmailField(label=_("Email address"))
+    #url           = forms.URLField(label=_("URL"), required=False)
+    comment       = forms.CharField(label=_('Comment'), widget=forms.Textarea,
+                                    max_length=COMMENT_MAX_LENGTH)
+
+    def get_comment_object(self):
+        """
+        Return a new (unsaved) comment object based on the information in this
+        form. Assumes that the form is already validated and will throw a
+        ValueError if not.
+
+        Does not set any of the fields that would come from a Request object
+        (i.e. ``user`` or ``ip_address``).
+        """
+        if not self.is_valid():
+            raise ValueError("get_comment_object may only be called on valid forms")
+
+        CommentModel = self.get_comment_model()
+        new = CommentModel(**self.get_comment_create_data())
+        new = self.check_for_duplicate_comment(new)
+
+        return new
+
+    def get_comment_model(self):
+        """
+        Get the comment model to create with this form. Subclasses in custom
+        comment apps should override this, get_comment_create_data, and perhaps
+        check_for_duplicate_comment to provide custom comment models.
+        """
+        return Comment
+
+    def get_comment_create_data(self):
+        """
+        Returns the dict of data to be used to create a comment. Subclasses in
+        custom comment apps that override get_comment_model can override this
+        method to add extra fields onto a custom comment model.
+        """
+        return dict(
+            content_type=ContentType.objects.get_for_model(self.target_object),
+            object_pk=force_text(self.target_object._get_pk_val()),
+            comment=self.cleaned_data["comment"],
+            submit_date=timezone.now(),
+            site_id=settings.SITE_ID,
+            is_public=True,
+            is_removed=False,
+        )
+
+    def check_for_duplicate_comment(self, new):
+        """
+        Check that a submitted comment isn't a duplicate. This might be caused
+        by someone posting a comment twice. If it is a dup, silently return the *previous* comment.
+        """
+        possible_duplicates = self.get_comment_model()._default_manager.using(
+            self.target_object._state.db
+        ).filter(
+            content_type=new.content_type,
+            object_pk=new.object_pk,
+            user_name=new.user_name,
+            user_email=new.user_email,
+            user_url=new.user_url,
+        )
+        for old in possible_duplicates:
+            if old.submit_date.date() == new.submit_date.date() and old.comment == new.comment:
+                return old
+
+        return new
+
+    def clean_comment(self):
+        """
+        If COMMENTS_ALLOW_PROFANITIES is False, check that the comment doesn't
+        contain anything in PROFANITIES_LIST.
+        """
+        comment = self.cleaned_data["comment"]
+        if settings.COMMENTS_ALLOW_PROFANITIES == False:
+            bad_words = [w for w in settings.PROFANITIES_LIST if w in comment.lower()]
+            if bad_words:
+                raise forms.ValidationError(ungettext(
+                    "Watch your mouth! The word %s is not allowed here.",
+                    "Watch your mouth! The words %s are not allowed here.",
+                    len(bad_words)) % get_text_list(
+                        ['"%s%s%s"' % (i[0], '-'*(len(i)-2), i[-1])
+                         for i in bad_words], ugettext('and')))
+        return comment
+
+class CommentForm(CommentDetailsForm):
+    honeypot      = forms.CharField(required=False,
+                                    label=_('If you enter anything in this field '\
+                                            'your comment will be treated as spam'))
+
+    def clean_honeypot(self):
+        """Check that nothing's been entered into the honeypot."""
+        value = self.cleaned_data["honeypot"]
+        if value:
+            raise forms.ValidationError(self.fields["honeypot"].label)
+        return value
+
+# added by Hong Yi for customizing THreadedCommentForm
+class ThreadedCommentForm(CommentForm, Html5Mixin):
+
+    #name = forms.CharField(label=_("Name"), help_text=_("required"),
+    #                       max_length=50, widget=forms.HiddenInput())
+    #email = forms.EmailField(label=_("Email"),
+    #                         help_text=_("required (not published)"))
+    #url = forms.URLField(label=_("Website"), help_text=_("optional"),
+    #                     required=False)
+
+    # These are used to get/set prepopulated fields via cookies.
+    #cookie_fields = ("name", "email", "url")
+    #cookie_prefix = "mezzanine-comment-"
+
+    def __init__(self, request, *args, **kwargs):
+        """
+        Set some initial field values from cookies or the logged in
+        user, and apply some HTML5 attributes to the fields if the
+        ``FORMS_USE_HTML5`` setting is ``True``.
+        """
+        kwargs.setdefault("initial", {})
+        user = request.user
+        # for field in ThreadedCommentForm.cookie_fields:
+        #     cookie_name = ThreadedCommentForm.cookie_prefix + field
+        #     value = request.COOKIES.get(cookie_name, "")
+        #     if not value and user.is_authenticated():
+        #         if field == "name":
+        #             value = user.get_full_name()
+        #             if not value and user.username != user.email:
+        #                 value = user.username
+        #         elif field == "email":
+        #             value = user.email
+        #     kwargs["initial"][field] = value
+        super(ThreadedCommentForm, self).__init__(*args, **kwargs)
+
+    def get_comment_model(self):
+        """
+        Use the custom comment model instead of the built-in one.
+        """
+        return ThreadedComment
+
+    def save(self, request):
+        """
+        Saves a new comment and sends any notification emails.
+        """
+        comment = self.get_comment_object()
+        obj = comment.content_object
+        if request.user.is_authenticated():
+            comment.user = request.user
+            comment.user.first_name = request.user.get_short_name()
+            comment.user.last_name = request.user.get_full_name()
+        comment.by_author = request.user == getattr(obj, "user", None)
+        comment.ip_address = ip_for_request(request)
+        comment.replied_to_id = self.data.get("replied_to")
+        comment.save()
+        comment_was_posted.send(sender=comment.__class__, comment=comment,
+                                request=request)
+        notify_emails = split_addresses(settings.COMMENTS_NOTIFICATION_EMAILS)
+        if notify_emails:
+            subject = ugettext("New comment for: ") + str(obj)
+            context = {
+                "comment": comment,
+                "comment_url": add_cache_bypass(comment.get_absolute_url()),
+                "request": request,
+                "obj": obj,
+            }
+            send_mail_template(subject, "email/comment_notification",
+                               settings.DEFAULT_FROM_EMAIL, notify_emails,
+                               context)
+        return comment
