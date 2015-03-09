42eeb14fd7d88e72dbd0a6ebaffabf984df64aaa
hyi <hongyi@renci.org>
2015-03-09 18:04:52 -0400

    first phase of implementation to address issue #186

--- theme/views.py
+++ theme/views.py
@@ -2,7 +2,13 @@
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
 from hs_core.hydroshare import get_resource_types
-
+from mezzanine.generic.views import initial_validation
+from django.http import HttpResponse
+from .forms import ThreadedCommentForm
+from mezzanine.utils.views import render, set_cookie, is_spam
+from django.shortcuts import redirect
+from mezzanine.utils.cache import add_cache_bypass
+from json import dumps
 
 class UserProfileView(TemplateView):
     template_name='accounts/profile.html'
@@ -29,3 +35,33 @@ class UserProfileView(TemplateView):
             'u' : u,
             'resources' :  res
         }
+
+# added by Hong Yi to address issue #186 to customize Mezzanine-based commenting form and view
+def comment(request, template="generic/comments.html"):
+    """
+    Handle a ``ThreadedCommentForm`` submission and redirect back to its
+    related object.
+    """
+    response = initial_validation(request, "comment")
+    if isinstance(response, HttpResponse):
+        return response
+    obj, post_data = response
+    form = ThreadedCommentForm(request, obj, post_data)
+    if form.is_valid():
+        url = obj.get_absolute_url()
+        if is_spam(request, form, url):
+            return redirect(url)
+        comment = form.save(request)
+        response = redirect(add_cache_bypass(comment.get_absolute_url()))
+        # Store commenter's details in a cookie for 90 days.
+        # for field in ThreadedCommentForm.cookie_fields:
+        #     cookie_name = ThreadedCommentForm.cookie_prefix + field
+        #     cookie_value = post_data.get(field, "")
+        #     set_cookie(response, cookie_name, cookie_value)
+        return response
+    elif request.is_ajax() and form.errors:
+        return HttpResponse(dumps({"errors": form.errors}))
+    # Show errors with stand-alone comment form.
+    context = {"obj": obj, "posted_comment_form": form}
+    response = render(request, template, context)
+    return response
