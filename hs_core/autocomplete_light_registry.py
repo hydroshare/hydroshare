673d3969d0f7b39694922de67be2915b0eeba08d
Mauriel <mauriel.ramirez@gmail.com>
2015-03-16 15:31:10 -0400

    [#165] Added form for 'Rights' field.

--- hs_core/autocomplete_light_registry.py
+++ hs_core/autocomplete_light_registry.py
@@ -5,6 +5,8 @@ class UserAutocomplete(autocomplete_light.AutocompleteModelBase):
     search_fields=['username','email','first_name','last_name']
     
     def choice_label(self, choice):
+        if (choice.first_name + choice.last_name) and choice.email:
+            return choice.first_name + " " + choice.last_name + " - " + choice.email
         return choice.email
 
 autocomplete_light.register(User, UserAutocomplete)
