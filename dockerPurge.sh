35960eb1c2f88c61930c219657f95e3f84adcbcf
Michael J. Stealey <stealey@renci.org>
2015-03-27 17:23:39 -0400

    update fig vs docker-compose checks in deployment scripts

--- dockerPurge.sh
+++ dockerPurge.sh
@@ -10,46 +10,49 @@ echo "*** enter hydroshare directory"
 cd $1
 
 # stop and display docker contaiers defined by fig or docker-compose and remove them
-DC_OR_FIG=$(which docker-compose | grep "no docker-compose")
-if [ ${#DC_OR_FIG} -gt 0 ];
+FIG_VER=$(fig --version)
+DC_VER=$(docker-compose --version)
+if [ ${#FIG_VER} -gt 0 ];
 then
+    echo "*** found ${FIG_VER} installed ***"
     echo "*** list status of docker containers (via fig)"
     fig ps
-    sleep 1 s
+    sleep 1
     echo "*** stop docker containers (via fig)"
     fig stop
-    sleep 1 s
+    sleep 1
     echo "*** list status of docker containers (via fig)"
     fig ps
-    sleep 1 s
+    sleep 1
     echo "*** remove docker containers (via fig)"
     yes | fig rm
 else
+    echo "*** found ${DC_VER} installed ***"
     echo "*** list status of docker containers (via docker-compose)"
-    fig ps
-    sleep 1 s
+    docker-compose ps
+    sleep 1
     echo "*** stop docker containers (via docker-compose)"
-    fig stop
-    sleep 1 s
+    docker-compose stop
+    sleep 1
     echo "*** list status of docker containers (via docker-compose)"
-    fig ps
-    sleep 1 s
+    docker-compose ps
+    sleep 1
     echo "*** remove docker containers (via docker-compose)"
-    yes | fig rm
+    yes | docker-compose rm
 fi
-sleep 1 s
+sleep 1
 echo "*** list of docker containers (via docker)"
 docker ps -a
-sleep 1 s
+sleep 1
 echo "*** stop docker containers (via docker)"
 docker stop $(docker ps -a -q)
-sleep 1 s
+sleep 1
 echo "*** remove docker containers (via docker)"
 docker rm -fv $(docker ps -a -q)
-sleep 1 s
+sleep 1
 echo "*** list docker images (via docker)"
 docker images
-sleep 1 s
+sleep 1
 echo "*** remove docker images (via docker)"
 docker rmi -f $(docker images -q)
 echo "*** list docker images (via docker)"
