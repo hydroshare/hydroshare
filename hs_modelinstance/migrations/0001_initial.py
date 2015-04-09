577bad28710e7ca4c2b8a85a44932bfaaf1ade8b
Michael J. Stealey <Michael.J.Stealey@gmail.com>
2015-04-09 09:23:01 -0400

    [#5] keep prior migration file for hs_modelinstance

--- hs_modelinstance/migrations/0001_initial.py
+++ hs_modelinstance/migrations/0001_initial.py
@@ -12,8 +12,8 @@ class Migration(migrations.Migration):
         ('auth', '0001_initial'),
         ('pages', '__first__'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('hs_core', '0002_auto_20150310_1927'),
         ('contenttypes', '0001_initial'),
+        ('hs_core', '0001_initial'),
     ]
 
     operations = [
@@ -45,9 +45,6 @@ class Migration(migrations.Migration):
             fields=[
                 ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page')),
                 ('comments_count', models.IntegerField(default=0, editable=False)),
-                ('rating_count', models.IntegerField(default=0, editable=False)),
-                ('rating_sum', models.IntegerField(default=0, editable=False)),
-                ('rating_average', models.FloatField(default=0, editable=False)),
                 ('public', models.BooleanField(default=True, help_text=b'If this is true, the resource is viewable and downloadable by anyone')),
                 ('frozen', models.BooleanField(default=False, help_text=b'If this is true, the resource should not be modified')),
                 ('do_not_distribute', models.BooleanField(default=False, help_text=b'If this is true, the resource owner has to designate viewers')),
@@ -78,7 +75,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('object_id', models.PositiveIntegerField()),
-                ('includes_output', models.BooleanField(default=False)),
+                ('includes_output', models.BooleanField()),
                 ('content_type', models.ForeignKey(related_name='hs_modelinstance_modeloutput_related', to='contenttypes.ContentType')),
             ],
             options={
