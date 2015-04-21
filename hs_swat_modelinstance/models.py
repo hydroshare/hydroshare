c5949688b82ea7dcd6b617252c43f815447a5054
hydrodev <mohamedmorsyanwar@gmail.com>
2015-04-21 12:01:22 -0400

    [#303] Initial implementation of SWAT Model resource type

---
+++ hs_swat_modelinstance/models.py
@@ -0,0 +1,117 @@
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.contrib.auth.models import User, Group
+from django.db import models
+from mezzanine.pages.models import Page, RichText
+from mezzanine.core.models import Ownable
+from mezzanine.pages.page_processors import processor_for
+from hs_core.models import AbstractResource, resource_processor, CoreMetaData, AbstractMetaDataElement
+from hs_modelinstance.models import ModelOutput, ExecutedBy
+
+
+
+# extended metadata elements for SWAT Model Instance resource type
+
+#Model Instance Resource type
+class SWATModelInstanceResource(Page, AbstractResource):
+
+    class Meta:
+        verbose_name = 'SWAT Model Instance Resource'
+
+
+    @property
+    def metadata(self):
+        md = SWATModelInstanceMetaData()
+        return self._get_metadata(md)
+
+    def can_add(self, request):
+        return AbstractResource.can_add(self, request)
+
+    def can_change(self, request):
+        return AbstractResource.can_change(self, request)
+
+    def can_delete(self, request):
+        return AbstractResource.can_delete(self, request)
+
+    def can_view(self, request):
+        return AbstractResource.can_view(self, request)
+
+processor_for(SWATModelInstanceResource)(resource_processor)
+
+# metadata container class
+class SWATModelInstanceMetaData(CoreMetaData):
+    _model_output = generic.GenericRelation(ModelOutput)
+    _executed_by = generic.GenericRelation(ExecutedBy)
+    _swat_model_instance_resource = generic.GenericRelation(SWATModelInstanceResource)
+
+    @property
+    def resource(self):
+        return self._swat_model_instance_resource.all().first()
+
+    @property
+    def model_output(self):
+        return self._model_output.all().first()
+
+    @property
+    def executed_by(self):
+        return self._executed_by.all().first()
+
+    @classmethod
+    def get_supported_element_names(cls):
+        # get the names of all core metadata elements
+        elements = super(SWATModelInstanceMetaData, cls).get_supported_element_names()
+        # add the name of any additional element to the list
+        elements.append('ModelOutput')
+        elements.append('ExecutedBy')
+        return elements
+
+    def has_all_required_elements(self):
+        if not super(SWATModelInstanceMetaData, self).has_all_required_elements():
+            return False
+        if not self.model_output:
+            return False
+        if not self.executed_by:
+            return False
+
+        return True
+
+    def get_required_missing_elements(self):
+        missing_required_elements = super(SWATModelInstanceMetaData, self).get_required_missing_elements()
+        if not self.model_output:
+            missing_required_elements.append('ModelOutput')
+        if not self.executed_by:
+            missing_required_elements.append('ExecutedBy')
+        return missing_required_elements
+
+
+    def get_xml(self, pretty_print=True):
+        from lxml import etree
+        # get the xml string representation of the core metadata elements
+        xml_string = super(SWATModelInstanceMetaData, self).get_xml(pretty_print=False)
+
+        # create an etree xml object
+        RDF_ROOT = etree.fromstring(xml_string)
+
+        # get root 'Description' element that contains all other elements
+        container = RDF_ROOT.find('rdf:Description', namespaces=self.NAMESPACES)
+
+        if self.model_output:
+            hsterms_model_output = etree.SubElement(container, '{%s}variable' % self.NAMESPACES['hsterms'])
+            hsterms_model_output_rdf_Description = etree.SubElement(hsterms_model_output, '{%s}Description' % self.NAMESPACES['rdf'])
+            hsterms_model_output_value = etree.SubElement(hsterms_model_output_rdf_Description, '{%s}IncludesModelOutput' % self.NAMESPACES['hsterms'])
+            if self.model_output.includes_output == True:
+                hsterms_model_output_value.text = "Yes"
+            else:
+                hsterms_model_output_value.text = "No"
+        if self.executed_by:
+            hsterms_executed_by = etree.SubElement(container, '{%s}variable' % self.NAMESPACES['hsterms'])
+            hsterms_executed_by_rdf_Description = etree.SubElement(hsterms_executed_by, '{%s}Description' % self.NAMESPACES['rdf'])
+            hsterms_executed_by_name = etree.SubElement(hsterms_executed_by_rdf_Description, '{%s}ModelProgramName' % self.NAMESPACES['hsterms'])
+            hsterms_executed_by_name.text = self.executed_by.name
+            hsterms_executed_by_url = etree.SubElement(hsterms_executed_by_rdf_Description, '{%s}ModelProgramURL' % self.NAMESPACES['hsterms'])
+            hsterms_executed_by_url.text = self.executed_by.url
+
+        return etree.tostring(RDF_ROOT, pretty_print=True)
+
+import receivers
