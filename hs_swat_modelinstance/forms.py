3de5d5c1cc54eb8a795547907edaee955627ca0e
hydrodev <mohamedmorsyanwar@gmail.com>
2015-04-21 12:01:22 -0400

    [#303] Initial implementation of SWAT Model resource type

---
+++ hs_swat_modelinstance/forms.py
@@ -0,0 +1,9 @@
+__author__ = 'Mohamed'
+from django.forms import ModelForm
+from django import forms
+from crispy_forms.layout import *
+from crispy_forms.bootstrap import *
+from models import *
+from hs_core.forms import BaseFormHelper
+
+
