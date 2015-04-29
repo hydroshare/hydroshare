9ebd36d044de59440afe85df55eaf257935b7e48
hydrodev <mohamedmorsyanwar@gmail.com>
2015-04-29 11:52:34 -0400

    [#303] implement modelObjective metadata element

--- hs_swat_modelinstance/forms.py
+++ hs_swat_modelinstance/forms.py
@@ -7,6 +7,38 @@ from models import *
 from hs_core.forms import BaseFormHelper
 
 #SWATmodelParameters element forms
+class modelObjectiveFormHelper(BaseFormHelper):
+    def __init__(self, allow_edit=True, res_short_id=None, element_id=None, element_name=None,  *args, **kwargs):
+
+        # the order in which the model fields are listed for the FieldSet is the order these fields will be displayed
+        field_width = 'form-control input-sm'
+        layout = Layout(
+                        Field('swat_model_objective', css_class=field_width),
+                        Field('other_objectives', css_class=field_width),
+                 )
+        kwargs['element_name_label'] = 'Model objective'
+        super(modelObjectiveFormHelper, self).__init__(allow_edit, res_short_id, element_id, element_name, layout,  *args, **kwargs)
+
+class modelObjectiveForm(ModelForm):
+    def __init__(self, allow_edit=True, res_short_id=None, element_id=None, *args, **kwargs):
+        objective_choices = tuple([('Choose an objective', 'Choose an objective'), ('Hydrology', 'Hydrology'),\
+                                   ('Water quality', 'Water quality'), ('BMPs', 'BMPs'),\
+                                   ('Climate / Landuse Change', 'Climate / Landuse Change'), ('Other', 'Other')])
+        super(modelObjectiveForm, self).__init__(*args, **kwargs)
+        self.helper = modelObjectiveFormHelper(allow_edit, res_short_id, element_id, element_name='modelObjective')
+        self.fields['swat_model_objective'].choices = objective_choices
+
+    class Meta:
+        model = modelObjective
+        fields = ['swat_model_objective',
+                  'other_objectives']
+        exclude = ['content_object']
+
+
+class modelObjectiveValidationForm(forms.Form):
+    swat_model_objective = forms.CharField(max_length=100, required=False)
+    other_objectives = forms.CharField(max_length=500, required=False)
+
 class SWATmodelParametersFormHelper(BaseFormHelper):
     def __init__(self, allow_edit=True, res_short_id=None, element_id=None, element_name=None,  *args, **kwargs):
 
@@ -34,7 +66,7 @@ class SWATmodelParametersForm(ModelForm):
     has_tillage_operation = forms.TypedChoiceField(choices=((True, 'Yes'), (False, 'No')), widget=forms.RadioSelect)
     has_inlet_of_draining_watershed = forms.TypedChoiceField(choices=((True, 'Yes'), (False, 'No')), widget=forms.RadioSelect)
     has_irrigation_operation = forms.TypedChoiceField(choices=((True, 'Yes'), (False, 'No')), widget=forms.RadioSelect)
-    has_other_parameters = forms.CharField(max_length=200)
+
 
     def __init__(self, allow_edit=True, res_short_id=None, element_id=None, *args, **kwargs):
         super(SWATmodelParametersForm, self).__init__(*args, **kwargs)
@@ -47,8 +79,6 @@ class SWATmodelParametersForm(ModelForm):
         self.fields['has_inlet_of_draining_watershed'].widget.attrs['style'] = "width:auto;margin-top:-5px"
         self.fields['has_irrigation_operation'].widget.attrs['style'] = "width:auto;margin-top:-5px"
 
-
-
     class Meta:
         model = SWATmodelParameters
         fields = ['has_crop_rotation',
@@ -60,7 +90,6 @@ class SWATmodelParametersForm(ModelForm):
                   'has_irrigation_operation',
                   'has_other_parameters']
         exclude = ['content_object']
-        widgets = {'has_other_parameters': forms.TextInput()}
 
 class SWATmodelParametersValidationForm(forms.Form):
     has_crop_rotation = forms.TypedChoiceField(choices=((True, 'Yes'), (False, 'No')), required=False)
@@ -71,49 +100,6 @@ class SWATmodelParametersValidationForm(forms.Form):
     has_inlet_of_draining_watershed = forms.TypedChoiceField(choices=((True, 'Yes'), (False, 'No')), required=False)
     has_irrigation_operation = forms.TypedChoiceField(choices=((True, 'Yes'), (False, 'No')), required=False)
     has_other_parameters = forms.CharField(max_length=500, required=False)
-
-    def clean_has_crop_rotation(self):
-        data = self.cleaned_data['has_crop_rotation']
-        if data == u'False':
-            return False
-        else:
-            return True
-    def clean_has_title_drainage(self):
-        data = self.cleaned_data['has_title_drainage']
-        if data == u'False':
-            return False
-        else:
-            return True
-    def clean_has_point_source(self):
-        data = self.cleaned_data['has_point_source']
-        if data == u'False':
-            return False
-        else:
-            return True
-    def clean_has_fertilizer(self):
-        data = self.cleaned_data['has_fertilizer']
-        if data == u'False':
-            return False
-        else:
-            return True
-    def clean_has_tillage_operation(self):
-        data = self.cleaned_data['has_tillage_operation']
-        if data == u'False':
-            return False
-        else:
-            return True
-    def clean_has_inlet_of_draining_watershed(self):
-        data = self.cleaned_data['has_inlet_of_draining_watershed']
-        if data == u'False':
-            return False
-        else:
-            return True
-    def clean_has_irrigation_operation(self):
-        data = self.cleaned_data['has_irrigation_operation']
-        if data == u'False':
-            return False
-        else:
-            return True
 
         # the order in which the model fields are listed for the FieldSet is the order these fields will be displayed
         field_width = 'form-control input-sm'
--- hs_swat_modelinstance/page_processors.py
+++ hs_swat_modelinstance/page_processors.py
@@ -36,6 +36,9 @@ def landing_page(request, page):
         executed_by_form = ExecutedByForm(instance=content_model.metadata.executed_by, res_short_id=content_model.short_id,
                              element_id=content_model.metadata.executed_by.id if content_model.metadata.executed_by else None)
 
+        model_objective_form = modelObjectiveForm(instance=content_model.metadata.model_objective, res_short_id=content_model.short_id,
+                             element_id=content_model.metadata.model_objective.id if content_model.metadata.model_objective else None)
+
         swat_model_parameters_form = SWATmodelParametersForm(instance=content_model.metadata.swat_model_parameters, res_short_id=content_model.short_id,
                              element_id=content_model.metadata.swat_model_parameters.id if content_model.metadata.swat_model_parameters else None)
 
