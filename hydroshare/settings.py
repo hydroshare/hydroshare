bec542271e7cd7075078b2a46ff0329430b45ffe
Brian Miles <selimnairb@gmail.com>
2015-05-04 13:51:41 -0400

    Explicitly add django-rest default authentication methods to settings

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -415,5 +415,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
     'PAGE_SIZE_QUERY_PARAM': 'PAGE_SIZE',
-    'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler'
+    'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
