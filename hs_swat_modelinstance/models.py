d11b54d5af32236dc4fb12942d715421fd1de275
hydrodev <mohamedmorsyanwar@gmail.com>
2015-05-04 15:25:42 -0400

    [#303] change SWATmodelParameters metadata elements from required to optional

--- hs_swat_modelinstance/models.py
+++ hs_swat_modelinstance/models.py
@@ -274,9 +274,6 @@ class SWATModelInstanceMetaData(CoreMetaData):
             return False
         if not self.model_objective:
             return False
-        if not self.swat_model_parameters:
-            return False
-
         return True
 
     def get_required_missing_elements(self):
@@ -287,8 +284,6 @@ class SWATModelInstanceMetaData(CoreMetaData):
             missing_required_elements.append('ExecutedBy')
         if not self.model_objective:
             missing_required_elements.append('modelObjective')
-        if not self.swat_model_parameters:
-            missing_required_elements.append('SWATmodelParameters')
         return missing_required_elements
 
 
