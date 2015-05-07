051724c5affde64f8dd6bc7f46e6230dccd006bf
hydrodev <mohamedmorsyanwar@gmail.com>
2015-05-07 11:42:15 -0400

    [#303] separate SWAT Model Instance resource from Model Instance resource

--- hs_swat_modelinstance/page_processors.py
+++ hs_swat_modelinstance/page_processors.py
@@ -3,7 +3,6 @@ from mezzanine.pages.page_processors import processor_for
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, HTML
 from hs_core import page_processors
 from hs_core.views import *
-from hs_modelinstance.forms import *
 from forms import *
 from hs_swat_modelinstance.models import SWATModelInstanceResource
 
