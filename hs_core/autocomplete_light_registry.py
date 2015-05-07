a6e0bd789d7591aefe61763fbca443fa825433ca
Mauriel <mauriel.ramirez@gmail.com>
2015-05-07 16:50:09 -0400

    Removed username from search fields and added test cases for autocomplete

--- hs_core/autocomplete_light_registry.py
+++ hs_core/autocomplete_light_registry.py
@@ -2,10 +2,25 @@ import autocomplete_light
 from django.contrib.auth.models import User, Group
 
 class UserAutocomplete(autocomplete_light.AutocompleteModelBase):
-    search_fields=['username','email','first_name','last_name']
+    search_fields=['email','first_name','last_name']
     
     def choice_label(self, choice):
-        return choice.email
+        label = ""
+
+        if choice.first_name:
+            label += choice.first_name
+
+        if choice.last_name:
+            if choice.first_name:
+                label += " "
+            label += choice.last_name
+
+        if choice.email:
+            if choice.first_name or choice.last_name:
+                label += " - "
+            label += choice.email
+
+        return label
 
 autocomplete_light.register(User, UserAutocomplete)
 
