38aeb1957ec3cc2ea647c9dd33fbcdd5d74120c3
Tony Castronova <castronova.anthony@gmail.com>
2015-05-20 10:50:43 -0600

    Removed old/unused code from model_instance.js [#383]

--- hs_modelinstance/static/js/model_instance.js
+++ hs_modelinstance/static/js/model_instance.js
@@ -1,42 +1,10 @@
 
 // subscribe to the OnChange event for the model_name listbox
 var model_list = document.getElementById('id_model_name');
-model_list.addEventListener('change', show_model_details);
-
-// Build a text area to display model information
-var div = document.getElementById("div_id_model_name");
-var desc = document.createElement("div");
-desc.id = 'div_mp_details';
-desc.style.height = "auto";
-//desc.overflow= auto;
-desc.style.width = "100%";
-//desc.style.backgroundColor = '#f0f0f0';
-desc.style.border = "1px solid #a0a0a0";
-div.appendChild(desc);
-
-// set the initial visibility of the description div
-setTextAreaVisibility();
-if (!desc.hidden){
-    show_model_details(null);
+if(model_list){
+    model_list.addEventListener('change', show_model_details);
 }
-
-function setTextAreaVisibility() {
-    // get the text value of the select list
-    var txt = model_list.item(model_list.selectedIndex).text
-
-    // set the visibility of the text area
-    if (txt != 'Unknown') {
-        desc.hidden = false;
-    }
-    else {
-        desc.hidden = true;
-    }
-}
-
 function show_model_details(e) {
-
-    // update the text area visibility
-    setTextAreaVisibility();
 
     // get the id of the current model program name
     var shortid = model_list.item(model_list.selectedIndex).value;
