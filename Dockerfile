0510d7120784e0982ca7f65f92396dc2b91c98da
mauriel133 <mauriel.ramirez@gmail.com>
2015-05-27 16:19:57 -0400

    Merge branch 'develop' of https://github.com/hydroshare/hydroshare into 165-minor-layout-changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
 FROM mjstealey/hs_base
 
+WORKDIR /home/docker
+
+# Install iRODS packages
+RUN apt-get install -y libssl0.9.8 libfuse2
+RUN curl ftp://ftp.renci.org/pub/irods/releases/4.0.3/irods-runtime-4.0.3-64bit.deb -o irods-runtime-4.0.3-64bit.deb
+RUN curl ftp://ftp.renci.org/pub/irods/releases/4.0.3/irods-icommands-4.0.3-64bit.deb -o irods-icommands-4.0.3-64bit.deb
+RUN sudo dpkg -i irods-runtime-4.0.3-64bit.deb irods-icommands-4.0.3-64bit.deb
+RUN sudo apt-get -f install
+RUN pip install -e git+https://github.com/iPlantCollaborativeOpenSource/python-irodsclient.git@master#egg=python-irodsclient
+
 ADD . /home/docker/hydroshare
 WORKDIR /home/docker
 RUN chown -R docker:docker /home/docker
@@ -17,6 +27,11 @@ RUN mkdir -p /home/docker/hydroshare/static/media/.cache
 RUN chown -R docker:docker /home/docker 
 RUN mkdir -p /tmp
 RUN chmod 777 /tmp
+
+# Cleanup iRODS install files
+WORKDIR /home/docker
+RUN rm irods-runtime-4.0.3-64bit.deb
+RUN rm irods-icommands-4.0.3-64bit.deb
 
 WORKDIR /home/docker/hydroshare
 
--- hs_geo_raster_resource/page_processors.py
+++ hs_geo_raster_resource/page_processors.py
@@ -14,7 +14,7 @@ def landing_page(request, page):
     content_model = page.get_content_model()
     edit_resource = page_processors.check_resource_mode(request)
 
-    context = page_processors.get_page_context(page, request.user, resource_edit=edit_resource, extended_metadata_layout=None)
+    context = page_processors.get_page_context(page, request.user, resource_edit=edit_resource, extended_metadata_layout=None, request=request)
     extended_metadata_exists = False
     if content_model.metadata.cellInformation or content_model.metadata.bandInformation:
         extended_metadata_exists = True
@@ -24,7 +24,6 @@ def landing_page(request, page):
         # get the context from content_model
         if content_model.metadata.originalCoverage:
             ori_coverage_data_dict = {}
-            ori_coverage_data_dict['name'] = content_model.metadata.originalCoverage.value.get('name', None)
             ori_coverage_data_dict['units'] = content_model.metadata.originalCoverage.value['units']
             ori_coverage_data_dict['projection'] = content_model.metadata.originalCoverage.value.get('projection', None)
             ori_coverage_data_dict['northlimit'] = content_model.metadata.originalCoverage.value['northlimit']
@@ -35,61 +34,57 @@ def landing_page(request, page):
         context['cellInformation'] = content_model.metadata.cellInformation
         context['bandInformation'] = content_model.metadata.bandInformation
     else:
+        # cellinfo_form
         cellinfo_form = CellInfoForm(instance=content_model.metadata.cellInformation, res_short_id=content_model.short_id,
                                      allow_edit = True,
                                     element_id=content_model.metadata.cellInformation.id if content_model.metadata.cellInformation else None)
-
+        cellinfo_layout = HTML("<div class='form-group col-lg-6 col-xs-12' id='CellInformation'> "
+                                 '{% load crispy_forms_tags %} '
+                                 '{% crispy cellinfo_form %} '
+                                 '</div>')
+        # bandinfo_formset
         BandInfoFormSetEdit = formset_factory(wraps(BandInfoForm)(partial(BandInfoForm, allow_edit=edit_resource)), formset=BaseBandInfoFormSet, extra=0)
         bandinfo_formset = BandInfoFormSetEdit(initial=content_model.metadata.bandInformation.values(), prefix='BandInformation')
-
-        ori_coverage_form = None
-        if content_model.metadata.originalCoverage:
-            ori_coverage_data_dict = {}
-            ori_coverage_data_dict['name'] = content_model.metadata.originalCoverage.value.get('name', None)
-            ori_coverage_data_dict['units'] = content_model.metadata.originalCoverage.value['units']
-            ori_coverage_data_dict['projection'] = content_model.metadata.originalCoverage.value.get('projection', None)
-            ori_coverage_data_dict['northlimit'] = content_model.metadata.originalCoverage.value['northlimit']
-            ori_coverage_data_dict['eastlimit'] = content_model.metadata.originalCoverage.value['eastlimit']
-            ori_coverage_data_dict['southlimit'] = content_model.metadata.originalCoverage.value['southlimit']
-            ori_coverage_data_dict['westlimit'] = content_model.metadata.originalCoverage.value['westlimit']
-
-            allow_edit_flag = True
-            ori_coverage_form = OriginalCoverageSpatialForm(initial=ori_coverage_data_dict, res_short_id=content_model.short_id,
-                                                            allow_edit = allow_edit_flag, element_id=content_model.metadata.originalCoverage.id)
-            ori_coverage_layout = HTML('<div class="form-group" id="originalcoverage"> '
-                                                            '{% load crispy_forms_tags %} '
-                                                            '{% crispy ori_coverage_form %} '
-                                                         '</div>'),
-
-            ext_md_layout = Layout(
-                            ori_coverage_layout,
-                                HTML("<div class='form-group col-md-6 col-sm-12' id='CellInformation'> "
-                                '{% load crispy_forms_tags %} '
-                                '{% crispy cellinfo_form %} '
-                                '</div>'),
-                            BandInfoLayoutEdit
-                        )
-        else:
-            ext_md_layout = Layout(
-                            HTML("<div class='form-group col-md-6 col-sm-12' id='CellInformation'> "
-                                '{% load crispy_forms_tags %} '
-                                '{% crispy cellinfo_form %} '
-                                '</div>'),
-                            BandInfoLayoutEdit
-                        )
 
         for form in bandinfo_formset.forms:
             if len(form.initial) > 0:
                 form.action = "/hsapi/_internal/%s/bandinformation/%s/update-metadata/" % (content_model.short_id, form.initial['id'])
                 form.number = form.initial['id']
 
-        # get the context from hs_core
+        # origina coverage_form
+        ori_cov_obj = content_model.metadata.originalCoverage
+        ori_coverage_data_dict = {}
+        if ori_cov_obj:
+            ori_coverage_data_dict['units'] = ori_cov_obj.value['units']
+            ori_coverage_data_dict['projection'] = ori_cov_obj.value.get('projection', None)
+            ori_coverage_data_dict['northlimit'] = ori_cov_obj.value['northlimit']
+            ori_coverage_data_dict['eastlimit'] = ori_cov_obj.value['eastlimit']
+            ori_coverage_data_dict['southlimit'] = ori_cov_obj.value['southlimit']
+            ori_coverage_data_dict['westlimit'] = ori_cov_obj.value['westlimit']
+        else:
+            ori_cov_obj = None
+
+        ori_coverage_form = OriginalCoverageSpatialForm(initial=ori_coverage_data_dict,
+                                                        res_short_id=content_model.short_id,
+                                                        allow_edit=edit_resource,
+                                                        element_id=ori_cov_obj.id if ori_cov_obj else None)
+        ori_coverage_layout = HTML('<div class="form-group col-lg-6 col-xs-12" id="originalcoverage"> '
+                                   '{% load crispy_forms_tags %} '
+                                   '{% crispy ori_coverage_form %} '
+                                   '</div>')
+
+        # update context
+        ext_md_layout = Layout(
+                                ori_coverage_layout,
+                                cellinfo_layout,
+                                BandInfoLayoutEdit
+        )
         context = page_processors.get_page_context(page, request.user, resource_edit=edit_resource, extended_metadata_layout=ext_md_layout)
-        if content_model.metadata.originalCoverage:
-            context['ori_coverage_form'] = ori_coverage_form
+        context['ori_coverage_form'] = ori_coverage_form
         context['cellinfo_form'] = cellinfo_form
         context['bandinfo_formset'] = bandinfo_formset
 
     hs_core_context = add_generic_context(request, page)
     context.update(hs_core_context)
+
     return context
