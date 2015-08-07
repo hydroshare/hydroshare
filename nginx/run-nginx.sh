975c60cf4258be208dcb80adeea12e4b861fef40
Michael J. Stealey <Michael.J.Stealey@gmail.com>
2015-08-07 16:15:17 -0400

    [#491] create hsctl script

--- nginx/run-nginx.sh
+++ nginx/run-nginx.sh
@@ -6,57 +6,59 @@
 echo "*** RUN SCRIPT run-nginx.sh ***"
 
 ### Load Configuration Variables ###
-CONFIG_DIRECTORY='./config-files'
+NGINX_CONFIG_DIRECTORY='./config-files'
+NGINX_DOCKER_IMG='hs-nginx'
+NGINX_DOCKER_CNTR='web-nginx'
 
 # check for --clean flag
 if [[ ${1} = '--clean' ]]; then
-    echo "*** remove and rebuild IMG: ${HS_NGINX_IMG} and CONTAINER: ${HS_NGINX} ***"
-    docker stop ${HS_NGINX}
-    docker rm ${HS_NGINX}
-    docker rmi ${HS_NGINX_IMG};
+    echo "*** remove and rebuild IMG: ${NGINX_DOCKER_IMG} and CONTAINER: ${NGINX_DOCKER_CNTR} ***"
+    docker stop ${NGINX_DOCKER_CNTR}
+    docker rm ${NGINX_DOCKER_CNTR}
+    docker rmi ${NGINX_DOCKER_IMG};
 fi
 
 # create hs-nginx.conf file
 if [ "${USE_SSL,,}" = true ]; then
-    yes | cp -rf ${CONFIG_DIRECTORY}/hydroshare-ssl-nginx.conf ${CONFIG_DIRECTORY}/hs-nginx.conf
-    sed -i 's/FQDN_OR_IP/'${FQDN_OR_IP}'/g' ${CONFIG_DIRECTORY}/hs-nginx.conf
-    sed -i 's/SSL_CERT_FILE/'${SSL_CERT_FILE}'/g' ${CONFIG_DIRECTORY}/hs-nginx.conf
-    sed -i 's/SSL_KEY_FILE/'${SSL_KEY_FILE}'/g' ${CONFIG_DIRECTORY}/hs-nginx.conf;
+    yes | cp -rf ${NGINX_CONFIG_DIRECTORY}/hydroshare-ssl-nginx.conf ${NGINX_CONFIG_DIRECTORY}/hs-nginx.conf
+    sed -i 's/FQDN_OR_IP/'${FQDN_OR_IP}'/g' ${NGINX_CONFIG_DIRECTORY}/hs-nginx.conf
+    sed -i 's/SSL_CERT_FILE/'${SSL_CERT_FILE}'/g' ${NGINX_CONFIG_DIRECTORY}/hs-nginx.conf
+    sed -i 's/SSL_KEY_FILE/'${SSL_KEY_FILE}'/g' ${NGINX_CONFIG_DIRECTORY}/hs-nginx.conf;
 else
-    yes | cp -rf ${CONFIG_DIRECTORY}/hydroshare-nginx.conf ${CONFIG_DIRECTORY}/hs-nginx.conf
-    sed -i 's/FQDN_OR_IP/'${FQDN_OR_IP}'/g' ${CONFIG_DIRECTORY}/hs-nginx.conf;
+    yes | cp -rf ${NGINX_CONFIG_DIRECTORY}/hydroshare-nginx.conf ${NGINX_CONFIG_DIRECTORY}/hs-nginx.conf
+    sed -i 's/FQDN_OR_IP/'${FQDN_OR_IP}'/g' ${NGINX_CONFIG_DIRECTORY}/hs-nginx.conf;
 fi
 
 # build hs-nginx if it doesn't exist
-CHECK_NGINX_IMAGE=`docker images | tr -s ' ' | cut -d ' ' -f 1 | grep ${HS_NGINX_IMG}`
+CHECK_NGINX_IMAGE=`docker images | tr -s ' ' | cut -d ' ' -f 1 | grep ${NGINX_DOCKER_IMG}`
 if [[ -z "${CHECK_NGINX_IMAGE}" ]]; then
-    echo "*** docker build -t ${HS_NGINX_IMG} . ***"
-    docker build -t ${HS_NGINX_IMG} .;
+    echo "*** docker build -t ${NGINX_DOCKER_IMG} . ***"
+    docker build -t ${NGINX_DOCKER_IMG} .;
 else
-    echo "*** IMG: ${HS_NGINX_IMG} already exists ***";
+    echo "*** IMG: ${NGINX_DOCKER_IMG} already exists ***";
 fi
 
 # Launch nginx as docker container web-nginx
-HYDROSHARE_CID=`docker ps | grep ${HS_CNAME} | tr -s ' ' | cut -d ' ' -f 1`
-CHECK_NGINX_CID=`docker ps -a | tr -s ' ' | grep ${HS_NGINX} | cut -d ' ' -f 1`
+HYDROSHARE_CID=`docker ps | grep hydroshare_hydroshare_1 | tr -s ' ' | cut -d ' ' -f 1`
+CHECK_NGINX_CID=`docker ps -a | tr -s ' ' | grep ${NGINX_DOCKER_CNTR} | cut -d ' ' -f 1`
 if [[ -z "${CHECK_NGINX_CID}" ]]; then
-    echo "*** docker run ${HS_NGINX_IMG} as ${HS_NGINX} ***"
-    docker run -d --name ${HS_NGINX} \
+    echo "*** docker run ${NGINX_DOCKER_IMG} as ${NGINX_DOCKER_CNTR} ***"
+    docker run -d --name ${NGINX_DOCKER_CNTR} \
         --link ${HYDROSHARE_CID}:hydroshare \
         -p 80:80 -p 443:443 \
         --volume ${HOST_SSL_DIR}:/hs-certs \
         --volumes-from ${HYDROSHARE_CID} \
-        -ti ${HS_NGINX_IMG};
+        -ti ${NGINX_DOCKER_IMG};
 else
-    CHECK_NGINX_CID=`docker ps | tr -s ' ' | grep ${HS_NGINX} | cut -d ' ' -f 1`
+    CHECK_NGINX_CID=`docker ps | tr -s ' ' | grep ${NGINX_DOCKER_CNTR} | cut -d ' ' -f 1`
     if [[ -z "${CHECK_NGINX_CID}" ]]; then
-        echo "*** CONTAINER: ${HS_NGINX} already exists but is not running, restarting the container ***"
-        docker start ${HS_NGINX};
+        echo "*** CONTAINER: ${NGINX_DOCKER_CNTR} already exists but is not running, restarting the container ***"
+        docker start ${NGINX_DOCKER_CNTR};
     else
-        echo "*** CONTAINER: ${HS_NGINX} already exists as CID: ${CHECK_NGINX_CID}, restart current container ***";
-        docker stop ${HS_NGINX}
+        echo "*** CONTAINER: ${NGINX_DOCKER_CNTR} already exists as CID: ${CHECK_NGINX_CID}, restart current container ***";
+        docker stop ${NGINX_DOCKER_CNTR}
         sleep 1s
-        docker start ${HS_NGINX};
+        docker start ${NGINX_DOCKER_CNTR};
     fi
 fi
 
