4db1ff1a39316942107f010f5648c72fb7601364
pkdash <pkdash_reena@hotmail.com>
2015-08-17 01:07:53 -0400

    [#554] fixed broken unit test related to get resource by shortkey

--- hs_core/tests/api/native/test_get_resource_by_shortkey.py
+++ hs_core/tests/api/native/test_get_resource_by_shortkey.py
@@ -2,24 +2,27 @@ __author__ = 'Pabitra'
 
 import unittest
 
-from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+
 from hs_core import hydroshare
 from hs_core.models import GenericResource
 
-class TestGetResourceByShortkeyAPI(TestCase):
+
+class TestGetResourceByShortkeyAPI(unittest.TestCase):
     def setUp(self):
+        self.hydroshare_author_group, _ = Group.objects.get_or_create(name='Hydroshare Author')
         pass
 
     def tearDown(self):
+        self.user_creator.uaccess.delete()
         User.objects.all().delete()
+        self.hydroshare_author_group.delete()
         GenericResource.objects.all().delete()
-        pass
 
-    @unittest.skip
+
     def test_get_resource_by_shortkey(self):
         # create a user to be used for creating the resource
-        user_creator = hydroshare.create_account(
+        self.user_creator = hydroshare.create_account(
             'creator@usu.edu',
             username='creator',
             first_name='Creator_FirstName',
@@ -29,12 +32,10 @@ class TestGetResourceByShortkeyAPI(TestCase):
         )
 
         # create a resource
-        resource = GenericResource.objects.create(
-            user=user_creator,
-            title='My resource',
-            creator=user_creator,
-            last_changed_by=user_creator,
-            doi='doi1000100010001'
+        resource = hydroshare.create_resource(
+            'GenericResource',
+            self.user_creator,
+            'My Test Resource'
         )
 
         # do the test of the api
