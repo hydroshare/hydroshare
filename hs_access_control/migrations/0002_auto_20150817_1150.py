ba9fc2505f24dac394df1ce6c3af8941a4c7fd4b
pkdash <pkdash_reena@hotmail.com>
2015-08-17 20:12:40 -0400

    [#554] custom migration to associate User with UserAccess

---
+++ hs_access_control/migrations/0002_auto_20150817_1150.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.contrib.auth.models import User
+from hs_access_control.models import UserAccess
+
+
+def migrate_users(apps, schema_editor):
+    # create a 'UserAccess' record for each existing user - needed for the new access control to work
+    UserAccess.objects.all().delete()
+    for u in User.objects.all():
+        ua = UserAccess(user=u, admin=u.is_superuser)
+        ua.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hs_access_control', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_users),
+    ]
