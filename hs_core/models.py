5c6e5f8852aba8be0d5f2d4b4cc5627e2804d9bc
Calvin Spealman <calvin@caktusgroup.com>
2015-08-25 13:13:30 -0400

    Mark files and bags as concrete

--- hs_core/models.py
+++ hs_core/models.py
@@ -1404,8 +1404,8 @@ class AbstractResource(ResourcePermissionsMixin):
     #     help_text='The dublin core metadata of the resource'
     # )
 
-    files = generic.GenericRelation('hs_core.ResourceFile', help_text='The files associated with this resource', for_concrete_model=False)
-    bags = generic.GenericRelation('hs_core.Bags', help_text='The bagits created from versions of this resource', for_concrete_model=False)
+    files = generic.GenericRelation('hs_core.ResourceFile', help_text='The files associated with this resource', for_concrete_model=True)
+    bags = generic.GenericRelation('hs_core.Bags', help_text='The bagits created from versions of this resource', for_concrete_model=True)
     short_id = models.CharField(max_length=32, default=short_id, db_index=True)
     doi = models.CharField(max_length=1024, blank=True, null=True, db_index=True,
                            help_text='Permanent identifier. Never changes once it\'s been set.')
