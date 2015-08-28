81b4c7b3d81f20ee3cfc8467a37bf0ce00c3a94f
hyi <hongyi@renci.org>
2015-08-28 17:35:13 -0400

    added multiple file selection capability on irods file browser front end

--- irods_browser_app/static/js/irods.js
+++ irods_browser_app/static/js/irods.js
@@ -47,7 +47,7 @@ $('#btn-select-irods-file').on('click',function(){
     $('#irods_view_store').val(store);
     // loading file structs
     var parent = $('#file_struct');
-    get_store(store,parent,0);
+    get_store(store, parent, 0);
     $('body').on('click', '.folder', click_folder_opr);
 });
 
@@ -68,26 +68,64 @@ function get_store(store, parent, margin){
         success: function (json) {
             var files = json.files;
             var folder = json.folder;
+            var lastSelected = [];
             if (files.length == 0 && folder.length == 0) {
                 $(parent).append("<div class='file' style='margin-left:" + margin + "px;'></div>");
             }
             else {
-                $.each(folder, function(i,v) {
+                $.each(folder, function(i, v) {
                     $(parent).append("<div class='folder' id='irods_folder_" + v + "' name='" + store + "/" + v + "' style='margin-left:" + margin + "px;'><img src='/static/img/folder.png' width='15' height='15'>&nbsp; " + v + "</div>");
                 });
 
-                $.each(files, function(i,v) {
+                $.each(files, function(i, v) {
                     $(parent).append("<div class='file' id='irods_file_" + v + "' name='" + store + "/" + v + "' style='margin-left:" + margin + "px;'><img src='/static/img/file.png' width='15' height='15'>&nbsp; " + v + "</div>")
-                    $('.file').on('click',function(e){
-                        $('.file').css('background','');
-                        $('.file').removeClass('selected');
-                        $(e.target).css('background','#cecece');
-                        $(e.target).addClass('selected');
-                        set_datastore($(this).parent('div')[0], false)
-                        return false
-                    });
                 });
             }
+            $('.file').attr('unselectable', 'on'); // disable default browser shift text selection highlighting in IE
+            $('.file').on('click',function(e){
+                var current = $(this), selected;
+                if(e.shiftKey) {
+                    if(lastSelected.length > 0) {
+                        if(current[0] == lastSelected[0]) {
+                            return false;
+                        }
+                        if(current.nextAll('.lastSelected').length > 0) {
+                            // last selected is after the current selection
+                            selected = current.nextUntil(lastSelected);
+                        }
+                        else {
+                            // last selected is before the current selection
+                            selected = lastSelected.nextUntil(current);
+                        }
+                        $('.file').removeClass('selected');
+                        selected.addClass('selected');
+                        lastSelected.addClass('selected');
+                        current.addClass('selected');
+                    }
+                    else {
+                        lastSelected = current;
+                        current.addClass('lastSelected');
+                        $('.file').removeClass('selected');
+                        current.addClass('selected');
+                    }
+                }
+                else { // Not a shift select
+                    $('.file').removeClass('lastSelected');
+                    if (!e.ctrlKey) { // not a ctrl select - clear up old selections
+                        $('.file').removeClass('selected');
+                    }
+
+                    if (e.ctrlKey && $(e.target).hasClass('selected')) { // ctrl-clicking on a selected item clears the selection
+                        $(e.target).removeClass('selected');
+                    }
+                    else {
+                        $(e.target).addClass('lastSelected selected');
+                        lastSelected = current;
+                    }
+                }
+                set_datastore(current.parent('div')[0], false);
+                return false;
+            });
         },
 
         error: function(status) {
@@ -141,7 +179,7 @@ $('#irods_view_store').keypress(function(e) {
 
         // loading file structs
         var parent = $('#file_struct');
-        var got_store = get_store(store,parent,0);
+        var got_store = get_store(store, parent, 0);
         if (got_store) {
             $('#file_struct').children().remove();
             click_folder_opr();
@@ -161,8 +199,11 @@ $("#irodsContent form").bind("keypress", function (e) {
 });
 
 $('#iget_irods').on('click',function(){
-    var name = $('.selected').attr('name');
-    $('#upload_store').val(name);
+    var selected = [];
+    $('.selected').each( function() {
+        selected.push($(this).attr('name'));
+    });
+    $('#upload_store').val(selected);
     $("#irods-username").val(sessionStorage.IRODS_username)
     $("#irods-password").val(sessionStorage.IRODS_password)
     $("#irods-host").val(sessionStorage.IRODS_host)
