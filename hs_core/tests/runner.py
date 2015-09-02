bb372611bd1860aafe86a71ef621d1db9fa71aa7
Calvin Spealman <calvin@caktusgroup.com>
2015-09-02 12:33:46 -0400

    Excluding 3rd party oauth2_provider tests. Making test exclusions easier to find and update.

--- hs_core/tests/runner.py
+++ hs_core/tests/runner.py
@@ -11,25 +11,8 @@ class CustomTestSuiteRunner(NoseTestSuiteRunner):
         if not test_labels:
             # No appnames specified on the command line, so we run all
             # tests, but remove those which we know are troublesome.
-            APPS_TO_NOT_RUN = (
-                'ga_ows',
-                'ga_resources',
-                'jquery_ui',
-                'djcelery',
-                'rest_framework',
-                'django_docker_processes',
-                'dublincore',
-                'django_nose',
-                'inplaceeditform',
-                'grappelli_safe',
-                'django_irods',
-                'crispy_forms',
-                'autocomplete_light',
-                'widget_tweaks'
-                # etc...
-            )
 
-            test_labels = [app for app in settings.INSTALLED_APPS if app not in APPS_TO_NOT_RUN
+            test_labels = [app for app in settings.INSTALLED_APPS if app not in settings.APPS_TO_NOT_RUN
                            and (not app.startswith('django.') and not app.startswith('mezzanine.'))]
 
         return super(CustomTestSuiteRunner, self).run_tests(test_labels, extra_tests, **kwargs)
