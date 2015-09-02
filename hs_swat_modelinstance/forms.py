0f49ea608d49fe165a1a6447919f1aa76a2d9d92
Tony Castronova <castronova.anthony@gmail.com>
2015-09-02 09:28:54 -0600

    Implemented model program foreign key relationship in swat model instance.  Modified the fk definition in model instance.

--- hs_swat_modelinstance/forms.py
+++ hs_swat_modelinstance/forms.py
@@ -6,7 +6,7 @@ from crispy_forms.layout import *
 from crispy_forms.bootstrap import *
 from models import *
 from hs_core.forms import BaseFormHelper
-
+from hs_core.hydroshare import users
 
 model_objective_choices = (
                             ('Hydrology', 'Hydrology'),
@@ -74,16 +74,29 @@ class ModelOutputValidationForm(forms.Form):
         else:
             return True
 
+# ExecutedBy element forms
 class ExecutedByFormHelper(BaseFormHelper):
-    def __init__(self, allow_edit=True, res_short_id=None, element_id=None, element_name=None,  *args, **kwargs):
-
+    def __init__(self, allow_edit=True, res_short_id=None, element_id=None, element_name=None, *args, **kwargs):
         # the order in which the model fields are listed for the FieldSet is the order these fields will be displayed
         layout = Layout(
-                        MetadataField('name'),
-                 )
+            MetadataField('model_name'),
+            HTML("""
+            <div id=program_details_div style="display:none">
+                <table id="program_details_table" class="modelprogram">
+                <tr><td>Description: </td><td></td></tr>
+                <tr><td>Release Date: </td><td></td></tr>
+                <tr><td>Version: </td><td></td></tr>
+                <tr><td>Language: </td><td></td></tr>
+                <tr><td>Operating System: </td><td></td></tr>
+                <tr><td>Url: </td><td></td></tr>
+            </table>
+            </div>
+            """),
+        )
 
         kwargs['element_name_label'] = 'Model Program used for execution'
-        super(ExecutedByFormHelper, self).__init__(allow_edit, res_short_id, element_id, element_name, layout,  *args, **kwargs)
+        super(ExecutedByFormHelper, self).__init__(allow_edit, res_short_id, element_id, element_name, layout, *args,
+                                                   **kwargs)
 
 
 class ExecutedByForm(ModelForm):
@@ -91,12 +104,50 @@ class ExecutedByForm(ModelForm):
         super(ExecutedByForm, self).__init__(*args, **kwargs)
         self.helper = ExecutedByFormHelper(allow_edit, res_short_id, element_id, element_name='ExecutedBy')
 
+        # get all model program resources
+        mp_resource = users.get_resource_list(type=['ModelProgramResource'])
+
+        # set model programs resources in choice list
+        CHOICES = (('Unspecified', 'Unspecified'),) + tuple((r.short_id, r.title) for r in mp_resource.values()[0])
+
+        # Set the choice lists as the file names in the content model
+        self.fields['model_name'].choices = CHOICES
+
     class Meta:
         model = ExecutedBy
-        fields = ('name',)
+        exclude = ('content_object', 'model_program_fk',)
+
 
 class ExecutedByValidationForm(forms.Form):
-    name = forms.CharField(max_length=200)
+    model_name = forms.CharField(max_length=200)
+    model_program_fk = forms
+
+
+
+
+# class ExecutedByFormHelper(BaseFormHelper):
+#     def __init__(self, allow_edit=True, res_short_id=None, element_id=None, element_name=None,  *args, **kwargs):
+#
+#         # the order in which the model fields are listed for the FieldSet is the order these fields will be displayed
+#         layout = Layout(
+#                         MetadataField('name'),
+#                  )
+#
+#         kwargs['element_name_label'] = 'Model Program used for execution'
+#         super(ExecutedByFormHelper, self).__init__(allow_edit, res_short_id, element_id, element_name, layout,  *args, **kwargs)
+#
+#
+# class ExecutedByForm(ModelForm):
+#     def __init__(self, allow_edit=True, res_short_id=None, element_id=None, *args, **kwargs):
+#         super(ExecutedByForm, self).__init__(*args, **kwargs)
+#         self.helper = ExecutedByFormHelper(allow_edit, res_short_id, element_id, element_name='ExecutedBy')
+#
+#     class Meta:
+#         model = ExecutedBy
+#         fields = ('name',)
+#
+# class ExecutedByValidationForm(forms.Form):
+#     name = forms.CharField(max_length=200)
 
 
 class ModelObjectiveFormHelper(BaseFormHelper):
