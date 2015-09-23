3b88532756414d911d700c74e14f233f685f059c
Brian Miles <selimnairb@gmail.com>
2015-09-23 15:02:12 -0400

    Data types for time series metadata

--- hs_app_timeseries/serialization.py
+++ hs_app_timeseries/serialization.py
@@ -15,3 +15,54 @@ class TimeSeriesResourceMeta(GenericResourceMeta):
 
     def write_metadata_to_resource(self, resource):
         super(TimeSeriesResourceMeta, self).write_metadata_to_resource(resource)
+
+    class Site(object):
+        site_code = None
+        site_name = None
+        elevation_m = None
+        elevation_datum = None
+        site_type = None
+
+        def __init__(self):
+            pass
+
+    class Variable(object):
+        variable_code = None
+        variable_name = None
+        variable_type = None
+        no_data_value = None
+        variable_definition = None
+        speciation = None
+
+        def __init__(self):
+            pass
+
+    class Method(object):
+        method_code = None
+        method_name = None
+        method_type = None
+        method_description = None
+        method_link = None
+
+        def __init__(self):
+            pass
+
+    class ProcessingLevel(object):
+        processing_level_code = None
+        definition = None
+        explanation = None
+
+        def __init__(self):
+            pass
+
+    class TimeSeriesResult(object):
+        units_type = None
+        units_name = None
+        units_abbreviation = None
+        status = None
+        sample_medium = None
+        value_count = None
+        aggregation_statistics = None
+
+        def __init__(self):
+            pass
