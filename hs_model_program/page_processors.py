f0c472cf6f25abe25befb67fa38983f91bdf0edd
Tony Castronova <castronova.anthony@gmail.com>
2015-10-12 16:28:56 -0600

    updated mp html to reflect metadata changes.  Added helptext tooltips to the landing page [#632]

--- hs_model_program/page_processors.py
+++ hs_model_program/page_processors.py
@@ -25,6 +25,12 @@ def landing_page(request, page):
         context['extended_metadata_exists'] = extended_metadata_exists
         context['mpmetadata'] = content_model.metadata.program
 
+        # get the helptext for each mp field
+        attributes = content_model.metadata.modelprogrammetadata._mpmetadata.model._meta.get_fields_with_model()
+        attribute_dict = {}
+        for att in attributes:
+             attribute_dict[att[0].attname] = att[0].help_text
+        context["mphelptext" ] = attribute_dict
 
     else:
         output_form = mp_form(files=content_model.files, instance=content_model.metadata.program,
@@ -44,6 +50,7 @@ def landing_page(request, page):
 
         context['resource_type'] = 'Model Program Resource'
         context['output_form'] = output_form
+
 
     hs_core_context = add_generic_context(request, page)
     context.update(hs_core_context)
---
+++ hs_model_program/static/js/model_program.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tonycastronova on 10/12/15.
+ */
+
+$(document).ready(function () {
+
+    $('[data-toggle="popover"]').popover({
+        container: 'body'
+    });
+});
+
