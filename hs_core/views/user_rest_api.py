cbfa41729279bbc2aef4df8f13af68970dcd1c9b
Brian Miles <selimnairb@gmail.com>
2015-10-26 23:04:12 -0400

    Remove debug information

--- hs_core/views/user_rest_api.py
+++ hs_core/views/user_rest_api.py
@@ -4,10 +4,6 @@ from rest_framework.response import Response
 
 class UserInfo(APIView):
     def get(self, request):
-        import logging
-        logger = logging.getLogger('django')
-        logger.debug(str(request.user))
-
         user_info = {"username": request.user.username}
         if request.user.email:
             user_info['email'] = request.user.email
