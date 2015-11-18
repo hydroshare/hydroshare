1d40d547e10a885cf235ba6a51ccecfda653c87b
xuezhaokun <helloxzk@gmail.com>
2015-11-18 15:03:15 -0500

    update faceting order

--- hs_core/customer_form.py
+++ hs_core/customer_form.py
@@ -7,7 +7,7 @@ class MyForm(FacetedSearchForm):
     #faceted_choices = (('author', 'Author'), ('creators', 'Creators'),('subjects', 'Subjects'),
                       # ('public', 'Public'),('discoverable', 'Discoverable'), ('language', 'Language'), ('resource_type', 'Resource Type'))
     #faceted_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=faceted_choices)
-    facted_fields = ['author', 'subjects', 'resource_type', 'public', 'creators', 'discoverable', 'language']
+    faceted_fields = ['author', 'subjects', 'resource_type', 'public', 'creators', 'discoverable', 'language']
     def search(self):
         sqs = super(MyForm, self).search().filter(discoverable=True)
 
@@ -16,7 +16,7 @@ class MyForm(FacetedSearchForm):
 
         #if self.cleaned_data['faceted_field']:
        # for field in self.cleaned_data['faceted_field']:
-        for field in self.facted_fields:
-            sqs = sqs.facet(field, mincount=2)
+        for field in self.faceted_fields:
+            sqs = sqs.facet(field, mincount=2,)
 
         return sqs
