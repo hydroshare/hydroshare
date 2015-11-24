e7e75ec751c3bcf201fb1adbf55267de46750532
Tony Castronova <castronova.anthony@gmail.com>
2015-11-24 13:44:30 -0700

    removed staticfiles_urlpatterns() from urls.py [#632]

--- hs_model_program/urls.py
+++ hs_model_program/urls.py
@@ -3,9 +3,7 @@ __author__ = 'tonycastronova'
 from django.conf.urls import patterns, url
 
 from hs_model_program import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     url(r'^_internal/get-model-metadata/$', views.get_model_metadata),
     )
-urlpatterns += staticfiles_urlpatterns()
