d584857f520318ecc9730874b683429e0dc9d5b1
xuezhaokun <helloxzk@gmail.com>
2015-12-01 23:15:53 -0500

    comment out date facet stuff in the haystack form

--- hs_core/customer_form.py
+++ hs_core/customer_form.py
@@ -21,8 +21,8 @@ class MyForm(FacetedSearchForm):
         #sqs.stats('viewers_count').stats_results()['viewers_count']['max']
         #sqs = sqs.range_facet('viewers_count', start=0.0, end=100.0, gap=20.0)
 
-        sqs = sqs.date_facet('created', start_date=datetime.date(2015, 01, 01), end_date=datetime.date(2015, 12, 01), gap_by='month')
-        sqs = sqs.date_facet('modified', start_date=datetime.date(2015, 01, 01), end_date=datetime.date(2015, 12, 01), gap_by='month')
+        #sqs = sqs.date_facet('created', start_date=datetime.date(2015, 01, 01), end_date=datetime.date(2015, 12, 01), gap_by='month')
+        #sqs = sqs.date_facet('modified', start_date=datetime.date(2015, 01, 01), end_date=datetime.date(2015, 12, 01), gap_by='month')
         #sqs = sqs.stats('created')
         #sqs = sqs.stats_results()
         return sqs
