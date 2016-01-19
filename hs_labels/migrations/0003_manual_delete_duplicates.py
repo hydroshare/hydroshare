efc48975df73ed185e0d039e9f0632001bce2995
Alva Couch <acouch@cuahsi.org>
2016-01-19 14:44:46 -0500

    Added list() environment around second QuerySet to force QuerySet evaluation and prevent abstract iteration

--- hs_labels/migrations/0003_manual_delete_duplicates.py
+++ hs_labels/migrations/0003_manual_delete_duplicates.py
@@ -9,30 +9,30 @@ def delete_duplicates(apps, schema_editor):
     oldrecord = None 
     # order by reverse start so that the first object should be preserved
     for record in list(UserResourceLabels.objects.all()\
-	.order_by("user", "resource", "label", "-start")):
-	# since these are pointer comparisons, no access to models is needed
-	if (oldrecord is not None 
+        .order_by("user", "resource", "label", "-start")):
+        # since these are pointer comparisons, no access to models is needed
+        if (oldrecord is not None 
         and oldrecord.user==record.user
-	and oldrecord.resource==record.resource 
-	and oldrecord.label==record.label): 
-	    record.delete() 
-	else: 
-	    oldrecord = record
+        and oldrecord.resource==record.resource 
+        and oldrecord.label==record.label): 
+            record.delete() 
+        else: 
+            oldrecord = record
 
     # delete oldest copies of records from UserResourceFlags
     UserResourceFlags = apps.get_model("hs_labels", "UserResourceFlags")
     oldrecord = None 
     # order by reverse start so that the first object should be preserved
-    for record in UserResourceFlags.objects.all()\
-	.order_by("user", "resource", "kind", "-start"): 
-	# since these are pointer comparisons, no access to models is needed
-	if (oldrecord is not None 
+    for record in list(UserResourceFlags.objects.all()\
+        .order_by("user", "resource", "kind", "-start")): 
+        # since these are pointer comparisons, no access to models is needed
+        if (oldrecord is not None 
             and oldrecord.user==record.user
-	    and oldrecord.resource==record.resource 
-	    and oldrecord.kind==record.kind): 
-	    record.delete() 
-	else: 
-	    oldrecord = record
+            and oldrecord.resource==record.resource 
+            and oldrecord.kind==record.kind): 
+            record.delete() 
+        else: 
+            oldrecord = record
 
 
 class Migration(migrations.Migration): 
@@ -42,5 +42,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-	migrations.RunPython(delete_duplicates), 
+        migrations.RunPython(delete_duplicates), 
     ]
