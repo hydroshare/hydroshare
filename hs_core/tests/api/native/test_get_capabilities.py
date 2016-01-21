50077f5c47436189957e603e24738e98ab2fcddb
Pabitra Dash <pkdash_reena@hotmail.com>
2016-01-21 10:19:23 -0700

    [#891] added Mocking of iRODS to test_get_capabilities

--- hs_core/tests/api/native/test_get_capabilities.py
+++ hs_core/tests/api/native/test_get_capabilities.py
@@ -1,37 +1,36 @@
-'''
+"""
 Unittest for def get_capabilities(pk)
 
 author's notes- 
 I think this should be renamed get_extra_capabilities
 must be extended to test other types of resources for release 3
 
-'''
-__author__='shaunjl'
+"""
 
 import unittest
-from hs_core import hydroshare
+
 from django.contrib.auth.models import User, Group
+
+from hs_core import hydroshare
 from hs_core.models import GenericResource
 from hs_core.hydroshare.resource import create_resource, get_capabilities
+from hs_core.testing import MockIRODSTestCaseMixin
 
-class TestGetCapabilities(unittest.TestCase):
-    def setUp(self): #runs at the beginning of every test
-        pass
 
-    def tearDown(self): #runs at the end of every test
+class TestGetCapabilities(MockIRODSTestCaseMixin, unittest.TestCase):
+    def setUp(self):
+        super(TestGetCapabilities, self).setUp()
+
+    def tearDown(self):
         GenericResource.objects.all().delete()
         User.objects.filter(username='shaun').delete()
         Group.objects.all().delete()
+        super(TestGetCapabilities, self).tearDown()
 
     def test_generic(self):
-        #user = User.objects.create_user('shaun', 'shauntheta@gmail.com', 'shaun6745')
-        hydroshare_author_group , _ = Group.objects.get_or_create(name='Hydroshare Author') # hydroshare.create_group('Hydroshare Author')
+        hydroshare_author_group, _ = Group.objects.get_or_create(name='Hydroshare Author')
         user = hydroshare.create_account(email='shauntheta@gmail.com', username='shaun', first_name='shaun',
                                          last_name='livingston', superuser=False, groups=[hydroshare_author_group])
         res1 = create_resource('GenericResource', user, 'res1')
         extras = get_capabilities(res1.short_id)
         self.assertIsNone(extras)
-
-    def test_othertypes(self):
-         pass
-
