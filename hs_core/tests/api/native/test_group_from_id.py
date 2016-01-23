fd0c7feedd0fca7ad39943bab2492e3f29162605
Pabitra Dash <pkdash_reena@hotmail.com>
2016-01-23 16:29:39 -0700

    [#891] test_group_from_id() cleanup - made the skipped tests to work

--- hs_core/tests/api/native/test_group_from_id.py
+++ hs_core/tests/api/native/test_group_from_id.py
@@ -1,14 +1,11 @@
-## test case for group_from_id API  Tian Gan
-## the first 3 test functions are similar from the test_utils.py
-## add the test_accept_group_pk(self) test function
 
 from __future__ import absolute_import
-import unittest
+
 from unittest import TestCase
 
-from hs_core import hydroshare
-from hs_core.models import GenericResource
 from django.contrib.auth.models import Group, User
+
+from hs_core import hydroshare
 
 
 class TestGroupFromId(TestCase):
@@ -22,12 +19,7 @@ class TestGroupFromId(TestCase):
             superuser=False,
             groups=[]
         )
-
-        self.group = hydroshare.create_group(
-            'Jamy group',
-            members=[self.user],
-            owners=[self.user]
-        )
+        self.group = self.user.uaccess.create_group('Jamy group')
 
     def tearDown(self):
         User.objects.all().delete()
@@ -37,10 +29,9 @@ class TestGroupFromId(TestCase):
         self.assertEquals(
             hydroshare.group_from_id(self.group),
             self.group,
-            msg='group passthrough failed'
+            msg='group did not match'
         )
 
-    @unittest.skip
     def test_accept_group_name(self):
         self.assertEqual(
             hydroshare.group_from_id(self.group.name),
@@ -48,7 +39,6 @@ class TestGroupFromId(TestCase):
             msg='lookup group name failed'
         )
 
-    @unittest.skip
     def test_accept_group_pk(self):
         self.assertEqual(
             hydroshare.group_from_id(self.group.pk),
