347d42e0050eff61655bfdda98c979cfbab04f15
Brian Miles <selimnairb@gmail.com>
2016-01-22 19:46:10 -0500

    Add tests for resource creation with files

--- hs_core/tests/api/native/test_create_resource.py
+++ hs_core/tests/api/native/test_create_resource.py
@@ -1,10 +1,15 @@
 __author__ = 'tonycastronova'
 
-import unittest
+import os
+import tempfile
+import zipfile
+import shutil
+import time
 
 from unittest import TestCase
 import datetime as dt
 
+from django.core.files.uploadedfile import UploadedFile
 from django.contrib.auth.models import Group
 
 from hs_core.hydroshare import resource, get_resource_by_shortkey
@@ -12,9 +17,23 @@ from hs_core.hydroshare import users
 from hs_core.models import GenericResource
 
 
+class MyTemporaryUploadedFile(UploadedFile):
+    def __init__(self, file=None, name=None, content_type=None, size=None, charset=None, content_type_extra=None):
+        super(UploadedFile, self).__init__(file, name)
+        self.orig_name = name
+        self.size = size
+        self.content_type = content_type
+        self.charset = charset
+        self.content_type_extra = content_type_extra
+
+    def temporary_file_path(self):
+        return self.orig_name
+
+
 class TestCreateResource(TestCase):
 
     def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp()
         self.hs_group, _ = Group.objects.get_or_create(name='Hydroshare Author')
         # create a user
         self.user = users.create_account(
@@ -27,6 +46,7 @@ class TestCreateResource(TestCase):
         )
 
     def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
         self.user.uaccess.delete()
         self.user.delete()
         self.hs_group.delete()
@@ -135,3 +155,74 @@ class TestCreateResource(TestCase):
                       msg="Subject element with value of %s does not exist." % 'sub-1')
         self.assertIn('sub-2', [sub.value for sub in res.metadata.subjects.all()],
                       msg="Subject element with value of %s does not exist." % 'sub-1')
+
+    def test_create_resource_with_file(self):
+        raster = open('hs_core/tests/data/cea.tif')
+        res = resource.create_resource('GenericResource',
+                                       self.user,
+                                       'My Test resource',
+                                       files=(raster,))
+        pid = res.short_id
+
+        # get the resource by pid
+        res = get_resource_by_shortkey(pid)
+        self.assertEqual(res.resource_type, 'GenericResource')
+        self.assertTrue(isinstance(res, GenericResource), type(res))
+        self.assertEqual(res.metadata.title.value, 'My Test resource')
+
+    def test_create_resource_with_zipfile(self):
+
+        # Make a text file
+        txt_file_path = os.path.join(self.tmp_dir, 'text.txt')
+        txt = open(txt_file_path, 'w')
+        txt.write("Hello World\n")
+        txt.close()
+
+        # Make a zip file
+        zip_path = os.path.join(self.tmp_dir, 'test.zip')
+        with zipfile.ZipFile(zip_path, 'w') as zfile:
+            zfile.write('hs_core/tests/data/cea.tif')
+            zfile.write(txt_file_path)
+
+        # Create a resource with zipfile, do not un-pack
+        payload = MyTemporaryUploadedFile(open(zip_path, 'rb'), name=zip_path,
+                                        content_type='application/zip',
+                                        size=os.stat(zip_path).st_size)
+        res = resource.create_resource('GenericResource',
+                                       self.user,
+                                       'My Test resource',
+                                       files=(payload,))
+
+        pid = res.short_id
+
+        # get the resource by pid
+        res = get_resource_by_shortkey(pid)
+        self.assertEquals(res.files.all().count(), 1)
+
+        # Create a resource with zipfile, un-pack
+        payload2 = MyTemporaryUploadedFile(open(zip_path, 'rb'), name=zip_path,
+                                        content_type='application/zip',
+                                        size=os.stat(zip_path).st_size)
+        res = resource.create_resource('GenericResource',
+                                       self.user,
+                                       'My Test resource',
+                                       files=(payload2,),
+                                       unpack_file=True)
+
+        pid = res.short_id
+
+        # Wait until zip unpacking finishes
+        itr = 0
+        res_tmp = get_resource_by_shortkey(pid)
+        while res_tmp.file_unpack_status != 'Done':
+            if itr > 10:
+                self.fail("File unpack status is {0} after {1} iterations".format(res_tmp.file_unpack_status,
+                                                                                  itr))
+            time.sleep(10)
+            res_tmp = get_resource_by_shortkey(pid)
+            itr += 1
+
+        self.assertEquals(res_tmp.files.all().count(), 2)
+
+
+
