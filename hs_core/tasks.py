838b9caf6135b0c6865a2d0a4b835b3a0ee61d2e
hyi <hongyi@renci.org>
2016-02-02 11:32:41 -0500

    verified check_doi_activation task works properly if called directly

--- hs_core/tasks.py
+++ hs_core/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import requests
+import logging
 from xml.etree import ElementTree
 
 from django.conf import settings
@@ -13,7 +14,7 @@ from hs_core.models import BaseResource
 from hs_core.hydroshare.resource import get_activated_doi
 #periodic_task(ignore_result=True, run_every=crontab(minute=0, hour=0)) execute daily at midnight
 
-@periodic_task(ignore_result=True, run_every=crontab(minute='*/3'))
+@periodic_task(ignore_result=True, run_every=crontab(minute='*/10'))
 def check_doi_activation():
     msg_lst = []
     pending_resources = BaseResource.objects.filter(raccess__published=True, doi__contains='pending')
@@ -27,22 +28,26 @@ def check_doi_activation():
             response = requests.get('https://test.crossref.org/servlet/submissionDownload?usr={USERNAME}&pwd={PASSWORD}&doi_batch_id={DOI_BATCH_ID}&type={TYPE}'.format(
                                 USERNAME=settings.CROSSREF_LOGIN_ID, PASSWORD=settings.CROSSREF_LOGIN_PWD, DOI_BATCH_ID=res.short_id, TYPE='result'))
             root = ElementTree.fromstring(response.content)
-            rec_cnt_elem = root.find('record_count')
-            success_cnt_elem = root.find('success_count')
+            rec_cnt_elem = root.find('.//record_count')
+            success_cnt_elem = root.find('.//success_count')
             success = False
-            if rec_cnt_elem and success_cnt_elem:
+            if rec_cnt_elem is not None and success_cnt_elem is not None:
                 rec_cnt = rec_cnt_elem.text
                 success_cnt = success_cnt_elem.text
                 if rec_cnt == success_cnt:
                     res.doi = act_doi
+                    res.save()
                     success = True
             if not success:
-                msg_lst.append("Published resource DOI {res_doi} is not yet activated with request data deposited since {pub_date}\n.".format(res_doi=act_doi, pub_date=pub_date))
+                msg_lst.append("Published resource DOI {res_doi} is not yet activated with request data deposited since {pub_date}.".format(res_doi=act_doi, pub_date=pub_date))
+                msg_lst.append(response.content)
+                msg_log = logging.getLogger('django')
+                msg_log.debug(response.content)
         else:
-           msg_lst.append("{res_id} does not have published date in its metadata.\n".format(res_id=res.short_id))
+           msg_lst.append("{res_id} does not have published date in its metadata.".format(res_id=res.short_id))
 
     if msg_lst:
-        email_msg = ''.join(msg_lst)
+        email_msg = '\n'.join(msg_lst)
         # send email to support@hydroshare.org
         send_mail('Notification of pending DOI activation of published resources', email_msg,
                    settings.DEFAULT_FROM_EMAIL, ['hongyi@renci.org'])
