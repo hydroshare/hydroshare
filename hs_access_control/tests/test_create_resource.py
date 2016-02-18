4d64015a0681681cadcfb357dc8d3d5a6329918a
pkdash <pkdash_reena@hotmail.com>
2016-02-17 22:27:17 -0700

    [#990] fix for access control tests

--- hs_access_control/tests/test_create_resource.py
+++ hs_access_control/tests/test_create_resource.py
@@ -191,6 +191,8 @@ class T03CreateResource(MockIRODSTestCaseMixin, TestCase):
 
         # django admin access
         self.assertFalse(self.admin.uaccess.owns_resource(holes))
+
+        # can still change the immutable/published
         self.assertFalse(self.admin.uaccess.can_change_resource(holes))
         self.assertTrue(self.admin.uaccess.can_view_resource(holes))
         self.assertTrue(self.admin.uaccess.can_change_resource_flags(holes))
