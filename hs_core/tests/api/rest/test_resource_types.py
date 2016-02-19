4deb1db32213f2d4b9b9f28336983c91dc1857ff
Brian Miles <selimnairb@gmail.com>
2016-02-19 15:40:00 -0500

    Add test for hsapi/resourceTypes/

---
+++ hs_core/tests/api/rest/test_resource_types.py
@@ -0,0 +1,23 @@
+import json
+
+from rest_framework.test import APIClient
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from hs_core.hydroshare.utils import get_resource_types
+
+
+class TestResourceList(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_resource_typelist(self):
+        resource_types = set([t.__name__ for t in get_resource_types()])
+
+        response = self.client.get('/hsapi/resourceTypes/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        rest_resource_types = set([t['resource_type'] for t in content])
+
+        self.assertEqual(resource_types, rest_resource_types)
