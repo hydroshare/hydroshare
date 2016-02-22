a8481e693394d532ef0a462efc1d164d70f288ff
Brian Miles <selimnairb@gmail.com>
2016-02-22 11:42:05 -0500

    Refactor to use HSRESTTestCase

--- hs_core/tests/api/rest/test_resource_list.py
+++ hs_core/tests/api/rest/test_resource_list.py
@@ -1,39 +1,18 @@
 import json
 
-from django.contrib.auth.models import Group
-
-from rest_framework.test import APIClient
 from rest_framework import status
-from rest_framework.test import APITestCase
 
-from hs_core.hydroshare import users
 from hs_core.hydroshare import resource
+from .base import HSRESTTestCase
 
 
-class TestResourceList(APITestCase):
+class TestResourceList(HSRESTTestCase):
 
     def setUp(self):
-        self.maxDiff = None
-        self.client = APIClient()
-
-        self.group, _ = Group.objects.get_or_create(name='Hydroshare Author')
-        # create a user
-        self.user = users.create_account(
-            'test_user@email.com',
-            username='testuser',
-            first_name='some_first_name',
-            last_name='some_last_name',
-            superuser=False)
-
-        self.client.force_authenticate(user=self.user)
-
-        self.resources_to_delete = []
+        super(TestResourceList, self).setUp()
 
     def tearDown(self):
-        for r in self.resources_to_delete:
-            resource.delete_resource(r)
-
-        self.user.delete()
+        super(TestResourceList, self).tearDown()
 
     def test_resource_list(self):
 
