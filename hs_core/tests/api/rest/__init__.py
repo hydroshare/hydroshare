a172ad955110ac1311e30407f00694c6c10d0d77
Brian Miles <selimnairb@gmail.com>
2016-02-22 14:43:07 -0500

    Refactor test_resource_file_list to test_resource_file.  Add tests for test_get_resource_file() and test_create_resource_file()

--- hs_core/tests/api/rest/__init__.py
+++ hs_core/tests/api/rest/__init__.py
@@ -1,6 +1,6 @@
 from test_resource_list import *
 from test_user_info import *
-from test_resource_file_list import *
+from test_resource_file import *
 from test_resource_types import *
 from test_create_resource import *
 from test_set_access_rules import *
--- hs_core/tests/api/rest/test_resource_file.py
+++ hs_core/tests/api/rest/test_resource_file.py
@@ -11,10 +11,10 @@ from hs_core.tests.api.utils import MyTemporaryUploadedFile
 from .base import HSRESTTestCase
 
 
-class TestResourceFileList(HSRESTTestCase):
+class TestResourceFile(HSRESTTestCase):
 
     def setUp(self):
-        super(TestResourceFileList, self).setUp()
+        super(TestResourceFile, self).setUp()
 
         self.tmp_dir = tempfile.mkdtemp()
 
@@ -27,13 +27,6 @@ class TestResourceFileList(HSRESTTestCase):
 
         self.raster_file_name = 'cea.tif'
         self.raster_file_path = 'hs_core/tests/data/cea.tif'
-
-    def tearDown(self):
-        shutil.rmtree(self.tmp_dir)
-
-        super(TestResourceFileList, self).tearDown()
-
-    def test_resource_file_list(self):
 
         # Make a zip file
         zip_path = os.path.join(self.tmp_dir, 'test.zip')
@@ -50,13 +43,47 @@ class TestResourceFileList(HSRESTTestCase):
                                        'My Test resource',
                                        files=(payload,),
                                        unpack_file=True)
-        pid = res.short_id
-        self.resources_to_delete.append(pid)
+        self.pid = res.short_id
+        self.resources_to_delete.append(self.pid)
 
-        response = self.client.get("/hsapi/resource/{pid}/file_list/".format(pid=pid),
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
+
+        super(TestResourceFile, self).tearDown()
+
+    def test_resource_file_list(self):
+        response = self.client.get("/hsapi/resource/{pid}/file_list/".format(pid=self.pid),
                                    format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         content = json.loads(response.content)
         self.assertEqual(content['count'], 2)
         self.assertEqual(os.path.basename(content['results'][0]['url']), self.txt_file_name)
         self.assertEqual(os.path.basename(content['results'][1]['url']), self.raster_file_name)
+
+    def test_get_resource_file(self):
+        file_response = self.getResourceFile(self.pid, self.txt_file_name)
+
+    def test_create_resource_file(self):
+        # Make a new text file
+        txt_file_name = 'text2.txt'
+        txt_file_path = os.path.join(self.tmp_dir, txt_file_name)
+        txt = open(txt_file_path, 'w')
+        txt.write("Hello World, again.\n")
+        txt.close()
+        # Upload the new resource file
+        params = {'file': (txt_file_name,
+                           open(txt_file_path),
+                           'text/plain')}
+        url = "/hsapi/resource/{pid}/files/".format(pid=self.pid)
+        response = self.client.post(url, params)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        content = json.loads(response.content)
+        self.assertEquals(content['resource_id'], self.pid)
+
+        # Make sure the new file appears in the file list
+        response = self.client.get("/hsapi/resource/{pid}/file_list/".format(pid=self.pid),
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertEqual(content['count'], 3)
+        self.assertEqual(os.path.basename(content['results'][0]['url']), txt_file_name)
