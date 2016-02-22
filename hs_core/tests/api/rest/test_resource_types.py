5346741d0d5360cdf776252dcbe400ff839ab9fc
Brian Miles <selimnairb@gmail.com>
2016-02-22 12:12:33 -0500

    Make resource type list static

--- hs_core/tests/api/rest/test_resource_types.py
+++ hs_core/tests/api/rest/test_resource_types.py
@@ -4,20 +4,30 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from hs_core.hydroshare.utils import get_resource_types
-
 
 class TestResourceTypes(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
 
-    def test_resource_typelist(self):
-        resource_types = set([t.__name__ for t in get_resource_types()])
+        # Use a static list so that this test breaks when a resource type is
+        # added or removed (so that the test can be updated)
+        self.resource_types = {'GenericResource',
+                               'RasterResource',
+                               'RefTimeSeriesResource',
+                               'TimeSeriesResource',
+                               'NetcdfResource',
+                               'ModelProgramResource',
+                               'ModelInstanceResource',
+                               'ToolResource',
+                               'SWATModelInstanceResource',
+                               'GeographicFeatureResource',
+                               'ScriptResource'}
 
+    def test_resource_typelist(self):
         response = self.client.get('/hsapi/resourceTypes/', format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         content = json.loads(response.content)
         rest_resource_types = set([t['resource_type'] for t in content])
 
-        self.assertEqual(resource_types, rest_resource_types)
+        self.assertEqual(self.resource_types, rest_resource_types)
