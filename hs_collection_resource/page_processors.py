4b87b25b4a9a42c9e43ad3973b8e94a8e6fc61e9
zhiyuli <zyli2004@gmail.com>
2016-02-24 23:19:56 -0500

    [#839] remove old code

--- hs_collection_resource/page_processors.py
+++ hs_collection_resource/page_processors.py
@@ -46,44 +46,13 @@ def landing_page(request, page):
             else:
                 collection_items_inaccessible.append(res)
 
+    # get the context from hs_core
+    context = page_processors.get_page_context(page, request.user,
+                                               resource_edit=edit_resource,
+                                               extended_metadata_layout=None,
+                                               request=request)
     if not edit_resource:
-        # get the context from hs_core
-        context = page_processors.get_page_context(page, request.user,
-                                                   resource_edit=edit_resource,
-                                                   extended_metadata_layout=None,
-                                                   request=request)
-        extended_metadata_exists = False
-
-        collection_message = ""
-        if collection_items_list is not None:
-            collection_length = len(collection_items_list)
-            collection_message = "This collection holds {0} resource(s) in total. ".format(collection_length)
-            hide_count = len(collection_items_inaccessible)
-            if hide_count > 0:
-                collection_message += "You have NO permission to view the {} grey resource(s). \
-                You may request for permission by leaving a comment.".format(hide_count)
-
-        if collection_items_list is not None:
-            extended_metadata_exists = True
-
-            link_html = ""
-            for res in collection_items_accessible:
-                sharing_status = get_sharing_status_string(res)
-                link_html += '<a style="color: blue" href="/resource/' + res.short_id + '" target="_blank">' + \
-                             res.resource_type + ' : ' + res.title + ' : ' + sharing_status + ' : ' + \
-                             res.short_id + '</a><br/>'
-            for res in collection_items_inaccessible:
-                sharing_status = get_sharing_status_string(res)
-                link_html += '<a style="color: grey" href="/resource/' + res.short_id + '" target="_blank">' + \
-                             res.resource_type + ' : ' + res.title + ' : ' + sharing_status + ' : ' + \
-                             res.short_id + '</a><br/>'
-            context['collection_items'] = link_html
-
-
-            if hide_count > 0:
-                context['collection_message'] = collection_message
-
-        context['extended_metadata_exists'] = extended_metadata_exists
+        pass
     else:
 
         collection_itmes_meta = content_model.metadata.collection_items.first()
@@ -97,71 +66,6 @@ def landing_page(request, page):
                 continue # skip the res that is type of collection
             candidate_resources_list.append(res)
 
-        disable_private_flag = False
-        if authorize(request, content_model.short_id, edit=True, raises_exception=False)[1] and \
-                not authorize(request, content_model.short_id, full=True, raises_exception=False)[1]:
-            # if current user has Edit permission but not owner
-            if content_model.raccess.public or content_model.raccess.discoverable:
-                # if current sharing status is public or discoveralbe
-                disable_private_flag = True
-
-        color_code_with_permission = "blue"
-        color_code_no_permission = "grey"
-        html_candidate = ""
-        for res in candidate_resources_list:
-            sharing_status = get_sharing_status_string(res)
-            if disable_private_flag and sharing_status.lower() == "private":
-                html_candidate += '<option disabled style="color: {0}" value="{1}">{2} : {3} : {4} : {5} (Not Selectable)</option>'\
-                    .format(color_code_with_permission, res.short_id, res.resource_type, res.title, sharing_status, res.short_id)
-            else:
-                html_candidate += '<option style="color: {0}" value="{1}">{2} : {3} : {4} : {5}</option>'\
-                    .format(color_code_with_permission, res.short_id, res.resource_type, res.title, sharing_status, res.short_id)
-
-        html_collection = ""
-        if collection_itmes_meta is not None:
-            for res_checked in collection_itmes_meta.collection_items.all():
-                sharing_status = get_sharing_status_string(res_checked)
-                if res_checked in user_all_accessible_resource_list or res_checked.raccess.discoverable or res_checked.raccess.public:
-                    html_collection += '<option style="color: {0}" value="{1}">{2} : {3} : {4} : {5}</option>'\
-                    .format(color_code_with_permission, res_checked.short_id, res_checked.resource_type, \
-                    res_checked.title, sharing_status, res_checked.short_id)
-                else:
-                    html_collection += '<option style="color: {0}" value="{1}">{2} : {3} : {4} : {5}</option>'\
-                    .format(color_code_no_permission, res_checked.short_id, res_checked.resource_type, \
-                    res_checked.title, sharing_status, res_checked.short_id)
-
-        ext_md_layout = Layout(HTML(
-                                    '<h4>My Resource Pool</h4>'
-                                    '<select class="form-control" multiple="multiple" id="select1">'
-                                ),
-                                HTML(
-                                    html_candidate
-                                ),
-                                HTML(
-                                    '</select>'
-                                    '<input class="btn btn-success" type="button" id="add" value ="v Add v"/>'
-                                    '&nbsp;&nbsp;&nbsp;&nbsp;'
-                                    '<input class="btn btn-danger" type="button" id="remove" value ="^ Remove ^" />'
-                                    '<form id="collector" name="collector" action="/hsapi/_internal/' + content_model.short_id + \
-                                    '/update-collection/" method="POST">'
-                                    '<h4>My Collection</h4>'
-                                    '<select class="form-control" multiple="multiple" id="select2" name="collection_items">'
-                                ),
-                                HTML(html_collection),
-                                HTML('</select>'
-                                     '</form>'
-                                     '<br/><input class="btn btn-primary" type="button" id="save" value="Save Changes"/>'
-                                )
-                               )
-
-        # get the context from hs_core
-        context = page_processors.get_page_context(page, request.user,
-                                                   resource_edit=edit_resource,
-                                                   extended_metadata_layout=ext_md_layout,
-                                                   request=request)
-
-        context['html_candidate'] = html_candidate
-        context['html_collection'] = html_collection
 
     context['collection'] = collection_items_list
     if edit_resource:
