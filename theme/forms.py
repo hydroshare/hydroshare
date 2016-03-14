8e9fd609d88369eff933d2e4d1869a6dcc817580
hyi <hongyi@renci.org>
2016-03-14 11:25:39 -0400

    fixed commenting issue with django 1.8 upgrade

--- theme/forms.py
+++ theme/forms.py
@@ -101,22 +101,6 @@ class CommentDetailsForm(CommentSecurityForm):
 
         return new
 
-    def clean_comment(self):
-        """
-        If COMMENTS_ALLOW_PROFANITIES is False, check that the comment doesn't
-        contain anything in PROFANITIES_LIST.
-        """
-        comment = self.cleaned_data["comment"]
-        if settings.COMMENTS_ALLOW_PROFANITIES == False:
-            bad_words = [w for w in settings.PROFANITIES_LIST if w in comment.lower()]
-            if bad_words:
-                raise forms.ValidationError(ungettext(
-                    "Watch your mouth! The word %s is not allowed here.",
-                    "Watch your mouth! The words %s are not allowed here.",
-                    len(bad_words)) % get_text_list(
-                        ['"%s%s%s"' % (i[0], '-'*(len(i)-2), i[-1])
-                         for i in bad_words], ugettext('and')))
-        return comment
 
 class CommentForm(CommentDetailsForm):
     honeypot      = forms.CharField(required=False,
