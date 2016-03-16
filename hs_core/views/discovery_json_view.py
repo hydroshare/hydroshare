954ed84b9dcf1cc69bb2cc3c488541033cc04dcd
xuezhaokun <helloxzk@gmail.com>
2016-03-15 21:53:40 -0400

    fix duplicate marker positions issue

--- hs_core/views/discovery_json_view.py
+++ hs_core/views/discovery_json_view.py
@@ -11,31 +11,15 @@ class DiscoveryJsonView(FacetedSearchView):
     facet_fields = ['author', 'subjects', 'resource_type', 'public', 'owners_names', 'discoverable']
     form_class = DiscoveryForm
 
-    #def get_queryset(self):
-    #    queryset = super(DiscoveryJsonView, self).get_queryset()
-        # further filter queryset based on some set of criteria
-    #    return queryset.filter(content="river")
-
     def form_valid(self, form):
-        #the_data = []
         coor_values = []
         coordinate_dictionary = []
         self.queryset = form.search()
         if len(self.request.GET):
-            #print("#############")
-            #print (self.request.GET)
             for result in self.get_queryset():
                 json_obj = {}
                 json_obj['title'] = result.object.title
                 json_obj['get_absolute_url'] = result.object.get_absolute_url()
-                #print(dir(result.object.get_absolute_url))
-                #print(result.object.get_absolute_url)
-                # the_data.append({
-                #     'result' : result.object
-                # })
-                #the_data.append(result.object)
-                #json_obj = json.dumps(result.object,ensure_ascii=False)
-                #json_objects.append(json_obj)
                 for coverage in result.object.metadata.coverages.all():
 
                     if coverage.type == 'point':
@@ -51,19 +35,7 @@ class DiscoveryJsonView(FacetedSearchView):
                         json_obj['westlimit'] = coverage.value['westlimit']
                     else:
                         continue
-                    #print(json_obj)
                     coor_obj = json.dumps(json_obj)
                     coor_values.append(coor_obj)
-                        #east.append(coverage.value['east'])
-                        #print(coverage.value['east'])
-                        #for attr, value in coverage.value.__dict__.iteritems():
-                        #    print attr, value
-
-                #for attr, value in result.object.__dict__.iteritems():
-                #    print attr, value
-            #suggestions = [result.title for result in sqs]
-            #urls = [result.object.get_absolute_url for result in sqs]
             the_data = json.dumps(coor_values)
             return HttpResponse(the_data, content_type='application/json')
-        #else:
-        #    return super(DiscoveryView, self).form_valid(form)
