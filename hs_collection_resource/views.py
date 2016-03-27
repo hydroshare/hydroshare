d4776a130cde683edae41ce5ac72f0dbbae8f9c4
pkdash <pkdash_reena@hotmail.com>
2016-03-27 15:50:20 -0600

    [#839] added docstring to view functions

--- hs_collection_resource/views.py
+++ hs_collection_resource/views.py
@@ -1,15 +1,25 @@
 import logging
 
-from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.db import transaction
 
 from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
-from hs_core.hydroshare.utils import user_from_id, get_resource_by_shortkey, resource_modified
+from hs_core.hydroshare.utils import get_resource_by_shortkey, resource_modified
 
 logger = logging.getLogger(__name__)
 
+
 # update collection
 def update_collection(request, shortkey, *args, **kwargs):
+    """
+    Add resources to a collection. The POST request should contain a
+    list of resource ids for those resources to be part of the collection. Any existing resources
+    from the collection are removed before adding resources as specified by the list of
+    resource ids in the post request. Requesting user must at least have metadata view permission
+    for any new resources being added to the collection.
+
+    :param shortkey: id of the collection resource to which resources are to be added.
+    """
 
     status = "success"
     msg = ""
@@ -29,6 +39,8 @@ def update_collection(request, shortkey, *args, **kwargs):
                 raise Exception("Duplicate resources were found for adding to the collection")
 
             # check authorization for all new resources being added to the collection
+            # the requesting user should at least have metadata view permission for each of the
+            # new resources to be added to the collection
             for updated_contained_res_id in updated_contained_res_id_list:
                 if not collection_res_obj.resources.filter(short_id=updated_contained_res_id).exists():
                     res_to_add, _, _ = authorize(request, updated_contained_res_id,
@@ -52,10 +64,10 @@ def update_collection(request, shortkey, *args, **kwargs):
             resource_modified(collection_res_obj, user)
 
     except Exception as ex:
-        logger.error("update_collection: {0} ; username: {1}; collection_id: {2} ".
-                         format(ex.message,
-                                request.user.username if request.user.is_authenticated() else "anonymous",
-                                shortkey))
+        err_msg = "update_collection: {0} ; username: {1}; collection_id: {2} ."
+        logger.error(err_msg.format(ex.message,
+                     request.user.username if request.user.is_authenticated() else "anonymous",
+                     shortkey))
         status = "error"
         msg = ex.message
     finally:
@@ -64,6 +76,13 @@ def update_collection(request, shortkey, *args, **kwargs):
 
 
 def update_collection_for_deleted_resources(request, shortkey, *args, **kwargs):
+    """
+    If there are any tracked deleted resource objects for a collection resource
+    (identified by shortkey), those are deleted and resource bag is regenerated
+    for the collection resource to avoid the possibility of broken links in resource map
+    as a result of collection referenced resource being deleted by resource owner.
+    """
+
     ajax_response_data = {'status': "success"}
     try:
         collection_res, is_authorized, user = authorize(request, shortkey,
