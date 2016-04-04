eeadc12638c528c5da8429f2f6e24ded65b85ffc
Brian Miles <selimnairb@gmail.com>
2016-04-04 15:07:56 -0400

    Use defusedexpat library to protect against XML vulnerabilities

--- hydroshare/__init__.py
+++ hydroshare/__init__.py
@@ -1,3 +1,7 @@
 from __future__ import absolute_import
 
+# Import defusedexpat here so that it can patch the standard library's expat XML parser, which is used by
+# rdflib and other libraries in other parts of the app.
+import defusedexpat
+
 from .celery import app as celery_app
