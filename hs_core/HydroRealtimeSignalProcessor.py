07ceb771028797d9bab6d642d208a3e2733e2021
xuezhaokun <helloxzk@gmail.com>
2016-04-26 21:38:00 -0400

    Alva's work on realtime processor

---
+++ hs_core/HydroRealtimeSignalProcessor.py
@@ -0,0 +1,96 @@
+from django.db import models
+from haystack.signals import RealtimeSignalProcessor
+from hs_core.models import BaseResource
+from hs_access_control.models import ResourceAccess
+from haystack.exceptions import NotHandled
+import logging
+import types
+
+
+logger = logging.getLogger(__name__)
+logger.debug("load real time processor")
+
+class HydroRealtimeSignalProcessor(RealtimeSignalProcessor):
+
+    """ 
+    Customized for the fact that the index is a subclass of BaseResource. 
+
+    Notes: 
+    1. RealtimeSignalProcessor already plumbs in all class updates. We might want to be more specific. 
+    2. The class sent to this is a subclass of BaseResource, or another class. 
+    3. Thus, we want to capture cases in which it is an appropriate instance, and respond. 
+    """
+
+    def handle_save(self, sender, instance, **kwargs):
+        """
+        Given an individual model instance, determine which backends the
+        update should be sent to & update the object on those backends.
+        """
+
+        logger.debug("at handle update: type of instance is "+str(type(instance)))
+
+        if isinstance(instance, BaseResource): 
+            newinstance = super(BaseResource, instance)
+            newsender = BaseResource         
+            using_backends = self.connection_router.for_write(instance=newinstance)
+            for using in using_backends:
+                try:
+                    index = self.connections[using].get_unified_index().get_index(newsender)
+                    index.update_object(newinstance, using=using)
+                except NotHandled:
+                    # TODO: log failures 
+                    pass
+
+        elif isinstance(instance, ResourceAccess): 
+            newinstance = instance.resource # automatically a BaseResource 
+            newsender = BaseResource         
+            using_backends = self.connection_router.for_write(instance=newinstance)
+            for using in using_backends:
+
+                try:
+                    index = self.connections[using].get_unified_index().get_index(newsender)
+                    index.update_object(newinstance, using=using)
+                except NotHandled:
+                    # TODO: log failures 
+                    pass
+
+        else: 
+            # log failures 
+            pass 
+
+
+    def handle_delete(self, sender, instance, **kwargs):
+        """
+        Given an individual model instance, determine which backends the
+        delete should be sent to & delete the object on those backends.
+        """
+        logger.debug("at handle delete: type of instance is "+str(type(instance)))
+
+        if isinstance(instance, BaseResource): 
+            newinstance = super(BaseResource, instance)
+            newsender = BaseResource         
+            using_backends = self.connection_router.for_write(instance=newinstance)
+            for using in using_backends:
+                try:
+                    index = self.connections[using].get_unified_index().get_index(newsender)
+                    index.remove_object(newinstance, using=using)
+                except NotHandled:
+                    # TODO: log failures 
+                    pass
+
+        elif isinstance(instance, ResourceAccess): 
+            newinstance = instance.resource # automatically a BaseResource 
+            newsender = BaseResource         
+            using_backends = self.connection_router.for_write(instance=newinstance)
+            for using in using_backends:
+
+                try:
+                    index = self.connections[using].get_unified_index().get_index(newsender)
+                    index.remove_object(newinstance, using=using)
+                except NotHandled:
+                    # TODO: log failures 
+                    pass
+
+        else: 
+            # log failures 
+            pass 
