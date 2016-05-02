d28668cb9f3a15ba4fc50b06118f539dda306783
hyi <hongyi@renci.org>
2016-05-02 16:40:13 -0400

    merge with develop

--- irods_browser_app/static/js/irods.js
+++ irods_browser_app/static/js/irods.js
@@ -1,22 +1,94 @@
-$('#btn-select-irods-file').on('click',function() {
+$('#btn-select-irods-file, #btn-select-irods-file-uz').on('click',function(event) {
     $('#res_type').val($('#resource-type').val());
     $('#file_struct').children().remove();
     $('.ajax-loader').hide();
     var store = '';
-    if (sessionStorage.IRODS_signininfo) {
-        $("#irods_content_label").text(sessionStorage.IRODS_username);
-        $('#root_store').val(sessionStorage.IRODS_datastore);
-        store = sessionStorage.IRODS_datastore;
+    if(event.target.id=='btn-select-irods-file') {
+        if (sessionStorage.IRODS_signininfo) {
+            $("#irods_content_label").text(sessionStorage.IRODS_username);
+            $('#root_store').val(sessionStorage.IRODS_datastore);
+            store = sessionStorage.IRODS_datastore;
+        }
     }
-    
+    else {
+        $("#irods_content_label").text($("#irods_username_uz").val())
+        $('#root_store').val($("#irods_store_uz").val());
+        store = $("#irods_store_uz").val();
+    }
     // Setting up the view tab
     $('#file_struct').attr('name',store);
     $('#irods_view_store').val(store);
     // loading file structs
     var parent = $('#file_struct');
-    get_store(store, parent, 0);
+    if(event.target.id=='btn-select-irods-file')
+        get_store(store, parent, 0);
+    else
+        get_store_uz(store, parent, 0)
     $('body').on('click', '.folder', click_folder_opr);
 });
+
+function set_store_display(store, parent, margin, json) {
+    var files = json.files;
+    var folder = json.folder;
+    var lastSelected = [];
+    if (files.length == 0 && folder.length == 0) {
+        $(parent).append("<div class='file' style='margin-left:" + margin + "px;'></div>");
+    }
+    else {
+        $.each(folder, function(i, v) {
+            $(parent).append("<div class='folder' id='irods_folder_" + v + "' name='" + store + "/" + v + "' style='margin-left:" + margin + "px;'><img src='/static/img/folder.png' width='15' height='15'>&nbsp; " + v + "</div>");
+        });
+
+        $.each(files, function(i, v) {
+            $(parent).append("<div class='file' id='irods_file_" + v + "' name='" + store + "/" + v + "' style='margin-left:" + margin + "px;'><img src='/static/img/file.png' width='15' height='15'>&nbsp; " + v + "</div>")
+        });
+    }
+    $('.file').attr('unselectable', 'on'); // disable default browser shift text selection highlighting in IE
+    $('.file').on('click',function(e) {
+        var current = $(this), selected;
+        if (e.shiftKey) {
+            if (lastSelected.length > 0) {
+                if (current[0] == lastSelected[0]) {
+                    return false;
+                }
+                if (current.nextAll('.lastSelected').length > 0) {
+                    // last selected is after the current selection
+                    selected = current.nextUntil(lastSelected);
+                }
+                else {
+                    // last selected is before the current selection
+                    selected = lastSelected.nextUntil(current);
+                }
+                $('.file').removeClass('selected');
+                selected.addClass('selected');
+                lastSelected.addClass('selected');
+                current.addClass('selected');
+            }
+            else {
+                lastSelected = current;
+                current.addClass('lastSelected');
+                $('.file').removeClass('selected');
+                current.addClass('selected');
+            }
+        }
+        else { // Not a shift select
+            $('.file').removeClass('lastSelected');
+            if (!e.ctrlKey) { // not a ctrl select - clear up old selections
+                $('.file').removeClass('selected');
+            }
+
+            if (e.ctrlKey && $(e.target).hasClass('selected')) { // ctrl-clicking on a selected item clears the selection
+                $(e.target).removeClass('selected');
+            }
+            else {
+                $(e.target).addClass('lastSelected selected');
+                lastSelected = current;
+            }
+        }
+        set_datastore(current.parent('div')[0], false);
+        return false;
+    });
+}
 
 function get_store(store, parent, margin) {
     $.ajax({
@@ -33,66 +105,28 @@ function get_store(store, parent, margin) {
             host: sessionStorage.IRODS_host
         },
         success: function (json) {
-            var files = json.files;
-            var folder = json.folder;
-            var lastSelected = [];
-            if (files.length == 0 && folder.length == 0) {
-                $(parent).append("<div class='file' style='margin-left:" + margin + "px;'></div>");
-            }
-            else {
-                $.each(folder, function(i, v) {
-                    $(parent).append("<div class='folder' id='irods_folder_" + v + "' name='" + store + "/" + v + "' style='margin-left:" + margin + "px;'><img src='/static/img/folder.png' width='15' height='15'>&nbsp; " + v + "</div>");
-                });
+                return set_store_display(store, parent, margin, json);
+        },
 
-                $.each(files, function(i, v) {
-                    $(parent).append("<div class='file' id='irods_file_" + v + "' name='" + store + "/" + v + "' style='margin-left:" + margin + "px;'><img src='/static/img/file.png' width='15' height='15'>&nbsp; " + v + "</div>")
-                });
-            }
-            $('.file').attr('unselectable', 'on'); // disable default browser shift text selection highlighting in IE
-            $('.file').on('click',function(e){
-                var current = $(this), selected;
-                if(e.shiftKey) {
-                    if(lastSelected.length > 0) {
-                        if(current[0] == lastSelected[0]) {
-                            return false;
-                        }
-                        if(current.nextAll('.lastSelected').length > 0) {
-                            // last selected is after the current selection
-                            selected = current.nextUntil(lastSelected);
-                        }
-                        else {
-                            // last selected is before the current selection
-                            selected = lastSelected.nextUntil(current);
-                        }
-                        $('.file').removeClass('selected');
-                        selected.addClass('selected');
-                        lastSelected.addClass('selected');
-                        current.addClass('selected');
-                    }
-                    else {
-                        lastSelected = current;
-                        current.addClass('lastSelected');
-                        $('.file').removeClass('selected');
-                        current.addClass('selected');
-                    }
-                }
-                else { // Not a shift select
-                    $('.file').removeClass('lastSelected');
-                    if (!e.ctrlKey) { // not a ctrl select - clear up old selections
-                        $('.file').removeClass('selected');
-                    }
+        error: function(status) {
+            console.error(status);
+            return false;
+        }
+    });
+    return true;
+}
 
-                    if (e.ctrlKey && $(e.target).hasClass('selected')) { // ctrl-clicking on a selected item clears the selection
-                        $(e.target).removeClass('selected');
-                    }
-                    else {
-                        $(e.target).addClass('lastSelected selected');
-                        lastSelected = current;
-                    }
-                }
-                set_datastore(current.parent('div')[0], false);
-                return false;
-            });
+function get_store_uz(store, parent, margin) {
+    $.ajax({
+        mode: "queue",
+        url: '/irods/store_uz/',
+        async: true,
+        type: "POST",
+        data: {
+            store: store
+        },
+        success: function (json) {
+                return set_store_display(store, parent, margin, json);
         },
 
         error: function(status) {
@@ -171,11 +205,13 @@ $('#iget_irods').on('click',function() {
         selected.push($(this).attr('name'));
     });
     $('#upload_store').val(selected);
-    $("#irods-username").val(sessionStorage.IRODS_username)
-    $("#irods-password").val(sessionStorage.IRODS_password)
-    $("#irods-host").val(sessionStorage.IRODS_host)
-    $("#irods-zone").val(sessionStorage.IRODS_zone)
-    $("#irods-port").val(sessionStorage.IRODS_port)
+    if($("input_trigger").val()=='default') {
+        $("#irods-username").val(sessionStorage.IRODS_username)
+        $("#irods-password").val(sessionStorage.IRODS_password)
+        $("#irods-host").val(sessionStorage.IRODS_host)
+        $("#irods-zone").val(sessionStorage.IRODS_zone)
+        $("#irods-port").val(sessionStorage.IRODS_port)
+    }
     $('#irodsContent .modal-backdrop.up-load').show();
     $('#irodsContent .ajax-loader').show();
 });
@@ -189,14 +225,23 @@ function irods_upload() {
             res_type: $('#res_type').val()
         },
         success: function(json) {
-            $("#irods-sel-file").text(json.irods_sel_file);
+            if ($('#input_trigger').val()=='default') {
+                $("#irods-sel-file").text(json.irods_sel_file);
+                $('#irods_file_names').val(json.irods_file_names);
+            }
+            else{
+                $("#irods-sel-file-uz").text(json.irods_sel_file);
+                $('#irods_file_names_uz').val(json.irods_file_names);
+            }
             $("#file-type-error").text(json.file_type_error);
             $('#irodsContent').modal('hide');
-            $('#irods_file_names').val(json.irods_file_names)
         },
         error: function(xhr, errmsg, err) {
             console.log(xhr.status + ": " + xhr.responseText + ". Error message: " + errmsg);
-            $("#irods-sel-file").text("No file selected");
+            if ($('#input_trigger').val()=='default')
+                $("#irods-sel-file").text("No file selected");
+            else
+                $("#irods-sel-file-uz").text("No file selected");
             $('#irodsContent').modal('hide');
         }
     });
