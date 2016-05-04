806eb91d22c1c717e6d3887d783f2189e02f614b
hyi <hongyi@renci.org>
2016-05-04 16:57:01 -0400

    fixed migration conflict after merging with develop

---
+++ hs_core/migrations/0023_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hs_core', '0021_fundingagency'),
+        ('hs_core', '0022_resourcefile_resource_file_size'),
+    ]
+
+    operations = [
+    ]
