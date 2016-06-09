872dd98eeed7f3f147d1e44e35a35f6dd2f04159
pkdash <pkdash_reena@hotmail.com>
2016-06-09 11:28:46 -0600

    [#1220] adding the migration file

---
+++ hs_app_timeseries/migrations/0005_auto_20160608_2305.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.contrib.postgres.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hs_app_timeseries', '0004_auto_20160526_2026'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='method',
+            name='series_ids',
+            field=django.contrib.postgres.fields.ArrayField(default=[], base_field=models.CharField(max_length=36, null=True, blank=True), size=None),
+        ),
+        migrations.AddField(
+            model_name='processinglevel',
+            name='series_ids',
+            field=django.contrib.postgres.fields.ArrayField(default=[], base_field=models.CharField(max_length=36, null=True, blank=True), size=None),
+        ),
+        migrations.AddField(
+            model_name='site',
+            name='series_ids',
+            field=django.contrib.postgres.fields.ArrayField(default=[], base_field=models.CharField(max_length=36, null=True, blank=True), size=None),
+        ),
+        migrations.AddField(
+            model_name='timeseriesresult',
+            name='series_ids',
+            field=django.contrib.postgres.fields.ArrayField(default=[], base_field=models.CharField(max_length=36, null=True, blank=True), size=None),
+        ),
+        migrations.AddField(
+            model_name='variable',
+            name='series_ids',
+            field=django.contrib.postgres.fields.ArrayField(default=[], base_field=models.CharField(max_length=36, null=True, blank=True), size=None),
+        ),
+        migrations.AlterUniqueTogether(
+            name='method',
+            unique_together=set([]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='processinglevel',
+            unique_together=set([]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='site',
+            unique_together=set([]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='timeseriesresult',
+            unique_together=set([]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='variable',
+            unique_together=set([]),
+        ),
+    ]
