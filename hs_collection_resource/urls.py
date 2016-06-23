5f0e941c3092962146f046cedf71b7c8f1dbf405
zhiyuli <zyli2004@gmail.com>
2016-06-23 19:40:41 -0400

    [#1263] update

--- hs_collection_resource/urls.py
+++ hs_collection_resource/urls.py
@@ -6,4 +6,8 @@ urlpatterns = patterns(
     url(r'^_internal/(?P<shortkey>[A-z0-9]+)/update-collection/$', views.update_collection),
     url(r'^_internal/(?P<shortkey>[A-z0-9]+)/update-collection-for-deleted-resources/$',
         views.update_collection_for_deleted_resources),
+    url(r'^_internal/(?P<shortkey>[A-z0-9]+)/update-collection-coverages/$',
+        views.update_collection_coverages),
+     url(r'^_internal/(?P<shortkey>[A-z0-9]+)/calculate-collection-coverages/$',
+        views.calculate_collection_coverages),
     )
--- hs_collection_resource/views.py
+++ hs_collection_resource/views.py
@@ -8,7 +8,7 @@ from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
 from hs_core.hydroshare.utils import get_resource_by_shortkey, resource_modified
 
 logger = logging.getLogger(__name__)
-
+ui_datetime_format = "%m/%d/%Y"
 
 # update collection
 def update_collection(request, shortkey, *args, **kwargs):
@@ -112,81 +112,142 @@ def update_collection_for_deleted_resources(request, shortkey, *args, **kwargs):
         return JsonResponse(ajax_response_data)
 
 
+def update_collection_coverages(request, shortkey, *args, **kwargs):
+
+    ajax_response_data = {'status': "success"}
+    try:
+        collection_res, is_authorized, user = authorize(request, shortkey,
+                                                        needed_permission=ACTION_TO_AUTHORIZE.EDIT_RESOURCE)
+
+        if collection_res.resource_type.lower() != "collectionresource":
+            raise Exception("Resource {0} is not a collection resource.".format(shortkey))
+
+        new_coverage_list = _update_collection_coverages(collection_res)
+        ajax_response_data['new_coverage_list'] = new_coverage_list
+
+        resource_modified(collection_res, user)
+
+    except Exception as ex:
+        logger.error("Failed to update collection coverages. Collection resource ID: {0}. "
+                     "Error:{1} ".format(shortkey, ex.message))
+
+        ajax_response_data = {'status': "error", 'message': ex.message}
+    finally:
+        return JsonResponse(ajax_response_data)
+
+
 def _update_collection_coverages(collection_res_obj):
-    # calculate the overall coverages of all contained resources
-    # to update the collection coverages metadata.
+    #  update the collection coverages metadata.
     res_id = collection_res_obj.short_id
-    new_coverage_list = []
+    new_coverage_list = _calculate_collection_coverages(collection_res_obj)
     try:
         with transaction.atomic():
-            lon_list = []
-            lat_list = []
-            time_list = []
-            output_spatial_projection_str = "WGS84 EPSG:4326"
-            output_spatial_units_str = "Decimal degrees"
-            for contained_res_obj in collection_res_obj.resources.all():
-                for cvg in contained_res_obj.metadata.coverages.all():
-                    if cvg.type.lower() == "box":
-                        lon_list.append(float(cvg.value["eastlimit"]))
-                        lon_list.append(float(cvg.value["westlimit"]))
-                        lat_list.append(float(cvg.value["northlimit"]))
-                        lat_list.append(float(cvg.value["southlimit"]))
-                    elif cvg.type.lower() == "point":
-                        lon_list.append(float(cvg.value["east"]))
-                        lat_list.append(float(cvg.value["north"]))
-                    elif cvg.type.lower() == "period":
-                        try:
-                            if cvg.value.get("start", None) is not None:
-                                time_list.append(parser.parse(cvg.value["start"]))
-                            if cvg.value.get("end", None) is not None:
-                                time_list.append(parser.parse(cvg.value["end"]))
-                        except ValueError as ex:
-                            # skip the res if it has invalid datetime string
-                            logger.warning("Ignore unknown datetime string. Collection resource ID: {0}. "
-                                           "Contained res ID: {1}"
-                                           "Msg: {2} ".format(res_id, contained_res_obj.short_id, ex.message))
-
             collection_res_obj.metadata.coverages.all().delete()
-
-            # spatial coverage
-            if len(lon_list) > 0 and len(lon_list) > 0:
-                value_dict = {}
-                type_str = 'point'
-                lon_min = min(lon_list)
-                lon_max = max(lon_list)
-                lat_min = min(lat_list)
-                lat_max = max(lat_list)
-                if lon_min == lon_max and lat_min == lat_max:
-                    type_str = 'point'
-                    value_dict['east'] = lon_min
-                    value_dict['north'] = lat_min
-                    value_dict['units'] = output_spatial_units_str
-                else:
-                    type_str = 'box'
-                    value_dict['eastlimit'] = lon_max
-                    value_dict['westlimit'] = lon_min
-                    value_dict['northlimit'] = lat_max
-                    value_dict['southlimit'] = lat_min
-                    value_dict['units'] = output_spatial_units_str,
-                    value_dict['projection'] = output_spatial_projection_str
-                collection_res_obj.metadata.create_element('Coverage',
-                                                           type=type_str,
-                                                           value=value_dict)
-                new_coverage_list.append({'type': type_str, 'value': value_dict})
-
-            # temporal coverage
-            if len(time_list) > 0:
-                time_start = min(time_list)
-                time_end = max(time_list)
-                value_dict = {'start': str(time_start), 'end': str(time_end)}
-                collection_res_obj.metadata.create_element('Coverage',
-                                                           type='period',
-                                                           value=value_dict)
-                new_coverage_list.append({'type': 'period', 'value': value_dict})
+            for cvg in new_coverage_list:
+                element = collection_res_obj.metadata.create_element('Coverage',
+                                                           type=cvg['type'],
+                                                           value=cvg['value'])
+                cvg["element_id_str"] = str(element.id)
 
     except Exception as ex:
         logger.exception("Failed to update collection coverages. Collection resource ID: {0}. "
                          "Error: {1} ".format(res_id, ex.message))
         raise Exception("Failed to update collection coverages")
+    finally:
+        return new_coverage_list
+
+
+def calculate_collection_coverages(request, shortkey, *args, **kwargs):
+
+    ajax_response_data = {'status': "success"}
+    try:
+        collection_res, is_authorized, user = authorize(request, shortkey,
+                                                        needed_permission=ACTION_TO_AUTHORIZE.EDIT_RESOURCE)
+
+        if collection_res.resource_type.lower() != "collectionresource":
+            raise Exception("Resource {0} is not a collection resource.".format(shortkey))
+
+        new_coverage_list = _calculate_collection_coverages(collection_res)
+        ajax_response_data['new_coverage_list'] = new_coverage_list
+
+        resource_modified(collection_res, user)
+
+    except Exception as ex:
+        logger.error("Failed to calculate collection coverages. Collection resource ID: {0}. "
+                     "Error:{1} ".format(shortkey, ex.message))
+
+        ajax_response_data = {'status': "error", 'message': ex.message}
+    finally:
+        return JsonResponse(ajax_response_data)
+
+
+def _calculate_collection_coverages(collection_res_obj):
+    # calculate the overall coverages of all contained resources
+    res_id = collection_res_obj.short_id
+    new_coverage_list = []
+    try:
+        lon_list = []
+        lat_list = []
+        time_list = []
+        output_spatial_projection_str = "WGS84 EPSG:4326"
+        output_spatial_units_str = "Decimal degrees"
+        for contained_res_obj in collection_res_obj.resources.all():
+            for cvg in contained_res_obj.metadata.coverages.all():
+                if cvg.type.lower() == "box":
+                    lon_list.append(float(cvg.value["eastlimit"]))
+                    lon_list.append(float(cvg.value["westlimit"]))
+                    lat_list.append(float(cvg.value["northlimit"]))
+                    lat_list.append(float(cvg.value["southlimit"]))
+                elif cvg.type.lower() == "point":
+                    lon_list.append(float(cvg.value["east"]))
+                    lat_list.append(float(cvg.value["north"]))
+                elif cvg.type.lower() == "period":
+                    try:
+                        if cvg.value.get("start", None) is not None:
+                            start_date = parser.parse(cvg.value["start"])
+                            time_list.append(start_date)
+                        if cvg.value.get("end", None) is not None:
+                            end_date = parser.parse(cvg.value["end"])
+                            time_list.append(end_date)
+                    except ValueError as ex:
+                        # skip the res if it has invalid datetime string
+                        logger.warning("_calculate_collection_coverages: Ignore unknown datetime string. Collection resource ID: {0}. "
+                                       "Contained res ID: {1}"
+                                       "Msg: {2} ".format(res_id, contained_res_obj.short_id, ex.message))
+
+        # spatial coverage
+        if len(lon_list) > 0 and len(lon_list) > 0:
+            value_dict = {}
+            type_str = 'point'
+            lon_min = min(lon_list)
+            lon_max = max(lon_list)
+            lat_min = min(lat_list)
+            lat_max = max(lat_list)
+            if lon_min == lon_max and lat_min == lat_max:
+                type_str = 'point'
+                value_dict['east'] = lon_min
+                value_dict['north'] = lat_min
+                value_dict['units'] = output_spatial_units_str
+            else:
+                type_str = 'box'
+                value_dict['eastlimit'] = lon_max
+                value_dict['westlimit'] = lon_min
+                value_dict['northlimit'] = lat_max
+                value_dict['southlimit'] = lat_min
+                value_dict['units'] = output_spatial_units_str,
+                value_dict['projection'] = output_spatial_projection_str
+
+            new_coverage_list.append({'type': type_str, 'value': value_dict, 'element_id_str': "-1"})
+
+            # temporal coverage
+            if len(time_list) > 0:
+                time_start = min(time_list)
+                time_end = max(time_list)
+                value_dict = {'start': time_start.strftime(ui_datetime_format), 'end': time_end.strftime(ui_datetime_format)}
+
+                new_coverage_list.append({'type': 'period', 'value': value_dict, 'element_id_str': "-1"})
+
+    except Exception as ex:
+        raise Exception("Failed to calculate collection coverages")
     finally:
         return new_coverage_list
