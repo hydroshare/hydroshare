ca9b227ff778c82c95327e9e8cad584556b5185b
pkdash <pkdash_reena@hotmail.com>
2016-06-29 14:10:26 -0600

    [#1220] unit test for update sqlite file view function

--- hs_app_timeseries/views.py
+++ hs_app_timeseries/views.py
@@ -1,5 +1,8 @@
+import logging
+
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.core.exceptions import ValidationError
 
 from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
 
@@ -7,14 +10,20 @@ from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
 def update_sqlite_file(request, resource_id, *args, **kwargs):
     res, _, user = authorize(request, resource_id,
                              needed_permission=ACTION_TO_AUTHORIZE.EDIT_RESOURCE)
+    log = logging.getLogger()
     if res.resource_type != "TimeSeriesResource":
-        messages.error(request, "This is not a timeseries resource.")
+        log.exception("Failed to update SQLite file. Error:This is not a timeseries resource. "
+                      "Resource ID:{}".format(res.short_id))
+        raise ValidationError("The resource is not of type TimeSeries. SQLite file update is "
+                              "allowed only for timeseries resources.")
     else:
         try:
             res.metadata.update_sqlite_file()
             messages.success(request, "SQLite file update was successful.")
+            log.info("SQLite file update was successful for resource ID:{}.".format(res.short_id))
         except Exception, ex:
-            messages.error(request, "Failed to update SQLite file. Error:" + ex.message)
+            messages.error(request, "Failed to update SQLite file. Error:{}".format(ex.message))
+            log.exception("Failed to update SQLite file. Error:{}".format(ex.message))
 
     if 'resource-mode' in request.POST:
         request.session['resource-mode'] = 'edit'
