f531b05ffb9f9e6965fa359f8925b1739f6fe5db
pkdash <pkdash_reena@hotmail.com>
2016-07-08 11:57:53 -0600

    [#1313] pylint cleanup of hs_core/receivers.py

--- hs_core/receivers.py
+++ hs_core/receivers.py
@@ -1,20 +1,24 @@
 
-## Note: this module has been imported in the models.py in order to receive signals
-## se the end of the models.py for the import of this module
+# Note: this module has been imported in the models.py in order to receive signals
+# se the end of the models.py for the import of this module
 
 from django.dispatch import receiver
 from hs_core.signals import pre_metadata_element_create, pre_metadata_element_update
-from hs_core.models import BaseResource
-from forms import *
+from hs_core.models import GenericResource
+from forms import SubjectsForm, AbstractValidationForm, CreatorValidationForm, \
+    ContributorValidationForm, RelationValidationForm, SourceValidationForm, RightsValidationForm, \
+    LanguageValidationForm, ValidDateValidationForm, FundingAgencyValidationForm, \
+    CoverageSpatialForm, CoverageTemporalForm, IdentifierForm, TitleValidationForm
+
 
 # This handler is executed only when a metadata element is added as part of editing a resource
 @receiver(pre_metadata_element_create, sender=GenericResource)
 def metadata_element_pre_create_handler(sender, **kwargs):
     element_name = kwargs['element_name']
     request = kwargs['request']
-    if element_name == "subject":   #keywords
+    if element_name == "subject":   # keywords
         element_form = SubjectsForm(data=request.POST)
-    elif element_name == "description":   #abstract
+    elif element_name == "description":   # abstract
         element_form = AbstractValidationForm(request.POST)
     elif element_name == "creator":
         element_form = CreatorValidationForm(request.POST)
@@ -34,7 +38,7 @@ def metadata_element_pre_create_handler(sender, **kwargs):
         element_form = FundingAgencyValidationForm(request.POST)
     elif element_name == 'coverage':
         if 'type' in request.POST:
-            if request.POST['type'].lower() == 'point' or  request.POST['type'].lower() == 'box':
+            if request.POST['type'].lower() == 'point' or request.POST['type'].lower() == 'box':
                 element_form = CoverageSpatialForm(data=request.POST)
             else:
                 element_form = CoverageTemporalForm(data=request.POST)
@@ -50,11 +54,11 @@ def metadata_element_pre_create_handler(sender, **kwargs):
     else:
         return {'is_valid': False, 'element_data_dict': None}
 
+
 # This handler is executed only when a metadata element is added as part of editing a resource
 @receiver(pre_metadata_element_update, sender=GenericResource)
 def metadata_element_pre_update_handler(sender, **kwargs):
     element_name = kwargs['element_name'].lower()
-    element_id = kwargs['element_id']
     request = kwargs['request']
     repeatable_elements = {'creator': CreatorValidationForm,
                            'contributor': ContributorValidationForm,
@@ -64,14 +68,14 @@ def metadata_element_pre_update_handler(sender, **kwargs):
 
     if element_name == 'title':
         element_form = TitleValidationForm(request.POST)
-    elif element_name == "description":   #abstract
+    elif element_name == "description":   # abstract
         element_form = AbstractValidationForm(request.POST)
     elif element_name == "fundingagency":
         element_form = FundingAgencyValidationForm(request.POST)
     elif element_name in repeatable_elements:
-        # since element_name is a repeatable element (e.g creator) and data for the element is displayed on the
-        # landing page using formset, the data coming from a single element form in the request for update
-        # needs to be parsed to match with element field names
+        # since element_name is a repeatable element (e.g creator) and data for the element
+        # is displayed on the landing page using formset, the data coming from a single element
+        # form in the request for update needs to be parsed to match with element field names
         element_validation_form = repeatable_elements[element_name]
         form_data = {}
         for field_name in element_validation_form().fields:
