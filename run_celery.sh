08535471c354b6d9dcb6f1ba1434c83dd2ea91c5
Michael J. Stealey <michael.j.stealey@gmail.com>
2016-08-12 12:57:14 -0400

    path and permissions updates

--- run_celery.sh
+++ run_celery.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cd /home/docker/hydroshare
+cd /hydroshare
 
-celery beat -A hydroshare -s /home/docker/hydroshare/celery/celerybeat-schedule &
+celery beat -A hydroshare -s /hydroshare/celery/celerybeat-schedule &
 
 celery worker -A hydroshare -E -Q default
