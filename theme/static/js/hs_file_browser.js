dc3105a73dcee8e1d273777ead41dca690d5332d
mauriel133 <mauriel.ramirez@gmail.com>
2016-08-17 14:18:06 -0600

    Drag & drop file upload

--- theme/static/js/hs_file_browser.js
+++ theme/static/js/hs_file_browser.js
@@ -2,6 +2,17 @@
  * Created by Mauriel on 8/16/2016.
  */
 
+function selectSelectableElement (selectableContainer, elementsToSelect) {
+    // add unselecting class to all elements in the styleboard canvas except the ones to select
+    $(".ui-selected", selectableContainer).not(elementsToSelect).removeClass("ui-selected").addClass("ui-unselecting");
+
+    // add ui-selecting class to the elements to select
+    $(elementsToSelect).not(".ui-selected").addClass("ui-selected");
+
+    // trigger the mouse stop event (this will select all .ui-selecting elements, and deselect all .ui-unselecting elements)
+    selectableContainer.data("selectable")._mouseStop(null);
+}
+
 $(document).ready(function () {
     // Drop
     $(".droppable").droppable({
@@ -16,20 +27,42 @@ $(document).ready(function () {
             greedy: true
         })
         .sortable({
-            handle: ".handle"
+            handle: ".fb-handle",
+            containment: "parent"
         })
         .selectable({
-            filter: "li", cancel: ".handle",
+            filter: "li", cancel: ".fb-dropdown-toggle, .fb-handle",
             selected:function(event, ui){
-                var top = $("#fb-list .ui-selected").position().top - 70;
-                var left = $("#fb-list .ui-selected").position().left - 80;
-                $("#selection-menu").css({top: top, left: left});
-                $("#selection-menu").show();
+
             },
             unselected: function( event, ui ) {
                 $("#selection-menu").hide();
             }
-        })
-        .find("li.fb-folder, li.fb-file").prepend("<div class='handle'><span class='fa fa-arrows'></span></div>");
+        });
 
+
+    $(".fb-dropdown-toggle").click(function () {
+        var menu = $("#selection-menu");
+        var offsetTop = 170;
+        var offsetLeft = 10;
+        var top = $(this).position().top - offsetTop;
+        var left = $(this).position().left - offsetLeft;
+
+        menu.css({top: top, left: left});
+
+        if (menu.css("display") == "none")
+            menu.show();
+        else {
+            menu.hide();
+        }
+
+        if (!$(this).parent().hasClass("ui-selected")) {
+            selectSelectableElement($("#fb-list"), $(this).parent());    // Mark item as selected
+        }
+    });
+
+    $(".fb-handle").click(function(){
+        selectSelectableElement($("#fb-list"), $(this).parent());    // Mark item as selected
+        $("#selection-menu").hide();
+    })
 });
