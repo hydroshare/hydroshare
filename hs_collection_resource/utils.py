a4003e1838c3ca5b836973541bed1b31d8d4c04d
zhiyuli <zyli2004@gmail.com>
2016-08-25 18:46:10 -0400

    [#1403] avoid loop

--- hs_collection_resource/utils.py
+++ hs_collection_resource/utils.py
@@ -1,3 +1,5 @@
+import copy
+
 from hs_core.hydroshare.utils import resource_modified
 
 
@@ -26,3 +28,32 @@ def add_or_remove_relation_metadata(add=True, target_res_obj=None, relation_type
 
     if set_res_modified:
        resource_modified(target_res_obj, last_change_user)
+
+class FoundLoopException(Exception):
+     def __init__(self, node_id, path_list):
+        self.node_id = node_id
+        self.path_list = path_list
+     def __str__(self):
+        return repr(self.node_id)
+
+def traverse_collection(collection_node):
+    list = [collection_node]
+    if collection_node.resource_type.lower() == "collectionresource":
+        for node in collection_node.resources.all():
+            list += traverse_collection(node)
+    return list
+
+def detect_loop_in_collection(collection_node, visited_collection_list=None):
+    if visited_collection_list is None:
+        visited_collection_list_this_level = []
+    else:
+        visited_collection_list_this_level = copy.copy(visited_collection_list)
+    if collection_node.resource_type.lower() == "collectionresource":
+        visited_collection_list_this_level.append(collection_node.short_id)
+        for node in collection_node.resources.all():
+            if collection_node.resource_type.lower() == "collectionresource":
+                if node.short_id in visited_collection_list_this_level:
+                    raise FoundLoopException(node.short_id, visited_collection_list_this_level)
+                else:
+                    detect_loop_in_collection(node, visited_collection_list=visited_collection_list_this_level)
+    return visited_collection_list
