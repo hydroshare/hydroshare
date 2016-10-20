ea10a39ddc70745c211e417b6b36615015b097da
pkdash <pkdash_reena@hotmail.com>
2016-10-20 09:30:48 -0600

    [#1477] unit tests for file level key/value metadata

--- hs_file_types/tests/test_raster_metadata.py
+++ hs_file_types/tests/test_raster_metadata.py
@@ -13,7 +13,7 @@ from hs_core import hydroshare
 from hs_core.models import Coverage
 
 from hs_file_types.utils import set_file_to_geo_raster_file_type
-from hs_file_types.models import GeoRasterLogicalFile
+from hs_file_types.models import GeoRasterLogicalFile, GeoRasterFileMetaData
 
 from hs_geo_raster_resource.models import OriginalCoverage, CellInformation, BandInformation
 
@@ -39,16 +39,30 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
 
         self.temp_dir = tempfile.mkdtemp()
         self.raster_file_name = 'small_logan.tif'
+        self.raster_zip_file_name = 'logan_vrt_small.zip'
         self.invalid_raster_file_name = 'raster_tif_invalid.tif'
+        self.invalid_raster_zip_file_name = 'bad_small_vrt.zip'
         self.raster_file = 'hs_file_types/tests/{}'.format(self.raster_file_name)
+        self.raster_zip_file = 'hs_file_types/tests/{}'.format(self.raster_zip_file_name)
         self.invalid_raster_file = 'hs_file_types/tests/{}'.format(self.invalid_raster_file_name)
+        self.invalid_raster_zip_file = 'hs_file_types/tests/{}'.format(
+            self.invalid_raster_zip_file_name)
+
         target_temp_raster_file = os.path.join(self.temp_dir, self.raster_file_name)
         shutil.copy(self.raster_file, target_temp_raster_file)
         self.raster_file_obj = open(target_temp_raster_file, 'r')
 
+        target_temp_raster_file = os.path.join(self.temp_dir, self.raster_zip_file_name)
+        shutil.copy(self.raster_zip_file, target_temp_raster_file)
+        self.raster_zip_file_obj = open(target_temp_raster_file, 'r')
+
         target_temp_raster_file = os.path.join(self.temp_dir, self.invalid_raster_file_name)
         shutil.copy(self.invalid_raster_file, target_temp_raster_file)
         self.invalid_raster_file_obj = open(target_temp_raster_file, 'r')
+
+        target_temp_raster_file = os.path.join(self.temp_dir, self.invalid_raster_zip_file_name)
+        shutil.copy(self.invalid_raster_zip_file, target_temp_raster_file)
+        self.invalid_raster_zip_file_obj = open(target_temp_raster_file, 'r')
 
     def tearDown(self):
         super(RasterFileTypeMetaData, self).tearDown()
@@ -85,6 +99,12 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
         self.assertEqual(logical_file.files.all().count(), 2)
         self.assertEqual(set(self.composite_resource.files.all()),
                          set(logical_file.files.all()))
+
+        # test that there should be 1 object of type GeoRasterFileMetaData
+        self.assertEqual(GeoRasterFileMetaData.objects.count(), 1)
+
+        # test that the metadata associated with logical file id of type GeoRasterFileMetaData
+        self.assertTrue(isinstance(logical_file.metadata, GeoRasterFileMetaData))
 
         # there should be 2 format elements associated with resource
         self.assertEqual(self.composite_resource.metadata.formats.all().count(), 2)
@@ -137,13 +157,8 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
     def test_zip_set_file_type_to_geo_raster(self):
         # here we are using a valid raster zip file for setting it
         # to Geo Raster file type which includes metadata extraction
-        # TODO: implement this after I have a small zip file
-        pass
 
-    def test_set_file_type_to_geo_raster_invalid_file_1(self):
-        # here we are using an invalid raster tif file for setting it
-        # to Geo Raster file type which should fail
-        self.raster_file_obj = open(self.invalid_raster_file, 'r')
+        self.raster_file_obj = open(self.raster_zip_file, 'r')
         self._create_composite_resource()
 
         self.assertEqual(self.composite_resource.files.all().count(), 1)
@@ -151,17 +166,74 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
 
         # check that the resource file is not associated with any logical file
         self.assertEqual(res_file.has_logical_file, False)
+        set_file_to_geo_raster_file_type(self.composite_resource, res_file.id, self.user)
 
-        # trying to set this invalid tif file to geo raster file type should raise
-        # ValidationError
-        with self.assertRaises(ValidationError):
-            set_file_to_geo_raster_file_type(self.composite_resource, res_file.id, self.user)
+        # test the resource now has 3 files (one vrt file and 2 tif files)
+        self.assertEqual(self.composite_resource.files.all().count(), 3)
+        tif_files = hydroshare.utils.get_resource_files_by_extension(
+            self.composite_resource, '.tif')
+        self.assertEqual(len(tif_files), 2)
+        vrt_files = hydroshare.utils.get_resource_files_by_extension(
+            self.composite_resource, '.vrt')
+        self.assertEqual(len(vrt_files), 1)
 
-        # test that the invalid file did not get deleted
-        self.assertEqual(self.composite_resource.files.all().count(), 1)
+        # check that the logicalfile is associated with 3 files
+        self.assertEqual(GeoRasterLogicalFile.objects.count(), 1)
+        res_file = self.composite_resource.files.first()
+        logical_file = res_file.logical_file
+        self.assertEqual(logical_file.dataset_name, 'logan_vrt_small')
+        self.assertEqual(logical_file.has_metadata, True)
+        self.assertEqual(logical_file.files.all().count(), 3)
+        self.assertEqual(set(self.composite_resource.files.all()),
+                         set(logical_file.files.all()))
 
-        # check that the resource file is not associated with any logical file
-        self.assertEqual(res_file.has_logical_file, False)
+        # test extracted metadata for the file type
+
+        # there should be 1 coverage element - box type
+        self.assertEqual(logical_file.metadata.coverages.all().count(), 1)
+        self.assertEqual(logical_file.metadata.coverages.all().filter(type='box').count(), 1)
+
+        box_coverage = logical_file.metadata.coverages.all().filter(type='box').first()
+        self.assertEqual(box_coverage.value['projection'], 'WGS 84 EPSG:4326')
+        self.assertEqual(box_coverage.value['units'], 'Decimal degrees')
+        self.assertEqual(box_coverage.value['northlimit'], 42.04959948647449)
+        self.assertEqual(box_coverage.value['eastlimit'], -111.5773750264389)
+        self.assertEqual(box_coverage.value['southlimit'], 41.987886149256404)
+        self.assertEqual(box_coverage.value['westlimit'], -111.65768822411239)
+
+        # testing extended metadata element: original coverage
+        ori_coverage = logical_file.metadata.originalCoverage
+        self.assertNotEqual(ori_coverage, None)
+        self.assertEqual(ori_coverage.value['northlimit'], 4655492.446916306)
+        self.assertEqual(ori_coverage.value['eastlimit'], 452174.01909127034)
+        self.assertEqual(ori_coverage.value['southlimit'], 4648592.446916306)
+        self.assertEqual(ori_coverage.value['westlimit'], 445574.01909127034)
+        self.assertEqual(ori_coverage.value['units'], 'meter')
+        self.assertEqual(ori_coverage.value['projection'],
+                         'NAD83 / UTM zone 12N Transverse_Mercator')
+
+        # testing extended metadata element: cell information
+        cell_info = logical_file.metadata.cellInformation
+        self.assertEqual(cell_info.rows, 230)
+        self.assertEqual(cell_info.columns, 220)
+        self.assertEqual(cell_info.cellSizeXValue, 30.0)
+        self.assertEqual(cell_info.cellSizeYValue, 30.0)
+        self.assertEqual(cell_info.cellDataType, 'Float32')
+
+        # testing extended metadata element: band information
+        self.assertEqual(logical_file.metadata.bandInformation.count(), 1)
+        band_info = logical_file.metadata.bandInformation.first()
+        self.assertEqual(band_info.noDataValue, '-3.40282346639e+38')
+        self.assertEqual(band_info.maximumValue, '2880.00708008')
+        self.assertEqual(band_info.minimumValue, '2274.95898438')
+
+    def test_set_file_type_to_geo_raster_invalid_file_1(self):
+        # here we are using an invalid raster tif file for setting it
+        # to Geo Raster file type which should fail
+        self.raster_file_obj = open(self.invalid_raster_file, 'r')
+        self._create_composite_resource()
+
+        self._test_invalid_file()
 
     def test_set_file_type_to_geo_raster_invalid_file_2(self):
         # here we are using a raster tif file for setting it
@@ -187,6 +259,14 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
         # ValidationError
         with self.assertRaises(ValidationError):
             set_file_to_geo_raster_file_type(self.composite_resource, res_file.id, self.user)
+
+    def test_set_file_type_to_geo_raster_invalid_file_3(self):
+        # here we are using an invalid raster zip file for setting it
+        # to Geo Raster file type - should fail
+        self.raster_file_obj = open(self.invalid_raster_zip_file, 'r')
+        self._create_composite_resource()
+
+        self._test_invalid_file()
 
     def test_metadata_CRUD(self):
         self.raster_file_obj = open(self.raster_file, 'r')
@@ -340,8 +420,30 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
         self.assertEquals(band_info.minimumValue, '1')
         self.assertEquals(band_info.noDataValue, '-9998')
 
+        # test extra_metadata for the logical file
+        # there should be no key/value metadata at this point
+        self.assertEqual(logical_file.metadata.extra_metadata, {})
+
+        # create key/vale metadata
+        logical_file.metadata.extra_metadata = {'key1': 'value 1', 'key2': 'value 2'}
+        logical_file.save()
+        self.assertEqual(logical_file.metadata.extra_metadata,
+                         {'key1': 'value 1', 'key2': 'value 2'})
+
+        # update key/value metadata
+        logical_file.metadata.extra_metadata = {'key1': 'value 1', 'key2': 'value 2',
+                                                'key 3': 'value3'}
+        logical_file.save()
+        self.assertEqual(logical_file.metadata.extra_metadata,
+                         {'key1': 'value 1', 'key2': 'value 2', 'key 3': 'value3'})
+
+        # delete key/value metadata
+        logical_file.metadata.extra_metadata = {}
+        logical_file.save()
+        self.assertEqual(logical_file.metadata.extra_metadata, {})
+
     def test_file_metadata_on_file_delete(self):
-        # test that when any file in GeorasterFileType is deleted
+        # test that when any file in GeoRasterFileType is deleted
         # all metadata associated with GeoRasterFileType is deleted
         # test for both .tif and .vrt delete
 
@@ -352,7 +454,7 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
         self._test_file_metadata_on_file_delete(ext='.vrt')
 
     def test_file_metadata_on_logical_file_delete(self):
-        # test that when the GeorasterFileType is deleted
+        # test that when the GeoRasterFileType is deleted
         # all metadata associated with GeoRasterFileType is deleted
         self.raster_file_obj = open(self.raster_file, 'r')
         self._create_composite_resource()
@@ -364,6 +466,7 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
         # test that we have one logical file of type GeoRasterFileType as a result
         # of metadata extraction
         self.assertEqual(GeoRasterLogicalFile.objects.count(), 1)
+        self.assertEqual(GeoRasterFileMetaData.objects.count(), 1)
 
         # test that we have the metadata elements
         self.assertEqual(Coverage.objects.count(), 1)
@@ -377,6 +480,7 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
         logical_file.logical_delete(self.user)
         # test that we have no logical file of type GeoRasterFileType
         self.assertEqual(GeoRasterLogicalFile.objects.count(), 0)
+        self.assertEqual(GeoRasterFileMetaData.objects.count(), 0)
 
         # test that all metadata deleted
         self.assertEqual(Coverage.objects.count(), 0)
@@ -397,6 +501,7 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
         # test that we have one logical file of type GeoRasterFileType as a result
         # of metadata extraction
         self.assertEqual(GeoRasterLogicalFile.objects.count(), 1)
+        self.assertEqual(GeoRasterFileMetaData.objects.count(), 1)
 
         # test that we have the metadata elements
         self.assertEqual(Coverage.objects.count(), 1)
@@ -409,6 +514,7 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
 
         # test that we have no logical file of type GeoRasterFileType
         self.assertEqual(GeoRasterLogicalFile.objects.count(), 0)
+        self.assertEqual(GeoRasterFileMetaData.objects.count(), 0)
 
         # test that all metadata deleted
         self.assertEqual(Coverage.objects.count(), 0)
@@ -492,6 +598,7 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
 
         # test that we have one logical file of type GeoRasterFileType
         self.assertEqual(GeoRasterLogicalFile.objects.count(), 1)
+        self.assertEqual(GeoRasterFileMetaData.objects.count(), 1)
 
         res_file = self.composite_resource.files.first()
         logical_file = res_file.logical_file
@@ -514,6 +621,7 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
                                         self.user)
         # test that we don't have logical file of type GeoRasterFileType
         self.assertEqual(GeoRasterLogicalFile.objects.count(), 0)
+        self.assertEqual(GeoRasterFileMetaData.objects.count(), 0)
 
         # test that all metadata deleted
         self.assertEqual(Coverage.objects.count(), 0)
@@ -542,3 +650,21 @@ class RasterFileTypeMetaData(MockIRODSTestCaseMixin, TransactionTestCase):
 
         self.assertEqual(self.composite_resource.files.all().count(), 0)
         self.assertEqual(GeoRasterLogicalFile.objects.count(), 0)
+
+    def _test_invalid_file(self):
+        self.assertEqual(self.composite_resource.files.all().count(), 1)
+        res_file = self.composite_resource.files.first()
+
+        # check that the resource file is not associated with any logical file
+        self.assertEqual(res_file.has_logical_file, False)
+
+        # trying to set this invalid tif file to geo raster file type should raise
+        # ValidationError
+        with self.assertRaises(ValidationError):
+            set_file_to_geo_raster_file_type(self.composite_resource, res_file.id, self.user)
+
+        # test that the invalid file did not get deleted
+        self.assertEqual(self.composite_resource.files.all().count(), 1)
+
+        # check that the resource file is not associated with any logical file
+        self.assertEqual(res_file.has_logical_file, False)
