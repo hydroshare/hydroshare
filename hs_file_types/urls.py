d28a33f1b5ce079ef612c16d1dc067ad3ab52f27
pkdash <pkdash_reena@hotmail.com>
2016-10-20 16:30:52 -0600

    [#1477] url pattern changed

--- hs_file_types/urls.py
+++ hs_file_types/urls.py
@@ -4,7 +4,7 @@ from hs_file_types import views
 
 urlpatterns = patterns('',
     url(r'^_internal/(?P<resource_id>[A-z0-9]+)/(?P<file_id>[0-9]+)/'
-        r'(?P<hs_file_type>[A-z]+)/extract-file-metadata/$',
+        r'(?P<hs_file_type>[A-z]+)/set-file-type/$',
         views.set_file_type,
         name="set_file_type"),
 
