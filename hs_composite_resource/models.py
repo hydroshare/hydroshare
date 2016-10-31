dfffca4084b0818a134ce12bf2d9abdfdf898960
pkdash <pkdash_reena@hotmail.com>
2016-10-31 12:34:38 -0600

    [#1477] composite resource property to check if resource can be public/discoverable

--- hs_composite_resource/models.py
+++ hs_composite_resource/models.py
@@ -12,6 +12,17 @@ class CompositeResource(BaseResource):
         verbose_name = 'Composite'
         proxy = True
 
+    @property
+    def can_be_public_or_discoverable(self):
+        # TODO: This needs to be unit tested
+        if not super(BaseResource, self).can_be_public_or_discoverable:
+            return False
+        for lf in self.logical_files:
+            if not lf.metadata.has_all_required_elements():
+                return False
+
+        return True
+
 # this would allow us to pick up additional form elements for the template before the template
 # is displayed
 processor_for(CompositeResource)(resource_processor)
