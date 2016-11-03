47d7b5be6c00e385cf83b2c81b6b38b827e06e85
hyi <hongyi@renci.org>
2016-11-03 10:11:33 -0400

    refined irods mock class and test_add_resource_files

--- hs_core/testing.py
+++ hs_core/testing.py
@@ -1,18 +1,24 @@
+from django.conf import settings
+
+
 class MockIRODSTestCaseMixin(object):
     def setUp(self):
         super(MockIRODSTestCaseMixin, self).setUp()
-        from mock import patch
-        self.irods_patchers = (
-            patch("hs_core.hydroshare.hs_bagit.delete_bag"),
-            patch("hs_core.hydroshare.hs_bagit.create_bag"),
-            patch("hs_core.hydroshare.hs_bagit.create_bag_files"),
-            patch("hs_core.tasks.create_bag_by_irods"),
-            patch("hs_core.hydroshare.utils.copy_resource_files_and_AVUs"),
-        )
-        for patcher in self.irods_patchers:
-            patcher.start()
+        # only mock up testing iRODS operations when local iRODS container is not used
+        if settings.IRODS_HOST != 'data.local.org':
+            from mock import patch
+            self.irods_patchers = (
+                patch("hs_core.hydroshare.hs_bagit.delete_bag"),
+                patch("hs_core.hydroshare.hs_bagit.create_bag"),
+                patch("hs_core.hydroshare.hs_bagit.create_bag_files"),
+                patch("hs_core.tasks.create_bag_by_irods"),
+                patch("hs_core.hydroshare.utils.copy_resource_files_and_AVUs"),
+            )
+            for patcher in self.irods_patchers:
+                patcher.start()
 
     def tearDown(self):
-        for patcher in self.irods_patchers:
-            patcher.stop()
+        if settings.IRODS_HOST != 'data.local.org':
+            for patcher in self.irods_patchers:
+                patcher.stop()
         super(MockIRODSTestCaseMixin, self).tearDown()
