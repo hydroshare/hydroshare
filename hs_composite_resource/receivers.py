5e4310808be9d1bf36325fef2c4417aed477771b
pkdash <pkdash_reena@hotmail.com>
2016-11-14 16:32:25 -0700

    [#1477] added signal for post file delete

--- hs_composite_resource/receivers.py
+++ hs_composite_resource/receivers.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from django.dispatch import receiver
 
-from hs_core.signals import post_add_files_to_resource, post_create_resource
+from hs_core.signals import post_add_files_to_resource, post_create_resource, \
+    post_delete_file_from_resource
 
 from .models import CompositeResource
 from hs_file_types.models import GenericLogicalFile
@@ -9,18 +10,32 @@ from hs_file_types.models import GenericLogicalFile
 
 @receiver(post_create_resource, sender=CompositeResource)
 def post_create_resource_handler(sender, **kwargs):
+    # create a GenericLogicalFile object for each of the
+    # content files in this new resource just created
     resource = kwargs['resource']
-    for res_file in resource.files.all():
-        if not res_file.has_logical_file:
-            logical_file = GenericLogicalFile.create()
-            res_file.logical_file_content_object = logical_file
-            res_file.save()
+    _set_genericlogicalfile_type(resource)
 
 
 @receiver(post_add_files_to_resource, sender=CompositeResource)
 def post_add_files_to_resource_handler(sender, **kwargs):
     """sets GenericLogicalFile type to any file that is not already part of any logical file"""
     resource = kwargs['resource']
+    _set_genericlogicalfile_type(resource)
+
+
+@receiver(post_delete_file_from_resource, sender=CompositeResource)
+def post_delete_file_from_resource_handler(sender, **kwargs):
+    """resource label coverage data needs to be updated when a content file
+    gets deleted from composite resource"""
+    from hs_file_types.utils import update_resource_coverage_element
+    resource = kwargs['resource']
+    update_resource_coverage_element(resource)
+
+
+def _set_genericlogicalfile_type(resource):
+    """sets GenericLogicalFile type to any file that is not already part of any logical file
+    in the specified resource
+    """
     for res_file in resource.files.all():
         if not res_file.has_logical_file:
             logical_file = GenericLogicalFile.create()
--- hs_core/signals.py
+++ hs_core/signals.py
@@ -5,6 +5,7 @@ post_create_resource = django.dispatch.Signal(providing_args=['resource', 'metad
 
 pre_add_files_to_resource = django.dispatch.Signal(providing_args=['files', 'resource'])
 pre_delete_file_from_resource = django.dispatch.Signal(providing_args=['file', 'resource'])
+post_delete_file_from_resource = django.dispatch.Signal(providing_args=['resource'])
 post_add_files_to_resource = django.dispatch.Signal(providing_args=['files', 'resource'])
 
 pre_metadata_element_create = django.dispatch.Signal(providing_args=['element_name', 'request'])
