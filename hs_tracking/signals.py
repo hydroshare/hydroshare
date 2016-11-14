17375c4d05a3c3fef8d8f6956bb38f86e1f8be69
Alex Lemann <lemanal@caktusgroup.com>
2016-11-14 08:50:10 -0500

    Creates tracking variable on download signal

--- hs_tracking/signals.py
+++ hs_tracking/signals.py
@@ -2,6 +2,7 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 
 from .models import Session
+from hs_core.signals import pre_download_file
 
 
 @receiver(user_logged_in, dispatch_uid='id_capture_login')
@@ -14,3 +15,11 @@ def capture_login(sender, **kwargs):
 def capture_logout(sender, **kwargs):
     session = Session.objects.for_request(kwargs['request'], kwargs['user'])
     session.record('logout')
+
+@receiver(pre_download_file)
+def capture_download(sender, **kwargs):
+    session = Session.objects.for_request(kwargs['request'], kwargs['user'])
+    resource = kwargs['resource']
+    filename = kwargs['download_file_name']
+    values = [resource.id, filename, resource.size, resouce.resource_type]
+    session.record('download', value=",".join(values))
