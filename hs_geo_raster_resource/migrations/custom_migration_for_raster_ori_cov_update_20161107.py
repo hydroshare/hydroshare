305db1a62a3d95c7b4b17f780ad4df799d54ce64
gantian127 <gantianhejimao@yahoo.com.cn>
2016-11-15 12:06:40 -0700

    [#1520] clean up migration code

--- hs_geo_raster_resource/migrations/custom_migration_for_raster_ori_cov_update_20161107.py
+++ hs_geo_raster_resource/migrations/custom_migration_for_raster_ori_cov_update_20161107.py
@@ -26,35 +26,38 @@ def migrate_tif_file(apps, schema_editor):
 
     # start migration for each raster resource that has raster files
     for res in RasterResource.objects.all():
-        if res.files.all():
-            # copy all the resource files to temp dir
-            temp_dir = ''
-            res_file_tmp_path = ''
 
-            try:
-                temp_dir = tempfile.mkdtemp()
-                for res_file in res.files.all():
-                    res_file_tmp_path = get_file_from_irods(res_file)
-                    shutil.copy(res_file_tmp_path,
-                                os.path.join(temp_dir, os.path.basename(res_file_tmp_path)))
-                    shutil.rmtree(os.path.dirname(res_file_tmp_path))
+        # copy all the resource files to temp dir
+        temp_dir = ''
+        res_file_tmp_path = ''
 
-                vrt_file_path = [os.path.join(temp_dir, f) for f in os.listdir(temp_dir) if '.vrt' == f[-4:]].pop()
+        try:
+            temp_dir = tempfile.mkdtemp()
+            for res_file in res.files.all():
+                res_file_tmp_path = get_file_from_irods(res_file)
+                shutil.copy(res_file_tmp_path,
+                            os.path.join(temp_dir, os.path.basename(res_file_tmp_path)))
+                shutil.rmtree(os.path.dirname(res_file_tmp_path))
 
-            except Exception as e:
-                if os.path.isdir(temp_dir):
-                    shutil.rmtree(temp_dir)
-                if os.path.isfile(res_file_tmp_path):
-                    shutil.rmtree(os.path.dirname(res_file_tmp_path))
-                log.exception(e.message)
-                copy_res_fail.append('{}:{}'.format(res.short_id, res.metadata.title.value))
-                continue
+            vrt_file_path = [os.path.join(temp_dir, f) for f in os.listdir(temp_dir) if '.vrt' == f[-4:]].pop()
 
-            # update the metadata for the original coverage information of all the raster resources
-            try:
+        except Exception as e:
+            if os.path.isdir(temp_dir):
+                shutil.rmtree(temp_dir)
+            if os.path.isfile(res_file_tmp_path):
+                shutil.rmtree(os.path.dirname(res_file_tmp_path))
+            log.exception(e.message)
+            copy_res_fail.append('{}:{}'.format(res.short_id, res.metadata.title.value))
+            continue
+
+        # update the metadata for the original coverage information of all the raster resources
+        try:
+            if temp_dir and vrt_file_path:
                 meta_updated = False
 
-                # extract meta
+                # extract meta.
+                # the reason to change current working directory to temp_dir is to make sure the raster files can be
+                # found by Gdal for metadata extraction when "relativeToVRT" parameter is set as "0"
                 ori_dir = os.getcwd()
                 os.chdir(temp_dir)
                 res_md_dict = raster_meta_extract.get_raster_meta_dict(vrt_file_path)
@@ -78,11 +81,11 @@ def migrate_tif_file(apps, schema_editor):
                     resource_modified(res, res.creator)
                     meta_update_success.append('{}:{}'.format(res.short_id, res.metadata.title.value))
 
-            except Exception as e:
-                if os.path.isdir(temp_dir):
-                    shutil.rmtree(temp_dir)
-                log.exception(e.message)
-                meta_update_fail.append('{}:{}'.format(res.short_id, res.metadata.title.value))
+        except Exception as e:
+            if os.path.isdir(temp_dir):
+                shutil.rmtree(temp_dir)
+            log.exception(e.message)
+            meta_update_fail.append('{}:{}'.format(res.short_id, res.metadata.title.value))
 
     # Print migration results
     print 'Copy resource to temp folder failure: Number: {} List: {}'.format(len(copy_res_fail), copy_res_fail)
