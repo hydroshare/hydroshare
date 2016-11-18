b8b3c38b4550711ab7c6e04c9e7d19bb2b416a53
pkdash <pkdash_reena@hotmail.com>
2016-11-18 10:27:06 -0700

    [#1477] minor changes to composite resource page processor

--- hs_composite_resource/page_processors.py
+++ hs_composite_resource/page_processors.py
@@ -13,12 +13,10 @@ def landing_page(request, page):
         A typical Mezzanine page processor.
 
     """
-    content_model = page.get_content_model()
     edit_resource = page_processors.check_resource_mode(request)
     context = page_processors.get_page_context(page, request.user, resource_edit=edit_resource,
                                                extended_metadata_layout=None, request=request)
 
-    context['resource_edit_mode'] = edit_resource
     hs_core_context = add_generic_context(request, page)
     context.update(hs_core_context)
 
