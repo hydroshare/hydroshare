7f7a0b4dd84963e68ac1bbb6bd6b337932ca584e
Alex Lemann <lemanal@caktusgroup.com>
2016-11-21 10:49:53 -0500

    flake8 fixes

--- theme/tests/test_functional.py
+++ theme/tests/test_functional.py
@@ -1,7 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+
 
 class FunctionalTests(StaticLiveServerTestCase):
     """Iteractive tests with selenium."""
@@ -25,11 +25,13 @@ class FunctionalTests(StaticLiveServerTestCase):
         self.mobile_browser.get(self.live_server_url)
         mobile_login_link = self.mobile_browser.find_element_by_id('signin-menu')
         self.mobile_browser.get_screenshot_as_file("mobile.png")
-        self.assertFalse(mobile_login_link.is_displayed(), 'Login link should not be visible on mobile.')
+        self.assertFalse(mobile_login_link.is_displayed(),
+                         'Login link should not be visible on mobile.')
 
     def test_show_login_link_desktop(self):
         """The login link but not form should be displayed on the home page"""
         self.desktop_browser.get(self.live_server_url)
         desktop_login_link = self.desktop_browser.find_element_by_id('signin-menu')
         self.desktop_browser.get_screenshot_as_file("desktop.png")
-        self.assertTrue(desktop_login_link.is_displayed(), 'Login link should be visible on desktop.')
+        self.assertTrue(desktop_login_link.is_displayed(),
+                        'Login link should be visible on desktop.')
