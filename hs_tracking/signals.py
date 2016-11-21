3018ab3e9d680991509bf9997dc5b29fce1c5966
Castronova <castronova.anthony@gmail.com>
2016-11-21 09:32:10 -0500

    #1531 replace  with the just the functions that are used

--- hs_tracking/signals.py
+++ hs_tracking/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 
-from hs_core.signals import *
+from hs_core.signals import pre_download_file, post_delete_resource, post_create_resource
 
 from .models import Session
 from .models import Variable
