76935c758bd23c6fc7183347489a3157e69935ee
pkdash <pkdash_reena@hotmail.com>
2016-11-28 12:43:14 -0700

    [#1477] removed importing of recievers from models.py as needed by pylint

---
+++ hs_composite_resource/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'hs_composite_resource.apps.CompositeResourceAppConfig'
--- hs_composite_resource/models.py
+++ hs_composite_resource/models.py
@@ -1,5 +1,3 @@
-# coding=utf-8
-from django.db import models
 from mezzanine.pages.page_processors import processor_for
 
 from hs_core.models import BaseResource, ResourceManager, resource_processor
@@ -14,7 +12,6 @@ class CompositeResource(BaseResource):
 
     @property
     def can_be_public_or_discoverable(self):
-        # TODO: This needs to be unit tested
         if not super(CompositeResource, self).can_be_public_or_discoverable:
             return False
         for lf in self.logical_files:
@@ -44,5 +41,3 @@ class CompositeResource(BaseResource):
 # this would allow us to pick up additional form elements for the template before the template
 # is displayed
 processor_for(CompositeResource)(resource_processor)
-
-import receivers
