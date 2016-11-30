c60eac70d3edccad1ae2d6efbc020e30b9454284
jsadler2 <jeffrey.sadler2@gmail.com>
2016-11-30 14:40:26 -0500

    [#855] adjust tests for change in get_user_data view

--- hs_core/tests/api/views/test_get_user_data.py
+++ hs_core/tests/api/views/test_get_user_data.py
@@ -40,11 +40,11 @@ class TestGetUserData(TestCase):
         self.factory = RequestFactory()
 
     def test_get_user_data(self):
-        post_data = {'user_id': self.john.id}
+        post_data = {'user_id': self.john.id, 'is_group': 'false'}
         url = reverse('get_user_data', kwargs=post_data)
         request = self.factory.post(url, data=post_data)
         request.user = self.mike
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John Clarson')
@@ -60,7 +60,7 @@ class TestGetUserData(TestCase):
         # test middle name
         self.john.userprofile.middle_name = 'K'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -74,7 +74,7 @@ class TestGetUserData(TestCase):
         # test address
         self.john.userprofile.state = 'Utah'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -88,7 +88,7 @@ class TestGetUserData(TestCase):
         self.john.userprofile.state = ''
         self.john.userprofile.country = 'USA'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -102,7 +102,7 @@ class TestGetUserData(TestCase):
         self.john.userprofile.state = 'Utah'
         self.john.userprofile.country = 'USA'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -116,7 +116,7 @@ class TestGetUserData(TestCase):
         # test phone
         self.john.userprofile.phone_1 = '678-890-7890'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -130,7 +130,7 @@ class TestGetUserData(TestCase):
         self.john.userprofile.phone_1 = ''
         self.john.userprofile.phone_2 = '555-333-9999'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -144,7 +144,7 @@ class TestGetUserData(TestCase):
         self.john.userprofile.phone_1 = '678-890-7890'
         self.john.userprofile.phone_2 = '555-333-9999'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -158,7 +158,7 @@ class TestGetUserData(TestCase):
         # test organization
         self.john.userprofile.organization = 'USU'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
@@ -172,7 +172,7 @@ class TestGetUserData(TestCase):
         # test website
         self.john.userprofile.website = 'www.usu.edu.org'
         self.john.userprofile.save()
-        response = get_user_data(request, user_id=self.john.id)
+        response = get_user_data(request, user_id=self.john.id, is_group='false')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         resp_json = json.loads(response.content)
         self.assertEqual(resp_json['name'], 'John K Clarson')
