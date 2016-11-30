505a2bb11c0de1618e465fd9ccb839b579c62f6f
Castronova <castronova.anthony@gmail.com>
2016-11-30 10:03:10 -0500

    #1531 added exception handling to hs_tracking.utils functions to resolve failing unit tests

--- hs_tracking/utils.py
+++ hs_tracking/utils.py
@@ -1,25 +1,30 @@
 
 
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
-    else:
-        ip = request.META.get('REMOTE_ADDR')
+    try:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[-1].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+    except AttributeError:
+        ip = None
     return ip
 
 
 def get_user_type(session):
-    user = session.visitor.user
-    usertype = 'None'
-    if user is not None:
+    try:
+        user = session.visitor.user
         usertype = user.userprofile.user_type or 'undefined'
+    except AttributeError:
+        usertype = None
     return usertype
 
 
 def get_user_email_domain(session):
-    user = session.visitor.user
-    emaildomain = 'None'
-    if user is not None:
+    try:
+        user = session.visitor.user
         emaildomain = user.email.split('.')[-1]
+    except AttributeError:
+        emaildomain = None
     return emaildomain
