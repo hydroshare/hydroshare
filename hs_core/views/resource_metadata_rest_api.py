476f90cd862ae720bca52d7619fd19e1392ae4d0
Mark Henderson <henderson.mark@gmail.com>
2016-12-02 11:19:03 -0500

    PEP8 Fix

--- hs_core/views/resource_metadata_rest_api.py
+++ hs_core/views/resource_metadata_rest_api.py
@@ -1,7 +1,7 @@
 import logging
 
 from rest_framework.response import Response
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework import status
 from rest_framework import generics
 from rest_framework import serializers
