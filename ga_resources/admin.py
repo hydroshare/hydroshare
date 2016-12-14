c84422443cdda2320fd540c0049dd4c3d16c8cce
hyi <hongyi@renci.org>
2016-12-14 12:37:58 -0500

    removed ga_resources from admin panel

--- ga_resources/admin.py
+++ ga_resources/admin.py
@@ -1,5 +1,4 @@
 from mezzanine.pages.admin import PageAdmin
-from django.contrib.gis import admin
 from ga_resources.models import *
 from django.forms import ModelForm
 from django import forms
@@ -15,29 +14,3 @@ class RenderedLayerAdminForm(ModelForm):
 
 class RenderedLayerAdmin(PageAdmin):
    form = RenderedLayerAdminForm
-
-# TODO add prepopulated fields to all models with slug fields.
-# class ArticleAdmin(admin.ModelAdmin):
-#   prepopulated_fields = {"slug": ("title",)}
-
-
-#class OSMPageAdmin(admin.OSMGeoAdmin):
-#    fieldsets = deepcopy(PageAdmin.fieldsets)
-
-#class DataResourceAdmin(admin.OSMGeoAdmin):
-#    fieldsets = deepcopy(PageAdmin.fieldsets) + ((None, {"fields" : (
-#        'content',
-#        "resource_file", "resource_url", "resource_irods_env", "resource_irods_file",
-#        "time_represented","perform_caching","cache_ttl","data_cache","bounding_box","kind","driver"
-#    )}),)
-
-admin.site.register(CatalogPage, PageAdmin)
-#admin.site.register(Verb, PageAdmin)
-admin.site.register(DataResource, PageAdmin)
-admin.site.register(ResourceGroup, PageAdmin)
-admin.site.register(OrderedResource)
-
-admin.site.register(RelatedResource, PageAdmin)
-
-admin.site.register(RenderedLayer, RenderedLayerAdmin)
-admin.site.register(Style, PageAdmin)
