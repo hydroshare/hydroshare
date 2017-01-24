34a81faec2690f87ea67920bfec39385241421ed
pkdash <pkdash_reena@hotmail.com>
2017-01-24 14:14:34 -0700

    [#931] form code cleanup

--- hs_core/page_processors.py
+++ hs_core/page_processors.py
@@ -1,6 +1,9 @@
+from functools import partial, wraps
+
+from django.forms.models import formset_factory
+
 from mezzanine.pages.page_processors import processor_for
 
-from hs_core.models import AbstractResource, GenericResource
 from hs_core import languages_iso
 from forms import *
 from hs_tools_resource.models import SupportedResTypes, ToolResource
@@ -26,8 +29,8 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
     :param resource_edit: True if and only if the page should render in edit mode
     :param extended_metadata_layout: layout information used to build an ExtendedMetadataForm
     :param request: the Django request associated with the page load
-    :return: the basic template context (a python dict) used to render a resource page. can and should be extended
-                by page/resource-specific page_processors
+    :return: the basic template context (a python dict) used to render a resource page. can and
+    should be extended by page/resource-specific page_processors
 
 
     TODO: refactor to make it clear that there are two different modes = EDITABLE | READONLY
@@ -59,7 +62,8 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
                 tool_homepage_url = content_model.metadata.homepage_url.first().value
 
         relevant_tools = []
-        # loop through all SupportedResTypes objs (one webapp resources has one SupportedResTypes obj)
+        # loop through all SupportedResTypes objs (one webapp resources has one
+        # SupportedResTypes obj)
         for res_type in SupportedResTypes.objects.all():
             supported_flag = False
             for supported_type in res_type.supported_res_types.all():
@@ -243,9 +247,6 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
     add_source_modal_form = SourceForm(allow_edit=can_change, res_short_id=content_model.short_id)
     add_fundingagency_modal_form = FundingAgencyForm(allow_edit=can_change, res_short_id=content_model.short_id)
 
-    title_form = TitleForm(instance=content_model.metadata.title, allow_edit=can_change, res_short_id=content_model.short_id,
-                             element_id=content_model.metadata.title.id if content_model.metadata.title else None)
-
     keywords = ",".join([sub.value for sub in content_model.metadata.subjects.all()])
     subjects_form = SubjectsForm(initial={'value': keywords}, allow_edit=can_change, res_short_id=content_model.short_id,
                              element_id=None)
@@ -257,37 +258,21 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
 
     creator_formset = CreatorFormSetEdit(initial=content_model.metadata.creators.all().values(), prefix='creator')
     index = 0
+
     # TODO: dont track index manually. use enumerate, or zip
-    creators = content_model.metadata.creators.all()
-    ProfileLinksFormSetEdit = formset_factory(ProfileLinksForm, formset=BaseProfileLinkFormSet, extra=0)
 
     for creator_form in creator_formset.forms:
         creator_form.action = "/hsapi/_internal/%s/creator/%s/update-metadata/" % (content_model.short_id, creator_form.initial['id'])
-        creator_form.profile_link_formset = ProfileLinksFormSetEdit(initial=creators[index].external_links.all().values('type', 'url'), prefix='creator_links-%s' % index)
-
-        # TODO: Temporarily the delete profile link is disabled on the resource landing page as we do not how the functionality for this button be implemented
-        for link_form in creator_form.profile_link_formset.forms:
-            link_form.helper.layout[1][0] = StrictButton('Delete link', css_class=link_form.helper.delete_btn_class, disabled="disabled")
-
-        creator_form.delete_modal_form = MetaDataElementDeleteForm(content_model.short_id, 'creator', creator_form.initial['id'])
         creator_form.number = creator_form.initial['id']
         index += 1
 
     ContributorFormSetEdit = formset_factory(wraps(ContributorForm)(partial(ContributorForm, allow_edit=can_change)), formset=BaseContributorFormSet, extra=0)
     contributor_formset = ContributorFormSetEdit(initial=content_model.metadata.contributors.all().values(), prefix='contributor')
 
-    contributors = content_model.metadata.contributors.all()
     index = 0
     # TODO: dont track index manually. use enumerate, or zip
     for contributor_form in contributor_formset.forms:
         contributor_form.action = "/hsapi/_internal/%s/contributor/%s/update-metadata/" % (content_model.short_id, contributor_form.initial['id'])
-        contributor_form.profile_link_formset = ProfileLinksFormSetEdit(initial=contributors[index].external_links.all().values('type', 'url'), prefix='contributor_links-%s' % index)
-
-        # TODO: Temporarily the delete profile link is disabled on the resource landing page as we do not know how the functionality for this button be implemented
-        for link_form in contributor_form.profile_link_formset.forms:
-            link_form.helper.layout[1][0] = StrictButton('Delete link', css_class=link_form.helper.delete_btn_class, disabled="disabled")
-
-        contributor_form.delete_modal_form = MetaDataElementDeleteForm(content_model.short_id, 'contributor', contributor_form.initial['id'])
         contributor_form.number = contributor_form.initial['id']
         index += 1
 
@@ -296,17 +281,10 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
 
     for relation_form in relation_formset.forms:
         relation_form.action = "/hsapi/_internal/%s/relation/%s/update-metadata/" % (content_model.short_id, relation_form.initial['id'])
-        relation_form.delete_modal_form = MetaDataElementDeleteForm(content_model.short_id, 'relation', relation_form.initial['id'])
         relation_form.number = relation_form.initial['id']
 
     SourceFormSetEdit = formset_factory(wraps(SourceForm)(partial(SourceForm, allow_edit=can_change)), formset=BaseFormSet, extra=0)
     source_formset = SourceFormSetEdit(initial=content_model.metadata.sources.all().values(), prefix='source')
-
-    # IdentifierFormSetEdit = formset_factory(IdentifierForm, formset=BaseFormSet, extra=0)
-    # identifier_formset = IdentifierFormSetEdit(initial=content_model.metadata.identifiers.all().values(), prefix='identifier')
-    #
-    # FormatFormSetEdit = formset_factory(FormatForm, formset=BaseFormSet, extra=0)
-    # format_formset = FormatFormSetEdit(initial=content_model.metadata.formats.all().values(), prefix='format')
 
     for source_form in source_formset.forms:
         source_form.action = "/hsapi/_internal/%s/source/%s/update-metadata/" % (content_model.short_id, source_form.initial['id'])
@@ -323,21 +301,7 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
         action = "/hsapi/_internal/{}/fundingagnecy/{}/update-metadata/"
         action = action.format(content_model.short_id, fundingagency_form.initial['id'])
         fundingagency_form.action = action
-        fundingagency_form.delete_modal_form = MetaDataElementDeleteForm(content_model.short_id, 'fundingagency',
-                                                                         fundingagency_form.initial['id'])
         fundingagency_form.number = fundingagency_form.initial['id']
-
-
-    rights_form = RightsForm(instance=content_model.metadata.rights,
-                             allow_edit=can_change,
-                             res_short_id=content_model.short_id,
-                             element_id=content_model.metadata.rights.id if content_model.metadata.rights else None)
-
-    language_form = LanguageForm(instance=content_model.metadata.language,
-                                 allow_edit=can_change,
-                                 res_short_id=content_model.short_id,
-                                 element_id=content_model.metadata.language.id if content_model.metadata.language
-                                 else None)
 
     temporal_coverages = content_model.metadata.coverages.all().filter(type='period')
     temporal_coverage_data_dict = {}
@@ -384,16 +348,12 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
                                                 res_short_id=content_model.short_id,
                                                 element_id=spatial_coverage.id if spatial_coverage else None)
 
-    # metadata_form = MetaDataForm(resource_mode='edit' if edit_mode else 'view',
-    #                              extended_metadata_layout=extended_metadata_layout)
-
     metadata_form = ExtendedMetadataForm(resource_mode='edit' if can_change else 'view',
                                  extended_metadata_layout=extended_metadata_layout)
 
     context = {
                'resource_edit_mode': resource_edit,
                'metadata_form': metadata_form,
-               'title_form': title_form,
                'creator_formset': creator_formset,
                'add_creator_modal_form': add_creator_modal_form,
                'creator_profilelink_formset': None,
@@ -407,8 +367,6 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
                'add_source_modal_form': add_source_modal_form,
                'fundingagnency_formset': fundingagency_formset,
                'add_fundinagency_modal_form': add_fundingagency_modal_form,
-               'rights_form': rights_form,
-               'language_form': language_form,
                'coverage_temporal_form': coverage_temporal_form,
                'coverage_spatial_form': coverage_spatial_form,
                'spatial_coverage': spatial_coverage_data_dict,
