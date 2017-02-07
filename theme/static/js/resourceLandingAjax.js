c6ee537aafe8198af73e125dc962df819e0fcccd
mauriel133 <mauriel.ramirez@gmail.com>
2017-02-07 12:04:40 -0700

    Made MODFLOW recommendation messages persist

--- theme/static/js/resourceLandingAjax.js
+++ theme/static/js/resourceLandingAjax.js
@@ -491,7 +491,7 @@ function metadata_update_ajax_submit(form_id){
                                     customAlert(promptMessage, 3000);
                                 }
                             }
-                            $("#missing-metadata-or-file").fadeOut();
+                            $("#missing-metadata-or-file:not(.persistent)").fadeOut();
                         }
                     }
                 }
@@ -721,7 +721,7 @@ function get_irods_folder_struct_ajax_submit(res_id, store_path) {
                 $('#fb-files-container').append('<span class="text-muted">This directory is empty</span>');
             }
             if (can_be_public) {
-                $("#missing-metadata-or-file").fadeOut();
+                $("#missing-metadata-or-file:not(.persistent)").fadeOut();
             }
             onSort();
 
