1eaebf63b789e315325b5f9ba2d87d2f9071b9d3
Mark Henderson <mhenderson@cuahsi.org>
2017-02-14 14:33:56 -0500

    Removing assert, etc

--- hs_core/views/resource_folder_hierarchy.py
+++ hs_core/views/resource_folder_hierarchy.py
@@ -231,13 +231,14 @@ def data_store_folder_unzip_public(request, pk, pathname):
     Public version of data_store_folder_unzip, incorporating path variables
 
     :param request:
+    :param pk:
+    :param pathname:
     :return HttpResponse:
     """
     request.POST = request.POST.copy()
     request.POST['res_id'] = pk
     request.POST['zip_with_rel_path'] = pathname
 
-    assert False, (request.POST, pk, pathname)
     return data_store_folder_unzip(request)
 
 
