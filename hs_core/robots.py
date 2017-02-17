2a623cc83e0d9b29581ed1e206539ef22705990b
Castronova <castronova.anthony@gmail.com>
2017-02-17 15:12:42 -0500

    #1735 removed crawler blocking and whitelist from robots.py.  removed whitelist from settings.py

--- hs_core/robots.py
+++ hs_core/robots.py
@@ -8,38 +8,16 @@ from django.http import HttpResponseForbidden
 # note: robot_detection needs to be updated periodically (possible inside the Dockerfile)
 # (wget http://www.robotstxt.org/db/all.txt, python robot_detection.py all.txt)
 
-class CrawlerBlocker:
-    def __init__(self):
-        self.whitelist = getattr(settings, "BOT_WHITELIST", [])
+class RobotFilter:
 
     def process_request(self, request):
+        """Adds is_human field to the request object.  This is used to filter non-human
+           activity from the usage logs"""
 
         user_agent = request.META.get('HTTP_USER_AGENT', None)
-        request.is_crawler = False
-        request.is_whitelisted = True
+        request.is_human = True
 
         if user_agent is None:
-            # calls without a user agent generally reflect internal calls (e.g. testing).
-            # whitelist these calls since they are not very common and not doing so will
-            # break unittests
-            request.is_crawler = True
-            request.is_whitelisted = True
+            request.is_human = False
         else:
-            # if user agent is a bot, tag it as a crawler (checks against
-            # robotstxt.org master list). Webcrawlers are also checked against
-            # the whitelist defined in settings.py to determine if
-            # site access will be granted
             request.is_crawler = robot_detection.is_robot(user_agent)
-            if len(self.whitelist) > 0 and request.is_crawler:
-                if not re.match("(" + ")|(".join(self.whitelist) + ")", user_agent):
-                    request.is_whitelisted = False
-
-        # return 403 if the request is not whitelisted
-        if not request.is_whitelisted:
-            return HttpResponseForbidden('Request could not be processed, see robots.txt')
-
-    def process_view(self, request, view_func, view_args, view_kwargs):
-
-        # only return the view if the request is NOT identified as a whitelisted crawler or human
-        if not request.is_whitelisted:
-            return HttpResponseForbidden('Request could not be processed, see robots.txt')
