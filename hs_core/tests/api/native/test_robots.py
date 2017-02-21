453f2ab3253d8d80bce3d858accae18614172558
Castronova <castronova.anthony@gmail.com>
2017-02-21 13:06:18 -0500

    #1735 removed functional tests and replaced with a single unittest

--- hs_core/tests/api/native/test_robots.py
+++ hs_core/tests/api/native/test_robots.py
@@ -1,50 +1,46 @@
 
-from django.utils import timezone
-
-from django.contrib.sites.models import Site
-import requests
-from hs_core.tests.api.rest.base import HSRESTTestCase
-from cStringIO import StringIO
-import csv
-from dateutil.parser import parse
+from unittest import TestCase
+from hs_core.robots import RobotFilter
 
 
-class TestRobots(HSRESTTestCase):
+class MockRequest(object):
+    def __init__(self):
+        self.data = {}
+        self.META = {}
 
-    def test_robot_filter(self):
+    def __get__(self, obj):
+        return self.data[obj]
 
-        agents = [
+    def __set__(self, obj, value):
+        self.data[obj] = value
+
+
+class TestRobots(TestCase):
+
+    def setUp(self):
+
+        # define a set of webcrawler and browser user-agents
+        self.agents = [
             ('Mozilla/5.0 (compatible; bingbot/2.0; '
-             '+http://www.bing.com/bingbot.htm)', 0),
-            ('Googlebot/2.1 (+http://www.googlebot.com/bot.html)', 0),
+             '+http://www.bing.com/bingbot.htm)', False),
+            ('Googlebot/2.1 (+http://www.googlebot.com/bot.html)', False),
             ('Mozilla/5.0 (compatible; Yahoo! Slurp; '
-             'http://help.yahoo.com/help/us/ysearch/slurp)', 0),
+             'http://help.yahoo.com/help/us/ysearch/slurp)', False),
             ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) '
-             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36', 1),
+             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36', True),
             ('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-US) '
-             'AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27', 1),
+             'AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27', True),
             ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 '
-             'Firefox/21.0', 1),
+             'Firefox/21.0', True),
             ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
              'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 '
-             'Edge/13.10586', 1)
+             'Edge/13.10586', True)
         ]
 
-        # make a request to hydroshare with each useragent and check
-        # if the activity has been recorded
-        for agent, expected_visits in agents:
-
-            start_dt = timezone.now()
-            headers = requests.utils.default_headers()
-            headers.update({'User-Agent': agent})
-
-            site_url = 'http://%s' % Site.objects.get_current().domain
-            requests.get(site_url, headers=headers)
-
-            history_response = requests.get('%s%s' % (site_url, '/tracking/reports/history/'))
-            reader = csv.reader(StringIO(history_response.content))
-
-            visits = list(filter(lambda c: parse(c[3]) >= start_dt,
-                                 filter(lambda c: 'visit' == c[4], reader)))
-
-            self.assertTrue(len(visits) == expected_visits)
+    def test_is_human(self):
+        self.robot = RobotFilter()
+        request = MockRequest()
+        for agent, is_human in self.agents:
+            request.META['HTTP_USER_AGENT'] = agent
+            self.robot.process_request(request)
+            self.assertTrue(request.is_human == is_human)
