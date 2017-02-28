ee69e34fe7d7822b388dac8590e511a8401c3af3
Alva Couch <acouch@cuahsi.org>
2017-02-28 12:19:56 -0500

    Fix merge conflict

---
+++ hs_access_control/migrations/0020_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hs_access_control', '0019_manual_populate_provenance'),
+        ('hs_access_control', '0017_groupaccess_auto_approve'),
+    ]
+
+    operations = [
+    ]
