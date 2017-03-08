338048d99724a6f4964d045871bcc803a8f8cef7
Mark Henderson <henderson.mark@gmail.com>
2017-03-08 09:22:41 -0500

    Code review comments

--- hs_core/tests/api/rest/test_create_resource_version.py
+++ hs_core/tests/api/rest/test_create_resource_version.py
@@ -13,19 +13,18 @@ class TestCreateResourceVersion(HSRESTTestCase):
         self.title = 'My Test resource'
         res = resource.create_resource(self.rtype,
                                        self.user,
-                                       self.title,
-                                       unpack_file=False)
+                                       self.title)
 
         self.pid = res.short_id
 
-    def test_create_resource(self):
+    def test_create_resource_version(self):
         version_url = "/hsapi/resource/%s/version/" % self.pid
         response = self.client.post(version_url, {}, format='json')
         new_pid = response.url.split('/')[4]
         self.resources_to_delete.append(new_pid)
         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
 
-    def test_create_bad_resource(self):
+    def test_create_version_bad_resource(self):
         version_url = "/hsapi/resource/%s/version/" % "fafafa"
         response = self.client.post(version_url, {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
