1255ab8e4f9b4f49551a307256f702f5acea319c
pkdash <pkdash_reena@hotmail.com>
2017-03-08 10:12:28 -0700

    [#1572] pep8

--- hs_file_types/models/generic.py
+++ hs_file_types/models/generic.py
@@ -3,7 +3,6 @@ from django.template import Template, Context
 
 from dominate.tags import div, form, button, h4
 
-from hs_core.models import Coverage
 from hs_core.forms import CoverageTemporalForm, CoverageSpatialForm
 
 from base import AbstractFileMetaData, AbstractLogicalFile
