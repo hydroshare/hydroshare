3ffcea41fa133a9ab380a1f1f41597e66aa6013f
Alva Couch <acouch@cuahsi.org>
2017-03-22 11:13:59 -0400

    Updated testing scripts

--- irods/test-fed-irods.sh
+++ irods/test-fed-irods.sh
@@ -2,15 +2,8 @@
 # I wrote this script to take me through the configuration steps.
 # -- Alva
 
-(cd ..; git checkout -- hsctl )
-(cd ..; git checkout -- hydroshare/local_settings.py)
-(cd ..; git checkout -- scripts/templates/docker-compose-local-irods.template)
+. ./turn-off-local-irods.sh 
 
-(cd ..; ./hsctl reset_all)
-docker kill `docker ps -a -q`
-docker rm -f `docker ps -a -q`
-docker rmi -f `docker images | awk '$1=="hydroshare_hydroshare" { print $3 }'`
-sudo rm -rf ~/icat1 ~/icat2
 ./use-local-irods.sh --persist
 (cd ..; ./hsctl rebuild --db)
 sleep 30
--- irods/turn-off-local-irods.sh
+++ irods/turn-off-local-irods.sh
@@ -1,11 +1,9 @@
-# As the mechanism for testing local irods is quite involved, 
-# I wrote this script to take me through the configuration steps. 
-# -- Alva
+(cd ..; git checkout -- hsctl )
+(cd ..; git checkout -- hydroshare/local_settings.py)
+(cd ..; git checkout -- scripts/templates/docker-compose-local-irods.template)
 
-(cd ..; git checkout -- hsctl ) 
-(cd ..; git checkout -- hydroshare/local_settings.py) 
-(cd ..; git checkout -- scripts/templates/docker-compose-local-irods.template) 
-
-(cd ..; ./hsctl reset_all) 
+(cd ..; ./hsctl reset_all)
 docker kill `docker ps -a -q`
+docker rm -f `docker ps -a -q`
+docker rmi -f `docker images | awk '$3 != "IMAGE" { print $3 }'`
 sudo rm -rf ~/icat1 ~/icat2
