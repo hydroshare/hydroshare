4d0348c3244b3eab84db50fd0580dc83539d1216
zhiyuli <zyli2004@gmail.com>
2017-04-19 17:09:12 -0400

    fix pep8

--- hs_tools_resource/admin.py
+++ hs_tools_resource/admin.py
@@ -8,9 +8,8 @@ from copy import deepcopy
 
 author_extra_fieldsets = ((None, {"fields": ("approved",)}),)
 
+
 class ToolAdmin(PageAdmin):
-    #inlines = (BookInline,)
     fieldsets = deepcopy(PageAdmin.fieldsets) + author_extra_fieldsets
 
 admin.site.register(ToolResource, ToolAdmin)
-#admin.site.register(ToolResource, PageAdmin)
