d32f6f82586ff06d88eb93c233f95a4458e00b86
Mark Henderson <mhenderson@cuahsi.org>
2017-05-01 15:34:36 -0400

    scimeta endpoints and tests

--- hs_api/urls.py
+++ hs_api/urls.py
@@ -17,15 +17,29 @@ urlpatterns = [
         hs_core.views.resource_access_api.ResourceAccessUpdateDelete.as_view(),
         name='get_update_delete_resource_access'),
 
-    url(r'^resource/(?P<pk>[0-9a-f-]+)/sysmeta/$',
-        hs_core.views.resource_rest_api.SystemMetadataRetrieve.as_view(),
-        name='get_update_delete_resource_sysmeta'),
-
-    url(r'^resource/(?P<pk>[0-9a-f-]+)/sysmeta/$',
-        hs_core.views.resource_rest_api.SystemMetadataRetrieve.as_view(),
-        name='get_update_delete_resource_sysmeta'),
-
     url(r'^resource/(?P<pk>[0-9a-f-]+)/map/$',
         hs_core.views.resource_rest_api.ResourceMapRetrieve.as_view(),
+        name='get_update_delete_resource_sysmeta'),
+
+    url(r'^resource/(?P<pk>[0-9a-f-]+)/scimeta/$',
+        hs_core.views.resource_rest_api.ScienceMetadataRetrieveUpdate.as_view(),
+        name='get_update_science_metadata'),
+
+    # Resource metadata editing
+    url(r'^resource/(?P<pk>[0-9a-f-]+)/scimeta/elements/$',
+        hs_core.views.resource_metadata_rest_api.MetadataElementsRetrieveUpdate.as_view(),
+        name='get_update_science_metadata_elements'),
+
+    # Update key-value metadata
+    url(r'^resource/(?P<pk>[0-9a-f-]+)/scimeta/custom/$',
+        hs_core.views.update_key_value_metadata_public,
+        name='update_custom_metadata'),
+
+    url(r'^resource/(?P<pk>[0-9a-f-]+)/sysmeta/$',
+        hs_core.views.resource_rest_api.SystemMetadataRetrieve.as_view(),
+        name='get_update_delete_resource_sysmeta'),
+
+    url(r'^resource/(?P<pk>[0-9a-f-]+)/sysmeta/$',
+        hs_core.views.resource_rest_api.SystemMetadataRetrieve.as_view(),
         name='get_update_delete_resource_sysmeta'),
 ]
--- hs_core/tests/api/rest/test_custom_scimeta.py
+++ hs_core/tests/api/rest/test_custom_scimeta.py
@@ -6,6 +6,8 @@ from .base import HSRESTTestCase
 
 
 class TestCustomScimetaEndpoint(HSRESTTestCase):
+    V2_API_ROOT = "/api/v2/resource"
+
     def setUp(self):
         super(TestCustomScimetaEndpoint, self).setUp()
 
@@ -18,8 +20,8 @@ class TestCustomScimetaEndpoint(HSRESTTestCase):
         self.pid = res.short_id
         self.resources_to_delete.append(self.pid)
 
-    def test_set_custom_metadata_multiple(self):
-        set_metadata = "/hsapi/resource/%s/scimeta/custom/" % self.pid
+    def test_DEPRECATED_set_custom_metadata_multiple(self, root="/hsapi/resource"):
+        set_metadata = root + "/%s/scimeta/custom/" % self.pid
 
         response = self.client.post(set_metadata, {
             "foo": "bar",
@@ -27,10 +29,16 @@ class TestCustomScimetaEndpoint(HSRESTTestCase):
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_set_custom_metadata_single(self):
-        set_metadata = "/hsapi/resource/%s/scimeta/custom/" % self.pid
+    def test_set_custom_metadata_multiple(self):
+        self.test_DEPRECATED_set_custom_metadata_multiple(root=self.V2_API_ROOT)
+
+    def test_DEPRECATED_set_custom_metadata_single(self, root="/hsapi/resource"):
+        set_metadata = root + "/%s/scimeta/custom/" % self.pid
         response = self.client.post(set_metadata, {
             "foo": "bar"
         }, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_set_custom_metadata_single(self):
+        self.test_DEPRECATED_set_custom_metadata_single(root=self.V2_API_ROOT)
