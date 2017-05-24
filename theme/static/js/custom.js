ea14147fe2f0a3292257408ca89528c2b87bc13c
Mark Henderson <mhenderson@cuahsi.org>
2017-05-24 12:52:25 -0400

    Front end piece of universal messaging block

--- theme/static/js/custom.js
+++ theme/static/js/custom.js
@@ -169,4 +169,25 @@ $(document).ready(function () {
     $(".format-date").each(function () {
         $(this).formatDate();
     });
+
+    $("#universalMessage a").on('click', function() {
+        $("#universalMessage").slideUp();
+        return false
+    })
 });
+
+function showUniversalMessage(type, message) {
+    return function(response,returnType,content) {
+        if(!message) message = content;
+        if(!type) type = returnType;
+
+        $("#universalMessage span").html(message);
+        $("#universalMessage").attr('class','');
+        $("#universalMessage").addClass(type);
+        $("#universalMessage").slideDown();
+
+        setTimeout(function() {
+            $("#universalMessage a").click()
+        }, 5000)
+    }
+}
