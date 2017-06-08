290a1f6dff2c68ce193ba2640a0406a02c54c4bb
jsadler2 <jeffrey.sadler2@gmail.com>
2017-06-08 15:28:54 -0400

    [#2092] pylint

--- hs_swat_modelinstance/models.py
+++ hs_swat_modelinstance/models.py
@@ -149,7 +149,7 @@ class SimulationType(AbstractMetaDataElement):
     @classmethod
     def _validate_simulation_type(cls, swat_simulation_type):
         types = [c[0] for c in cls.type_choices]
-        if swat_simulation_type!='' and swat_simulation_type not in types:
+        if swat_simulation_type != '' and swat_simulation_type not in types:
             raise ValidationError('Invalid swat_model_simulation_type:{} not in {}'.format(
                 swat_simulation_type, types)
             )
