60da08774ccd12511ff75d688ac5e20243776032
Mark Henderson <mhenderson@cuahsi.org>
2017-06-26 16:12:44 -0400

    Adding "flag" as a possible param for the flag endpoint

--- hs_core/tests/api/rest/test_resource_flags.py
+++ hs_core/tests/api/rest/test_resource_flags.py
@@ -52,9 +52,21 @@ class TestPublicResourceFlagsEndpoint(HSRESTTestCase):
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+        flag_url = "/hsapi/resource/%s/flag/" % self.pid
+        response = self.client.post(flag_url, {
+            "flag": "make_public"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
         flag_url = "/hsapi/resource/%s/flag/" % self.pid_two
         response = self.client.post(flag_url, {
             "t": "make_public"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        flag_url = "/hsapi/resource/%s/flag/" % self.pid_two
+        response = self.client.post(flag_url, {
+            "flag": "make_public"
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
 
@@ -65,10 +77,22 @@ class TestPublicResourceFlagsEndpoint(HSRESTTestCase):
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
 
+        flag_url = "/hsapi/resource/%s/flag/" % self.pid
+        response = self.client.post(flag_url, {
+            "flag": "make_private"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
     def test_set_resource_flag_make_discoverable(self):
         flag_url = "/hsapi/resource/%s/flag/" % self.pid_two
         response = self.client.post(flag_url, {
             "t": "make_discoverable"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        flag_url = "/hsapi/resource/%s/flag/" % self.pid_two
+        response = self.client.post(flag_url, {
+            "flag": "make_discoverable"
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
 
@@ -79,6 +103,12 @@ class TestPublicResourceFlagsEndpoint(HSRESTTestCase):
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
 
+        flag_url = "/hsapi/resource/%s/flag/" % self.pid
+        response = self.client.post(flag_url, {
+            "flag": "make_not_discoverable"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
     def test_set_resource_flag_make_not_shareable(self):
         flag_url = "/hsapi/resource/%s/flag/" % self.pid
         response = self.client.post(flag_url, {
@@ -86,9 +116,21 @@ class TestPublicResourceFlagsEndpoint(HSRESTTestCase):
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
 
+        flag_url = "/hsapi/resource/%s/flag/" % self.pid
+        response = self.client.post(flag_url, {
+            "flag": "make_not_shareable"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
     def test_set_resource_flag_make_shareable(self):
         flag_url = "/hsapi/resource/%s/flag/" % self.pid
         response = self.client.post(flag_url, {
             "t": "make_shareable"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        flag_url = "/hsapi/resource/%s/flag/" % self.pid
+        response = self.client.post(flag_url, {
+            "flag": "make_shareable"
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
