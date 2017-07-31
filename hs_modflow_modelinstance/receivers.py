bf424e55fa0ade7aae9fea6af292c09bb84ae38d
mih7gf <mih7gf@virginia.edu>
2017-07-31 15:49:41 -0400

    Bug Fix: incorrect method call

--- hs_modflow_modelinstance/receivers.py
+++ hs_modflow_modelinstance/receivers.py
@@ -97,7 +97,7 @@ def _process_metadata_update_create(update_or_create, **kwargs):
 @receiver(post_add_files_to_resource, sender=modflow_models.MODFLOWModelInstanceResource)
 def post_add_files_to_resource_handler(sender, **kwargs):
     resource = kwargs['resource']
-    add_metadata_from_dis_file(resource)
+    add_metadata_from_file(resource)
 
 
 @receiver(post_create_resource, sender=modflow_models.MODFLOWModelInstanceResource)
