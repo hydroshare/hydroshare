a39f8d390f78a24d1fde2c59f99fdbf5cdb2d0fa
Mark Henderson <mhenderson@cuahsi.org>
2017-09-14 16:36:42 -0400

    Normalize University Django Management Task

--- hs_dictionary/admin.py
+++ hs_dictionary/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import University
+from .models import University, UncategorizedTerm
 
 
 class UniversityAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(University, UniversityAdmin)
+
+class UncategorizedTermAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UncategorizedTerm, UncategorizedTermAdmin)
