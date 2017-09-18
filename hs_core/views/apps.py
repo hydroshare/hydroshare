d2204dfa745b38923b310ce8beb6879e37870300
Mark Henderson <mhenderson@cuahsi.org>
2017-09-18 16:55:39 -0400

    New Apps Page

--- hs_core/views/apps.py
+++ hs_core/views/apps.py
@@ -2,6 +2,8 @@ from django.views.generic import TemplateView
 
 from hs_tools_resource.models import ToolResource
 
+APPROVED_APPLICATIONS = [694]
+
 class AppsView(TemplateView):
     template_name = "pages/apps.html"
 
@@ -11,8 +13,9 @@ class AppsView(TemplateView):
 
         final_resource_list = []
         for resource in webapp_resources:
-            if resource.metadata.app_icon and resource.metadata.app_home_page_url:
-                final_resource_list.append(resource)
+            if resource.metadata.app_icon and resource.metadata.app_home_page_url: # \
+                # and resource.id in APPROVED_APPLICATIONS:
+                    final_resource_list.append(resource)
 
         context['webapp_resources'] = final_resource_list
         return context
