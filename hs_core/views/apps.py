6a059c216eca9382dd82ec0b360be02397262d81
Mark Henderson <mhenderson@cuahsi.org>
2017-09-20 14:14:57 -0400

    Feedback from code review

--- hs_core/views/apps.py
+++ hs_core/views/apps.py
@@ -2,27 +2,12 @@ from django.views.generic import TemplateView
 
 from hs_tools_resource.models import ToolResource
 
-APPROVED_APPLICATIONS = [
-    694,    # SWATShare
-    679,    # Hydroshare GIS
-    5316,   # JupyterHub NCSA
-    826,    # CUAHSI Data Series Viewer
-    2640    # National Water Model Forecast viewer
-]
-
 
 class AppsView(TemplateView):
     template_name = "pages/apps.html"
 
     def get_context_data(self, **kwargs):
         context = super(AppsView, self).get_context_data(**kwargs)
-        webapp_resources = ToolResource.objects.all()
 
-        final_resource_list = []
-        for resource in webapp_resources:
-            if resource.metadata.app_icon and resource.metadata.app_home_page_url \
-                    and resource.id in APPROVED_APPLICATIONS:
-                final_resource_list.append(resource)
-
-        context['webapp_resources'] = final_resource_list
+        context['webapp_resources'] = ToolResource.get_approved_apps()
         return context
