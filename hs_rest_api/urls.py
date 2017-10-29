e49463673e82d673e806015be905d85504a20a6a
Alex Lemann <lemanal@gmail.com>
2017-10-29 15:31:13 -0400

    copy_resource_public -> copy_resource consistency

--- hs_rest_api/urls.py
+++ hs_rest_api/urls.py
@@ -43,7 +43,7 @@ urlpatterns = patterns(
 
     # public copy resource endpoint
     url(r'^resource/(?P<pk>[0-9a-f-]+)/copy/$',
-        core_views.copy_resource_public, name='copy_resource_public'),
+        core_views.copy_resource, name='copy_resource'),
 
     # DEPRECATED: use form above instead
     url(r'^resource/accessRules/(?P<pk>[0-9a-f-]+)/$',
