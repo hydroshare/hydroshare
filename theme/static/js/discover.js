f0fa47b00b8090e99937c0f0c6416e22f8d26289
Mark Henderson <mhenderson@cuahsi.org>
2017-11-06 15:30:38 -0500

    Remove SOLR popup

--- theme/static/js/discover.js
+++ theme/static/js/discover.js
@@ -974,10 +974,10 @@ $(document).ready(function () {
     });
 
 
-    $("#solr-help-info").popover({
-        html: true,
-        container: '#body',
-        content: '<p>Search here to find all public and discoverable resources. This search box supports <a href="https://cwiki.apache.org/confluence/display/solr/Searching" target="_blank">SOLR Query syntax</a>.</p>',
-        trigger: 'click'
-    });
+    // $("#solr-help-info").popover({
+    //     html: true,
+    //     container: '#body',
+    //     content: '<p>Search here to find all public and discoverable resources. This search box supports <a href="https://cwiki.apache.org/confluence/display/solr/Searching" target="_blank">SOLR Query syntax</a>.</p>',
+    //     trigger: 'click'
+    // });
 });
