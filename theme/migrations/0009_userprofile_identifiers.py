6d785344af74dca0db11fc12c927a657091b94f7
pkdash <pkdash_reena@hotmail.com>
2017-11-12 18:40:54 -0500

    [#1666] new model field identifiers for user profile

---
+++ theme/migrations/0009_userprofile_identifiers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.contrib.postgres.fields.hstore
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('theme', '0008_auto_20170622_2141'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='identifiers',
+            field=django.contrib.postgres.fields.hstore.HStoreField(default={}),
+        ),
+    ]
