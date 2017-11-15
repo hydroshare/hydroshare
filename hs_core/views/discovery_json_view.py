1d70a86738a2f89ecaee41cb5431eb30e22cc4e6
Zhaokun Xue <helloxzk@gmail.com>
2017-11-15 15:06:50 -0500

    query solr instead of hitting db

--- hs_core/views/discovery_json_view.py
+++ hs_core/views/discovery_json_view.py
@@ -8,71 +8,76 @@ import time
 # returned JSON objects array is used for building the map view
 class DiscoveryJsonView(FacetedSearchView):
     # set facet fields
-    facet_fields = ['creators', 'subjects', 'resource_type', 'public', 'owners_names', 'discoverable', 'published', 'variable_names', 'sample_mediums', 'units_names']
+    facet_fields = ['creators', 'subjects', 'resource_type', 'public',
+                    'owners_names', 'discoverable', 'published', 'variable_names',
+                    'sample_mediums', 'units_names']
     # declare form class to use in this view
     form_class = DiscoveryForm
 
     # overwrite Haystack generic_view.py form_valid() function to generate JSON response
     def form_valid(self, form):
         start1 = time.time()
+
         # initialize an empty array for holding the result objects with coordinate values
         coor_values = []
         # get query set
         self.queryset = form.search()
+
         end1 = time.time()
         elapsed_time1 = end1 - start1
         # When we have a GET request with search query, build our JSON objects array
         if len(self.request.GET):
 
-            query_results = self.get_queryset()
-            end1 = time.time()
-            elapsed_time1 = end1 - start1
-
             start2 = time.time()
             # iterate all the search results
-            for result in query_results:
+            for result in self.get_queryset():
                 # initialize a null JSON object
                 json_obj = {}
 
-                # assign title and url values to the object
-                json_obj['short_id'] = result.object.short_id;
-                # This hits the Django database
-                json_obj['title'] = result.object.metadata.title.value
-                json_obj['resource_type'] = result.object.verbose_name
-                # This hits the Django database 
-                json_obj['get_absolute_url'] = result.object.get_absolute_url()
-                json_obj['first_author'] = result.object.first_creator.name
-                if result.object.first_creator.description:
-                    json_obj['first_author_description'] = result.object.first_creator.description
-                # iterate all the coverage values
-                for coverage in result.object.metadata.coverages.all():
+                # fetch as much information as possible from stored fields
+                solr = result.get_stored_fields()
 
-                    # if coverage type is point, assign 'east' and 'north' coordinates to the object
-                    if coverage.type == 'point':
-                        json_obj['coverage_type'] = coverage.type
-                        json_obj['east'] = coverage.value['east']
-                        json_obj['north'] = coverage.value['north']
-                    # elif coverage type is box, assign 'northlimit', 'eastlimit', 'southlimit' and 'westlimit' coordinates to the object
-                    elif coverage.type == 'box':
-                        json_obj['coverage_type'] = coverage.type
-                        json_obj['northlimit'] = coverage.value['northlimit']
-                        json_obj['eastlimit'] = coverage.value['eastlimit']
-                        json_obj['southlimit'] = coverage.value['southlimit']
-                        json_obj['westlimit'] = coverage.value['westlimit']
+                # assign title and url values to the object
+                json_obj['title'] = solr['title']
+                json_obj['resource_type'] = solr['resource_type']
+
+                json_obj['get_absolute_url'] = solr['absolute_url']
+                json_obj['first_author'] = solr['author']
+                # TODO: this is redundant. The value always exists but oft has value 'none'.
+                if solr['author_description']:
+                    json_obj['first_author_description'] = solr['author_description']
+
+                # iterate over all the coverage values
+                for coverage in solr['coverages']:
+                    json_coverage = json.loads(coverage)
+                    if 'east' in json_coverage:
+                        json_obj['coverage_type'] = 'point'
+                        json_obj['east'] = json_coverage['east']
+                        json_obj['north'] = json_coverage['north']
+                    elif 'northlimit' in json_coverage:
+                        json_obj['coverage_type'] = 'box'
+                        json_obj['northlimit'] = json_coverage['northlimit']
+                        json_obj['eastlimit'] = json_coverage['eastlimit']
+                        json_obj['southlimit'] = json_coverage['southlimit']
+                        json_obj['westlimit'] = json_coverage['westlimit']
                     # else, skip
                     else:
                         continue
-                    # encode object to JSON format
-                    coor_obj = json.dumps(json_obj)
-                    # add JSON object the results array
-                    coor_values.append(coor_obj)
+
+                # encode object to JSON string format
+                coor_obj = json.dumps(json_obj)
+                # add JSON object the results array
+                coor_values.append(coor_obj)
 
             # encode the results results array to JSON array
             the_data = json.dumps(coor_values)
             # return JSON response
+
             end2 = time.time()
             elapsed_time2 = end2 - start2
             output_file = open('hs_core/views/performance_results/server_json_request.txt', 'a')
             output_file.write("solr query time: " + str(elapsed_time1) + " json building time:  "  + str(elapsed_time2) + '\n')
             output_file.close()
             return HttpResponse(the_data, content_type='application/json')
+        else:
+            return HttpResponse(json.dumps('[]'), content_type='application/json')
