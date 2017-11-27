99f0ed2d4130e154965608a4e7d2ac4c665044d4
Mauriel <mauriel.ramirez@gmail.com>
2017-11-27 12:04:17 -0700

    Setting up identifiers in resource landing page

--- theme/static/js/profile.js
+++ theme/static/js/profile.js
@@ -30,17 +30,6 @@ function validateForm() {
     return  flagRequiredElements && flagPasswords && flagEmail;
 }
 
-function cleanIdentifiers() {
-    var identifiers = $("#edit-identifiers-container .well:not(#identifier-template)");
-
-    identifiers.each(function(index, item) {
-        if ($(item).find("[name='identifier_link']").val().trim() == "" ||
-            $(item).find("[name='identifier_name']").val().trim() == "") {
-            $(item).remove();
-        }
-    });
-}
-
 function validateRequiredElements() {
     var requiredElements = $(".form-required");
     for (var i = 0; i < requiredElements.length; i++) {
@@ -271,56 +260,6 @@ $(document).ready(function () {
             $("#selectUserType").attr("name", "user_type");
         }
     });
-
-    $("#edit-identifiers-container").on("change", ".select-identifier", function() {
-        var value = $(this).val();
-        var showOther = value === "Other";
-
-        var other = $(this).closest(".well").find(".identifier-specify");
-
-        other.toggleClass("hidden", !showOther);
-
-        if (showOther) {
-            other.find("input").attr("name", $(this).attr("name"));
-            $(this).removeAttr("name");
-        }
-        else {
-            $(this).attr("name", other.find("input").attr("name"));
-            other.find("input").removeAttr("name");
-        }
-    });
-
-    $("#edit-identifiers-container").on("click", ".close", function() {
-        $(this).closest(".well").remove();
-        updateIdentifierFormIndexes();
-    });
-
-    $("#btn-add-identifier").click(function() {
-        var templateInstance = $("#identifier-template").clone();
-        templateInstance.toggleClass("hidden", false);
-        templateInstance.removeAttr("id");
-
-        templateInstance.find("#selectIdentifier").attr("name", "identifier_name");
-        templateInstance.find(".identifier-link-container input").attr("name", "identifier_link");
-
-        $(templateInstance).hide().appendTo("#edit-identifiers-container").fadeIn(350);
-        updateIdentifierFormIndexes();
-    });
-
-    function updateIdentifierFormIndexes() {
-        var identifiers = $("#edit-identifiers-container").find(".well:not(#identifier-template)");
-        identifiers.each(function (index, item) {
-            // Set labels and references for screen readers
-            $(item).find(".identifier-specify input").attr("id", "identifier_name" + index);
-            $(item).find(".identifier-specify label").attr("for", "identifier_name" + index);
-
-            $(item).find(".select-identifier-fieldset select").attr("id", "select_identifier" + index);
-            $(item).find(".select-identifier-fieldset label").attr("for", "select_identifier" + index);
-
-            $(item).find(".identifier-link-container input").attr("id", "identifier_link" + index);
-            $(item).find(".identifier-link-container label").attr("for", "identifier_link" + index);
-        });
-    }
 
     $("[data-page-mode='edit']").hide();
 
