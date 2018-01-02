cb26836ffc40f38272a931bf6a31f41f91a97055
hyi <hongyi@renci.org>
2018-01-02 17:16:48 -0500

    doing apt-get update before doing apt-get install

--- irods/use-local-irods.sh
+++ irods/use-local-irods.sh
@@ -92,6 +92,9 @@ for pc in $(seq 20 -1 1); do
 done
 
 # Install OpenSSH on ${HS_USER_ZONE_HOST}
+echo "INFO: running apt-get update on ${HS_USER_ZONE_HOST}"
+docker exec ${HS_USER_ZONE_HOST} sh -c "apt-get update"
+
 echo "INFO: Install OpenSSH on ${HS_USER_ZONE_HOST}"
 echo "[root@${HS_USER_ZONE_HOST}]$ apt-get install -y openssh-client openssh-server && mkdir /var/run/sshd && sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config && sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd && /etc/init.d/ssh restart"
 docker exec ${HS_USER_ZONE_HOST} sh -c "apt-get install -y openssh-client openssh-server && mkdir /var/run/sshd && sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config && sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd && /etc/init.d/ssh restart"
