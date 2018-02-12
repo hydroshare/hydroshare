af222f7144739469a5b078143bf38d6b8f4d544b
Alva Couch <acouch@cuahsi.org>
2018-02-12 17:43:42 -0500

    Resolve merge conflict.

---
+++ theme/migrations/0012_1_15_RC_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('theme', '0009_userprofile_identifiers'),
+        ('theme', '0011_auto_20180211_0105'),
+    ]
+
+    operations = [
+    ]
