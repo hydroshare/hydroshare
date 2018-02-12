49cb8918a3b110a142a2f4ffcdf021f9d03524cf
Alva Couch <acouch@cuahsi.org>
2018-02-12 16:29:42 -0500

    Add resource feature vectors.

--- hs_core/management/commands/ara_examples.py
+++ hs_core/management/commands/ara_examples.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
+from hs_core.models import BaseResource
 from hs_core.hydroshare.features import Features
-from pprint import pprint
 from datetime import datetime
+from pprint import pprint
 
 
 class Command(BaseCommand):
@@ -24,9 +25,13 @@ class Command(BaseCommand):
         # x = Features.user_downloads(datetime(2017, 11, 20, 0, 0, 0, 0),
         #                             datetime(2018, 1, 1, 0, 0, 0, 0))
         # pprint(x)
-        x = Features.resource_apps(datetime(2017, 11, 20, 0, 0, 0, 0),
-                                   datetime(2018, 1, 1, 0, 0, 0, 0))
-        pprint(x)
-        x = Features.user_apps(datetime(2017, 11, 20, 0, 0, 0, 0),
-                               datetime(2018, 1, 1, 0, 0, 0, 0))
-        pprint(x)
+        # x = Features.resource_apps(datetime(2017, 11, 20, 0, 0, 0, 0),
+        #                            datetime(2018, 1, 1, 0, 0, 0, 0))
+        # pprint(x)
+        # x = Features.user_apps(datetime(2017, 11, 20, 0, 0, 0, 0),
+        #                        datetime(2018, 1, 1, 0, 0, 0, 0))
+        # pprint(x)
+        # print the feature vector for all resources.
+
+        for r in BaseResource.objects.all():
+            pprint(Features.resource_features(r))
