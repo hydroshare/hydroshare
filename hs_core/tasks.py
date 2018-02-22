f509dd6d68a1ccf50959c5b78ed82fe4d18bc73f
Scott Black <scott.black@usu.edu>
2018-02-22 10:48:10 -0700

    [#1867] - pep8 formatting

--- hs_core/tasks.py
+++ hs_core/tasks.py
@@ -34,17 +34,18 @@ from theme.utils import get_quota_message
 
 from django_irods.icommands import SessionException
 
-
 # Pass 'django' into getLogger instead of __name__
 # for celery tasks (as this seems to be the
 # only way to successfully log in code executed
 # by celery, despite our catch-all handler).
 logger = logging.getLogger('django')
 
+
 @periodic_task(ignore_result=True, run_every=crontab(minute=0, hour=0))
 def sync_email_subscriptions():
     sixty_days = datetime.today() - timedelta(days=60)
-    active_subscribed = UserProfile.objects.filter(email_opt_out=False, user__last_login__gte=sixty_days, user__is_active=True)
+    active_subscribed = UserProfile.objects.filter(email_opt_out=False, user__last_login__gte=sixty_days,
+                                                   user__is_active=True)
     sync_mailchimp(active_subscribed, "e210a70864")
     subscribed = UserProfile.objects.filter(email_opt_out=False, user__is_active=True)
     sync_mailchimp(subscribed, "f0c27254e3")
@@ -53,15 +54,22 @@ def sync_email_subscriptions():
 def sync_mailchimp(active_subscribed, list_id):
     session = requests.Session()
     # get total members
-    response = session.get("https://us3.api.mailchimp.com/3.0/lists/{list_id}/members".format(list_id=list_id), auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
+    response = session.get("https://us3.api.mailchimp.com/3.0/lists/{list_id}/members".format(list_id=list_id),
+                           auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
     total_items = json.loads(response.content)["total_items"]
     # get list of all member ids
-    response = session.get("https://us3.api.mailchimp.com/3.0/lists/{list_id}/members?offset=0&count={total_items}".format(list_id=list_id, total_items=total_items), auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
+    response = session.get(
+        "https://us3.api.mailchimp.com/3.0/lists/{list_id}/members?offset=0&count={total_items}".format(list_id=list_id,
+                                                                                                        total_items=total_items),
+        auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
     # clear the email list
     delete_count = 0
     for member in json.loads(response.content)["members"]:
         if member["status"] == "subscribed":
-            session_response = session.delete("https://us3.api.mailchimp.com/3.0/lists/{list_id}/members/{id}".format(list_id=list_id, id=member["id"]), auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
+            session_response = session.delete(
+                "https://us3.api.mailchimp.com/3.0/lists/{list_id}/members/{id}".format(list_id=list_id,
+                                                                                        id=member["id"]),
+                auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
             if session_response.status_code != 204:
                 logger.info("Expected 204 status code, got " + str(session_response.status_code))
                 logger.debug(session_response.content)
@@ -70,8 +78,11 @@ def sync_mailchimp(active_subscribed, list_id):
     # add active subscribed users to mailchimp
     add_count = 0
     for subscriber in active_subscribed:
-        json_data = { "email_address": subscriber.user.email, "status": "subscribed", "merge_fields": { "FNAME": subscriber.user.first_name, "LNAME": subscriber.user.last_name } }
-        session_response = session.post("https://us3.api.mailchimp.com/3.0/lists/{list_id}/members".format(list_id=list_id), json=json_data, auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
+        json_data = {"email_address": subscriber.user.email, "status": "subscribed",
+                     "merge_fields": {"FNAME": subscriber.user.first_name, "LNAME": subscriber.user.last_name}}
+        session_response = session.post(
+            "https://us3.api.mailchimp.com/3.0/lists/{list_id}/members".format(list_id=list_id), json=json_data,
+            auth=requests.auth.HTTPBasicAuth('hs-celery', settings.MAILCHIMP_PASSWORD))
         if session_response.status_code != 200:
             logger.info("Expected 200 status code, got " + str(session_response.status_code))
             logger.debug(session_response.content)
@@ -80,7 +91,8 @@ def sync_mailchimp(active_subscribed, list_id):
     if delete_count == active_subscribed.count():
         logger.info("successfully cleared mailchimp for list id " + list_id)
     else:
-        logger.info("cleared " + str(delete_count) + " out of " + str(active_subscribed.count()) + " for list id " + list_id)
+        logger.info(
+            "cleared " + str(delete_count) + " out of " + str(active_subscribed.count()) + " for list id " + list_id)
 
     if active_subscribed.count() == add_count:
         logger.info("successfully synced all subscriptions for list id " + list_id)
