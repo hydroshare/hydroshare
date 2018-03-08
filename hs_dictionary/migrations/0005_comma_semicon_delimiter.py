64ef81267bba670c25ac73e8dd01aec987390f7f
Scott Black <scott.black@usu.edu>
2018-03-07 19:05:04 -0500

    [#2673] add migration from commas to semicolons

---
+++ hs_dictionary/migrations/0005_comma_semicon_delimiter.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from theme.models import UserProfile
+from django.db.models import F, Func, Value
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+
+    ]
+
+    operations = [
+        UserProfile.objects.filter(organization__icontains=',').update(
+            organization=Func(
+                F('organization'),
+                Value(','), Value(';'),
+                function='replace',
+            )
+        )
+
+    ]
