995dfcbc86ef04c6b0178209ec90213391c070de
pkdash <pkdash_reena@hotmail.com>
2018-03-12 16:28:33 -0400

    [#2502] delete single file aggregation deletes associated xml files

--- hs_file_types/models/base.py
+++ hs_file_types/models/base.py
@@ -1064,6 +1064,14 @@ class AbstractLogicalFile(models.Model):
         """
 
         from hs_core.hydroshare.resource import delete_resource_file
+
+        # delete associated metadata and map xml documents
+        istorage = self.resource.get_irods_storage()
+        if istorage.exists(self.metadata_file_path):
+            istorage.delete(self.metadata_file_path)
+        if istorage.exists(self.map_file_path):
+            istorage.delete(self.map_file_path)
+
         # delete all resource files associated with this instance of logical file
         if delete_res_files:
             for f in self.files.all():
