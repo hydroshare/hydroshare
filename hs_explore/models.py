9c9cb1f478f3db1ccacb4ba13c6d2224da53f739
Alva Couch <acouch@cuahsi.org>
2018-03-14 18:29:09 -0400

    Add relevance score to Recommend class.

--- hs_explore/models.py
+++ hs_explore/models.py
@@ -18,25 +18,28 @@ class Status(object):
 
 
 class Recommend(models.Model):
-    user = models.ForeignKey(User)
-    resource = models.ForeignKey(BaseResource)
+    user = models.ForeignKey(User, editable=False)
+    resource = models.ForeignKey(BaseResource, editable=False)
+    relevance = models.FloatField(editable=False, default=0.0)
     state = models.IntegerField(choices=Status.STATUS_CHOICES,
-                                default=Status.STATUS_NEW)
+                                default=Status.STATUS_NEW,
+                                editable=False)
 
     class Meta:
         unique_together = ('user', 'resource')
 
     @classmethod
-    def recommend(u, r):
+    def recommend(u, r, relevance=0.0):
         with transaction.atomic:
-            Recommend.objects.get_or_create(user=u, resource=r)
+            Recommend.objects.get_or_create(user=u, resource=r,
+                                            default={'relevance': relevance})
 
     @classmethod
-    def recommend_ids(uid, rid):
+    def recommend_ids(uid, rid, relevance=0.0):
         """ use string ids rather than User and Resource objects """
         u = user_from_id(uid, raise404=False)
         r = get_resource_by_shortkey(rid, or_404=False)
-        Recommend.recommend(u, r)
+        Recommend.recommend(u, r, relevance)
 
     def viewed(self):
         self.state = Status.STATUS_VIEWED
@@ -49,3 +52,16 @@ class Recommend(models.Model):
     def dismissed(self):
         self.state = Status.STATUS_DISMISSED
         self.save()
+
+    @classmethod
+    def delete(u, r):
+        try:
+            record = Recommend.objects.get(user=u, resource=r)
+            record.delete()
+        except Exception:
+            pass
+
+    @classmethod
+    def clear():
+        for r in Recommend.objects.all():
+            r.delete()
