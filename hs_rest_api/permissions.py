d2eab769b6ada27ea18b97fc90366ea6d5e337de
hydro service <henderson.mark@gmail.com>
2018-03-28 15:13:25 -0400

    KeyError Hotfix

--- hs_rest_api/permissions.py
+++ hs_rest_api/permissions.py
@@ -9,10 +9,28 @@ class CanViewOrEditResourceMetadata(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
+        if not view.kwargs.get('pk', False):
+            return True
+
         if request.method == "GET":
-            _, authorized, _ = authorize(request, view.kwargs['pk'],
+            _, authorized, _ = authorize(request, view.kwargs.get('pk'),
                                          needed_permission=ACTION_TO_AUTHORIZE.VIEW_METADATA)
         else:
-            _, authorized, _ = authorize(request, view.kwargs['pk'],
+            _, authorized, _ = authorize(request, view.kwargs.get('pk'),
                                          needed_permission=ACTION_TO_AUTHORIZE.EDIT_RESOURCE)
+        return authorized
+
+
+class CanEditResourceMetadata(permissions.BasePermission):
+    """
+    Global API permission to edit resource metadata
+    """
+
+    def has_permission(self, request, view):
+        if not view.kwargs.get('pk', False):
+            return True
+
+        _, authorized, _ = authorize(request, view.kwargs.get('pk'),
+                                     needed_permission=ACTION_TO_AUTHORIZE.SET_RESOURCE_FLAG)
+
         return authorized
