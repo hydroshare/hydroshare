7456927b2844e39594e6eb539006c8769312cc55
hydro service <henderson.mark@gmail.com>
2018-04-09 14:37:57 -0400

    Admin panel niceties

--- hs_publication/admin.py
+++ hs_publication/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
 from .models import PublicationQueue
 
 
 class PublicationQueueAdmin(admin.ModelAdmin):
-    fields = (('resource',), 'status', 'note')
-    readonly_fields = ('resource',)
+    fields = ('resource_link', 'status', 'note')
+    readonly_fields = ('resource_link',)
+
+    list_display = ("resource", "status", "note",)
+    list_filter = ('status',)
+
+    def resource_link(self, instance):
+        return mark_safe('<a href="/resource/{}">{}</a>'.format(
+            instance.resource.short_id,
+            instance.resoruce.metadata.title[0].value
+        ))
+
+    resource_link.allow_tags = True
 
 admin.site.register(PublicationQueue, PublicationQueueAdmin)
