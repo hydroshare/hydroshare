b18433265095e5afdf0ced6f5b2fb958104c312c
hydro service <henderson.mark@gmail.com>
2018-04-09 15:23:00 -0400

    Adding link to admin

--- hs_publication/admin.py
+++ hs_publication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.utils.html import format_html_join
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 from .models import PublicationQueue
@@ -13,10 +13,8 @@ class PublicationQueueAdmin(admin.ModelAdmin):
     list_filter = ('status',)
 
     def resource_link(self, instance):
-        return mark_safe('<a href="/resource/{}">{}</a>'.format(
-            instance.resource.short_id,
-            instance.resoruce.metadata.title[0].value
-        ))
+        return format_html('<a href="/resource/{}">{}</a>',
+            instance.resource.short_id, instance.resource.title)
 
     resource_link.allow_tags = True
 
