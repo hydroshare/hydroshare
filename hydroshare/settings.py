efa83ff7e886f48400af65eaeb7e764c03c20860
Alva Couch <acouch@cuahsi.org>
2018-04-12 08:33:35 -0400

    Nginx has irods access

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -316,7 +316,7 @@ INSTALLED_APPS = (
     "hs_composite_resource",
     "hs_rest_api",
     "hs_dictionary",
-    "security",
+    "security", 
 )
 
 OAUTH2_PROVIDER_APPLICATION_MODEL = 'oauth2_provider.Application'
@@ -386,6 +386,7 @@ MIDDLEWARE_CLASSES = (
     "mezzanine.core.middleware.FetchFromCacheMiddleware",
     "hs_core.robots.RobotFilter",
     "hs_tracking.middleware.Tracking",
+    # "django_downloadview.SmartDownloadMiddleware", 
 )
 
 # security settings
