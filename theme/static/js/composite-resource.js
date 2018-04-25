fdec91845a65eb30f5a894d3b211e4a29d9635ed
pkdash <pkdash_reena@hotmail.com>
2018-04-24 22:07:34 -0400

    [#2502] removing commented code

--- theme/static/js/composite-resource.js
+++ theme/static/js/composite-resource.js
@@ -3,29 +3,6 @@
  */
 
 $(document).ready(function () {
-    // Don't allow the user to change the coverage type
-    // var $id_type_div = $("#div_id_type");
-    // var $box_radio = $id_type_div.find("#id_type_1");
-    // var $point_radio = $id_type_div.find("#id_type_2");
-    // if ($box_radio.attr("checked") !== "checked") {
-    //     $box_radio.parent().closest("label").addClass("text-muted");
-    //     $box_radio.attr('disabled', true);
-    // }
-    // else {
-    //     $point_radio.parent().closest("label").addClass("text-muted");
-    //     $point_radio.attr('disabled', true);
-    // }
-    //
-    // $id_type_div.css('pointer-events', 'none');
-    // $point_radio.attr('onclick', 'return false');
-    // $box_radio.attr('onclick', 'return false');
-    //
-    // // make the spatial coverage readonly
-    // $("#coverage-spatial :input").prop('readonly', true);
-    // // make the temporal coverage readonly
-    // $("#coverage-temporal :input").prop('disabled', true);
-    // // hide the submit button for resource level temporal coverage
-    // $("#id-coverage-temporal").find("button.btn-primary").hide();
 
     // Submit for resource spatial coverage update
     $("#btn-update-resource-spatial-coverage").click(function () {
