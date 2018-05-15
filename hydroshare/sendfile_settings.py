e2c0cf7cbcc76f9cf8da4f52a8353dae949249cb
Alva Couch <acouch@cuahsi.org>
2018-05-15 11:56:36 -0400

    Fix flake8

--- hydroshare/sendfile_settings.py
+++ hydroshare/sendfile_settings.py
@@ -1,4 +1,5 @@
-#sendfile support for large files
+# sendfile support for large files
+# These must match settings in nginx
 SENDFILE_ON = True
 IRODS_USER_URI = "/irods-user"
 IRODS_DATA_URI = "/irods-data"
