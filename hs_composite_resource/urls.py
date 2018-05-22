2e837ced37bcaa9d04b26afe2292d834bd62db32
pkdash <pkdash_reena@hotmail.com>
2018-05-21 21:19:48 -0400

    [#2502] fixing regex for resoure_id in url

--- hs_composite_resource/urls.py
+++ hs_composite_resource/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns(
-    '', url(r'^_internal/(?P<resource_id>[0-9a-f-]+)/(?P<coverage_type>[A-z]+)/update-coverage/$',
+    '', url(r'^_internal/(?P<resource_id>[0-9a-f]+)/(?P<coverage_type>[A-z]+)/update-coverage/$',
             views.update_resource_coverage,
             name="update_resource_coverage")
 )
