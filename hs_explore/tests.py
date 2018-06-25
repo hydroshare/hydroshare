38554ef7ec73c4bf5c9f88d50499d5e24c49e0f1
Alva Couch <acouch@cuahsi.org>
2018-06-25 19:02:27 -0400

    Preliminary tests for recommendation structures

--- hs_explore/tests.py
+++ hs_explore/tests.py
@@ -4,8 +4,8 @@ from django.test import TransactionTestCase
 from hs_core.testing import TestCaseCommonUtilities
 from hs_core import hydroshare
 from django.contrib.auth.models import Group
-from hs_explore import RecommendedResource, RecommendedUser, RecommendedGroup, \
-    KeyValuePair
+from hs_explore.models import RecommendedResource, RecommendedUser, RecommendedGroup, \
+    KeyValuePair, ResourceRecToPair, UserRecToPair, GroupRecToPair
 
 
 class TestkeyValuePair(TestCaseCommonUtilities, TransactionTestCase):
@@ -25,6 +25,15 @@ class TestCreateRecommendedResource(TestCaseCommonUtilities, TransactionTestCase
         self.hydroshare_author_group, _ = Group.objects.get_or_create(name='Hydroshare Author')
         # create a user to be used for creating the resource
         self.user = hydroshare.create_account(
+            'foo@usu.edu',
+            username='foo',
+            first_name='Creator_FirstName',
+            last_name='Creator_LastName',
+            superuser=False,
+            groups=[]
+        )
+
+        self.user2 = hydroshare.create_account(
             'creator@usu.edu',
             username='creator',
             first_name='Creator_FirstName',
@@ -42,17 +51,91 @@ class TestCreateRecommendedResource(TestCaseCommonUtilities, TransactionTestCase
     def tearDown(self):
         super(TestCreateRecommendedResource, self).tearDown()
 
-    def test_create_resource_recommendation(self):
+    def test_create_rec_resource(self):
         rec = RecommendedResource.recommend(self.user, self.res, relevance=0.5)
         self.assertEqual(rec.user, self.user)
         self.assertEqual(rec.candidate_resource, self.res)
         self.assertEqual(rec.relevance, 0.5)
-        self.assertFalse(rec.keywords)
+        self.assertEqual(rec.keywords.count(), 0)
+        rec.relate('foo', 'bar', 0.4) 
+        self.assertEqual(rec.keywords.count(), 1)
+        kv = rec.keywords.get(key='foo') 
+        self.assertEqual(kv.value, 'bar') 
+        rel = ResourceRecToPair.objects.get(pair=kv) 
+        self.assertEqual(rel.recommendation, rec) 
+        self.assertEqual(rel.weight, 0.4) 
+        rec.unrelate('foo', 'bar') 
+        self.assertEqual(rec.keywords.count(), 0)
+        RecommendedResource.clear()
+        self.assertEqual(RecommendedResource.objects.all().count(), 0) 
+        # does cascade delete work? 
+        self.assertEqual(ResourceRecToPair.objects.all().count(), 0) 
+        # deleting references doesn't delete key/value pairs
+        KeyValuePair.clear()
+        self.assertEqual(KeyValuePair.objects.all().count(), 0) 
 
+    def test_create_rec_resource_with_keywords(self): 
+        rec = RecommendedResource.recommend(self.user, self.res, relevance=0.5, 
+                                            keywords=(('subject', 'dogs', 1.0),))
+        self.assertEqual(rec.user, self.user)
+        self.assertEqual(rec.candidate_resource, self.res)
+        self.assertEqual(rec.relevance, 0.5)
+        self.assertEqual(rec.keywords.count(), 1)
+        kv = rec.keywords.get(key='subject', value='dogs') 
+        rel = ResourceRecToPair.objects.get(pair=kv) 
+        self.assertEqual(rel.recommendation, rec) 
+        self.assertEqual(rel.weight, 1.0) 
+        rec.unrelate('subject', 'dogs') 
+        self.assertEqual(rec.keywords.count(), 0)
+        RecommendedResource.clear()
+        self.assertEqual(RecommendedResource.objects.all().count(), 0) 
+        # does cascade delete work? 
+        self.assertEqual(ResourceRecToPair.objects.all().count(), 0) 
+        # deleting references doesn't delete key/value pairs
+        KeyValuePair.clear()
+        self.assertEqual(KeyValuePair.objects.all().count(), 0) 
 
-class TestCreateRecommendedUser(TestCaseCommonUtilities, TransactionTestCase):
-    pass
+    def test_create_rec_user(self): 
+        rec = RecommendedUser.recommend(self.user, self.user2, relevance=0.5)
+        self.assertEqual(rec.user, self.user)
+        self.assertEqual(rec.candidate_user, self.user2)
+        self.assertEqual(rec.relevance, 0.5)
+        self.assertEqual(rec.keywords.count(), 0)
+        rec.relate('foo', 'bar', 0.4) 
+        self.assertEqual(rec.keywords.count(), 1)
+        kv = rec.keywords.get(key='foo') 
+        self.assertEqual(kv.value, 'bar') 
+        rel = UserRecToPair.objects.get(pair=kv) 
+        self.assertEqual(rel.recommendation, rec) 
+        self.assertEqual(rel.weight, 0.4) 
+        rec.unrelate('foo', 'bar') 
+        self.assertEqual(rec.keywords.count(), 0)
+        RecommendedUser.clear()
+        self.assertEqual(RecommendedUser.objects.all().count(), 0) 
+        # does cascade delete work? 
+        self.assertEqual(UserRecToPair.objects.all().count(), 0) 
+        # deleting references doesn't delete key/value pairs
+        KeyValuePair.clear()
+        self.assertEqual(KeyValuePair.objects.all().count(), 0) 
 
+    def test_create_rec_user_with_keywords(self): 
+        rec = RecommendedUser.recommend(self.user, self.user2, relevance=0.5, 
+                                        keywords=(('subject', 'dogs', 1.0),))
+        self.assertEqual(rec.user, self.user)
+        self.assertEqual(rec.candidate_user, self.user2)
+        self.assertEqual(rec.relevance, 0.5)
+        self.assertEqual(rec.keywords.count(), 1)
+        kv = rec.keywords.get(key='subject', value='dogs') 
+        rel = UserRecToPair.objects.get(pair=kv) 
+        self.assertEqual(rel.recommendation, rec) 
+        self.assertEqual(rel.weight, 1.0) 
+        rec.unrelate('subject', 'dogs') 
+        self.assertEqual(rec.keywords.count(), 0)
+        RecommendedUser.clear()
+        self.assertEqual(RecommendedUser.objects.all().count(), 0) 
+        # does cascade delete work? 
+        self.assertEqual(UserRecToPair.objects.all().count(), 0) 
+        # deleting references doesn't delete key/value pairs
+        KeyValuePair.clear()
+        self.assertEqual(KeyValuePair.objects.all().count(), 0) 
 
-class TestCreateRecommendedGroup(TestCaseCommonUtilities, TransactionTestCase):
-    pass
