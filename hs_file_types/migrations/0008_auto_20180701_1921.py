5c9513cb9f885482f08db74a8a2ba894c29f4a8c
pkdash <pkdash_reena@hotmail.com>
2018-07-02 22:17:30 -0400

    [#2808] initial fileset aggregation related data models

---
+++ hs_file_types/migrations/0008_auto_20180701_1921.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.contrib.postgres.fields
+import django.contrib.postgres.fields.hstore
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hs_file_types', '0007_timeseriesfilemetadata_abstract'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FileSetLogicalFile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('dataset_name', models.CharField(max_length=255, null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='FileSetMetaData',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('extra_metadata', django.contrib.postgres.fields.hstore.HStoreField(default={})),
+                ('keywords', django.contrib.postgres.fields.ArrayField(default=[], base_field=models.CharField(max_length=100, null=True, blank=True), size=None)),
+                ('is_dirty', models.BooleanField(default=False)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.AddField(
+            model_name='filesetlogicalfile',
+            name='metadata',
+            field=models.OneToOneField(related_name='logical_file', to='hs_file_types.FileSetMetaData'),
+        ),
+    ]
---
+++ hs_file_types/models/fileset.py
@@ -0,0 +1,84 @@
+import os
+import logging
+
+from django.db import models
+
+from base import AbstractLogicalFile
+from generic import GenericFileMetaDataMixin
+
+
+class FileSetMetaData(GenericFileMetaDataMixin):
+    pass
+
+
+class FileSetLogicalFile(AbstractLogicalFile):
+    """ One more files in a specific folder can be part of this aggregation """
+    metadata = models.OneToOneField(FileSetMetaData, related_name="logical_file")
+    data_type = "GenericData"
+
+    @classmethod
+    def create(cls):
+        # this custom method MUST be used to create an instance of this class
+        generic_metadata = FileSetMetaData.objects.create(keywords=[])
+        return cls.objects.create(metadata=generic_metadata)
+
+    @staticmethod
+    def get_aggregation_display_name():
+        return 'File Set Content: One or more files with specific metadata'
+
+    @staticmethod
+    def get_aggregation_type_name():
+        return "FileSetAggregation"
+
+    @property
+    def is_single_file_aggregation(self):
+        """This aggregation supports one or more files"""
+        return False
+
+    @classmethod
+    def get_main_file_type(cls):
+        """The main file type for this aggregation - no specific main file"""
+        return ".*"
+
+    @classmethod
+    def check_files_for_aggregation_type(cls, files):
+        """Checks if the specified files can be used to set this aggregation type
+        :param  files: a list of ResourceFile objects
+
+        :return If the files meet the requirements of this aggregation type, then returns this
+        aggregation class name, otherwise empty string.
+        """
+        if len(files) == 0:
+            # no files
+            return ""
+
+        return cls.__name__
+
+    @classmethod
+    def get_primary_resouce_file(cls, resource_files):
+        """Gets any one resource file from the list of files *resource_files* """
+
+        return resource_files[0] if resource_files else None
+
+    @classmethod
+    def set_file_type(cls, resource, user, file_id=None, folder_path=None):
+        """Makes all physical files that are in a folder (*folder_path*) part of a file set
+        aggregation type.
+        Note: parameter file_id is ignored here and a value for folder_path is required
+        """
+
+        log = logging.getLogger()
+        if folder_path is None:
+            raise ValueError("Must specify folder to be set as a file set aggregation type")
+
+        res_file, folder_path = cls._validate_set_file_type_inputs(resource, file_id, folder_path)
+
+        folder_name = folder_path
+        if '/' in folder_path:
+            folder_name = os.path.basename(folder_path)
+
+        logical_file = cls.initialize(folder_name)
+        # make all the files in the selected folder as part of the aggregation
+        logical_file.add_resource_files_in_folder(resource, folder_path)
+        logical_file.create_aggregation_xml_documents()
+        log.info("Fie set aggregation was created for file:{}.".format(res_file.storage_path))
