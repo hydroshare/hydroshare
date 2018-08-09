f3dc2625d1007c5c8665221a7fa3a4eaf39aa39f
hyi <hongyi@renci.org>
2018-08-09 14:39:48 -0400

    fix #2865

--- hs_core/models.py
+++ hs_core/models.py
@@ -1647,7 +1647,7 @@ class AbstractResource(ResourcePermissionsMixin, ResourceIRODSMixin):
 
         and False otherwise
         """
-        has_files = self.has_required_content_files
+        has_files = self.has_required_content_files()
         has_metadata = self.has_required_metadata
         return has_files and has_metadata
 
@@ -1678,7 +1678,7 @@ class AbstractResource(ResourcePermissionsMixin, ResourceIRODSMixin):
 
         # check that there is sufficient resource content
         has_metadata = self.has_required_metadata
-        has_files = self.has_required_content_files
+        has_files = self.has_required_content_files()
         if value and not (has_metadata and has_files):
 
             if not has_metadata and not has_files:
@@ -1730,7 +1730,7 @@ class AbstractResource(ResourcePermissionsMixin, ResourceIRODSMixin):
 
         # check that there is sufficient resource content
         has_metadata = self.has_required_metadata
-        has_files = self.has_required_content_files
+        has_files = self.has_required_content_files()
         if value and not (has_metadata and has_files):
 
             if not has_metadata and not has_files:
