0d076161e2ed402c06c7161bce45200cc2d311ff
Scott Black <scott.black@usu.edu>
2018-09-07 11:51:36 -0600

    [#2892] add migration for dropping the dx in a doi

---
+++ hs_core/migrations/0038_doi_url.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from hs_core.models import Identifier
+
+from django.db import migrations
+
+def remove_dx(apps, schema_editor):
+    for id in Identifier.objects.filter(url__startswith='http://dx.'):
+        id.url = id.url.replace('http://dx.', 'https://')
+        id.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hs_core', '0037_auto_20180209_0309'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_dx),
+    ]
