98fd977f1e9fb96ddffa8beb5cf4e9ea93196eea
Mark O'Brien <mobrien@cuahsi.org>
2019-01-02 12:36:42 -0500

    Finalize

--- conf_irods/partial_build.sh
+++ conf_irods/partial_build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO fix or silence error that seems to have no negative impact -- WARNING: Error 25 disabling echo mode. Password will be displayed in plaintext.Enter your current iRODS password:Error reinstating echo mode.
-
 source env-files/use-local-irods.env
 
 echo "INFO: Create Linux user ${HS_USER_ZONE_PROXY_USER} on ${HS_USER_ZONE_HOST}"
@@ -14,7 +12,6 @@ docker cp delete_user.sh ${HS_USER_ZONE_HOST}:/home/${HS_USER_ZONE_PROXY_USER}/d
 docker exec ${HS_USER_ZONE_HOST} chown -R ${HS_USER_ZONE_PROXY_USER}:${HS_USER_ZONE_PROXY_USER} /home/${HS_USER_ZONE_PROXY_USER}
 docker exec ${HS_USER_ZONE_HOST} sh -c "echo "${HS_USER_ZONE_PROXY_USER}":"${HS_USER_ZONE_PROXY_USER}" | chpasswd"
 
-#echo "INFO: Create Linux user rods on ${HS_USER_ZONE_HOST}"
 docker exec ${HS_USER_ZONE_HOST} useradd rods -s /bin/bash
 
 # Store IPs for data.local.org and users.local.org for use in scripts
@@ -61,16 +58,14 @@ sleep 1s
 echo "iadmin mkzone ${IRODS_ZONE} remote ${ICAT1IP}:1247" | $RUN_ON_USER
 sleep 1s
 
-
-#TODO review if old use-local-irods.sh commands were failing silently via docker exec without --interactive flag
 echo "INFO: copy iRODS config"
 docker exec --user rods ${HS_USER_ZONE_HOST} whoami
 docker exec ${HS_USER_ZONE_HOST} mkdir -p /home/${HS_USER_ZONE_PROXY_USER}/.irods
 docker cp env-files/rods@${HS_USER_ZONE_HOST}.json ${HS_USER_ZONE_HOST}:/home/${HS_USER_ZONE_PROXY_USER}/.irods/irods_environment.json
 docker exec ${HS_USER_ZONE_HOST} chown -R ${HS_USER_ZONE_PROXY_USER}:${HS_USER_ZONE_PROXY_USER} /home/${HS_USER_ZONE_PROXY_USER}
 
-#TODO review iinit call and order of copying json configuration file in
 echo "INFO: iint the ${HS_USER_ZONE_PROXY_USER} in ${HS_USER_ZONE_HOST}"
+#TODO this throws error but succeeds research as low priority work
 echo "echo ${HS_USER_ZONE_PROXY_USER_PWD} | iinit" | docker exec -i -u hsuserproxy users.local.org /bin/bash
 
 echo "INFO: give ${IRODS_USERNAME} own rights over ${HS_USER_IRODS_ZONE}/home"
