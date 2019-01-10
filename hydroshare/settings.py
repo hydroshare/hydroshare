a5a34e7d63cce05f31465f95d019084a96c36d66
Mark O'Brien <mobrien@cuahsi.org>
2019-01-10 13:17:10 -0500

    Set up proper flow for local settings and settings in hydroshare

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import, unicode_literals
-#TEST_RUNNER='django_nose.NoseTestSuiteRunner'
-TEST_RUNNER = 'hs_core.tests.runner.CustomTestSuiteRunner'
-TEST_WITHOUT_MIGRATIONS_COMMAND = 'django_nose.management.commands.test.Command'
 
 import os
 import sys
-# import importlib
+
+import redis
+from kombu import Queue, Exchange
+
+TEST_RUNNER = 'hs_core.tests.runner.CustomTestSuiteRunner'
+TEST_WITHOUT_MIGRATIONS_COMMAND = 'django_nose.management.commands.test.Command'
 
 local_settings_module = os.environ.get('LOCAL_SETTINGS', 'hydroshare.local_settings')
 
@@ -88,7 +90,6 @@ local_settings_module = os.environ.get('LOCAL_SETTINGS', 'hydroshare.local_setti
 # INSTALLED_APPS setting.
 USE_SOUTH = False
 
-
 ########################
 # MAIN DJANGO SETTINGS #
 ########################
@@ -112,7 +113,7 @@ ALLOWED_HOSTS = []
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = "Etc/UTC"
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
@@ -181,23 +182,29 @@ ROBOTS_SITEMAP_URLS = [
 # DATABASES #
 #############
 
+POSTGIS_HOST = os.environ.get('POSTGIS_PORT_5432_TCP_ADDR', 'localhost')
+POSTGIS_PORT = 5432
+POSTGIS_DB = os.environ.get('POSTGIS_DB', 'postgres')
+POSTGIS_PASSWORD = os.environ.get('POSTGIS_PASSWORD', 'postgres')
+POSTGIS_USER = os.environ.get('POSTGIS_USER', 'postgres')
+POSTGIS_VERSION = (2, 1, 1)
+
 DATABASES = {
     "default": {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": POSTGIS_DB,
         # Not used with sqlite3.
-        "USER": "",
+        "USER": POSTGIS_USER,
         # Not used with sqlite3.
-        "PASSWORD": "",
+        "PASSWORD": POSTGIS_PASSWORD,
         # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
+        "HOST": POSTGIS_HOST,
         # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
+        "PORT": POSTGIS_PORT,
     }
 }
-
 
 #########
 # PATHS #
@@ -338,7 +345,7 @@ APPS_TO_NOT_RUN = (
 # List of processors used by RequestContext to populate the context.
 # Each one should be a callable that takes the request object as its
 # only parameter and returns a dictionary to add to the context.
-#TEMPLATE_CONTEXT_PROCESSORS = ()
+# TEMPLATE_CONTEXT_PROCESSORS = ()
 
 TEMPLATES = [
     {
@@ -511,7 +518,6 @@ HAYSTACK_CONNECTIONS = {
 }
 HAYSTACK_SIGNAL_PROCESSOR = "hs_core.hydro_realtime_signal_processor.HydroRealtimeSignalProcessor"
 
-
 # customized value for password reset token, email verification and group invitation link token
 # to expire in 7 days
 PASSWORD_RESET_TIMEOUT_DAYS = 7
@@ -553,7 +559,7 @@ LOGGING = {
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': '/hydroshare/log/system.log',
             'formatter': 'simple',
-            'maxBytes': 1024*1024*15,  # 15MB
+            'maxBytes': 1024 * 1024 * 15,  # 15MB
             'backupCount': 10,
         },
         'djangolog': {
@@ -561,7 +567,7 @@ LOGGING = {
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': '/hydroshare/log/django.log',
             'formatter': 'verbose',
-            'maxBytes': 1024*1024*15,  # 15MB
+            'maxBytes': 1024 * 1024 * 15,  # 15MB
             'backupCount': 10,
         },
         'hydrosharelog': {
@@ -569,7 +575,7 @@ LOGGING = {
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': '/hydroshare/log/hydroshare.log',
             'formatter': 'verbose',
-            'maxBytes': 1024*1024*15,  # 15MB
+            'maxBytes': 1024 * 1024 * 15,  # 15MB
             'backupCount': 10,
         },
     },
@@ -606,7 +612,6 @@ TRACKING_USER_FIELDS = ["username", "email", "first_name", "last_name"]
 
 # info django that a reverse proxy sever (nginx) is handling ssl/https for it
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 
 # Content Security Policy
 # See http://django-csp.readthedocs.io/en/latest/configuration.html#configuration-chapter
@@ -702,7 +707,6 @@ CSP_DICT = {
     ]
 }
 
-
 X_FRAME_OPTIONS = "deny"
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
@@ -718,3 +722,161 @@ SWAGGER_SETTINGS = {
 
 # detect test mode to turn off some features
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
+
+REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'localhost')
+REDIS_PORT = 6379
+
+REDIS_CONNECTION = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=4)
+WMS_CACHE_DB = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=5)
+PERMISSIONS_DB = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=6)
+
+RABBITMQ_HOST = os.environ.get('RABBITMQ_PORT_5672_TCP_ADDR', 'localhost')
+RABBITMQ_PORT = '5672'
+
+# celery settings
+# customizations: we need a special queue for broadcast signals to all
+# docker daemons.  we also need a special queue for direct messages to all
+# docker daemons.
+BROKER_URL = 'amqp://guest:guest@{RABBITMQ_HOST}:{RABBITMQ_PORT}//'.format(RABBITMQ_HOST=RABBITMQ_HOST,
+                                                                           RABBITMQ_PORT=RABBITMQ_PORT)
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_DEFAULT_QUEUE = 'default'
+DEFAULT_EXCHANGE = Exchange('default', type='topic')
+
+CELERY_QUEUES = (
+    Queue('default', DEFAULT_EXCHANGE, routing_key='task.default'),
+)
+CELERY_DEFAULT_EXCHANGE = 'tasks'
+CELERY_DEFAULT_EXCHANGE_TYPE = 'topic'
+CELERY_DEFAULT_ROUTING_KEY = 'task.default'
+CELERY_ROUTES = ('hs_core.router.HSTaskRouter',)
+
+# DEVELOPMENT EXAMPLE ONLY
+# Make these unique, and don't share it with anybody
+SECRET_KEY = "9e2e3c2d-8282-41b2-a027-de304c0bc3d944963c9a-4778-43e0-947c-38889e976dcab9f328cb-1576-4314-bfa6-70c42a6e773c"
+NEVERCACHE_KEY = "7b205669-41dd-40db-9b96-c6f93b66123496a56be1-607f-4dbf-bf62-3315fb353ce6f12a7d28-06ad-4ef7-9266-b5ea66ed2519"
+
+ALLOWED_HOSTS = "*"
+
+# for Django/Mezzanine comments and ratings to require user login
+COMMENTS_ACCOUNT_REQUIRED = True
+RATINGS_ACCOUNT_REQUIRED = True
+COMMENTS_USE_RATINGS = True
+
+IPYTHON_SETTINGS = []
+IPYTHON_BASE = '/hydroshare/static/media/ipython-notebook'
+IPYTHON_HOST = '127.0.0.1'
+
+DOCKER_URL = 'unix://docker.sock/'
+DOCKER_API_VERSION = '1.12'
+
+# CartoCSS
+CARTO_HOME = '/hs_tmp/node_modules/carto'
+
+SITE_TITLE = "CUAHSI HydroShare"
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# Local resource iRODS configuration
+USE_IRODS = True
+IRODS_ROOT = '/tmp'
+IRODS_ICOMMANDS_PATH = '/usr/bin'
+IRODS_HOST = 'hydrotest41.renci.org'
+IRODS_PORT = '1247'
+IRODS_DEFAULT_RESOURCE = 'hydrotest41Resc'
+IRODS_HOME_COLLECTION = '/hydrotest41Zone/home/hsproxy'
+IRODS_CWD = '/hydrotest41Zone/home/hsproxy'
+IRODS_ZONE = 'hydrotest41Zone'
+IRODS_USERNAME = 'hsproxy'
+IRODS_AUTH = 'proxywater1'
+IRODS_GLOBAL_SESSION = True
+
+# Remote user zone iRODS configuration
+REMOTE_USE_IRODS = False
+
+# iRODS customized bagit rule path
+IRODS_BAGIT_RULE = 'hydroshare/irods/ruleGenerateBagIt_HS.r'
+IRODS_BAGIT_PATH = 'bags'
+IRODS_BAGIT_POSTFIX = 'zip'
+
+IRODS_SERVICE_ACCOUNT_USERNAME = ''
+
+HS_BAGIT_README_FILE_WITH_PATH = 'docs/bagit/readme.txt'
+
+# crossref login credential for resource publication
+USE_CROSSREF_TEST = True
+CROSSREF_LOGIN_ID = ''
+CROSSREF_LOGIN_PWD = ''
+
+# Since Hyrax server on-demand update is only needed when private netCDF resources on www
+# are made public, in local development environments or VM deployments other than the www
+# production, this should not be run by setting RUN_HYRAX_UPDATE to False. RUN_HYRAX_UPDATE
+# should only be set to True on www.hydroshare.org
+RUN_HYRAX_UPDATE = False
+HYRAX_SSH_HOST = ''
+HYRAX_SSH_PROXY_USER = ''
+HYRAX_SSH_PROXY_USER_PWD = ''
+HYRAX_SCRIPT_RUN_COMMAND = ''
+
+# hsuserproxy system user configuration used to create hydroshare iRODS users on-demand
+HS_USER_ZONE_HOST = ''
+HS_USER_ZONE_PROXY_USER = ''
+HS_USER_ZONE_PROXY_USER_PWD = ''
+HS_USER_ZONE_PROXY_USER_CREATE_USER_CMD = ''
+HS_USER_ZONE_PROXY_USER_DELETE_USER_CMD = ''
+HS_USER_ZONE_PRODUCTION_PATH = '/hydroshareuserZone/home/localHydroProxy'
+
+# the local HydroShare proxy user (a counterpart of wwwHydroProxy) in a federated zone with HydroShare Zone
+HS_LOCAL_PROXY_USER_IN_FED_ZONE = 'localTestHydroProxy'
+
+# Please keep the line below unchanged since it is used to check whether
+# the current site is in production or not
+HS_WWW_IRODS_PROXY_USER = 'wwwHydroProxy'
+# credentials for HydroShare proxy user iRODS account which is set to have own access control
+# to all collections in any federated zone with HydroShare zone, which is only useful when
+# testing HydroShare federated zone in local test development environment since in www
+# production environment, IRODS_USERNAME and other associated settings already represent wwwHydroProxy settings
+HS_WWW_IRODS_PROXY_USER_PWD = ''
+HS_WWW_IRODS_HOST = ''
+HS_IRODS_LOCAL_ZONE_DEF_RES = 'hydroshareLocalResc'
+HS_WWW_IRODS_ZONE = ''
+HS_USER_IRODS_ZONE = 'hydroshareuserZone'
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+HYDROSHARE_SHARED_TEMP = '/shared_tmp'
+
+# used by the mailchimp subscription job in hs_core/tasks.py
+MAILCHIMP_ACTIVE_SUBSCRIBERS = "e210a70864"
+MAILCHIMP_SUBSCRIBERS = "f0c27254e3"
+
+# sendfile support for large files
+# These must match settings in nginx
+SENDFILE_ON = False
+IRODS_USER_URI = "/irods-user"
+IRODS_DATA_URI = "/irods-data"
+LOCAL_CACHE_URI = "/local-cache"
+
+RECAPTCHA_SITE_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+RECAPTCHA_SECRET_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+RECAPTCHA_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify'
+
+with open(os.path.dirname(os.path.abspath(__file__)) + "/../config/hydroshare-config.yaml", 'r') as stream:
+    try:
+        EXTERNAL_CONFIG = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+# insert a google maps key here when in production
+MAPS_KEY = ''
