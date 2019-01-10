61018cf1c4e2a166feb1de9452a4337a14761875
Mark O'Brien <mobrien@cuahsi.org>
2019-01-10 11:20:40 -0500

    iRODS containers use hostname instead of IP for federation

--- conf_irods/partial_build.sh
+++ conf_irods/partial_build.sh
@@ -2,22 +2,6 @@
 
 source env-files/use-local-irods.env
 
-#echo "INFO: Create Linux user ${HS_USER_ZONE_PROXY_USER} on ${HS_USER_ZONE_HOST}"
-#docker exec ${HS_USER_ZONE_HOST} sh -c "useradd -m -p ${HS_USER_ZONE_PROXY_USER_PWD} -s /bin/bash ${HS_USER_ZONE_PROXY_USER}"
-## TODO why does the container/image contain a /home/hsuserproxy bad file that should be a directory? and hsuserproxy user already exists
-#docker exec ${HS_USER_ZONE_HOST} rm -rf /home/hsuserproxy
-#docker exec ${HS_USER_ZONE_HOST} mkdir -p /home/hsuserproxy/.irods
-#docker cp create_user.sh ${HS_USER_ZONE_HOST}:/home/${HS_USER_ZONE_PROXY_USER}/create_user.sh
-#docker cp delete_user.sh ${HS_USER_ZONE_HOST}:/home/${HS_USER_ZONE_PROXY_USER}/delete_user.sh
-#docker exec ${HS_USER_ZONE_HOST} chown -R ${HS_USER_ZONE_PROXY_USER}:${HS_USER_ZONE_PROXY_USER} /home/${HS_USER_ZONE_PROXY_USER}
-#docker exec ${HS_USER_ZONE_HOST} sh -c "echo "${HS_USER_ZONE_PROXY_USER}":"${HS_USER_ZONE_PROXY_USER}" | chpasswd"
-
-#docker exec ${HS_USER_ZONE_HOST} useradd rods -s /bin/bash
-
-# Store IPs for data.local.org and users.local.org for use in scripts
-ICAT1IP=$(docker exec ${IRODS_HOST} /sbin/ip -f inet -4 -o addr | grep eth | cut -d '/' -f 1 | rev | cut -d ' ' -f 1 | rev)
-ICAT2IP=$(docker exec ${HS_USER_ZONE_HOST} /sbin/ip -f inet -4 -o addr | grep eth | cut -d '/' -f 1 | rev | cut -d ' ' -f 1 | rev)
-
 echo "echo rods | iinit" | $RUN_ON_DATA
 echo "echo rods | iinit" | $RUN_ON_USER
 
@@ -53,16 +37,10 @@ echo "iadmin mkresc ${HS_IRODS_LOCAL_ZONE_DEF_RES} unixfilesystem ${HS_USER_ZONE
 
 
 # Tips for piping strings to docker exec container terminal https://gist.github.com/ElijahLynn/72cb111c7caf32a73d6f#file-pipe_to_docker_examples
-echo "iadmin mkzone ${HS_USER_IRODS_ZONE} remote ${ICAT2IP}:1247" | $RUN_ON_DATA
+echo "iadmin mkzone ${HS_USER_IRODS_ZONE} remote users.local.org:1247" | $RUN_ON_DATA
 sleep 1s
-echo "iadmin mkzone ${IRODS_ZONE} remote ${ICAT1IP}:1247" | $RUN_ON_USER
+echo "iadmin mkzone ${IRODS_ZONE} remote data.local.org:1247" | $RUN_ON_USER
 sleep 1s
-
-#echo "INFO: copy iRODS config"
-#docker exec --user rods ${HS_USER_ZONE_HOST} whoami
-#docker exec ${HS_USER_ZONE_HOST} mkdir -p /home/${HS_USER_ZONE_PROXY_USER}/.irods/
-#docker cp env-files/rods@${HS_USER_ZONE_HOST}.json ${HS_USER_ZONE_HOST}:/home/${HS_USER_ZONE_PROXY_USER}/.irods/irods_environment.json
-#docker exec ${HS_USER_ZONE_HOST} chown -R ${HS_USER_ZONE_PROXY_USER}:${HS_USER_ZONE_PROXY_USER} /home/${HS_USER_ZONE_PROXY_USER}
 
 echo "INFO: iint the ${HS_USER_ZONE_PROXY_USER} in ${HS_USER_ZONE_HOST}"
 #TODO this throws error but succeeds research as low priority work
