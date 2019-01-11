d1ece565379d02772932e27b653526f3bda432e8
Mark O'Brien <mobrien@cuahsi.org>
2019-01-11 09:58:40 -0500

    Set allowed hosts

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -105,7 +105,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -753,8 +753,6 @@ CELERY_ROUTES = ('hs_core.router.HSTaskRouter',)
 # Make these unique, and don't share it with anybody
 SECRET_KEY = "9e2e3c2d-8282-41b2-a027-de304c0bc3d944963c9a-4778-43e0-947c-38889e976dcab9f328cb-1576-4314-bfa6-70c42a6e773c"
 NEVERCACHE_KEY = "7b205669-41dd-40db-9b96-c6f93b66123496a56be1-607f-4dbf-bf62-3315fb353ce6f12a7d28-06ad-4ef7-9266-b5ea66ed2519"
-
-ALLOWED_HOSTS = "*"
 
 # for Django/Mezzanine comments and ratings to require user login
 COMMENTS_ACCOUNT_REQUIRED = True
