0a27bf600b95992568028acb7f133573fd95dabb
Mark O'Brien <mobrien@cuahsi.org>
2019-01-31 16:28:16 -0500

    Remove stale reference to permissions db that came from redis

--- hs_core/models.py
+++ hs_core/models.py
@@ -148,11 +148,6 @@ class ResourcePermissionsMixin(Ownable):
 
         abstract = True
 
-    @property
-    def permissions_store(self):
-        """Use PERMISSIONS_DB constant. Unsure what 's' is here."""
-        return s.PERMISSIONS_DB
-
     def can_add(self, request):
         """Pass through can_change to determine if user can make changes to a resource."""
         return self.can_change(request)
