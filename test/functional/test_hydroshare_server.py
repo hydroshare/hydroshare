1eade3ee66f42e442ba2bb0efafdfcd66295fc85
Mark O'Brien <mobrien@cuahsi.org>
2019-02-20 23:25:51 -0500

    Use discover endpoint instead

--- test/functional/test_hydroshare_server.py
+++ test/functional/test_hydroshare_server.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.mark.django_db
 def test_top_url(admin_client):
-    response = admin_client.get('/collaborate/', follow=True)
+    response = admin_client.get('/search/', follow=True)
 
     print(response.content)
-    assert "My Groups" in response.content
+    assert "Discover" in response.content
