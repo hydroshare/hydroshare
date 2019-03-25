deaf5e1afe8394a6297886f9f206916bd29a7967
Mark O'Brien <mobrien@cuahsi.org>
2019-03-25 17:13:57 -0400

    Add authenticated PyTest for my resources

--- test/functional/test_hydroshare_server.py
+++ test/functional/test_hydroshare_server.py
@@ -1,7 +1,11 @@
 import pytest
+from rest_framework.test import APIClient
 
 
 @pytest.mark.django_db
-def test_search_url(admin_client):
-    response = admin_client.get('/search/', follow=True)
-    assert "Discover" in response.content
+def test_discover_page(resource_with_metadata):
+    client = APIClient()
+    client.login(username='testuser', password='foobar')
+
+    response = client.get('/my-resources/', follow=True)
+    assert "d49aebb3-4e64-4732-8ff2-5c43617effee" in response.content
