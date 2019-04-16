b73548819a0b72f463df0cc061f868c07caccebb
Scott Black <scott.black@usu.edu>
2019-04-16 15:12:16 -0600

    [#3294] - allow international characters in middle name

--- hs_core/tests/api/native/test_create_resource.py
+++ hs_core/tests/api/native/test_create_resource.py
@@ -29,6 +29,7 @@ class TestCreateResource(MockIRODSTestCaseMixin, TestCase):
             'test_user@email.com',
             username='mytestuser',
             first_name='some_first_name',
+            middle_name='ì', # testing international character
             last_name='some_last_name',
             superuser=False,
             groups=[self.hs_group]
