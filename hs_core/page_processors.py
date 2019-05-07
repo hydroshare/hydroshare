604736113d834994c2afbd37d6dcb98c84268213
pkdash <pkdash_reena@hotmail.com>
2019-05-06 22:28:56 -0400

    [#2556] pep8

--- hs_core/page_processors.py
+++ hs_core/page_processors.py
@@ -55,7 +55,7 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
         if user.is_authenticated():
             raise PermissionDenied()
         return redirect_to_login(request.path)
-        
+
     discoverable = content_model.raccess.discoverable
     validation_error = None
     resource_is_mine = False
