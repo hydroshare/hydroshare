0aef889579685b8d89c2f6c0e8426a6065f89171
Phuong Doan <phuongdm79@gmail.com>
2019-05-16 15:07:10 -0400

    Add quota support into local-dev setup

--- Dockerfile-postgis
+++
@@ -1,3 +0,0 @@
-FROM hydroshare/hs_postgres:9.5.0
-
-COPY ./pg.development.sql /app/pg.development.sql
--- conf_irods/partial_build.sh
+++ conf_irods/partial_build.sh
@@ -2,6 +2,9 @@
 
 source env-files/use-local-irods.env
 
+echo "mkdir /root/.irods && cp /tmp/irods_environment_data.json /root/.irods/irods_environment.json" | $RUN_ON_DATA
+echo "mkdir /root/.irods && cp /tmp/irods_environment_user.json /root/.irods/irods_environment.json" | $RUN_ON_USER
+
 echo "echo rods | iinit" | $RUN_ON_DATA
 echo "echo rods | iinit" | $RUN_ON_USER
 
@@ -9,13 +12,17 @@ echo "INFO: Federate ${IRODS_ZONE} with ${HS_USER_IRODS_ZONE}"
 
 echo "------------------------------------------------------------"
 echo "INFO: federation configuration for ${IRODS_HOST} - modify /etc/irods/server_config.json"
-docker exec ${IRODS_HOST} sh -c "jq '.federation[0].catalog_provider_hosts[0]=\"${HS_USER_ZONE_HOST}\" | .federation[0].zone_name=\"${HS_USER_IRODS_ZONE}\" | .federation[0].zone_key=\"${HS_USER_IRODS_ZONE}_KEY\" | .federation[0].negotiation_key=\"${SHARED_NEG_KEY}\"' /etc/irods/server_config.json > /tmp/tmp.json"
+docker exec ${IRODS_HOST} sh -c "cp /tmp/hydroshare-data.re /etc/irods && chown irods:irods /etc/irods/hydroshare-data.re"
+docker exec ${IRODS_HOST} sh -c "sed -i s/FQDN_OR_IP/"${HYDROSHARE_IP}:8000"/g /etc/irods/hydroshare-data.re"
+docker exec ${IRODS_HOST} sh -c "jq '.federation[0].catalog_provider_hosts[0]=\"${HS_USER_ZONE_HOST}\" | .federation[0].zone_name=\"${HS_USER_IRODS_ZONE}\" | .federation[0].zone_key=\"${HS_USER_IRODS_ZONE}_KEY\" | .federation[0].negotiation_key=\"${SHARED_NEG_KEY}\" | .plugin_configuration.rule_engines[0].plugin_specific_configuration.re_rulebase_set[0]=\"hydroshare-data,core\"' /etc/irods/server_config.json > /tmp/tmp.json"
 docker exec ${IRODS_HOST} sh -c "cat /tmp/tmp.json | jq '.' > /etc/irods/server_config.json && chown irods:irods /etc/irods/server_config.json && rm -f /tmp/tmp.json"
 docker exec ${IRODS_HOST} sh -c "cat /etc/irods/server_config.json | jq '.federation'"
 
 echo "------------------------------------------------------------"
 echo "INFO: federation configuration for ${HS_USER_ZONE_HOST}"
-docker exec ${HS_USER_ZONE_HOST} sh -c "jq '.federation[0].catalog_provider_hosts[0]=\"${IRODS_HOST}\" | .federation[0].zone_name=\"${IRODS_ZONE}\" | .federation[0].zone_key=\"${IRODS_ZONE}_KEY\" | .federation[0].negotiation_key=\"${SHARED_NEG_KEY}\"' /etc/irods/server_config.json > /tmp/tmp.json"
+docker exec ${HS_USER_ZONE_HOST} sh -c "cp /tmp/hydroshare-user.re /etc/irods && chown irods:irods /etc/irods/hydroshare-user.re"
+docker exec ${HS_USER_ZONE_HOST} sh -c "sed -i s/FQDN_OR_IP/"${HYDROSHARE_IP}:8000"/g /etc/irods/hydroshare-user.re"
+docker exec ${HS_USER_ZONE_HOST} sh -c "jq '.federation[0].catalog_provider_hosts[0]=\"${IRODS_HOST}\" | .federation[0].zone_name=\"${IRODS_ZONE}\" | .federation[0].zone_key=\"${IRODS_ZONE}_KEY\" | .federation[0].negotiation_key=\"${SHARED_NEG_KEY}\" | .plugin_configuration.rule_engines[0].plugin_specific_configuration.re_rulebase_set[0]=\"hydroshare-user,core\"' /etc/irods/server_config.json > /tmp/tmp.json"
 docker exec ${HS_USER_ZONE_HOST} sh -c "cat /tmp/tmp.json | jq '.' > /etc/irods/server_config.json && chown irods:irods /etc/irods/server_config.json && rm -f /tmp/tmp.json"
 docker exec ${HS_USER_ZONE_HOST} sh -c "cat /etc/irods/server_config.json | jq '.federation'"
 
@@ -87,4 +94,22 @@ echo "------------------------------------------------------------"
 echo "INFO: set ${HS_USER_IRODS_ZONE}/home to inherit"
 echo "ichmod -r -M inherit /"${HS_USER_IRODS_ZONE}"/home" | $RUN_ON_USER
 echo " - ichmod -r -M inherit /"${HS_USER_IRODS_ZONE}"/home" 
+
+echo "------------------------------------------------------------"
+echo "INFO: init quota setting in ${IRODS_ZONE}"
+echo "ichmod -r -M own rods /$IRODS_ZONE/home/$IRODS_USERNAME" | $RUN_ON_DATA
+echo " - ichmod -r -M own rods /$IRODS_ZONE/home/$IRODS_USERNAME" 
+echo "ichmod -r -M inherit /$IRODS_ZONE/home/$IRODS_USERNAME" | $RUN_ON_DATA
+echo " - ichmod -r -M inherit /$IRODS_ZONE/home/$IRODS_USERNAME" 
+echo "imkdir /$IRODS_ZONE/home/$IRODS_USERNAME/bags" | $RUN_ON_DATA
+echo " - imkdir /$IRODS_ZONE/home/$IRODS_USERNAME/bags" 
+
+echo "------------------------------------------------------------"
+echo "INFO: init quota setting in ${HS_USER_IRODS_ZONE}"
+echo "ichmod -r -M own rods /$HS_USER_IRODS_ZONE/home/$HS_LOCAL_PROXY_USER_IN_FED_ZONE" | $RUN_ON_USER
+echo " - ichmod -r -M own rods /$HS_USER_IRODS_ZONE/home/$HS_LOCAL_PROXY_USER_IN_FED_ZONE" 
+echo "ichmod -r -M inherit /$HS_USER_IRODS_ZONE/home/$HS_LOCAL_PROXY_USER_IN_FED_ZONE" | $RUN_ON_USER
+echo " - ichmod -r -M inherit /$HS_USER_IRODS_ZONE/home/$HS_LOCAL_PROXY_USER_IN_FED_ZONE" 
+echo "imkdir /$HS_USER_IRODS_ZONE/home/$HS_LOCAL_PROXY_USER_IN_FED_ZONE/bags" | $RUN_ON_USER
+echo " - imkdir /$HS_USER_IRODS_ZONE/home/$HS_LOCAL_PROXY_USER_IN_FED_ZONE/bags" 
 echo
