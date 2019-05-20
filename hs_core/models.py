0f111c7b948d1a9fc658668f04828764e0c55cd7
Scott Black <scott.black@usu.edu>
2019-05-20 11:41:23 -0600

    [#3242] - remove accidentally committed change

--- hs_core/models.py
+++ hs_core/models.py
@@ -3478,7 +3478,7 @@ class CoreMetaData(models.Model):
 "http://dublincore.org/documents/2002/07/31/dcmes-xml/dcmes-xml-dtd.dtd">'''
 
     NAMESPACES = {'rdf': "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                  'rdfs1': "http://www.w3.org/2000/01/rdf-schema#",
+                  'rdfs1': "http://www.w3.org/2001/01/rdf-schema#",
                   'dc': "http://purl.org/dc/elements/1.1/",
                   'dcterms': "http://purl.org/dc/terms/",
                   'hsterms': "http://hydroshare.org/terms/"}
