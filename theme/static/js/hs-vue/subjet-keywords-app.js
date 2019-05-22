1740b1190ce1d4e91bb50fddeb89500e362a70ee
Maurier <mauriel.ramirez@gmail.com>
2019-05-22 15:22:42 -0600

    Add error alert to subject keywords app

--- theme/static/js/hs-vue/subjet-keywords-app.js
+++ theme/static/js/hs-vue/subjet-keywords-app.js
@@ -9,6 +9,7 @@ let subjKeywordsApp = new Vue({
         newKeyword: '',
         resKeywords: RES_KEYWORDS,   // global constant defined in template
         showIsDuplicate: false,
+        error: ''
     },
     methods: {
         addKeyword: function (resIdShort) {
@@ -27,6 +28,8 @@ let subjKeywordsApp = new Vue({
 
             let newVal =  (this.resKeywords.join(",").length ? this.resKeywords.join(",") + ",": "") + this.newKeyword;
             let vue = this;
+            vue.error = "";
+
             $.post("/hsapi/_internal/" + resIdShort + "/subject/add-metadata/", {value: newVal}, function (resp) {
                 if (resp.status === "success") {
                     // Append new keywords to our data array
@@ -45,6 +48,10 @@ let subjKeywordsApp = new Vue({
                     // Reset input
                     vue.newKeyword = '';
                 }
+                else {
+                    vue.error = resp.message;
+                    console.log(resp);
+                }
                 showCompletedMessage(resp);
             }, "json");
         },
@@ -53,17 +60,22 @@ let subjKeywordsApp = new Vue({
             newVal.splice($.inArray(keywordName, this.resKeywords), 1);   // Remove the keyword
 
             let vue = this;
+            vue.error = "";
 
             $.post("/hsapi/_internal/" + resIdShort + "/subject/add-metadata/",
-              {value: newVal.join(",")}, function (resp) {
-                  if (resp.status === "success") {
-                      vue.resKeywords = newVal;
-                      if (!newVal.length) {
-                          // If no keywords, the metadata is no longer sufficient to make the resource public
-                          manageAccessApp.onMetadataInsufficient();
-                      }
-                  }
-              }, "json");
+                {value: newVal.join(",")}, function (resp) {
+                    if (resp.status === "success") {
+                        vue.resKeywords = newVal;
+                        if (!newVal.length) {
+                            // If no keywords, the metadata is no longer sufficient to make the resource public
+                            manageAccessApp.onMetadataInsufficient();
+                        }
+                    }
+                    else {
+                        vue.error = resp.message;
+                        console.log(resp);
+                    }
+                }, "json");
         }
     }
 });
