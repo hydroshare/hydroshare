66fc958054a5c82c390808cf2380eb4b2b08a40e
hyi <hongyi@renci.org>
2019-05-30 11:22:59 -0400

    fixed test error and updated migration command to fix file path

--- hs_core/management/commands/migrate_resources_from_userzone_to_datazone.py
+++ hs_core/management/commands/migrate_resources_from_userzone_to_datazone.py
@@ -1,5 +1,3 @@
-import os
-
 from django.core.management.base import BaseCommand
 
 from hs_core.models import BaseResource
@@ -38,14 +36,49 @@ class Command(BaseCommand):
                         else:
                             storage.setAVU(tgt_coll, avu_name, value)
 
+                    # Just to be on the safe side, it is better not to delete resources from user
+                    # zone after it is migrated over to data zone in case there are issues with
+                    # migration. A simple irm iRODS command can be issued to delete all resource
+                    # collections afterwards if all works well after some time. Commenting the
+                    # deletion statement below rather than deleting it to serve as a reminder
+                    # that additional cleanup to delete all resource collections in user zone
+                    # is needed after we can confirm migration is successfully.
                     # delete the original resource from user zone
-                    storage.delete(src_coll)
-                    # update resource federation path to point resource to data zone
-                    resource.resource_federation_path = ''
-                    resource.save()
-                    print("Resource {} has been moved from user zone to data zone "
-                          "successfully".format(resource.short_id))
-                    resource_counter += 1
+                    # storage.delete(src_coll)
+
+                    path_migrated = False
+                    for res_file in resource.files.all():
+                        if res_file.resource_file.name:
+                            print('The resource_file field should be empty for resource {} but '
+                                  'have the value of {}'.format(resource.short_id,
+                                                                res_file.resource_file.name))
+                            break
+                        file_path = res_file.fed_resource_file.name
+                        if not file_path:
+                            print('The fed_resource_file field should not be empty for '
+                                  'resource {}'.format(resource.short_id))
+                            break
+                        elif file_path.startswith(resource.resource_federation_path):
+                            file_path = file_path[len(resource.resource_federation_path)+1:]
+                            res_file.resource_file.name = file_path
+                            res_file.save()
+                            path_migrated = True
+                        else:
+                            res_file.resource_file.name = file_path
+                            res_file.save()
+                            path_migrated = True
+                            print('fed_resource_file field does not contain absolute federation '
+                                  'path which is an exception but can work after migration. '
+                                  'file_path is {}'.format(file_path))
+                    if path_migrated:
+                        # update resource federation path to point resource to data zone
+                        resource.resource_federation_path = ''
+                        resource.save()
+                        print("Resource {} has been moved from user zone to data zone "
+                              "successfully".format(resource.short_id))
+                        resource_counter += 1
+                    else:
+                        continue
                 except SessionException as ex:
                     print("Resource {} failed to move: {}".format(resource.short_id, ex.stderr))
 
