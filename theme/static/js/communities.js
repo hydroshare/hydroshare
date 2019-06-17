f1752846f42f663922b33587b32638b7f44ea502
Mark OBrien <mark_noreply@noreply.com>
2019-06-17 17:20:40 -0400

    Add querystring to filtering

--- theme/static/js/communities.js
+++ theme/static/js/communities.js
@@ -1,40 +1,26 @@
-function onRoleSelect(event) {
-    var el = $(event.target);
-    $("#selected_role").text(el.text());
-    $("#selected_role")[0].setAttribute("data-role", el[0].getAttribute("data-role"));
-}
-
 $(document).ready(function () {
-    $("title").text($(".group-title").text() + " | HydroShare"); // Fix page title
-
-    $("#id_user-autocomplete").addClass("form-control");
-
-    $("#list-roles a").click(onRoleSelect);
-
-    $("#id_user-autocomplete").attr("placeholder", "Search by name or username");
-
-    // Filter
-    $("#members").find(".table-members-list").hide();
-    $("#members").find(".table-members-list.active").show();
-
     let CommunitiesVm = new Vue({
         el: "#communities-app",
         data: {
             filterTo: [],
+            groupIds: []
         },
         mounted: function () {
             let groupIds = {};
-            let ids = [];
 
             $('#groups-list li').each(function(){
                 let groupId = parseInt($(this).attr('id'));
                 groupIds[$(this).text()] = groupId;
-                ids.push(groupId)
             });
-            this.$data.ids = ids
+            this.$data.groupIds = groupIds;
+
+            let filterGroup = $('#filter-querystring').text();
+            if (filterGroup && this.$data.groupIds[filterGroup]) {
+                this.$data.filterTo.push(this.$data.groupIds[filterGroup])
+            }
         },
         methods: {
-            showFrom(groupId) {
+            isVisible(groupId) {
                 if (this.$data.filterTo.length === 0) {  // If no selections show all
                     return true;
                 } else {  // Display row if Group ID found in the filterTo Array
