4ef0f80b0c5e4ca19e4036ebe2125e415c970d98
Mark OBrien <mark_noreply@noreply.com>
2019-07-05 10:22:11 -0400

    Topics are displayed in dual listbox

--- hs_communities/static/js/dual-listbox.js
+++ hs_communities/static/js/dual-listbox.js
@@ -1,138 +1,33 @@
 var TitleAssistantApp = new Vue({
     el: "#title-assistant",
     data: {
+        regionSelected: 'none',
         vm: {
-
-            unselectedItems: [
-                {
-                    value: "vitem1",
-                    displayValue: "vItem 1",
-                    isSelected: false
-                },
-                {
-                    value: "vitem2",
-                    displayValue: "vItem 2",
-                    isSelected: true
-                },
-                {
-                    value: "vitem3",
-                    displayValue: "vItem 3",
-                    isSelected: false
-                },
-                {
-                    value: "vitem4",
-                    displayValue: "vItem 4",
-                    isSelected: false
-                },
-                {
-                    value: "vitem5",
-                    displayValue: "vItem 5",
-                    isSelected: false
-                },
-                {
-                    value: "vitem6",
-                    displayValue: "vItem 6",
-                    isSelected: false
-                },
-                {
-                    value: "vitem7",
-                    displayValue: "vItem 7",
-                    isSelected: false
-                },
-                {
-                    value: "vitem8",
-                    displayValue: "vItem 8",
-                    isSelected: false
-                },
-                {
-                    value: "vitem9",
-                    displayValue: "vItem 9",
-                    isSelected: false
-                },
-                {
-                    value: "vitem10",
-                    displayValue: "vItem 10",
-                    isSelected: false
-                },
-                {
-                    value: "vitem11",
-                    displayValue: "vItem 11",
-                    isSelected: false
-                },
-                {
-                    value: "vitem12",
-                    displayValue: "vItem 12",
-                    isSelected: false
-                },
-                {
-                    value: "vitem13",
-                    displayValue: "vItem 13",
-                    isSelected: false
-                },
-                {
-                    value: "vitem14",
-                    displayValue: "vItem 14",
-                    isSelected: false
-                }
-            ],
-            selectedItems: [
-                {
-                    value: "xitem3",
-                    displayValue: "xItem 3",
-                    isSelected: false
-                },
-                {
-                    value: "xitem4",
-                    displayValue: "xItem 4",
-                    isSelected: false
-                },
-                {
-                    value: "vitem5",
-                    displayValue: "vItem 5",
-                    isSelected: false
-                }
-            ],
-            unselectedItems2: [
-                {
-                    value: "witem1",
-                    displayValue: "wItem 1",
-                    isSelected: false
-                },
-                {
-                    value: "witem2",
-                    displayValue: "wItem 2",
-                    isSelected: false
-                },
-                {
-                    value: "witem3",
-                    displayValue: "wItem 3",
-                    isSelected: false
-                }
-            ],
-            selectedItems2: [
-                {
-                    value: "yitem1",
-                    displayValue: "yItem 1",
-                    isSelected: false
-                },
-            ],
-
+            unselectedItems: [],
+            selectedItems: []
         }
+    },
+    mounted() {
+        topics.forEach( function(item) {  //topics is made available in the Django template, by passing serialized JSON data
+            this.$data.vm.unselectedItems.push({value: item, displayValue: item, isSelected: false})
+        }.bind(this))
     },
     methods: {
         saveItems: function () {
-            var s = "";
-            console.log(this.$data.vm)
-            for (var i = 0; i < this.$data.vm.selectedItems.length; i++) {
-                var element = this.$data.vm.selectedItems[i];
+            let s = "";
+            for (let i = 0; i < this.$data.vm.selectedItems.length; i++) {
+                let element = this.$data.vm.selectedItems[i];
                 s += element.displayValue + "\r\n";
             }
             alert('ready to save ' + this.$data.vm.selectedItems.length + ' items from Top Mover.\r\n' + s);
         },
         itemMoved: function (itemInfo) {
-            var item = itemInfo.Item;
-            var targetList = itemInfo.targetList;
+            let item = itemInfo.Item;
+            let targetList = itemInfo.targetList;
             console.log("Item moved to the " + itemInfo.listType, itemInfo);
+        },
+        updateRegion: function () {
+            console.log("Updating input on region select")
         }
     }
 });
