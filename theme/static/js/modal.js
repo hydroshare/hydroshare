3311d77ef48dd7d44e52a2c4fdba1666577c6abd
Mark OBrien <mark_noreply@noreply.com>
2019-07-09 12:33:03 -0400

    Insert title builder into modal

--- theme/static/js/modal.js
+++ theme/static/js/modal.js
@@ -3,14 +3,41 @@ Vue.component('modal', {
 });
 
 ModalApp = new Vue({
-    el: '#title-builder-app',
+    el: '#modal-app',
     data : {
         showModal: false,
-        selected: 'none',
+                title: '',
+        regionSelected: '',
+        subtopic: '',
+        topics: {
+            itemsList: [],
+            unselectedItems: [],
+            selectedItems: [],
+            selectedValues: ''
+        },
+    },
+    mounted() {
+        topics_from_page.forEach(function (item) {  //topics is made available in the Django template, by passing serialized JSON data
+            this.$data.topics.unselectedItems.push({value: item, displayValue: item, isSelected: false});
+            this.$data.topics.itemsList.push({value: item, displayValue: item, isSelected: false})
+        }.bind(this));
     },
     methods: {
         updateInput() {
-            $("#txt-title").val(this.$data.selected)
+            // $("#txt-title").val(this.$data.selected)
+            console.log("Text field would be updated")
+        },
+        itemMoved: function () {
+            // ES6 - let's discuss:
+            this.$data.topics.selectedValues = this.$data.topics.selectedItems.map(x => x.value).join(', ');
+            this.updateTitle()
+        },
+        updateRegion: function () {
+            console.log("Updating input on region select");
+            this.updateTitle(items.join(','))
+        },
+        updateTitle: function () {
+            this.$data.title = String(this.$data.regionSelected) + " " + String(this.$data.topics.selectedValues) + " " + String(this.$data.subtopic)
         }
     }
 });
@@ -18,3 +45,41 @@ ModalApp = new Vue({
 function titleClick() {
     ModalApp.$data.showModal = true
 }
+
+
+//
+// var TitleAssistantApp = new Vue({
+//     el: "#title-assistant",
+//     data: {
+//         title: '',
+//         regionSelected: '',
+//         subtopic: '',
+//         topics: {
+//             itemsList: [],
+//             unselectedItems: [],
+//             selectedItems: [],
+//             selectedValues: ''
+//         },
+//
+//     },
+//     mounted() {
+//         topics_from_page.forEach(function (item) {  //topics is made available in the Django template, by passing serialized JSON data
+//             this.$data.topics.unselectedItems.push({value: item, displayValue: item, isSelected: false});
+//             this.$data.topics.itemsList.push({value: item, displayValue: item, isSelected: false})
+//         }.bind(this));
+//     },
+//     methods: {
+//         itemMoved: function () {
+//             // ES6 - let's discuss:
+//             this.$data.topics.selectedValues = this.$data.topics.selectedItems.map(x => x.value).join(', ');
+//             this.updateTitle()
+//         },
+//         updateRegion: function () {
+//             console.log("Updating input on region select");
+//             this.updateTitle(items.join(','))
+//         },
+//         updateTitle: function () {
+//             this.$data.title = String(this.$data.regionSelected) + " " + String(this.$data.topics.selectedValues) + " " + String(this.$data.subtopic)
+//         }
+//     }
+// });
