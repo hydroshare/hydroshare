14df2181309f84bb44d733dec133b30bc9ba5dbd
Mark OBrien <mark_noreply@noreply.com>
2019-07-15 19:33:40 -0400

    Title builder updates title

--- hs_communities/static/js/dual-listbox.js
+++ hs_communities/static/js/dual-listbox.js
@@ -17,20 +17,27 @@ var TitleAssistantApp = new Vue({
             this.$data.topics.selectedValues = this.$data.topics.selectedItems.map(x => x.value).join(', ');
             this.updateTitle()
         },
-        updateRegion: function () {
+        updateRegion: function() {
             console.log("Updating input on region select");
             this.updateTitle(items.join(','))
         },
-        updateTitle: function () {
-            this.$data.title = String(this.$data.regionSelected) + " " + String(this.$data.topics.selectedValues) + " " + String(this.$data.subtopic)
+        updateTitle: function() {
+            this.$data.title = (String(this.$data.regionSelected) + " " + String(this.$data.topics.selectedValues) + " " + String(this.$data.subtopic)).trim()
+        },
+        saveTitle: function() {
+            $("#txt-title").val(this.$data.title);
+            $("#txt-title").trigger( "change" );
+            $("#title-modal").modal('hide');
         }
     }
 });
 
 function titleClick() {
-    $("#title-modal").modal('show');
-    topics_from_page.forEach(function (item) {  //topics is made available in the Django template, by passing serialized JSON data
-        this.$data.topics.unselectedItems.push({value: item, displayValue: item, isSelected: false});
-        this.$data.topics.itemsList.push({value: item, displayValue: item, isSelected: false})
-    }.bind(TitleAssistantApp));
+    if ($("#txt-title").val() === 'Untitled resource') {
+        $("#title-modal").modal('show');
+        topics_from_page.forEach(function (item) {  //topics is made available in the Django template, by passing serialized JSON data
+            this.$data.topics.unselectedItems.push({value: item, displayValue: item, isSelected: false});
+            this.$data.topics.itemsList.push({value: item, displayValue: item, isSelected: false})
+        }.bind(TitleAssistantApp));
+    }
 }
