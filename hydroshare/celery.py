f09db3edd9429cf54817657cb373bdc4800b9861
Scott Black <scott.black@usu.edu>
2019-08-14 15:19:21 -0600

    make celery an absolute_import

--- hydroshare/celery.py
+++ hydroshare/celery.py
@@ -1,8 +1,8 @@
-
+from __future__ import absolute_import
 
 import os
 
-from .celery import Celery
+from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hydroshare.settings')
