8c3612ea507da7b79bf0f707bfa8aeb8e0692321
Mark OBrien <mark_noreply@noreply.com>
2019-08-22 10:48:19 -0400

    Adding back swagger decorator that was apparently removed in dev merge 013171a

--- hs_core/views/__init__.py
+++ hs_core/views/__init__.py
@@ -584,6 +584,7 @@ def update_metadata_element(request, shortkey, element_name, element_id, *args, 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
+@swagger_auto_schema(method='get', auto_schema=None)
 @api_view(['GET'])
 def file_download_url_mapper(request, shortkey):
     """ maps the file URIs in resourcemap document to django_irods download view function"""
