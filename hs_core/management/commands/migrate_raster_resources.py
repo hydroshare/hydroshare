e8055f0854e3889ba235bbbaea36c690114fbc0c
pkdash <pkdash_reena@hotmail.com>
2019-08-25 18:42:01 -0400

    [3249] no folder creation needed during migration

--- hs_core/management/commands/migrate_raster_resources.py
+++ hs_core/management/commands/migrate_raster_resources.py
@@ -1,11 +1,9 @@
-import os
 import logging
 
 from django.core.management.base import BaseCommand
 
 from hs_core.hydroshare import current_site_url, set_dirty_bag_flag
-from hs_core.models import ResourceFile, CoreMetaData, Coverage
-from hs_core.views.utils import rename_irods_file_or_folder_in_django
+from hs_core.models import CoreMetaData, Coverage
 from hs_geo_raster_resource.models import RasterResource
 from hs_file_types.models import GeoRasterLogicalFile
 from ..utils import migrate_core_meta_elements
@@ -24,38 +22,50 @@ class Command(BaseCommand):
         logger.info(msg)
         print(">> {}".format(msg))
 
-        for r_res in RasterResource.objects.all():
-            if r_res.metadata.cellInformation is not None:
+        for rast_res in RasterResource.objects.all():
+            # check resource exists on irods
+            istorage = rast_res.get_irods_storage()
+            if not istorage.exists(rast_res.root_path):
+                err_msg = "Raster resource not found in irods (ID: {})".format(rast_res.short_id)
+                logger.error(err_msg)
+                print("Error:>> {}".format(err_msg))
+                # skip this raster resource
+                continue
+
+            if rast_res.metadata.cellInformation is not None:
                 # get the vrt file name which needs to be used to create a new folder for
                 # raster aggregation
                 vrt_file = None
-                for res_file in r_res.files.all():
+                for res_file in rast_res.files.all():
                     if res_file.extension.lower() == '.vrt':
                         vrt_file = res_file
                         break
 
                 create_raster_aggregation = vrt_file is not None
                 if create_raster_aggregation:
-                    # create a new folder based on vrt file name
-                    folder_name = vrt_file.file_name[:-4]
-                    ResourceFile.create_folder(r_res, folder=folder_name)
-                    # move the all the resource files to this new folder
-                    istorage = r_res.get_irods_storage()
-                    for res_file in r_res.files.all():
-                        tgt_path = 'data/contents/{}/{}'.format(folder_name, res_file.file_name)
-                        src_full_path = res_file.public_path
-                        tgt_full_path = os.path.join(r_res.root_path, tgt_path)
-
-                        istorage.moveFile(src_full_path, tgt_full_path)
-                        rename_irods_file_or_folder_in_django(r_res, src_full_path, tgt_full_path)
+                    # check resource files exist on irods
+                    file_missing = False
+                    for res_file in rast_res.files.all():
+                        file_path = res_file.public_path
+                        if not istorage.exists(file_path):
+                            err_msg = "File path not found in irods:{}".format(file_path)
+                            logger.error(err_msg)
+                            err_msg = "Failed to convert raster resource (ID: {}). " \
+                                      "Resource file is missing on irods".format(rast_res.short_id)
+                            print("Error:>> {}".format(err_msg))
+                            file_missing = True
+                            break
+                    if file_missing:
+                        # skip this corrupt raster resource
+                        continue
 
             # change the resource_type
-            ras_metadata_obj = r_res.metadata
-            r_res.resource_type = to_resource_type
-            r_res.content_model = to_resource_type.lower()
-            r_res.save()
+            ras_metadata_obj = rast_res.metadata
+            rast_res.resource_type = to_resource_type
+            rast_res.content_model = to_resource_type.lower()
+            rast_res.save()
             # get the converted resource object - CompositeResource
-            comp_res = r_res.get_content_model()
+            comp_res = rast_res.get_content_model()
 
             # set CoreMetaData object for the composite resource
             core_meta_obj = CoreMetaData.objects.create()
@@ -70,7 +80,16 @@ class Command(BaseCommand):
             type_element.save()
             if create_raster_aggregation:
                 # create a Raster aggregation
-                ras_aggr = GeoRasterLogicalFile.create(resource=comp_res)
+                try:
+                    ras_aggr = GeoRasterLogicalFile.create(resource=comp_res)
+                except Exception as ex:
+                    err_msg = 'Failed to create raster aggregation for resource (ID: {})'
+                    err_msg = err_msg.format(rast_res.short_id)
+                    err_msg = err_msg + '\n' + ex.message
+                    logger.error(err_msg)
+                    print("Error:>> {}".format(err_msg))
+                    continue
+
                 # set aggregation dataset title
                 ras_aggr.dataset_name = comp_res.metadata.title.value
                 ras_aggr.save()
@@ -111,7 +130,15 @@ class Command(BaseCommand):
                 logger.info(msg)
             # set resource to dirty so that resource level xml files (resource map and
             # metadata xml files) will be re-generated as part of next bag download
-            set_dirty_bag_flag(comp_res)
+            try:
+                set_dirty_bag_flag(comp_res)
+            except Exception as ex:
+                err_msg = 'Failed to set bag flag dirty for the converted resource (ID: {})'
+                err_msg = err_msg.format(rast_res.short_id)
+                err_msg = err_msg + '\n' + ex.message
+                logger.error(err_msg)
+                print("Error:>> {}".format(err_msg))
+
             resource_counter += 1
             # delete the instance of NetCdfMetaData that was part of the original netcdf resource
             ras_metadata_obj.delete()
