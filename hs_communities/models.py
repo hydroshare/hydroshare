5d9b4648eb93c6c129b8ae142ade2e40bcb8853b
Mark OBrien <mark_noreply@noreply.com>
2019-09-03 12:11:45 -0400

    Refactor topics model

--- hs_communities/models.py
+++ hs_communities/models.py
@@ -4,40 +4,25 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField(editable=True, null=False, max_length=255)
 
-    def __str__(self):
-        return "{}".format(self.name)
-
-
-class TopicEntry(models.Model):
-    topic = models.ForeignKey(Topic,
-                              null=False,
-                              editable=True,
-                              help_text='One topic entry for a resource')
     order = models.IntegerField(null=False,
+                                default=0,
                                 editable=True,
                                 help_text='Position of this entry: 1-n')
 
     def __str__(self):
-        return "{} {}".format(self.topic, self.order)
+        return "{}".format(self.name)
 
-
-class Topics(models.Model):
-    topics = models.ManyToManyField(TopicEntry,
-                                    editable=True,
-                                    help_text='A list of topics, in order')
-
-    def add(self, new_topic):
+    @classmethod
+    def add(cls, new_topic):
         """
+        Add a new topic
 
-        :param topic:
+        :param new_topic: string topic to be added
         :return:
         """
-        topic_entry = TopicEntry()
-        topic_entry.topic = new_topic
-        topics_list = self.topics.values_list("order", flat=True)
-        topic_entry.order = max(topics_list if topics_list else [0]) + 1
+        topic = cls()
+        topic.name = new_topic
+        topics_list = Topic.objects.all.values_list("order", flat=True)
+        topic.order = max(topics_list if topics_list else [0]) + 1
 
-        topic_entry.save()
-
-    def __str__(self):
-        return "{}".format(self.topics)
+        topic.save()
