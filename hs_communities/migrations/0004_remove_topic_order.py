c53f00f227f55811dbbc8ecd099164f5c9cca5b4
Mark OBrien <mark_noreply@noreply.com>
2019-09-04 11:25:41 -0400

    Use migration to populate topics

--- hs_communities/migrations/0004_remove_topic_order.py
+++ hs_communities/migrations/0004_remove_topic_order.py
@@ -4,6 +4,47 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
+from hs_communities.models import Topic
+
+
+def populate_topics():
+    topics = ["Air Temperature", "Barometric Pressure", "Chlorophyll", "Climate", "Diatoms",
+              "Digital Elevation Model (DEM)",
+              "Dissolved Organic Matter (DOM)", "Ecosystem model", "Electrical Conductivity",
+              "Flux Tower", "Geology",
+              "Geomorphology", "Geophysics", "GIS / Map Data",
+              "Ground Penetrating Radar (GPR)", "Groundwater Chemistry",
+              "Groundwater Depth", "Groundwater Temperatures", "Hydropedologic Properties",
+              "Land Cover", "Land Use History",
+              "LiDAR", "Lysimeter Water Samples Chemistry", "Matric Potential",
+              "Meteorology", "Nutrient Fluxes",
+              "Overland Water Chemistry", "Ozone", "Photographic Imagery", "Piezometer",
+              "Precipitation",
+              "Precipitation Chemistry", "Rainfall Chemistry", "Regolith Survey",
+              "Reservoir Height", "Rock Moisture",
+              "Sap Flow", "Sediment Transport", "Seismic Refraction", "Snow Depth",
+              "Snow Pits", "Snow Survey",
+              "Soil Biogeochemistry", "Soil Electrical Resistivity",
+              "Soil Evapotranspiration", "Soil Gas",
+              "Soil Geochemistry", "Soil Invertebrates", "Soil Microbes",
+              "Soil Mineralogy", "Soil Moisture",
+              "Soil Porewater Chemistry", "Soil Porosity", "Soil Redox Potential",
+              "Soil Respiration",
+              "Soil Survey",
+              "Soil Temperature", "Soil Texture", "Soil Water", "Soil Water Chemistry",
+              "Solar Radiation", "Stable Isotopes",
+              "Stage", "Stream Ecology", "Stream Suspended Sediment",
+              "Stream Water Chemistry", "Stream Water Temperatures",
+              "Streamflow / Discharge", "Surface Water Chemistry",
+              "Throughfall Chemistry", "Topographic Carbon Storage",
+              "Tree Growth & Physiology", "Vegetation", "Water Potential",
+              "Well Water Levels"]
+
+    for t in topics:
+        n = Topic()
+        n.name = t
+        n.save()
+
 
 class Migration(migrations.Migration):
 
@@ -16,4 +57,5 @@ class Migration(migrations.Migration):
             model_name='topic',
             name='order',
         ),
+        migrations.RunPython(populate_topics),
     ]
