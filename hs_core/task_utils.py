6fb69b8d1c97cf217d01da12b38b0c747b31e7bc
hyi <hongyi@renci.org>
2019-12-11 14:16:59 -0500

    address code review

--- hs_core/task_utils.py
+++ hs_core/task_utils.py
@@ -1,30 +1,26 @@
 from celery.task.control import inspect
 
 
-def get_resource_bag_task(res_id):
-    i = inspect()
-    active_jobs = i.active()
-    reserved_jobs = i.reserved()
-    scheduled_jobs = i.scheduled()
-    if active_jobs:
-        workers = active_jobs.keys()
+def _retrieve_job_id(res_id, job_dict, job_name='hs_core.tasks.create_bag_by_irods'):
+    if job_dict:
+        workers = job_dict.keys()
         for worker in workers:
-            for job in active_jobs[worker]:
-                if job['name'] == 'hs_core.tasks.create_bag_by_irods':
-                    if res_id in job['args']:
-                        return job['id']
-    if reserved_jobs:
-        workers = reserved_jobs.keys()
-        for worker in workers:
-            for job in reserved_jobs[worker]:
-                if job['name'] == 'hs_core.tasks.create_bag_by_irods':
-                    if res_id in job['args']:
-                        return job['id']
-    if scheduled_jobs:
-        workers = scheduled_jobs.keys()
-        for worker in workers:
-            for job in scheduled_jobs[worker]:
-                if job['name'] == 'hs_core.tasks.create_bag_by_irods':
+            for job in job_dict[worker]:
+                if job['name'] == job_name:
                     if res_id in job['args']:
                         return job['id']
     return None
+
+
+def get_resource_bag_task(res_id):
+    i = inspect()
+    active_jobs = i.active()
+    job_id = _retrieve_job_id(res_id, active_jobs)
+    if not job_id:
+        reserved_jobs = i.reserved()
+        job_id = _retrieve_job_id(res_id, reserved_jobs)
+        if not job_id:
+            scheduled_jobs = i.scheduled()
+            job_id = _retrieve_job_id(res_id, scheduled_jobs)
+
+    return job_id
