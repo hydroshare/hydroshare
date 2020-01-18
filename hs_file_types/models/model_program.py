d1265d9487d0b8597c541ba6d2e333a52ee1ca21
pkdash <pkdash_reena@hotmail.com>
2020-01-18 14:12:58 -0500

    [#3519] model program logical file class inheriting from base model logical file class

--- hs_file_types/models/model_program.py
+++ hs_file_types/models/model_program.py
@@ -1,18 +1,16 @@
-import os
-import logging
 import json
-import jsonschema
 
+import jsonschema
 from django.contrib.postgres.fields import ArrayField, JSONField
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.template import Template, Context
-from lxml import etree
 from dominate import tags as dom_tags
+from lxml import etree
 
-from hs_core.models import ResourceFile, CoreMetaData
-from base import AbstractLogicalFile, FileTypeContext
+from base_model_program_instance import AbstractModelLogicalFile
 from generic import GenericFileMetaDataMixin
+from hs_core.models import ResourceFile, CoreMetaData
 
 
 class ModelProgramResourceFileType(models.Model):
@@ -343,8 +341,8 @@ class ModelProgramFileMetaData(GenericFileMetaDataMixin):
         return rendered_html
 
 
-class ModelProgramLogicalFile(AbstractLogicalFile):
-    """ One file or more than one files in a specific folder can be part of this aggregation """
+class ModelProgramLogicalFile(AbstractModelLogicalFile):
+    """ One file or more than one file in a specific folder can be part of this aggregation """
 
     # attribute to store type of model program (SWAT, UEB etc)
     model_program_type = models.CharField(max_length=255, default="Unknown Model Program")
@@ -354,11 +352,6 @@ class ModelProgramLogicalFile(AbstractLogicalFile):
     # metadata schema (in json format) for model instance aggregation to which this aggregation
     # can be related. metadata for the related model instance aggregation is validated based on this schema
     mi_schema_json = JSONField(default=dict)
-
-    # folder path relative to {resource_id}/data/contents/ that represents this aggregation
-    # folder becomes the name of the aggregation. Where folder is not set, the one file that is part
-    # of this aggregation becomes the aggregation name
-    folder = models.CharField(max_length=4096, null=True, blank=True)
     data_type = "Model Program"
 
     @classmethod
@@ -383,79 +376,6 @@ class ModelProgramLogicalFile(AbstractLogicalFile):
         This must agree between Composite Types and native types).
         """
         return self.model_program_type
-
-    @classmethod
-    def get_main_file_type(cls):
-        """The main file type for this aggregation - no specific main file"""
-        return ".*"
-
-    @classmethod
-    def check_files_for_aggregation_type(cls, files):
-        """Checks if the specified files can be used to set this aggregation type
-        :param  files: a list of ResourceFile objects
-
-        :return If the files meet the requirements of this aggregation type, then returns this
-        aggregation class name, otherwise empty string.
-        """
-        if len(files) == 0:
-            # no files
-            return ""
-
-        return cls.__name__
-
-    @classmethod
-    def get_primary_resouce_file(cls, resource_files):
-        """Gets any one resource file from the list of files *resource_files* """
-
-        return resource_files[0] if resource_files else None
-
-    @classmethod
-    def set_file_type(cls, resource, user, file_id=None, folder_path=None):
-        """Makes all physical files that are in a folder (*folder_path*) part of a model program
-        aggregation type or a single file (*file_id*) part of this aggregation type.
-        Note: parameter file_id is ignored here and a value for folder_path is required
-        """
-
-        log = logging.getLogger()
-        with FileTypeContext(aggr_cls=cls, user=user, resource=resource, file_id=file_id,
-                             folder_path=folder_path,
-                             post_aggr_signal=None,
-                             is_temp_file=False) as ft_ctx:
-
-            if folder_path is not None:
-                res_files = []
-                dataset_name = folder_path
-                if '/' in folder_path:
-                    dataset_name = os.path.basename(folder_path)
-            else:
-                res_file = ft_ctx.res_file
-                res_files = [res_file]
-                folder_path = res_file.file_folder
-                dataset_name, _ = os.path.splitext(res_file.file_name)
-
-            # remove any previously associated logical files from the files
-            # before making them part of this new logical file
-            for res_file in res_files:
-                if res_file.has_logical_file:
-                    res_file.logical_file_content_object = None
-                    res_file.save()
-
-            # create a model program logical file object
-            logical_file = cls.create_aggregation(dataset_name=dataset_name,
-                                                  resource=resource,
-                                                  res_files=res_files,
-                                                  new_files_to_upload=[],
-                                                  folder_path=folder_path)
-
-            if folder_path is not None and file_id is None:
-                logical_file.folder = folder_path
-                logical_file.save()
-                # make all the files in the selected folder as part of the aggregation
-                logical_file.add_resource_files_in_folder(resource, folder_path)
-                log.info("Model Program aggregation was created for folder:{}.".format(folder_path))
-            else:
-                log.info("Model Program aggregation was created for file:{}.".format(res_file.storage_path))
-            ft_ctx.logical_file = logical_file
 
     def set_mi_schema(self, json_schema_string):
         """Sets the mi_schema_json fields of the aggregation with valid json schema
