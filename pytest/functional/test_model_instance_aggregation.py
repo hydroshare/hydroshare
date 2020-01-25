c5cadcd1d60e019b8e711af178d5d3d1ed7ebffb
pkdash <pkdash_reena@hotmail.com>
2020-01-25 16:35:31 -0500

    [#3519] adding tests for linking model instance to model program (executed_by)

--- pytest/functional/test_model_instance_aggregation.py
+++ pytest/functional/test_model_instance_aggregation.py
@@ -1,18 +1,159 @@
+import os
+
 import pytest
+from django.core.exceptions import PermissionDenied
+from django.core.files.uploadedfile import UploadedFile
 
-from hs_file_types.models import ModelInstanceLogicalFile
+from hs_access_control.models import PrivilegeCodes
+from hs_core.hydroshare import add_file_to_resource
+from hs_file_types.models import ModelInstanceLogicalFile, ModelProgramLogicalFile
 
 
-# TODO: implement tests for linking model instance aggregation to model program aggregation using the
-# FK (executed_by) attribute of model instance aggregation
-# The following tests cases for linking needs to be implemented:
-# 1. linking aggregations within the same resource
-#     a. linking one mp aggregation to one mi aggregation
-#     b. linking one mp aggregation to two mi aggregations
-# 2. linking aggregations that do not belong to the same resource
-# 3. test linking is not allowed if user does not have access to the resource that contains the mp aggregation
-# 4. test that mi aggregation does not get deleted if the linked mp aggregation gets deleted
-# 5. test that mi aggregation does not get deleted if the resource containing the linked mp aggregation gets deleted
+@pytest.mark.django_db(transaction=True)
+def test_link_model_aggregations_same_resource(composite_resource_with_mi_aggregation, mock_irods):
+    """Test that we can link one model instance aggregation to one model program aggregation within the same resource"""
+
+    res, user = composite_resource_with_mi_aggregation
+    assert ModelInstanceLogicalFile.objects.count() == 1
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    # check that mi_aggr is not related to any model program aggregation
+    assert mi_aggr.metadata.executed_by is None
+    # create a model program aggregation
+    file_path = 'pytest/assets/logan.vrt'
+    upload_folder = None
+    file_to_upload = UploadedFile(file=open(file_path, 'rb'),
+                                  name=os.path.basename(file_path))
+
+    res_file = add_file_to_resource(
+        res, file_to_upload, folder=upload_folder, check_target_folder=True
+    )
+
+    assert ModelProgramLogicalFile.objects.count() == 0
+    # set file to model program aggregation type
+    ModelProgramLogicalFile.set_file_type(res, user, res_file.id)
+    assert ModelProgramLogicalFile.objects.count() == 1
+    mp_aggr = ModelProgramLogicalFile.objects.first()
+    # link model instance aggregation to model program aggregation
+    mi_aggr.set_link_to_model_program(user=user, model_prog_aggr=mp_aggr)
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    # check that mi_aggr is related to model program aggregation
+    assert mi_aggr.metadata.executed_by is not None
+
+
+@pytest.mark.django_db(transaction=True)
+def test_link_model_aggregations_different_resources(composite_resource_with_mi_aggregation,
+                                                     composite_resource_2_with_mp_aggregation, mock_irods):
+    """Test that we can link one model instance aggregation in one resource to one model program aggregation
+    in another resource provided the user doing the link operation has at edit permission on the resource
+    containing the model instance aggregation and view permission on the resource containing the model program
+    aggregation"""
+
+    mi_res, mi_user = composite_resource_with_mi_aggregation
+    mp_res, mp_user = composite_resource_2_with_mp_aggregation
+    assert mi_res != mp_res
+    assert mi_user != mp_user
+
+    authorized = mi_user.uaccess.can_change_resource(mi_res)
+    assert authorized
+    assert ModelInstanceLogicalFile.objects.count() == 1
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    assert ModelProgramLogicalFile.objects.count() == 1
+    mp_aggr = ModelProgramLogicalFile.objects.first()
+    # check that mi_aggr is not related to any model program aggregation
+    assert mi_aggr.metadata.executed_by is None
+    # test that when the user does not have view access to the mp_res, trying to link the
+    # 2 aggregations should fail
+    authorized = mi_user.uaccess.can_view_resource(mp_res)
+    assert not authorized
+    with pytest.raises(PermissionDenied):
+        mi_aggr.set_link_to_model_program(user=mi_user, model_prog_aggr=mp_aggr)
+    # give mi_user view access to mp_res
+    mp_user.uaccess.share_resource_with_user(mp_res, mi_user, PrivilegeCodes.VIEW)
+    authorized = mi_user.uaccess.can_view_resource(mp_res)
+    assert authorized
+    # now we should be able to link model instance aggregation to model program aggregation
+    mi_aggr.set_link_to_model_program(user=mi_user, model_prog_aggr=mp_aggr)
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    # check that mi_aggr is related to model program aggregation
+    assert mi_aggr.metadata.executed_by is not None
+    # need to delete this resource as the fixture does not delete it
+    mp_res.delete()
+
+
+@pytest.mark.django_db(transaction=True)
+def test_model_instance_on_model_program_delete(composite_resource_with_mi_aggregation, mock_irods):
+    """Test that when we remove/delete a model program aggregation that the linked model instance aggregation does not
+    get deleted"""
+
+    res, user = composite_resource_with_mi_aggregation
+    assert ModelInstanceLogicalFile.objects.count() == 1
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    # check that mi_aggr is not related to any model program aggregation
+    assert mi_aggr.metadata.executed_by is None
+    # create a model program aggregation
+    file_path = 'pytest/assets/logan.vrt'
+    upload_folder = None
+    file_to_upload = UploadedFile(file=open(file_path, 'rb'),
+                                  name=os.path.basename(file_path))
+
+    res_file = add_file_to_resource(
+        res, file_to_upload, folder=upload_folder, check_target_folder=True
+    )
+
+    assert ModelProgramLogicalFile.objects.count() == 0
+    # set file to model program aggregation type
+    ModelProgramLogicalFile.set_file_type(res, user, res_file.id)
+    assert ModelProgramLogicalFile.objects.count() == 1
+    mp_aggr = ModelProgramLogicalFile.objects.first()
+    # link model instance aggregation to model program aggregation
+    mi_aggr.set_link_to_model_program(user=user, model_prog_aggr=mp_aggr)
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    # check that mi_aggr is related to model program aggregation
+    assert mi_aggr.metadata.executed_by is not None
+    # remove mp_aggregation
+    mp_aggr.remove_aggregation()
+    assert ModelProgramLogicalFile.objects.count() == 0
+    assert ModelInstanceLogicalFile.objects.count() == 1
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    # check that mi_aggr is not related to any model program aggregation
+    assert mi_aggr.metadata.executed_by is None
+
+
+@pytest.mark.django_db(transaction=True)
+def test_model_instance_on_model_program_resource_delete(composite_resource_with_mi_aggregation,
+                                                         composite_resource_2_with_mp_aggregation, mock_irods):
+    """Test that when we delete a resource containing a model program aggregation linked to a model
+    instance aggregation, the model instance aggregation doesn't get deleted"""
+
+    mi_res, mi_user = composite_resource_with_mi_aggregation
+    mp_res, mp_user = composite_resource_2_with_mp_aggregation
+    assert mi_res != mp_res
+    assert mi_user != mp_user
+
+    authorized = mi_user.uaccess.can_change_resource(mi_res)
+    assert authorized
+    assert ModelInstanceLogicalFile.objects.count() == 1
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    assert ModelProgramLogicalFile.objects.count() == 1
+    mp_aggr = ModelProgramLogicalFile.objects.first()
+    # check that mi_aggr is not related to any model program aggregation
+    assert mi_aggr.metadata.executed_by is None
+    # give mi_user view access to mp_res
+    mp_user.uaccess.share_resource_with_user(mp_res, mi_user, PrivilegeCodes.VIEW)
+    authorized = mi_user.uaccess.can_view_resource(mp_res)
+    assert authorized
+    # now we should be able to link model instance aggregation to model program aggregation
+    mi_aggr.set_link_to_model_program(user=mi_user, model_prog_aggr=mp_aggr)
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    # check that mi_aggr is related to model program aggregation
+    assert mi_aggr.metadata.executed_by is not None
+    # delete mp_resource
+    mp_res.delete()
+    assert ModelProgramLogicalFile.objects.count() == 0
+    # check that mi_aggr is not related to any model program aggregation
+    mi_aggr = ModelInstanceLogicalFile.objects.first()
+    assert mi_aggr.metadata.executed_by is None
+
 
 @pytest.mark.django_db(transaction=True)
 def test_set_metadata(composite_resource_with_mi_aggregation, mock_irods):
@@ -21,7 +162,6 @@ def test_set_metadata(composite_resource_with_mi_aggregation, mock_irods):
     res, user = composite_resource_with_mi_aggregation
     # mi_aggr = next(res.logical_files)
     mi_aggr = ModelInstanceLogicalFile.objects.first()
-    assert isinstance(mi_aggr, ModelInstanceLogicalFile)
 
     # test extra metadata
     assert not mi_aggr.metadata.extra_metadata
