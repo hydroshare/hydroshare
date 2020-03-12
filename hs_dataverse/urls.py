da1afec13246a2026d24cc102e7b376d9a6dc2cf
Alva Couch <acouch@cuahsi.org>
2020-03-12 11:57:54 -0400

    Change URL mapping

--- hs_dataverse/urls.py
+++ hs_dataverse/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     # internal API
     # url(r'^_internal/(?P<shortkey>[0-9a-f-]+)/add-files-to-resource/$',
     #     views.add_files_to_resource, name='add_files_to_resource'),
-    path('', views.datatest, name='datatest'),
+    url('^datatest$', views.datatest, name='datatest'),
 ]
--- hs_dataverse/views.py
+++ hs_dataverse/views.py
@@ -2,6 +2,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-
 def datatest(request):
     return HttpResponse("<h1>data test</h1>\n", 200)
