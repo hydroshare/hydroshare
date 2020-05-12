40a515e7f784126e1a7ed4abba5f11dabbab5796
Mark OBrien <mark_noreply@noreply.com>
2020-05-12 14:50:48 -0400

    Add src for client webapp

---
+++ hs_discover/src/components/Search.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{message}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'initial message'
+            }
+        }
+    }
+</script>
---
+++ hs_discover/src/main.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import BootstrapVue from 'bootstrap-vue';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+
+Vue.use(BootstrapVue)
+
+new Vue({
+    router,
+    render: h =>h(App),
+}).$mount('#app');
