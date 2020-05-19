4754d7d8d3ba8bc50998f255ac3e52f31d3f90c7
Mark OBrien <mark_noreply@noreply.com>
2020-05-19 12:36:14 -0400

    Update packages

--- hs_discover/src/components/Ping.vue
+++ hs_discover/src/components/Ping.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        {{ message }}
+        {{ message }}<br/>
     </div>
 </template>
 
--- hs_discover/src/router.js
+++ hs_discover/src/router.js
@@ -11,7 +11,7 @@ export default new Router({
         {
             path: '/',
             name: 'Ping',
-            component: Ping,
-        },
-    ],
+            component: Ping
+        }
+    ]
 });
