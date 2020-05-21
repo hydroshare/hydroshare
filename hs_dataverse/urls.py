c9031572f8b845bb71d76d845f4677c373bac3d6
hydro service <hydro-service@dev-hs-3.cuahsi.org>
2020-05-21 09:08:03 -0400

    Initial

--- hs_dataverse/urls.py
+++ hs_dataverse/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # url(r'^_internal/(?P<shortkey>[0-9a-f-]+)/add-files-to-resource/$',
     #     views.add_files_to_resource, name='add_files_to_resource'),
     url('^datatest$', views.datatest, name='datatest'),
-]
+    url(r'^upload_dataset/$', views.upload_dataset, name='upload_dataset'),
+datatest]
