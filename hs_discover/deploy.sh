66ef738675f4277edd00250c1e31a9844866267c
Mark OBrien <mark_noreply@noreply.com>
2020-06-09 15:39:58 -0400

    Update deploy

--- hs_discover/deploy.sh
+++ hs_discover/deploy.sh
@@ -2,6 +2,7 @@ npm run build
 rm static/css/*.css
 rm static/js/*.js
 rm static/js/*.map
+mkdir static
 cp templates/hs_discover/css/app*.css static/css/app.css
 cp templates/hs_discover/css/chunk-vendors*.css static/css/chunk-vendors.css
 cp templates/hs_discover/js/app*.js static/js/app.js
