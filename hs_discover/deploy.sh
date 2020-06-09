da6a9c180f0567858cc6a7f24459ac15acc018e4
Mark OBrien <mark_noreply@noreply.com>
2020-06-09 15:43:04 -0400

    Update deploy to create more dirs

--- hs_discover/deploy.sh
+++ hs_discover/deploy.sh
@@ -2,7 +2,8 @@ npm run build
 rm static/css/*.css
 rm static/js/*.js
 rm static/js/*.map
-mkdir static
+mkdir -p static/css
+mkdir -p static/js
 cp templates/hs_discover/css/app*.css static/css/app.css
 cp templates/hs_discover/css/chunk-vendors*.css static/css/chunk-vendors.css
 cp templates/hs_discover/js/app*.js static/js/app.js
