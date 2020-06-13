380d3578e1af71f87bd26db94b12782af09862cb
Mark OBrien <mark_noreply@noreply.com>
2020-06-12 23:27:37 -0400

    Display author counter debug info

--- hs_discover/src/components/Resources.vue
+++ hs_discover/src/components/Resources.vue
@@ -2,6 +2,7 @@
     <div>
         <p v-if="filteredResources.length">Results: {{filteredResources.length}}</p>
         <div class="table-wrapper">
+            This will show Author counts beside checkboxes like current interface {{counters}}
             <table v-if="filteredResources.length"
                    class="table-hover table-striped resource-custom-table" id="items-discovered">
                 <thead>
@@ -46,6 +47,7 @@
 export default {
   data() {
     return {
+      counters: {},
       authorFilter: [],
       sortKey: '',
       sortOrders: { Type: -1, Title: 1, 'First Author': -1 },
@@ -58,18 +60,26 @@ export default {
     filteredResources() {
       const { sortKey } = this;
       if (sortKey) {
-        // sort by column
+        // do nothing
       }
       return this.resources.filter(element => this.authorFilter.indexOf(element.author) > -1);
     },
   },
   mounted() {
-    this.authors.forEach(author => this.authorFilter.push(author));
+    this.authors.forEach(author => this.authorFilter.push(author)); // populate checkboxes
+    const authorbox = [];
+    this.resources.forEach(res => authorbox.push(res.author));
+    // console.log(new this.Counter(authorbox)); // eslint-disable-line
+    this.counters = new this.Counter(authorbox);
   },
   methods: {
     sortBy(key) {
       this.sortKey = key;
       this.sortOrders[key] = this.sortOrders[key] * -1;
+    },
+    Counter(array) {
+      // eslint-disable-next-line no-return-assign
+      array.forEach(val => this[val] = (this[val] || 0) + 1);
     },
   },
 };
