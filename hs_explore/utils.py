f3c16f8e2a290c322469613f9365f767353dcba3
xuezhaokun <helloxzk@gmail.com>
2020-06-16 17:11:21 -0400

    move user activity tracking to hs_explore utils

--- hs_explore/utils.py
+++ hs_explore/utils.py
@@ -1,13 +1,34 @@
-class Utils(object):
-    """
-    Features appropriate for machine learning analysis of our databases.
-    """
+from django.contrib.auth.models import User
+from hs_core.models import BaseResource
+from hs_tracking.models import Variable
+from django.db.models import Max
 
-    @staticmethod
-    def write_dict(filename, some_dict):
-        target = open(filename, 'a')
-        # s = ""
-        # for user_resource, val in user_resource_dict.iteritems():
-        #     s = user_resource[0] + " " + user_resource[1] + " " + str(val) + "\n"
-        #     print s
-        target.write(str(some_dict))
+
+def user_resource_matrix(fromdate, todate):
+    """ return a list of (username, resource_id, last_access_timestamp), which
+        shows the latest time that a user accessed a resource during the given
+        period.
+        :param fromdate (date type), the start date of the time period
+        :param todate (date, type), the end date of the time period
+    """
+    users = User.objects.filter(
+        visitor__session__variable__timestamp__gte=fromdate,
+        visitor__session__variable__timestamp__lte=todate)\
+            .distinct()
+    events = []
+    for u in users:
+        resources = BaseResource.objects.filter(
+                variable__timestamp__gte=fromdate,
+                variable__timestamp__lte=todate,
+                variable__session__visitor__user=u)
+        for r in resources:
+            latest = Variable.objects.filter(
+                    resource=r,
+                    session__visitor__user=u,
+                    timestamp__gte=fromdate,
+                    timestamp__lte=todate)\
+                    .aggregate(latest=Max('timestamp'))
+
+            events.append((u.username, r.short_id, latest['latest']))
+
+    return events
