5d3e104c8d0b3a443a2677f02fcea58726f1e1e4
Maxwell <maxwell3954@gmail.com>
2020-07-23 08:59:11 -0400

    modularized and cleaned up code

--- hs_dataverse/management/commands/dataverse.py
+++ hs_dataverse/management/commands/dataverse.py
@@ -22,23 +22,36 @@ import tempfile
 from django_irods.views import download as download_bag_from_irods
 from pprint import pprint
 import shutil
+#from hs_dataverse import run_tests
 
-
+BUFSIZE = 4096
 
 def export_bag(rid, options): 
-    mkdir = tempfile.mkdtemp(prefix=rid, suffix='_dataverse_tempdir', dir='/tmp')
     requests.packages.urllib3.disable_warnings()
     try:
         # database handle
         resource = BaseResource.objects.get(short_id=rid)
-        # file handle
-        istorage = resource.get_irods_storage()
+ 
+        # instance with proper subclass type and access
+        res = resource.get_content_model()
+        assert res, (resource, resource.content_model)
+        print("resource is {}".format(res.short_id))
+        print("content type: {}".format(res.discovery_content_type))
+        if (res.discovery_content_type != 'Composite'): 
+            print("resource type '{}' is not supported. Aborting.".format(res.discovery_content_type))
+            exit(1)
 
-        root_exists = istorage.exists(resource.root_path)
+        # create temporary directory 
+        mkdir = tempfile.mkdtemp(prefix=rid, suffix='_dataverse_tempdir', dir='/tmp')
+
+        # file handle
+        istorage = res.get_irods_storage()
+
+        root_exists = istorage.exists(res.root_path)
 
         if root_exists:
             # print status of metadata/bag system
-            scimeta_path = os.path.join(resource.root_path, 'data',
+            scimeta_path = os.path.join(res.root_path, 'data',
                                         'resourcemetadata.xml')
             scimeta_exists = istorage.exists(scimeta_path)
             
@@ -53,53 +66,60 @@ def export_bag(rid, options):
                 args = ('-') # redirect to stdout
                 fd = session.run_safe('iget', None, scimeta_path, *args)
                 #read(fd) to get file contents
-                contents = ''
-                for temp in fd.stdout:
-                    contents += str(temp.decode('utf8'))
-                outfile = open('hs_dataverse/tempfiles/resourcemetadata.xml', 'w')
-                outfile.write(contents)
-                outfile.close()
-
                 
+                contents = b""
+                #for temp in fd.stdout:
+                #    contents += str(temp.decode('utf8'))
+                
+                BUFSIZE = 4096
+                block = fd.stdout.read(BUFSIZE) 
+                while block !=   b"": 
+                    # Do stuff with byte. 
+                    contents += block
+                    block = fd.stdout.read(BUFSIZE) 
+        
+                _, mkfile_path = tempfile.mkstemp(prefix='resourcemetadata.xml', dir=mkdir)
+                with open(mkfile_path, 'wb') as mkfile:
+                    mkfile.write(contents)
             else:
                 print("resource metadata {} not found".format(scimeta_path))
 
-            resmap_path = os.path.join(resource.root_path, 'data', 'resourcemap.xml')
+            resmap_path = os.path.join(res.root_path, 'data', 'resourcemap.xml')
             resmap_exists = istorage.exists(resmap_path)
             if resmap_exists:
                 print("resource map {} found".format(resmap_path))
             else:
                 print("resource map {} not found".format(resmap_path))
 
-            bag_exists = istorage.exists(resource.bag_path)
+            bag_exists = istorage.exists(res.bag_path)
             if bag_exists:
-                print("bag {} found".format(resource.bag_path))
+                print("bag {} found".format(res.bag_path))
             else:
-                print("bag {} NOT FOUND".format(resource.bag_path))
+                print("bag {} NOT FOUND".format(res.bag_path))
 
-            dirty = resource.getAVU('metadata_dirty')
+            dirty = res.getAVU('metadata_dirty')
             print("{}.metadata_dirty is {}".format(rid, str(dirty)))
 
-            modified = resource.getAVU('bag_modified')
+            modified = res.getAVU('bag_modified')
             print("{}.bag_modified is {}".format(rid, str(modified)))
 
             # make sure that the metadata file syncs with the database
             if dirty or not scimeta_exists or options['generate_metadata']:
                 try:
-                    create_bag_files(resource)
+                    create_bag_files(res)
                 except ValueError as e:
                     print(("{}: value error encountered: {}".format(rid, str(e))))
                     return
                 print("{}: metadata generated from Django".format(rid))
-                resource.setAVU('metadata_dirty', 'false')
+                res.setAVU('metadata_dirty', 'false')
                 print("{}.metadata_dirty set to false".format(rid))
-                resource.setAVU('bag_modified', 'true')
+                res.setAVU('bag_modified', 'true')
                 print("{}.bag_modified set to false".format(rid))
 
             if modified or not bag_exists or options['generate_bag']:
                 create_bag_by_irods(rid)
                 print("{}: bag generated from iRODs".format(rid))
-                resource.setAVU('bag_modified', 'false')
+                res.setAVU('bag_modified', 'false')
                 print("{}.bag_modified set to false".format(rid))
 
                 if icommands.ACTIVE_SESSION:
@@ -107,66 +127,79 @@ def export_bag(rid, options):
                 else:
                     raise keyerror('settings must have irods_global_session set')
 
-                dir = '/'.join([resource.root_path, 'data/contents'])
-                istorage = resource.get_irods_storage()
+                dir = '/'.join([res.root_path, 'data/contents'])
+                istorage = res.get_irods_storage()
                 data = istorage.listdir(dir)
                 
-                #pprint(data)
                 for file in data[1]:
-                    bag_data_path = '/'.join([resource.root_path, 'data/contents', file])
+                    bag_data_path = '/'.join([res.root_path, 'data/contents', file])
                     args = ('-') # redirect to stdout
                     fd = session.run_safe('iget', None, bag_data_path, *args)
-                    #read(fd) to get file contents
-                    contents = ''
-                    for temp in fd.stdout:
-                        contents += str(temp.decode('utf8'))
+
+                    #contents = ''
+                    #for temp in fd.stdout:
+                    #    contents += str(temp.decode('utf8'))
+                    #_, mkfile_path = tempfile.mkstemp(prefix=file, dir=mkdir)
+                    #with open(mkfile_path, 'w') as mkfile:
+                    #    mkfile.write(contents)
+
+                    
+                    contents = b""
+                    block = fd.stdout.read(BUFSIZE) 
+                    while block !=   b"": 
+                        # Do stuff with byte. 
+                        contents += block
+                        block = fd.stdout.read(BUFSIZE) 
                     _, mkfile_path = tempfile.mkstemp(prefix=file, dir=mkdir)
-                    with open(mkfile_path, 'w') as mkfile:
+                    with open(mkfile_path, 'wb') as mkfile:
                         mkfile.write(contents)
 
 
-#                try:
-#                    istorage.getFile(resource.bag_path, mkfile)
-#                except SessionException as e:
-#                    print("bag file not found: {}".format(e.message))
-#                    exit(1)
+                #try:
+                #    istorage.getFile(res.bag_path, mkfile)
+                #except SessionException as e:
+                #    print("bag file not found: {}".format(e.message))
+                #    exit(1)
 
 
-
-            ### now we have a bag and it's in tmp/export_bag.zip
-            ### work your own magic on this.
 
             ### get owners. An owner is a user
             ### see https://docs.djangoproject.com/en/3.0/ref/contrib/auth/
-            owners = resource.raccess.owners
-            for o in owners: 
-                print("username: {}".format(o.username))
-                print("first_name: {}".format(o.first_name))
-                print("last_name: {}".format(o.last_name))
-                print("email: {}".format(o.email))
-                profile = o.userprofile
-                party = get_party_data_from_user(o)
-                print("organization: {}".format(party['organization']))
+            
+            owners = list(resource.raccess.owners)
 
+            print('o:', owners)
+            if len(owners) == 0:
                 owner_dict = {
+                    'username': '',
+                    'first_name': '',
+                    'last_name': '',
+                    'email': '',
+                    'organization': ''
+                }
+            else:
+                for o in owners: 
+                    print("username: {}".format(o.username))
+                    print("first_name: {}".format(o.first_name))
+                    print("last_name: {}".format(o.last_name))
+                    print("email: {}".format(o.email))
+                    profile = o.userprofile
+                    party = get_party_data_from_user(o)
+                    print("organization: {}".format(party['organization']))
+
+                    owner_dict = {
                         'username': format(o.username),
                         'first_name': format(o.first_name),
                         'last_name': format(o.last_name),
                         'email': format(o.email),
                         'organization': format(party['organization'])
-                }
+                    }
 
-                outfile = open('hs_dataverse/tempfiles/ownerdata.json', 'w')
-                json.dump(owner_dict, outfile)
-                outfile.close()
-
+            _, mkfile_path = tempfile.mkstemp(prefix='ownerdata.json', dir=mkdir)
+            with open(mkfile_path, 'w') as mkfile:
+                json.dump(owner_dict, mkfile)
             # get other metadata
              
-            # instance with proper subclass type and access
-            res = resource.get_content_model()
-            assert res, (resource, resource.content_model)
-            print("resource is {}".format(res.short_id))
-            print("content type: {}".format(res.discovery_content_type))
 
             # read extended metadata as key/value pairsi
             ext_metadata = ''
@@ -206,20 +239,27 @@ def export_bag(rid, options):
                 'published': published,
                 'doi': doi,
                 'extended_metadata_notes': ext_metadata,
-                'language': str(resource.metadata.language),
+                'language': str(res.metadata.language),
                 'funding_agency_names': funding_agency_names,
                 'award_numbers': award_numbers,
                 'contributors': contributors
             }
 
-            outfile = open('hs_dataverse/tempfiles/other_metadata.json', 'w')
-            json.dump(other_metadata_dict, outfile)
-            outfile.close()
+            _, mkfile_path = tempfile.mkstemp(prefix='other_metadata.json', dir=mkdir)
+            with open(mkfile_path, 'w') as mkfile:
+                json.dump(other_metadata_dict, mkfile)
 
         else:
             print("Resource with id {} does not exist in iRODS".format(rid))
     except BaseResource.DoesNotExist:
         print("Resource with id {} NOT FOUND in Django".format(rid))
+
+    # before returning the temporary directory, rename all the files by 
+    # removing the extra characters inserted by mkstemp()
+    for file in os.listdir(mkdir):
+        file_path = '/'.join([mkdir, file])
+        os.rename(file_path, file_path[:-8]) # remove last 8 characters generated by tempfile
+        file_path = file_path[:-8]
 
     return mkdir
 
@@ -261,6 +301,14 @@ class Command(BaseCommand):
             help='HydroShare password'
         )
 
+        #parser.add_argument(
+        #    '--test',
+        #    default=None,
+        #    dest='test',  # value is options['test']
+        #    help='run tests'
+        #)
+
+
     def handle(self, *args, **options):
         # server url
         base_url = 'https://dataverse.harvard.edu'
@@ -271,13 +319,12 @@ class Command(BaseCommand):
         # parent given here
         dv = 'mydv'
 
+        #if len(options['test']) > 0:
+        #    run_tests()
         if len(options['resource_ids']) > 0:  # an array of resource short_id to check.
             for rid in options['resource_ids']:
                 temp_dir = export_bag(rid, options)
                 upload_dataset(base_url, api_token, dv, temp_dir)
-                print('contents of tmpdir before rmtree:', os.listdir(temp_dir))
-                print('contents of tmp b4 rmtree:', os.listdir('/tmp'))
-                print('path:', os.getcwd())
                 shutil.rmtree(temp_dir, ignore_errors=False)
         else:
             print("no resource id specified: aborting")
--- hs_dataverse/utils.py
+++ hs_dataverse/utils.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 import json
 import xml.etree.ElementTree as ET
 import re
@@ -9,7 +8,7 @@ import os
 import sys
 from django.conf import settings
 import googlemaps
-from pprint import pprint
+import time
 
 # global variables
 
@@ -18,19 +17,23 @@ true = True
 
 # helper functions
 
+
 # if the given string contains a url, returns it. Otherwise, returns the
 # generic hydroshare url
 def find_url(string, alt_url):
-    regex = r'(https?://[^\s]+)'
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/" + \
+        ")(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>" + \
+        "]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, string)
     if len([x[0] for x in url]) == 0:
         return alt_url
     else:
         return [x[0] for x in url][0]
 
-# returns the field_name of the text of the etree value val, or the empty string if null 
+
+# returns the field_name of the text of the etree value val, or the empty string if null
 def set_field(val):
-    if val == None:
+    if val is None:
         return 'None'
     else:
         return val.text
@@ -38,8 +41,8 @@ def set_field(val):
 
 # utility functions
 
-# uploads a dataset to the specified dataverse location, using the data specified in the file resourcemetadata.xml
-def upload_dataset(base_url, api_token, dv, temp_dir):
+
+def create_metadata_dict(temp_dir):
     # parse the xml metadata file as an etree
     with open(os.path.join(sys.path[0], "hs_dataverse",  "template.json"), "r") as read_file:
         data = json.load(read_file)
@@ -47,14 +50,15 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
     fields = data['datasetVersion']['metadataBlocks']['citation']['fields']
     geofields = data['datasetVersion']['metadataBlocks']['geospatial']['fields']
 
-    tree = ET.parse('hs_dataverse/tempfiles/resourcemetadata.xml')
+    meta_path = '/'.join([temp_dir, 'resourcemetadata.xml'])
+    tree = ET.parse(meta_path)
+    os.remove(meta_path)
     root = tree.getroot()
 
     # define all the relevant tags from the metadata.xml document for parsing
     title_tag = "{http://purl.org/dc/elements/1.1/}title"
 
     creator_tag = "{http://purl.org/dc/elements/1.1/}creator"
-    creator_description_tag = "{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description"
     name_tag = "{http://hydroshare.org/terms/}name"
     org_tag = '{http://hydroshare.org/terms/}organization'
     email_tag = '{http://hydroshare.org/terms/}email'
@@ -63,30 +67,22 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
 
     abstract_tag = '{http://purl.org/dc/terms/}abstract'
 
-    awardinfo_tag = '{http://hydroshare.org/terms/}awardInfo'
-    awardinfo_description_tag = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description'
-    funding_agency_name_tag = '{http://hydroshare.org/terms/}fundingAgencyName'
-    award_title_tag = '{http://hydroshare.org/terms/}awardTitle'
-    award_number_tag = '{http://hydroshare.org/terms/}awardNumber'
-
     period_tag = '{http://purl.org/dc/terms/}period'
     value_tag = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}value'
 
     date_tag = '{http://purl.org/dc/elements/1.1/}date'
     modified_tag = '{http://purl.org/dc/terms/}modified'
     created_tag = '{http://purl.org/dc/terms/}created'
-       
-    box_tag = '{http://purl.org/dc/terms/}box' 
-    spot_tag = '{http://purl.org/dc/terms/}point' 
 
-    source_tag = '{http://purl.org/dc/elements/1.1/}source'
+    box_tag = '{http://purl.org/dc/terms/}box'
+    spot_tag = '{http://purl.org/dc/terms/}point'
+
+    # source_tag = '{http://purl.org/dc/elements/1.1/}source'
     description_tag = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description'
-    derived_tag = '{http://hydroshare.org/terms/}isDerivedFrom'
 
     keyword_tag = '{http://purl.org/dc/elements/1.1/}subject'
 
     relation_tag = '{http://purl.org/dc/elements/1.1/}relation'
-    hosted_by_tag = '{http://hydroshare.org/terms/}isHostedBy'
 
     # Define the dicts to be filled in for dataverse fields that accept multpile values
     other_id_dict = {
@@ -130,7 +126,6 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
         }
     }
 
-    
     contact_dict = {
         "datasetContactName": {
           "typeName": "datasetContactName",
@@ -287,7 +282,6 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
         }
     }
 
-
     geo_coverage_dict = {
             "country": {
               "typeName": "country",
@@ -346,7 +340,7 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
     title = set_field(root.find(".//%s" % title_tag))
     abstract = set_field(root.find(".//%s" % abstract_tag))
 
-    alternative_url = set_field(root.find(".//%s" % hs_identifier_tag))
+    alt_url = set_field(root.find(".//%s" % hs_identifier_tag))
 
     author_vals = []
     for creator in root.findall(".//%s" % creator_tag):
@@ -359,8 +353,7 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
         author['authorAffiliation']['value'] = set_field(root.find(
             ".//%s[%s]/%s/%s" % (creator_tag, i + 1, description_tag, org_tag)))
         author['authorIdentifier']['value'] = find_url(set_field(root.find(
-            ".//%s[%s]/%s" % (creator_tag, i + 1, description_tag))), alternative_url)
-
+            ".//%s[%s]/%s" % (creator_tag, i + 1, description_tag))), alt_url)
 
         if i == 0:  # use the first author as the contact person for the dataset
             contact = contact_dict
@@ -389,7 +382,7 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
     if (deposit_date_text != 'None'):
         [deposit_date, deposit_time] = deposit_date_text.split('T')
     else:
-        depoist_date = ''
+        deposit_date = ''
 
     last_modified_date_text = set_field(root.find(".//%s/%s/%s" % (date_tag, modified_tag, value_tag)))
     if (last_modified_date_text != 'None'):
@@ -410,17 +403,16 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
         start_period_date = ''
         end_period_date = ''
 
-    related_publications = root.findall(".//%s/%s//*" % (relation_tag, description_tag))
-
     related_publications_vals = []
     related_resources = []
     for i, related_publication in enumerate(root.findall(".//%s/%s//*" % (relation_tag, description_tag))):
         related_publications_vals.append(copy.deepcopy(related_publications_dict))
         related_publications_vals[i]['publicationCitation']['value'] = set_field(related_publication)
-        related_publications_vals[i]['publicationURL']['value'] = find_url(set_field(related_publication), alternative_url)
+
+        print('string:', set_field(related_publication))
+        related_publications_vals[i]['publicationURL']['value'] = find_url(set_field(related_publication), alt_url)
 
         related_resources.append(related_publication.text)
-
 
     # use the google location services api to find the location
     maps_api_token = getattr(settings, 'MAPS_KEY', '')
@@ -429,30 +421,29 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
     geo_units = []
     bounding_box_vals = []
     geo_coverage_vals = []
- 
+    other_geo_info = ''
+
     old_geo_coverage_dict = geo_coverage_dict
 
     bounding_box_text = set_field(root.find('.//%s/%s' % (box_tag, value_tag)))
     if (bounding_box_text != 'None'):
         print('bounding_box_text:', bounding_box_text)
-        [name, northlimit, eastlimit, southlimit, westlimit, units, EPSG] = bounding_box_text.split(';')
+        [northlimit, eastlimit, southlimit, westlimit, units, projection] = bounding_box_text.split(';')
         northlimit = re.sub('northlimit=', '', northlimit)
         eastlimit = re.sub('eastlimit=', '', eastlimit)
         westlimit = re.sub('westlimit=', '', westlimit)
         southlimit = re.sub('southlimit=', '', southlimit)
-        other_geo_info = re.sub('name=', '', name)
         geo_units.append(re.sub('units=', '', units))
-        
+
         for box in root.findall(".//%s" % box_tag):
             bounding_box_vals.append(copy.deepcopy(bounding_box_dict))
-
 
         for i, box in enumerate(bounding_box_vals):
             box['westLongitude']['value'] = westlimit
             box['eastLongitude']['value'] = eastlimit
             box['northLongitude']['value'] = northlimit
             box['southLongitude']['value'] = southlimit
-         
+
     spot_text = set_field(root.find('.//%s/%s' % (spot_tag, value_tag)))
     if (spot_text != 'None'):
         print('spot_text:', spot_text)
@@ -463,6 +454,16 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
         geo_units.append(re.sub('units=', '', units))
         reverse_geo_code_result = gmaps.reverse_geocode((north, east))
 
+        for box in root.findall(".//%s" % spot_tag):
+            bounding_box_vals.append(copy.deepcopy(bounding_box_dict))
+
+        # for point coordinates, westlimit == eastlimit, and northlimit == southlimit
+        for i, box in enumerate(bounding_box_vals):
+            box['westLongitude']['value'] = east
+            box['eastLongitude']['value'] = east
+            box['northLongitude']['value'] = north
+            box['southLongitude']['value'] = north
+
         for comp in reverse_geo_code_result[0]['address_components']:
             if 'country' in comp['types']:
                 geo_coverage_dict['country']['value'] = comp['long_name']
@@ -470,22 +471,22 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
                 geo_coverage_dict['state']['value'] = comp['long_name']
             if 'locality' in comp['types']:
                 geo_coverage_dict['city']['value'] = comp['long_name']
-    geo_coverage_dict['otherGeographicCoverage']['value'] = other_geo_info    
+    geo_coverage_dict['otherGeographicCoverage']['value'] = other_geo_info
 
     if (geo_coverage_dict != old_geo_coverage_dict):
         geo_coverage_vals.append(geo_coverage_dict)
 
     # Extract more fields using the extended metadata from other_metadata.json
     e = dict()
-    with open('hs_dataverse/tempfiles/other_metadata.json') as f:
+    with open('/'.join([temp_dir, 'other_metadata.json'])) as f:
         e = json.load(f)
- 
+
     other_id_dict['otherIdValue']['value'] = str(e['rid'])
     if e['published']:
         other_id_dict['otherIdValue']['value'] = str(e['doi'])
     notes_text = e['extended_metadata_notes']
-   
-    grant_vals = [] 
+
+    grant_vals = []
     for i, grant in enumerate(e['award_numbers']):
         grant_info = copy.deepcopy(grant_number_dict)
         grant_info['grantNumberAgency']['value'] = e['funding_agency_names'][i]
@@ -500,30 +501,30 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
 
     # Extract more fields using the owner data from ownerdata.json
     o = dict()
-    with open('hs_dataverse/tempfiles/ownerdata.json') as f:
+    with open('/'.join([temp_dir, 'ownerdata.json'])) as f:
         o = json.load(f)
     contact = contact_dict
     if 'username' in o:
         producer_dict['producerAbbreviation']['value'] = o['username']
-    else: 
+    else:
         producer_dict['producerAbbreviation']['value'] = ''
 
     if 'first_name' in o and 'last_name' in o:
         producer_dict['producerName']['value'] = o['last_name'] + ', ' + o['first_name']
-        contact['datasetContactName']['value'] =  o['last_name'] + ', ' + o['first_name']
-        depositor =  o['last_name'] + ', ' + o['first_name']
-    else: 
+        contact['datasetContactName']['value'] = o['last_name'] + ', ' + o['first_name']
+        depositor = o['last_name'] + ', ' + o['first_name']
+    else:
         producer_dict['producerName']['value'] = ''
-        contact['datasetContactName']['value'] =  ''
-        depositor =  ''
+        contact['datasetContactName']['value'] = ''
+        depositor = ''
 
     if 'organization' in o:
         producer_dict['producerAffiliation']['value'] = o['organization']
         contact['datasetContactAffiliation']['value'] = o['organization']
-    else: 
+    else:
         producer_dict['producerAffiliation']['value'] = ''
         contact['datasetContactAffiliation']['value'] = ''
-    
+
     if 'email' in o:
         contact['datasetContactEmail']['value'] = o['email']
     else:
@@ -535,7 +536,7 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
     # update the json dict with the field values
     fields[0]['value'] = title
 
-    fields[1]['value'] = alternative_url
+    fields[1]['value'] = alt_url
 
     fields[2]['value'] = [other_id_dict]
 
@@ -581,9 +582,13 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
     geofields[0]['value'] = geo_coverage_vals
     geofields[1]['value'] = geo_units
     geofields[2]['value'] = bounding_box_vals
-    
-    metadata = data
 
+    return data
+
+
+# uploads a dataset to the specified dataverse location, using the data specified in the file resourcemetadata.xml
+def upload_dataset(base_url, api_token, dv, temp_dir):
+    metadata = create_metadata_dict(temp_dir)
     api = Api(base_url, api_token)
 
     query_str = '/dataverses/' + dv + '/contents'
@@ -604,21 +609,37 @@ def upload_dataset(base_url, api_token, dv, temp_dir):
 
     r = api.create_dataset(dv, json.dumps(metadata))
     print(r)
+    print(r.text)
 
     r_dict = json.loads(r.text)
     persistent_id = r_dict['data']['persistentId']
-   
-    #print('path:', temp_dir)
-    #print(os.listdir(temp_dir)) 
+
+    # before uploading all files in the temp directory, remove the datafiles
+    data_files = ['ownerdata.json', 'other_metadata.json']
+    for file in data_files:
+        file_path = '/'.join([temp_dir, file])
+        os.remove(file_path)
+
+    # upload all files in the temp directory (the bag of resource files)
     for file in os.listdir(temp_dir):
         file_path = '/'.join([temp_dir, file])
-        os.rename(file_path, file_path[:-8]) # remove last 8 characters generated by tempfile
-        file_path = file_path[:-8]
-        api.upload_file(persistent_id, file_path)   
- 
-    # now delete the dataset, as to not fill up the datverse while testing 
-    #sleep(5)
-    r2 = api.delete_dataset(persistent_id, is_pid=True, auth=True)
 
+        count = 0
+        done = False
+        while not done:
+            try:
+                api.upload_file(persistent_id, file_path)
+                done = True
+            except:
+                count += 1
+                if count >= 5:
+                    done = True
+                    print('Could not upload file: {}'.format(file))
+                else:
+                    time.sleep(7)
 
-
+    # now delete the dataset, as to not fill up the datverse while testing
+    time.sleep(3)
+    # r2 = api.delete_dataset(persistent_id, is_pid=True, auth=True)
+    # print(r2)
+    # print(r2.text)
