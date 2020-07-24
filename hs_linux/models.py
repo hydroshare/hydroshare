99e35ce4091fcae8d8ec4edaca8ed8b745469e1a
hydro service <hydro-service@dev-hs-2.cuahsi.org>
2020-07-24 09:15:11 -0400

    adding models

--- hs_linux/models.py
+++ hs_linux/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.db import models  
 
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 # Create your models here.
