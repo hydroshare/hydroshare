a3f5c6ed92f5f399b077dc2d1c4920ac025c2445
hydro service <hydro-service@dev-hs-2.cuahsi.org>
2020-07-24 04:52:14 -0400

    added the linux storage replacement for irods along with the tests

---
+++ hs_linux/storage.py
@@ -0,0 +1,137 @@
+from django.core.files.storage import  FileSystemStorage
+import os, tarfile, shutil, errno
+from pprint import pprint
+
+IRODS_PATH = "./irods/"
+class LinuxStorage(FileSystemStorage):
+        def prepend_path(path):
+                if(not os.path.exists(IRODS_PATH)):
+                        os.makedirs(IRODS_PATH)        
+                if( path[0] != "/" ):
+                        path = path[2:] if path[0:2] == "./" else path
+                        return IRODS_PATH + path
+                else:
+                        return path
+        
+        @property
+        def getUniqueTmpPath(self):
+                pass
+
+        def download(self, name):
+                irods_name = self.prepend_path(name)
+                return self.open(irods_name, mode='rb')
+
+        def runBagitRule(self, rule_name, input_path, input_resouce):
+                pass #irule
+
+        def zipup(self, input_name, output_name):
+                file_name = output_name.rsplit("/", 1)[1]
+                tar = tarfile.open(file_name + ".tar.gz", "w:gz")
+                tar.add(input_name, arcname=output_name)
+                tar.close()
+
+        def unzip(self, zip_file_path, unzipped_folder=None):
+                abs_path = os.path.dirname(zip_file_path)
+                if not unzipped_folder:
+                        unzipped_folder = os.path.splitext(os.path.basename(zip_file_path))[0].strip()
+
+                unzipped_folder = self._get_nonexistant_path(os.path.join(abs_path, unzipped_folder))
+                tar = tarfile.open(zip_file_path)
+                for member in tar.getmembers():
+                        tar.extract(member, path=unzipped_folder)
+                        tar.close()
+                return unzipped_folder
+
+        def _get_nonexistant_path(self, path):
+                if not os.path.exists(path):
+                        return path
+                i = 1
+                new_path = "{}-{}".format(path, i)
+                while os.path.exists(new_path):
+                        i += 1
+                        new_path = "{}-{}".format(path, i)
+                return new_path
+        '''
+                What is AVU doing?
+                setAVU -- stores the metadata associated with the file
+                getAVU -- There is a lookup table for each one and its deafault value; check that and return the default value
+                Alva: he will generate default AVUs for everything.
+        '''
+        def setAVU(self, name, attName, attVal, attUnit=None):
+                pass
+
+        def getAVU(self, name, attName):
+                return False
+
+
+
+
+##########################################################################################################################
+
+        def removeDirecotry(self, dirname):
+                directory = self.prepend_path(dirname)
+                if(os.path.isfile(directory)):
+                        os.remove(directory)
+                elif(os.path.isdir(directory)):
+                        shutil.rmtree(directory)
+
+        # copies files or directories recursively within irods
+        def copyFiles(self, src_name, dest_name, ires=None):
+                src_irods = self.prepend_path(src_name)
+                dest_irods = self.prepend_path(dest_name)
+                try:
+                        shutil.copytree(src_irods, dest_irods)
+                except FileExistsError as e: 
+                        # if a dest_name is already present, create a directory within dest_name
+                        dest_irods = dest_irods + "/" + src_irods.rsplit("/",1)[1]
+                        shutil.copytree(src_irods, dest_irods)
+                except NotADirectoryError as exc: 
+                        # if copying a file, use shutil.copy
+                        if(not os.path.exists(dest_irods.rsplit('/', 1)[0])):
+                                os.makedirs(dest_irods.rsplit('/', 1)[0]) 
+                        shutil.copy(src_irods, dest_irods)
+
+        # moves (copy and remove the source directory) files or directories recursively
+        def moveFile(self, src_name, dest_name):
+                self.copyFiles(LinuxStorage, src_name, dest_name)
+                self.removeDirecotry(LinuxStorage, src_name)
+        
+        # copies files or directories recursively from linux to irods
+        def saveFile(self, from_name, to_name, create_directory=False, data_type_str=''):
+                if create_directory == True:
+                        splitted_directory = to_name.rsplit("/",1)
+                        os.makedirs(splitted_directory[0])
+                        if (len(splitted_directory) <= 0):
+                                return
+                else:
+                        if from_name:
+                                dest_irods = self.prepend_path(to_name)
+                                try:
+                                        shutil.copytree(from_name, dest_irods)
+                                except FileExistsError as e: 
+                                        # if a dest_name is already present, create a directory within dest_name
+                                        dest_irods = dest_irods + "/" + from_name.rsplit("/",1)[1]
+                                        shutil.copytree(from_name, dest_irods)
+                                except NotADirectoryError as exc: 
+                                        # if copying a file, use shutil.copy
+                                        if(not os.path.exists(dest_irods.rsplit('/', 1)[0])):
+                                                os.makedirs(dest_irods.rsplit('/', 1)[0]) 
+                                        shutil.copy(from_name, dest_irods)
+
+        # copies files or directories recursively from irods to linux
+        def getFile(self, source_name, destination_name):
+                src_irods = self.prepend_path(source_name)
+                try:
+                        shutil.copytree(src_irods, destination_name)
+                except FileExistsError as e: 
+                        # if a dest_name is already present, create a directory within dest_name
+                        destination_name = destination_name + "/" + src_irods.rsplit("/",1)[1]
+                        shutil.copytree(src_irods, destination_name)
+                except NotADirectoryError as exc: 
+                        # if copying a file, use shutil.copy
+                        if(not os.path.exists(destination_name.rsplit('/', 1)[0])):
+                                os.makedirs(destination_name.rsplit('/', 1)[0]) 
+                        shutil.copy(src_irods, destination_name)
+
+        def ils_l(self, path): 
+                return os.listdir(path)
--- hs_linux/tests.py
+++ hs_linux/tests.py
@@ -1,3 +1,117 @@
 from django.test import TestCase
+from hs_linux.storage import LinuxStorage
+import os, shutil
 
-# Create your tests here.
+class LinuxStorageTest(TestCase):
+    def setUp(self):
+        if (not os.path.exists("./test")):
+            os.makedirs("./test/subtest1")
+            os.makedirs("./test/subtest2")
+            os.chdir("./test")
+            open("testfile.txt","w+")
+            os.chdir("./subtest1")
+            open("testfile11.txt", "w+")
+            open("testfile12.txt", "w+")
+            os.chdir("../subtest2")
+            open("testfile21.txt", "w+")
+            open("testfile22.txt", "w+")
+            os.chdir("../../")
+        
+    def tearDown(self):   
+        for root, dirs, files in os.walk("./test", topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.removedirs(os.path.join(root, name))
+
+    def test_savefile(self):
+        LinuxStorage.saveFile(LinuxStorage, "./test/subtest2/testfile21.txt", "test2")
+        self.assertTrue(os.path.exists("./irods/test2"))
+
+        LinuxStorage.saveFile(LinuxStorage, "./test/subtest1", "test1")
+        LinuxStorage.saveFile(LinuxStorage, "./test/subtest1", "test1")
+        self.assertTrue(os.path.exists("./irods/test1"))
+        self.assertTrue(os.path.exists("./irods/test1/subtest1"))
+        self.assertTrue(os.path.exists("./irods/test1/testfile11.txt"))
+        self.assertTrue(os.path.exists("./irods/test1/testfile12.txt"))
+        shutil.rmtree("./irods")
+
+    def test_movefile(self):
+        LinuxStorage.saveFile(LinuxStorage, "./test", "./")
+        print("Here!!!")
+        #file --> directory
+        LinuxStorage.moveFile(LinuxStorage, "test/testfile.txt", "test/subtest1")
+        self.assertTrue(os.path.exists("./irods/test/subtest1/testfile.txt"))
+        self.assertFalse(os.path.exists("./irods/test/testfile.txt"))
+        
+        #directory -> directory
+        LinuxStorage.moveFile(LinuxStorage, "test/subtest2", "test/subtest1")
+        self.assertTrue(os.path.exists("./irods/test/subtest1/subtest2/testfile21.txt"))
+        self.assertTrue(os.path.exists("./irods/test/subtest1/subtest2/testfile22.txt"))
+        self.assertFalse(os.path.exists("./irods/test/subtest2"))
+        
+        #file -> file
+        with open('./irods/test/subtest1/testfile11.txt') as f: testfile11_string = f.read()
+        LinuxStorage.moveFile(LinuxStorage, "test/subtest1/testfile11.txt", "test/subtest1/subtest2/testfile22.txt")
+        with open('./irods/test/subtest1/subtest2/testfile22.txt') as f: testfile22_string = f.read()
+        self.assertTrue(testfile11_string == testfile22_string)
+        self.assertFalse(os.path.exists("./irods/test/subtest1/testfile11.txt"))
+
+        shutil.rmtree("./irods")
+    
+    def test_getfile(self):
+        os.makedirs("./irods/manish/aryal")
+        os.makedirs("./irods/alva/couch")
+        os.makedirs("linuxtest")
+        open("./irods/manish/aryal/manishfile.txt","w+")
+        open("./irods/alva/couch/alvafile.txt","w+")
+
+        LinuxStorage.getFile(LinuxStorage, "manish", "linuxtest")
+        self.assertTrue(os.path.exists("./linuxtest/manish"))
+
+        LinuxStorage.getFile(LinuxStorage, "./alva/couch/alvafile.txt", "linuxtest")
+        self.assertTrue(os.path.exists("./linuxtest/alvafile.txt"))
+        shutil.rmtree("./irods")
+        shutil.rmtree("./linuxtest")
+    
+    def test_copyfile(self):
+        LinuxStorage.saveFile(LinuxStorage, "./test", "./")
+        print("Here!!!")
+        #file --> directory
+        LinuxStorage.copyFiles(LinuxStorage, "test/testfile.txt", "test/subtest1")
+        self.assertTrue(os.path.exists("./irods/test/subtest1/testfile.txt"))
+        self.assertTrue(os.path.exists("./irods/test/testfile.txt"))
+        
+        #directory -> directory
+        LinuxStorage.copyFiles(LinuxStorage, "test/subtest2", "test/subtest1")
+        self.assertTrue(os.path.exists("./irods/test/subtest1/subtest2/testfile21.txt"))
+        self.assertTrue(os.path.exists("./irods/test/subtest1/subtest2/testfile22.txt"))
+        self.assertTrue(os.path.exists("./irods/test/subtest2"))
+        
+        #file -> file
+        with open('./irods/test/subtest1/testfile11.txt') as f: testfile11_string = f.read()
+        LinuxStorage.copyFiles(LinuxStorage, "test/subtest1/testfile11.txt", "test/subtest1/subtest2/testfile22.txt")
+        with open('./irods/test/subtest1/subtest2/testfile22.txt') as f: testfile22_string = f.read()
+        self.assertTrue(testfile11_string == testfile22_string)
+        self.assertTrue(os.path.exists("./irods/test/subtest1/testfile11.txt"))
+
+        shutil.rmtree("./irods")
+
+
+'''
+storage class methods     | filesystem storage class
+open                      | base_location
+save                      | location
+get_valid_name            | base_url
+get_available_name ..     | file_permissions_mode
+generate_filename         | directory_permissions_mode
+path                      | get_storage_class
+delete ..
+exists ..
+lsitdir ..
+size ..
+url ..
+get_accessed_time
+get_created_time
+get_modified_time
+'''
