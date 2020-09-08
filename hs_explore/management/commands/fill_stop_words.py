da5882d9ddef7add3fbd3101528c9565628d3fdd
xuezhaokun <helloxzk@gmail.com>
2020-09-08 14:43:50 -0400

    only store stopwords now

--- hs_explore/management/commands/fill_stop_words.py
+++ hs_explore/management/commands/fill_stop_words.py
@@ -1,47 +1,9 @@
 """
-This stores ODM2 and CSDMS keywords used for our LDA model.
-Also, this stors English and customized common hydrological
+This storEs English and customized common hydrological
 stop words used in our LDA model
 """
-from hs_explore.models import LDAWord
+from hs_explore.models import LDAStopWord
 from django.core.management.base import BaseCommand
-from hs_odm2.models import ODM2Variable
-from hs_csdms.models import CSDMSName
-
-
-def fill_csdms_names():
-    """ Store CSDMS names into LDAWord model
-    """
-    csdms_names = list(CSDMSName.list_all_names())
-    splitted_names = set()
-    for csdms_name in csdms_names:
-        if len(csdms_name) <= 1:
-            continue
-        tokens = csdms_name.split(" ")
-        LDAWord.add_word('CSDMS', 'keep', 'name', csdms_name)
-        splitted_names.update(tokens)
-
-    for splitted_name in splitted_names:
-        if len(splitted_name) <= 1:
-            continue
-        LDAWord.add_word('CSDMS', 'keep', 'name', splitted_name)
-
-
-def fill_odm2():
-    """ Store ODM2 names into LDAWord model
-    """
-    odm2_list = list(ODM2Variable.all())
-    modified_list = []
-    for odm2 in odm2_list:
-        tokens = odm2.split(' - ')
-        new_string = tokens[0]
-        if len(tokens) > 1:
-            new_string = tokens[1] + ' ' + tokens[0]
-        modified_list.append(new_string.lower())
-
-    for odm2_word in modified_list:
-        if len(odm2_word) > 1:
-            LDAWord.add_word('ODM2', 'keep', 'name', odm2_word)
 
 
 def fill_stop_words():
@@ -83,18 +45,15 @@ def fill_stop_words():
                      'against', 'hasn', 'you', 'most']
 
     for stop_word in customized_stops:
-        LDAWord.add_word('Customized', 'stop', 'name', stop_word)
+        LDDStopWord.add_word('Customized', 'name', stop_word)
     for stop_word in english_stops:
-        LDAWord.add_word('English', 'stop', 'name', stop_word)
+        LDAStopWord.add_word('English', 'name', stop_word)
 
 
 class Command(BaseCommand):
-    help = "Filling keep words and stop words used in the LDA algorithm"
+    help = "Filling stop words used in the LDA algorithm"
 
     def handle(self, *args, **options):
-        LDAWord.clear()
-        print("filling in keep words")
-        fill_csdms_names()
-        fill_odm2()
+        LDAStopWord.clear()
         print("filling in stop words")
         fill_stop_words()
