05f2e914e4a35d09ea35aa471a1c4d5fda440a16
Mark OBrien <mark_noreply@noreply.com>
2020-10-30 14:59:51 -0400

    WIP

--- hs_core/urls.py
+++ hs_core/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
         views.change_quota_holder, name='change_quota_holder'),
     url(r'^_internal/(?P<shortkey>[0-9a-f-]+)/(?P<element_name>[A-z]+)/add-metadata/$',
         views.add_metadata_element, name='add_metadata_element'),
+    url(r'^_internal/(?P<shortkey>[0-9a-f-]+)/(?P<element_name>[A-z]+)/add-citation/$',
+        views.add_citation, name='add_citation'),
     url(r'^_internal/(?P<shortkey>[0-9a-f-]+)/(?P<element_name>[A-z]+)/(?P<element_id>[A-z0-9]+)/update-metadata/$',
         views.update_metadata_element, name='update_metadata_element'),
     url(r'^_internal/(?P<shortkey>[0-9a-f-]+)/(?P<element_name>[A-z]+)/(?P<element_id>[A-z0-9]+)/delete-metadata/$',
