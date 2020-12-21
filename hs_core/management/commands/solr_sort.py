a2c0253eab3803f39320741f9e47041b22af1cd4
Alva Couch <acouch@cuahsi.org>
2020-12-21 11:15:52 -0500

    fix flake8

--- hs_core/management/commands/solr_sort.py
+++ hs_core/management/commands/solr_sort.py
@@ -1,18 +1,14 @@
 """
-This tests sorting of SOLR resources. 
+This tests sorting of SOLR resources.
 """
 
 from django.core.management.base import BaseCommand
-from django.db.models import Q
-from hs_core.models import BaseResource
-from hs_core.search_indexes import BaseResourceIndex
-from pprint import pprint
 from haystack.query import SearchQuerySet
 
 
 def debug_harvest():
     sqs = SearchQuerySet().all().order_by('author_lower')
-    for s in sqs: 
+    for s in sqs:
         print("author: " + s.author)
         print("author_lower: " + s.author_lower)
 
