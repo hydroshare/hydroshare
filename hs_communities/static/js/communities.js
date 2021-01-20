7a4cf5a0d9e034279aa55b0d6819c7c5ad027e9b
Mark OBrien <mark_noreply@noreply.com>
2021-01-20 15:53:40 -0500

    Apply vue to resource list

--- hs_communities/static/js/communities.js
+++ hs_communities/static/js/communities.js
@@ -1,42 +1,131 @@
-$(document).ready(function () {
+$(document)
+  .ready(function () {
     let CommunitiesVm = new Vue({
-        el: "#communities-app",
-        data: {
-            filterTo: [],
-            groupIds: [],
-            groups: ['group1', 'group2']
+      el: '#resource-rows',
+      data: {
+        filterTo: [],
+        groupIds: [],
+        groups: ['group1', 'group2'],
+        resources: [],
+        sharedByFilter: '',
+        pnum: 1,
+        sortingBy: 'modified',
+        sortDir: -1,
+        rescount: 0,
+        pagenum: 1, // initial page number to show
+        pagedisp: 1, // page being displayed
+        perpage: 0,
+        pagecount: 0,
+        columns: ['name', 'author', 'created', 'modified'],
+        labels: ['Title', 'First Author', 'Date Created', 'Last Modified'],
+        resIconName: {
+          'Composite Resource': '/static/img/resource-icons/composite48x48.png',
+          Generic: '/static/img/resource-icons/generic48x48.png',
+          'Geopgraphic Raster': '/static/img/resource-icons/geographicraster48x48.png',
+          'Model Program Resource': '/static/img/resource-icons/modelprogram48x48.png',
+          'Collection Resource': '/static/img/resource-icons/collection48x48.png',
+          'Web App Resource': '/static/img/resource-icons/webapp48x48.png',
+          'Time Series': '/static/img/resource-icons/timeseries48x48.png',
+          'Script Resource': '/static/img/resource-icons/script48x48.png',
+          'Model Instance Resource': '/static/img/resource-icons/modelinstance48x48.png',
+          'SWAT Model Instance Resource': '/static/img/resource-icons/swat48x48.png',
+          'MODFLOW Model Instance Resource': '/static/img/resource-icons/modflow48x48.png',
+          'Multidimensional (NetCDF)': '/static/img/resource-icons/multidimensional48x48.png',
+          'HIS Referenced Time Series': '/static/img/resource-icons/his48x48.png',
         },
-        mounted: function () {
-            let groupIds = {};
-
-            $('#groups-list li').each(function(){
-                let groupId = parseInt($(this).attr('id'));
-                groupIds[$(this).text()] = groupId;
-            });
-            this.$data.groupIds = groupIds;
-
-            let filterGroup = $('#filter-querystring').text();
-            if (filterGroup && this.$data.groupIds[filterGroup]) {
-                this.$data.filterTo.push(this.$data.groupIds[filterGroup])
-            }
+        sortMap: {
+          'First Author': 'author',
+          Owner: 'owner',
+          Creator: 'contributor',
+          Title: 'title',
+          Type: 'type',
+          Subject: 'subject',
+          'Date Created': 'created',
+          'Last Modified': 'modified',
         },
-        methods: {
-            isVisible(groupId) {
-                if (this.$data.filterTo.length === 0) {  // If no selections show all
-                    return true;
-                } else {  // Display row if Group ID found in the filterTo Array
-                    return this.$data.filterTo.indexOf(groupId) > -1;
-                }
-            },
-            updateContribs(groupId) {
-                let loc = this.$data.filterTo.indexOf(groupId);
 
-                if (loc < 0) {
-                    this.$data.filterTo.push(groupId)
-                } else {
-                    this.$data.filterTo.splice(loc, 1);
-                }
-            }
+      },
+      mounted: function () {
+        this.searchClick();
+        let groupIds = {};
+
+        $('#groups-list li')
+          .each(function () {
+            let groupId = parseInt($(this)
+              .attr('id'));
+            groupIds[$(this)
+              .text()] = groupId;
+          });
+        this.$data.groupIds = groupIds;
+
+        let filterGroup = $('#filter-querystring')
+          .text();
+        if (filterGroup && this.$data.groupIds[filterGroup]) {
+          this.$data.filterTo.push(this.$data.groupIds[filterGroup]);
         }
+      },
+      methods: {
+        isVisible(groupId) {
+          if (this.$data.filterTo.length === 0) {  // If no selections show all
+            return true;
+          } else {  // Display row if Group ID found in the filterTo Array
+            return this.$data.filterTo.indexOf(groupId) > -1;
+          }
+        },
+        updateContribs(groupId) {
+          let loc = this.$data.filterTo.indexOf(groupId);
+
+          if (loc < 0) {
+            this.$data.filterTo.push(groupId);
+          } else {
+            this.$data.filterTo.splice(loc, 1);
+          }
+        },
+        ellip(input, size) {
+          if (input && size) {
+            return input.length > size ? `${input.substring(0, size)}...` : input;
+          }
+          return '';
+        },
+        nameList(names) {
+          try {
+            return names.join(' | ');
+          } catch {
+            return names;
+          }
+        },
+        sortStyling(key) {
+          if (this.sortMap[key] === this.sortingBy) {
+            return this.sortDir === 1 ? 'fa fa-fw fa-sort-asc interactive' : 'fa fa-fw fa-sort-desc interactive';
+          }
+          return this.sortMap[key] === 'type' ? '' : 'fa fa-fw fa-sort interactive';
+        },
+        searchClick(paging, dofilters, reset) { // paging flag to skip the page reset after data retrieval
+          document.body.style.cursor = 'wait';
+          axios.get('/discoverapi/', {})
+            .then((response) => {
+              if (response) {
+
+                try {
+                  this.resources = JSON.parse(response.data.resources);
+                  console.log(this.resources)
+                  if (paging !== true) {
+                    this.pagenum = 1;
+                  }
+                  this.pagecount = response.data.pagecount;
+                  this.rescount = response.data.rescount;
+                  this.perpage = response.data.perpage;
+
+                  document.body.style.cursor = 'default';
+                } catch (e) {
+                  document.body.style.cursor = 'default';
+                }
+              }
+            })
+            .catch((error) => { // eslint-disable-line
+              document.body.style.cursor = 'default';
+            });
+        },
+      }
     });
-});
+  });
