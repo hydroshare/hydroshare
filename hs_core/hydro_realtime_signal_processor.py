5fded256f87feaa4647e117b5deccec38fcf3367
Alva Couch <acouch@cuahsi.org>
2021-03-22 18:19:40 -0400

    Initial version

--- hs_core/hydro_realtime_signal_processor.py
+++ hs_core/hydro_realtime_signal_processor.py
@@ -13,7 +13,6 @@ class HydroRealtimeSignalProcessor(RealtimeSignalProcessor):
 
     """ 
     Customized for the fact that all indexed resources are subclasses of BaseResource. 
-
     Notes: 
     1. RealtimeSignalProcessor already plumbs in all class updates. We might want to be more specific. 
     2. The class sent to this is a subclass of BaseResource, or another class. 
@@ -27,47 +26,70 @@ class HydroRealtimeSignalProcessor(RealtimeSignalProcessor):
         """
         from hs_core.models import BaseResource
         from hs_access_control.models import ResourceAccess
-    
+
         if isinstance(instance, BaseResource):
-            if hasattr(instance, 'raccess') and hasattr(instance, 'metadata'): 
+            if hasattr(instance, 'raccess') and hasattr(instance, 'metadata'):
                 # work around for failure of super(BaseResource, instance) to work properly.
-                # this always succeeds because this is a post-save object action. 
-                newinstance = BaseResource.objects.get(pk=instance.pk)
+                # this always succeeds because this is a post-save object action.
+                newbase = BaseResource.objects.get(pk=instance.pk)
                 newsender = BaseResource
-                using_backends = self.connection_router.for_write(instance=newinstance)
+                using_backends = self.connection_router.for_write(instance=newbase)
                 for using in using_backends:
-                    # if object is public/discoverable or becoming public/discoverable, index it 
-                    # test whether the object should be exposed. 
-                    if instance.show_in_discover: 
+                    # if object is public/discoverable or becoming public/discoverable, index it
+                    # test whether the object should be exposed.
+                    if instance.show_in_discover:
                         try:
                             index = self.connections[using].get_unified_index().get_index(newsender)
-                            index.update_object(newinstance, using=using)
+                            index.update_object(newbase, using=using)
                         except NotHandled:
-                            logger.exception("Failure: changes to %s with short_id %s not added to Solr Index.", 
-                                             str(type(instance)), newinstance.short_id)
+                            logger.exception("Failure: changes to %s with short_id %s not added to Solr Index.",
+                                             str(type(instance)), newbase.short_id)
 
 
                     # if object is private or becoming private, delete from index
                     else:  # not to be shown in discover
                         try:
                             index = self.connections[using].get_unified_index().get_index(newsender)
-                            index.remove_object(newinstance, using=using)
+                            index.remove_object(newbase, using=using)
                         except NotHandled:
-                            logger.exception("Failure: delete of %s with short_id %s failed.", 
-                                             str(type(instance)), newinstance.short_id)
+                            logger.exception("Failure: delete of %s with short_id %s failed.",
+                                             str(type(instance)), newbase.short_id)
 
         elif isinstance(instance, ResourceAccess):
-            # automatically a BaseResource; just call the routine on it. 
-            newinstance = instance.resource
-            newsender = BaseResource
-            self.handle_save(newsender, newinstance)
+            # automatically a BaseResource; just call the routine on it.
+            try:
+                newbase = instance.resource
+                self.handle_save(BaseResource, newbase)
+            except Exception as e:
+                logger.error("ResourceAccess id={} exception: {}".format(instance.id, e.message))
 
+        elif isinstance(instance, CoreMetaData):
+            try:
+                newbase = instance.resource
+                self.handle_save(BaseResource, newbase)
+            except Exception:
+                logger.error("CoreMetaData id={} exception: {}".format(instance.id, e.message))
+
+        elif isinstance(instance, AbstractMetaDataElement):
+            try:
+                # resolve the BaseResource corresponding to the metadata element.
+                # fields used here are the union of all fields in the metadata element
+                # some of these are deprecated, but in the interest of time,
+                # I included them now and will delete later.
+                newbase = instance.metadata.resource
+                self.handle_save(BaseResource, newbase)
+            except Exception as e:
+                print("{} {} exception: {}".format(type(instance), instance.id, e.message))
+
+        else:  # could be extended metadata element
+            try:
+                newbase = BaseResource.objects.get(extra_metadata=instance)
+                self.handle_save(BaseResource, newbase)
+            except Exception as e:
+                print("{} {} exception: {}".format(type(instance), instance.id, e.message))
 
     def handle_delete(self, sender, instance, **kwargs):
         """
-        Given an individual model instance, determine which backends the
-        delete should be sent to & delete the object on those backends.
-        This never worked well. Just set the resource to not discoverable 
-        before deleting it. This will delete it from SOLR properly. 
+        Ignore delete events as this is accomplished separately. 
         """
         pass
