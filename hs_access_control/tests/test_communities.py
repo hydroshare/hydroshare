ddf71ed58d43e59a07daca317824ca3534ad1c8e
Alva Couch <acouch@cuahsi.org>
2021-09-20 10:23:44 -0400

    Fix flake8

--- hs_access_control/tests/test_communities.py
+++ hs_access_control/tests/test_communities.py
@@ -428,7 +428,7 @@ class TestCommunities(MockIRODSTestCaseMixin, TestCase):
         self.assertTrue(is_equal_to_as_set(self.bat2.uaccess.view_groups,
                                            [self.bats]))
 
-        # all groups are public --> can view all of them. 
+        # all groups are public --> can view all of them.
         self.assertTrue(self.dog2.uaccess.can_view_group(self.dogs))
         self.assertTrue(self.dog2.uaccess.can_view_group(self.cats))
         self.assertTrue(self.dog2.uaccess.can_view_group(self.bats))
