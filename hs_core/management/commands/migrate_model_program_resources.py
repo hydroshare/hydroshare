7c5c1acd3d48892be2953ceb25f434673cef02f0
pkdash <pkdash_reena@hotmail.com>
2021-10-08 17:52:28 -0400

    [#3071] initial implementation of model program resources migration

---
+++ hs_core/management/commands/migrate_model_program_resources.py
@@ -0,0 +1,242 @@
+import logging
+import os
+
+from django.core.management.base import BaseCommand
+
+from hs_core.hydroshare import current_site_url, set_dirty_bag_flag
+from hs_core.models import CoreMetaData, ResourceFile
+from hs_file_types.models import ModelProgramLogicalFile, ModelProgramResourceFileType
+from hs_model_program.models import ModelProgramResource
+from ..utils import migrate_core_meta_elements
+
+
+class Command(BaseCommand):
+    help = "Convert all model program resources to composite resource"
+
+    # def add_arguments(self, parser):
+    #
+    #     # Number of resources to create
+    #     parser.add_argument('resource_id', type=str, required=False, help='ID of the resource to migrate')
+
+    def create_aggr_folder(self, mp_aggr, comp_res):
+        new_folder = "mp"
+        ResourceFile.create_folder(comp_res, new_folder)
+        mp_aggr.folder = new_folder
+        mp_aggr.dataset_name = new_folder
+        mp_aggr.save()
+        # move files to the new folder
+        istorage = comp_res.get_irods_storage()
+
+        for res_file in comp_res.files.all():
+            if res_file != comp_res.readme_file:
+                src_full_path = os.path.join(comp_res.file_path, res_file.file_name)
+                tgt_full_path = os.path.join(comp_res.file_path, new_folder, res_file.file_name)
+                istorage.moveFile(src_full_path, tgt_full_path)
+                res_file.set_storage_path(tgt_full_path)
+
+        print(">> Created aggregation folder:{} and moved files to the new folder\n".format(new_folder))
+
+    def create_mp_file_type(self, file_name, file_type, mp_aggr):
+        for aggr_file in mp_aggr.files.all():
+            if aggr_file.file_name == file_name:
+                if not ModelProgramResourceFileType.objects.filter(
+                        file_type=file_type,
+                        res_file=aggr_file,
+                        mp_metadata=mp_aggr.metadata
+                ).exists():
+                    ModelProgramResourceFileType.objects.create(file_type=file_type,
+                                                                res_file=aggr_file,
+                                                                mp_metadata=mp_aggr.metadata)
+                break
+
+    def handle(self, *args, **options):
+        # res_id = options['resource_id']
+        logger = logging.getLogger(__name__)
+        resource_counter = 0
+        to_resource_type = 'CompositeResource'
+        msg = "THERE ARE CURRENTLY {} MODEL PROGRAM RESOURCES PRIOR TO CONVERSION.".format(
+            ModelProgramResource.objects.count())
+        logger.info(msg)
+        self.stdout.write(self.style.SUCCESS(msg))
+
+        for mp_res in ModelProgramResource.objects.all():
+            # check resource exists on irods
+            istorage = mp_res.get_irods_storage()
+            if not istorage.exists(mp_res.root_path):
+                err_msg = "Model program resource not found in irods (ID: {})".format(mp_res.short_id)
+                logger.error(err_msg)
+                self.stdout.write(self.style.ERROR(err_msg))
+                # skip this mp resource
+                continue
+
+            # check resource files exist on irods
+            file_missing = False
+            for res_file in mp_res.files.all():
+                file_path = res_file.public_path
+                if not istorage.exists(file_path):
+                    err_msg = "File path not found in irods:{}".format(file_path)
+                    logger.error(err_msg)
+                    err_msg = "Failed to convert model program resource (ID: {}). " \
+                              "Resource file is missing on irods".format(mp_res.short_id)
+                    self.stdout.write(self.style.ERROR(err_msg))
+                    file_missing = True
+                    break
+            if file_missing:
+                # skip this corrupt raster resource for migration
+                continue
+
+            # change the resource_type
+            mp_metadata_obj = mp_res.metadata
+            mp_res.resource_type = to_resource_type
+            mp_res.content_model = to_resource_type.lower()
+            mp_res.save()
+            # get the converted resource object - CompositeResource
+            comp_res = mp_res.get_content_model()
+
+            # set CoreMetaData object for the composite resource
+            core_meta_obj = CoreMetaData.objects.create()
+            comp_res.content_object = core_meta_obj
+            # migrate mp resource core metadata elements to composite resource
+            migrate_core_meta_elements(mp_metadata_obj, comp_res)
+
+            # update url attribute of the metadata 'type' element
+            type_element = comp_res.metadata.type
+            type_element.url = '{0}/terms/{1}'.format(current_site_url(), to_resource_type)
+            type_element.save()
+
+            # create a mp aggregation
+            try:
+                mp_aggr = ModelProgramLogicalFile.create(resource=comp_res)
+                mp_aggr.save()
+            except Exception as ex:
+                err_msg = 'Failed to create model program aggregation for resource (ID: {})'
+                err_msg = err_msg.format(mp_res.short_id)
+                err_msg = err_msg + '\n' + str(ex)
+                logger.error(err_msg)
+                self.stdout.write(self.style.ERROR(err_msg))
+                continue
+
+            if comp_res.files.count() == 0:
+                self.create_aggr_folder(mp_aggr=mp_aggr, comp_res=comp_res)
+            elif comp_res.readme_file is not None:
+                if comp_res.files.count() > 2:
+                    self.create_aggr_folder(mp_aggr=mp_aggr, comp_res=comp_res)
+                # make the all res files part of the aggregation excluding the readme file
+                for res_file in comp_res.files.all():
+                    if res_file != comp_res.readme_file:
+                        print(">> Adding file {} to mp aggregation\n".format(res_file.file_name))
+                        mp_aggr.add_resource_file(res_file)
+            else:
+                if comp_res.files.count() > 1:
+                    self.create_aggr_folder(mp_aggr=mp_aggr, comp_res=comp_res)
+                # make the all res files part of the aggregation
+                for res_file in comp_res.files.all():
+                    print(">> Adding file {} to mp aggregation\n".format(res_file.file_name))
+                    mp_aggr.add_resource_file(res_file)
+
+            if mp_aggr.files.count() > 0:
+                print(">> 1. Aggregation contains files.")
+
+            # set the dataset_name field of the aggregation in the case of file based aggregation
+            if not mp_aggr.folder:
+                print(">> Setting aggregation title based on aggregation filename\n")
+                if mp_aggr.files.count() > 0:
+                    print(">> 2. Aggregation contains files.")
+                aggr_file = mp_aggr.files.first()
+                aggr_filename, _ = os.path.splitext(aggr_file.file_name)
+                print(">>> aggregation dataset name:{}".format(aggr_filename))
+                mp_aggr.dataset_name = aggr_filename
+                mp_aggr.save()
+                if mp_aggr.files.count() > 0:
+                    print(">> 3. Aggregation contains files.")
+                if mp_aggr.files.first() is not None:
+                    print(">> 4. Aggregation contains files.")
+
+            # copy the resource level keywords to aggregation level
+            if comp_res.metadata.subjects:
+                keywords = [sub.value for sub in comp_res.metadata.subjects.all()]
+                mp_aggr.metadata.keywords = keywords
+                mp_aggr.metadata.save()
+
+            if mp_metadata_obj.program:
+                if mp_aggr.files.count() > 0:
+                    # create mp program file types
+                    file_type = ModelProgramResourceFileType.ENGINE
+                    for file_name in mp_metadata_obj.program.get_engine_list():
+                        self.create_mp_file_type(file_name=file_name, file_type=file_type, mp_aggr=mp_aggr)
+
+                    file_type = ModelProgramResourceFileType.SOFTWARE
+                    for file_name in mp_metadata_obj.program.get_software_list():
+                        self.create_mp_file_type(file_name=file_name, file_type=file_type, mp_aggr=mp_aggr)
+
+                    file_type = ModelProgramResourceFileType.DOCUMENTATION
+                    for file_name in mp_metadata_obj.program.get_documentation_list():
+                        self.create_mp_file_type(file_name=file_name, file_type=file_type, mp_aggr=mp_aggr)
+
+                    file_type = ModelProgramResourceFileType.RELEASE_NOTES
+                    for file_name in mp_metadata_obj.program.get_releasenotes_list():
+                        self.create_mp_file_type(file_name=file_name, file_type=file_type, mp_aggr=mp_aggr)
+
+                if mp_metadata_obj.program.modelReleaseDate:
+                    mp_aggr.metadata.release_date = mp_metadata_obj.program.modelReleaseDate
+                if mp_metadata_obj.program.modelVersion:
+                    mp_aggr.metadata.version = mp_metadata_obj.program.modelVersion
+                if mp_metadata_obj.program.modelWebsite:
+                    mp_aggr.metadata.website = mp_metadata_obj.program.modelWebsite
+                if mp_metadata_obj.program.modelCodeRepository:
+                    mp_aggr.metadata.code_repository = mp_metadata_obj.program.modelCodeRepository
+                if mp_metadata_obj.program.modelProgramLanguage:
+                    languages = mp_metadata_obj.program.modelProgramLanguage.split(',')
+                    mp_aggr.metadata.programming_languages = languages
+                if mp_metadata_obj.program.modelOperatingSystem:
+                    op_systems = mp_metadata_obj.program.modelOperatingSystem.split(',')
+                    mp_aggr.metadata.operating_systems = op_systems
+
+                mp_aggr.save()
+
+            # create aggregation level xml files
+            if mp_aggr.files.count() > 0:
+                print(">> 5. Aggregation contains files.")
+
+            mp_aggr.create_aggregation_xml_documents()
+            msg = 'One Model program aggregation was created in resource (ID: {})'
+            msg = msg.format(comp_res.short_id)
+            logger.info(msg)
+            self.stdout.write(self.style.SUCCESS(msg))
+
+            # set resource to dirty so that resource level xml files (resource map and
+            # metadata xml files) will be re-generated as part of next bag download
+            comp_res.save()
+            try:
+                set_dirty_bag_flag(comp_res)
+            except Exception as ex:
+                err_msg = 'Failed to set bag flag dirty for the converted resource (ID: {})'
+                err_msg = err_msg.format(comp_res.short_id)
+                err_msg = err_msg + '\n' + str(ex)
+                logger.error(err_msg)
+                self.stdout.write(self.style.ERROR(err_msg))
+
+            resource_counter += 1
+            # delete the instance of model program metadata that was part of the original raster resource
+            mp_metadata_obj.delete()
+            msg = 'Model program resource (ID: {}) was converted to Composite Resource type'
+            msg = msg.format(comp_res.short_id)
+            logger.info(msg)
+            self.stdout.write(self.style.SUCCESS(msg))
+            if resource_counter > 0:
+                break
+
+        if resource_counter > 0:
+            msg = "{} MODEL PROGRAM RESOURCES WERE CONVERTED TO COMPOSITE RESOURCE.".format(
+                resource_counter)
+            logger.info(msg)
+            self.stdout.write(self.style.SUCCESS(msg))
+
+        if ModelProgramResource.objects.all().count() > 0:
+            msg = "NOT ALL MODEL PROGRAM RESOURCES WERE CONVERTED TO COMPOSITE RESOURCE TYPE"
+            logger.error(msg)
+            self.stdout.write(self.style.WARNNG(msg))
+            msg = "THERE ARE CURRENTLY {} MODEL PROGRAM RESOURCES AFTER CONVERSION.".format(
+                ModelProgramResource.objects.all().count())
+            logger.info(msg)
+            self.stdout.write(self.style.WARNNG(msg))
