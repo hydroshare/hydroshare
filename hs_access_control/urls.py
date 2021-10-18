4dc6bc1375035522fb1c3855462f7b1d89cecb56
Alva Couch <acouch@cuahsi.org>
2021-10-18 16:07:19 -0400

    Basic approval permissions.

--- hs_access_control/urls.py
+++ hs_access_control/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 from hs_access_control import views
 
 urlpatterns = [
+    # tests
     url(r'^test/community/approval/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/$',
         views.TestCommunityApproval.as_view()),
+
+    url(r'^test/community/invite/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/$',
+        views.TestCommunityInvite.as_view()),
 
     url(r'^test/group/approval/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/$',
         views.TestGroupApproval.as_view()),
@@ -11,6 +15,7 @@ urlpatterns = [
     url(r'^test/debug/$', 
         views.DebugView.as_view()),
 
+    # responders
     url(r'^_internal/group/respond/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/$',
         views.GroupApprovalView.as_view()),
     url(r'^_internal/group/respond/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/(?P<action>[a-z_ ]*)/(?P<cid>[0-9]+)/$',
@@ -20,5 +25,10 @@ urlpatterns = [
         views.CommunityApprovalView.as_view()),
     url(r'^_internal/community/respond/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/(?P<action>[a-z_ ]*)/(?P<gid>[0-9]+)/$',
         views.CommunityApprovalView.as_view()),
+
+    url(r'^_internal/community/invite/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/$',
+        views.CommunityInviteView.as_view()),
+    url(r'^_internal/community/invite/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/(?P<action>[a-z_ ]*)/(?P<gid>[0-9]+)/$',
+        views.CommunityInviteView.as_view()),
 
 ]
--- hs_access_control/views.py
+++ hs_access_control/views.py
@@ -10,92 +10,227 @@ logger = logging.getLogger(__name__)
 class GroupApprovalView(TemplateView):
     template_name = 'hs_access_control/group_approval.html'
 
+    def hydroshare_permissions(request, uid, gid, cid=None):
+        try:
+            user = User.objects.get(id=uid)
+            ## when in production:
+            # user = request.user
+            # if not user.is_authenticated: 
+            #     message = "You must be logged in to access this function." 
+            #     return message
+            group = Group.objects.get(id=gid)
+            if user.uaccess.owns_group(group):
+                if cid is None:
+                    return ""
+                else:
+                    community = Community.objects.get(id=cid)
+                    gcr = GroupCommunityRequest.objects.filter(group=group, community=community, redeemed=False)
+                    if gcr.count() < 1:
+                        message = "no request for user {} ({}), group {} ({}), and community {} ({})"\
+                                  .format(user.username, user.id, group.name, group.id, community.name, community.id)
+                        logger.error(message)
+                        return message
+                    else:
+                        return ""
+
+            else:
+                message = "user {} ({}) does not own group {} ({})"\
+                          .format(user.username, user.id, group.name, group.id)
+                logger.error(message)
+                return message
+        except Exception as e:
+            message = "invalid user id {} or group id {}".format(uid, gid)
+            logger.error("{}: exception {}".format(message, e))
+            return message
+
     def get_context_data(request, uid, gid, *args, **kwargs):
         context = {}
         message = ''
-        logger.debug("got to get_context_data: uid={} gid={}".format(uid, gid))
-        if 'action' in kwargs:
-            if kwargs['action'] == 'approve':
-                gcr = GroupCommunityRequest.objects.get(
-                    group__id=int(gid),
-                    community__id=int(kwargs['cid']))
-                message, worked = gcr.approve(responder=User.objects.get(id=int(uid)))
-            elif kwargs['action'] == 'decline':
-                gcr = GroupCommunityRequest.objects.get(
-                    group__id=int(gid),
-                    community__id=int(kwargs['cid']))
-                message, worked = gcr.decline(responder=User.objects.get(id=int(uid)))
-            else:
-                message = "improper action '{}'".format(kwargs['action'])
-                logger.error(message)
-        context['message'] = message
-        context['group'] = Group.objects.get(id=int(gid))
-        context['user'] = User.objects.get(id=int(uid))
-        context['uid'] = uid
-        context['gid'] = gid
-        context['gcr'] = GroupCommunityRequest.objects.filter(
-            group__id=int(gid), 
-            group__gaccess__active=True, 
-            group_owner__isnull=True)
-        return context
+        if 'cid' in kwargs:
+            cid = kwargs['cid']
+        else:
+            cid = None
+        status = request.hydroshare_permissions(uid, gid, cid=cid)
+        if status == "":
+            if 'action' in kwargs:
+                if kwargs['action'] == 'approve':
+                    gcr = GroupCommunityRequest.objects.get(
+                        group__id=int(gid),
+                        community__id=int(kwargs['cid']))
+                    message, worked = gcr.approve(responder=User.objects.get(id=int(uid)))
+                elif kwargs['action'] == 'decline':
+                    gcr = GroupCommunityRequest.objects.get(
+                        group__id=int(gid),
+                        community__id=int(kwargs['cid']))
+                    message, worked = gcr.decline(responder=User.objects.get(id=int(uid)))
+                else:
+                    message = "improper action '{}'".format(kwargs['action'])
+                    status = 'improper action'
+                    logger.error(message)
+            context['status'] = status  # empty string = ok
+            context['message'] = message
+            context['group'] = Group.objects.get(id=int(gid))
+            context['user'] = User.objects.get(id=int(uid))
+            context['uid'] = uid
+            context['gid'] = gid
+            context['gcr'] = GroupCommunityRequest.objects.filter(
+                group__id=int(gid),
+                group__gaccess__active=True,
+                group_owner__isnull=True)
+            return context
+        else:  # non-empty status means an error.
+            context['status'] = status
+            return context
+
+
+class GroupRequestView(TemplateView):
+    template_name = 'hs_access_control/group_request.html'
 
 
 class CommunityApprovalView(TemplateView):
     template_name = 'hs_access_control/community_approval.html'
 
+    def hydroshare_permissions(request, uid, cid, gid=None):
+        try:
+            user = User.objects.get(id=uid)  # becomes request.user
+            ## when in production:
+            # user = request.user
+            # if not user.is_authenticated: 
+            #     message = "You must be logged in to access this function." 
+            #     return message
+            community = Community.objects.get(id=gid)
+            if user.uaccess.owns_community(community):
+                if gid is None:
+                    return ""
+                else:
+                    group = Group.objects.get(id=gid)
+                    gcr = GroupCommunityRequest.objects.filter(group=group, community=community, redeemed=False)
+                    if gcr.count() < 1:
+                        message = "no request for user {} ({}), group {} ({}), and community {} ({})"\
+                                  .format(user.username, user.id, group.name, group.id, community.name, community.id)
+                        logger.error(message)
+                        return message
+                    else:
+                        return ""
+
+            else:
+                message = "user {} ({}) does not own group {} ({})"\
+                          .format(user.username, user.id, group.name, group.id)
+                logger.error(message)
+                return message
+        except Exception as e:
+            message = "invalid user id {} or group id {}".format(uid, gid)
+            logger.error("{}: exception {}".format(message, e))
+            return message
+
     def get_context_data(request, uid, cid, *args, **kwargs):
         message = ''
         context = {}
-        if 'action' in kwargs:
-            if kwargs['action'] == 'approve':
-                gcr = GroupCommunityRequest.objects.get(
-                    community__id=int(cid),
-                    group__id=int(kwargs['gid'])
-                )
-                message, worked = gcr.approve(responder=User.objects.get(id=int(uid)))
-            elif kwargs['action'] == 'decline':
-                gcr = GroupCommunityRequest.objects.get(
-                    community__id=int(cid),
-                    group__id=int(kwargs['gid']))
-                message, worked = gcr.decline(responder=User.objects.get(id=int(uid)))
-            else:
-                message = "improper action '{}'".format(kwargs['action'])
-                logger.error(message)
-        context['message'] = message
-        context['community'] = Community.objects.get(id=int(cid))
-        context['gcr'] = GroupCommunityRequest.objects.filter(
-            community=Community.objects.get(id=int(cid)),
-            group__gaccess__active=True, 
-            community_owner__isnull=True,
-            redeemed=False)
-        return context
+        if 'gid' in kwargs:
+            gid = kwargs['gid']
+        else:
+            gid = None
+        status = request.hydroshare_permissions(uid, gid, cid)
+        if status == "":
+            if 'action' in kwargs:
+                if kwargs['action'] == 'approve':
+                    gcr = GroupCommunityRequest.objects.get(
+                        community__id=int(cid),
+                        group__id=int(kwargs['gid'])
+                    )
+                    message, worked = gcr.approve(responder=User.objects.get(id=int(uid)))
+                elif kwargs['action'] == 'decline':
+                    gcr = GroupCommunityRequest.objects.get(
+                        community__id=int(cid),
+                        group__id=int(kwargs['gid']))
+                    message, worked = gcr.decline(responder=User.objects.get(id=int(uid)))
+                else:
+                    message = "improper action '{}'".format(kwargs['action'])
+                    logger.error(message)
+            context['status'] = status
+            context['message'] = message
+            context['community'] = Community.objects.get(id=int(cid))
+            context['gcr'] = GroupCommunityRequest.objects.filter(
+                community=Community.objects.get(id=int(cid)),
+                group__gaccess__active=True,
+                community_owner__isnull=True,
+                redeemed=False)
+            return context
+        else:  # non-empty status means an error.
+            context['status'] = status
+            return context
 
 
-class CommunityInviteView(TemplateView): 
+class CommunityInviteView(TemplateView):
     template_name = 'hs_access_control/community_invite.html'
-    
+
+    def hydroshare_permissions(request, uid, cid, gid=None):
+        try:
+            user = User.objects.get(id=uid)  
+            ## when in production:
+            # user = request.user
+            # if not user.is_authenticated: 
+            #     message = "You must be logged in to access this function." 
+            #     return message
+                
+            community = Community.objects.get(id=cid)
+            if user.uaccess.owns_community(community):
+                return ""
+            else:
+                message = "user {} ({}) does not own community {} ({})"\
+                          .format(user.username, user.id, community.name, community.id)
+                logger.error(message)
+                return message
+        except Exception as e:
+            message = "invalid user id {} or community id {}".format(uid, cid)
+            logger.error("{}: exception {}".format(message, e))
+            return message
+
     def get_context_data(request, uid, cid, *args, **kwargs):
         message = ''
         context = {}
-        if 'action' in kwargs:
-            if kwargs['action'] == 'invite':
-                message, worked = GroupCommunityRequest.create_or_update(
-                    group=Group.objects.get(id=int(kwargs['gid'])), 
-                    community=Community.objects.get(id=int(cid)),
-                    requester=User.objects.get(id=int(uid)))
-            else:
-                message = "improper action '{}'".format(kwargs['action'])
-                logger.error(message)
-        context['message'] = message
-        context['community'] = Community.objects.get(id=int(cid))
-        context['groups'] = Group.objects.filter(gaccess__active=True)\
-            .exclude(g2gcp__community=context['community'])\
-            .exclude(g2gcr__community=context['community']) 
-        return context
+        if 'gid' in kwargs:
+            gid = kwargs['gid']
+        else:
+            gid = None
+        status = request.hydroshare_permissions(uid, cid, gid=gid)
+        if status == "":
+            if 'action' in kwargs:
+                if kwargs['action'] == 'invite':
+                    message, worked = GroupCommunityRequest.create_or_update(
+                        group=Group.objects.get(id=int(kwargs['gid'])),
+                        community=Community.objects.get(id=int(cid)),
+                        requester=User.objects.get(id=int(uid)))
+                else:
+                    message = "improper action '{}'".format(kwargs['action'])
+                    logger.error(message)
+
+            context['status'] = status
+            context['message'] = message
+            context['uid'] = uid
+            context['cid'] = cid
+            context['community'] = Community.objects.get(id=int(cid))
+            context['groups'] = Group.objects.filter(gaccess__active=True)\
+                .exclude(invite_g2gcr__community=context['community'])\
+                .exclude(g2gcp__community=context['community'])
+            return context
+        else:
+            context['status'] = status
+            return context
 
 
 class TestCommunityApproval(TemplateView):
     template_name = 'hs_access_control/community_approval_test.html'
+
+    def get_context_data(request, uid, cid, *args, **kwargs):
+        context = {}
+        context['uid'] = uid
+        context['cid'] = cid
+        return context
+
+
+class TestCommunityInvite(TemplateView):
+    template_name = 'hs_access_control/community_invite_test.html'
 
     def get_context_data(request, uid, cid, *args, **kwargs):
         context = {}
