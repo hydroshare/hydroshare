bb1ddaeb86e6cf9d371a76204997f0da28b448f7
Alva Couch <acouch@cuahsi.org>
2021-10-18 10:15:28 -0400

    First version of containerized, self-updating subviews.

--- hs_access_control/urls.py
+++ hs_access_control/urls.py
@@ -2,13 +2,23 @@ from django.conf.urls import url
 from hs_access_control import views
 
 urlpatterns = [
-    url(r'^test/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/$', views.TestView.as_view()),
+    url(r'^test/community/approval/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/$',
+        views.TestCommunityApproval.as_view()),
 
-    url(r'^_internal/group/respond/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/$', views.GroupView.as_view()),
-    url(r'^_internal/group/respond/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/(?P<action>[a-z_ ]*)/(?P<nuid>[0-9]+)/$', views.GroupView.as_view()),
+    url(r'^test/group/approval/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/$',
+        views.TestGroupApproval.as_view()),
 
-    url(r'^_internal/community/respond/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/$', views.CommunityView.as_view()),
-    url(r'^_internal/community/respond/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/(?P<action>[a-z_ ]*)/(?P<gid>[0-9]+)/$', views.CommunityView.as_view()),
+    url(r'^test/debug/$', 
+        views.DebugView.as_view()),
 
-    url(r'^_internal/debug/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/', views.DebugView.as_view()), 
+    url(r'^_internal/group/respond/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/$',
+        views.GroupApprovalView.as_view()),
+    url(r'^_internal/group/respond/(?P<uid>[0-9]+)/(?P<gid>[0-9]+)/(?P<action>[a-z_ ]*)/(?P<cid>[0-9]+)/$',
+        views.GroupApprovalView.as_view()),
+
+    url(r'^_internal/community/respond/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/$',
+        views.CommunityApprovalView.as_view()),
+    url(r'^_internal/community/respond/(?P<uid>[0-9]+)/(?P<cid>[0-9]+)/(?P<action>[a-z_ ]*)/(?P<gid>[0-9]+)/$',
+        views.CommunityApprovalView.as_view()),
+
 ]
