8b6ca1e087de97fa93e7f595e5111be728c88ad9
Devin Cowan <dcowan@cuahsi.org>
2022-02-28 20:30:32 -0500

    also hide context menu from non file R-click

--- theme/static/js/hs_file_browser.js
+++ theme/static/js/hs_file_browser.js
@@ -935,12 +935,16 @@ function bindFileBrowserItemEvents() {
 
         $(".selection-menu").hide();    // Hide other menus
 
-        // register a listener for clicks outside of the context menu
+        // register a listener for clicks outside of the context menus
         $(document).on('click.menu', function(event) {
             var $target = $(event.target);
             if(!$target.closest('#right-click-menu').length && 
                 $('#right-click-menu').css("display") !== "none") {
                 $('#right-click-menu').hide();
+            }
+            if(!$target.closest('#right-click-container-menu').length && 
+                $('#right-click-container-menu').css("display") !== "none") {
+                $('#right-click-container-menu').hide();
             }
             // de-register listener to allow bubbling
             $(document).off('click.menu');
