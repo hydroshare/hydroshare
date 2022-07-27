45bc810a4ef8f2ccb7f07e579d0ef27412c23d30
Devin Cowan <dcowan@cuahsi.org>
2022-07-27 17:14:15 -0400

    handle res metadata corruption with a check
    Revert "[#4695] account for res with corrupted metadata"
    This partially reverts commit 9e63200475a72d05beb15ea8d255b910b1f995bd.

--- hs_core/management/commands/reorder_authors.py
+++ hs_core/management/commands/reorder_authors.py
@@ -8,7 +8,6 @@ Related to https://github.com/hydroshare/hydroshare/issues/4695
 
 from django.core.management.base import BaseCommand
 from hs_core.models import BaseResource
-from hs_core.models import Creator
 from hs_core.hydroshare.utils import set_dirty_bag_flag
 
 
@@ -18,15 +17,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         resources = BaseResource.objects.filter(raccess__published=False).only('object_id', 'short_id')
         for res in resources:
-            creators = Creator.objects.filter(object_id=res.object_id)
-            is_dirty = False
-            for index, creator in enumerate(creators, start=1):
-                if creator.order != index:
-                    print("*" * 100)
-                    print(f"Author out of order.\nR:{res.short_id}"
-                            f"\nExpected: {index}, got: {creator.order}")
-                    creator.order = index
-                    creator.save()
-                    is_dirty = True
-            if is_dirty:
-                set_dirty_bag_flag(res)
+            if res.metadata is not None:
+                creators = res.metadata.creators.all()
+                is_dirty = False
+                for index, creator in enumerate(creators, start=1):
+                    if creator.order != index:
+                        print("*" * 100)
+                        print(f"Author out of order.\nR:{res.short_id}"
+                                f"\nExpected: {index}, got: {creator.order}")
+                        creator.order = index
+                        creator.save()
+                        is_dirty = True
+                if is_dirty:
+                    set_dirty_bag_flag(res)
