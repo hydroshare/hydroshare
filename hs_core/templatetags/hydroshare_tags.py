ae3592a0ea6a33b2ba4b123646abadc8813c5fce
pkdash <pkdash_reena@hotmail.com>
2022-08-11 17:21:15 -0400

    [#4652] updating templates to remove reference to model program resource

--- hs_core/templatetags/hydroshare_tags.py
+++ hs_core/templatetags/hydroshare_tags.py
@@ -15,7 +15,6 @@ register = template.Library()
 
 RES_TYPE_TO_DISPLAY_TYPE_MAPPINGS = {"CompositeResource": "Composite Resource",
                                      "CollectionResource": "Collection Resource",
-                                     "ModelProgramResource": "Model Program Resource",
                                      "ToolResource": "Web App Resource"
                                      }
 
