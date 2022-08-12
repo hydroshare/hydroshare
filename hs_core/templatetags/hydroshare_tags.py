d2b8b1f3fdb64720a27f623e7a935463b74a7cb7
Devin Cowan <dcowan@cuahsi.org>
2022-08-12 14:47:32 -0400

    [#2368] check for creator None

--- hs_core/templatetags/hydroshare_tags.py
+++ hs_core/templatetags/hydroshare_tags.py
@@ -102,12 +102,13 @@ def resource_first_author(content):
         if creator.order == 1:
             first_creator = creator
             break
-    if first_creator.name and first_creator.relative_uri:
-        return format_html('<a href="{desc}">{name}</a>',
-                           desc=first_creator.relative_uri,
-                           name=first_creator.name)
-    elif first_creator.name:
-        return format_html('<span>{name}</span>', name=first_creator.name)
+    if first_creator:
+        if first_creator.name and first_creator.relative_uri:
+            return format_html('<a href="{desc}">{name}</a>',
+                            desc=first_creator.relative_uri,
+                            name=first_creator.name)
+        elif first_creator.name:
+            return format_html('<span>{name}</span>', name=first_creator.name)
     else:
         first_creator = content.metadata.creators.filter(order=1).first()
         if first_creator.name:
