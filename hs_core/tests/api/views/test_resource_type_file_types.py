785d1294838dadd8d8cd5a838b754eec4cff2126
Devin Cowan <dcowan@cuahsi.org>
2022-08-24 19:56:32 -0400

    Revert "refresh with develop"
    This reverts commit d4fa970fb872fc2d3b26497724c92cf331071422, reversing
    changes made to 8fe32f1fedd93c8ef1d0d1e09e79dd5fcc86d547.

--- hs_core/tests/api/views/test_resource_type_file_types.py
+++ hs_core/tests/api/views/test_resource_type_file_types.py
@@ -48,14 +48,6 @@ class TestResourceTypeFileTypes(TestCase):
         resp_json = self._make_request("ModelProgramResource")
         self.assertEqual(resp_json['file_types'], '".*"')
 
-        # test for ModelInstanceResource
-        resp_json = self._make_request("ModelInstanceResource")
-        self.assertEqual(resp_json['file_types'], '".*"')
-
-        # test for SWATModelInstanceResource
-        resp_json = self._make_request("SWATModelInstanceResource")
-        self.assertEqual(resp_json['file_types'], '".*"')
-
         # test for ToolResource
         resp_json = self._make_request("ToolResource")
         self.assertEqual(resp_json['file_types'], '[]')
