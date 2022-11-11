15d9da1c27f9d615ef30604dfe052ab86159cb1b
pkdash <pkdash_reena@hotmail.com>
2022-11-11 17:59:40 -0500

    [#2538] showing resource files/folders that don't meet the preferred naming convention

--- hs_core/page_processors.py
+++ hs_core/page_processors.py
@@ -118,6 +118,7 @@ def get_page_context(page, user, resource_edit=False, extended_metadata_layout=N
     topics = list(topics)  # force QuerySet evaluation
     content_model.update_relation_meta()
     creators = content_model.metadata.creators.all()
+    content_model.non_preferred_path_names = content_model.get_non_preferred_path_names()
 
     # user requested the resource in READONLY mode
     if not resource_edit:
