7f0d17dac24e571b461865ba6aca12d7d1cf1515
Alva Couch <acouch@cuahsi.org>
2022-11-28 07:20:19 -0500

    First version in which uppy runs.

--- uppy/server.js
+++ uppy/server.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
 const session = require('express-session'); 
-const companion = require('.'); 
+const companion = require('@uppy/companion'); 
 
 const app = express();
 
@@ -10,61 +10,68 @@ app.use(session({
   secret: 'some-fubarred-secret',
   resave: true,
   saveUninitialized: true,
-}))
+}));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*')
   next()
-})
+});
 
 // Routes
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain')
   res.send('Welcome to Companion')
-})
+});
+
+console.log("before options"); 
 
 // initialize uppy
-const uppyOptions = {
-  providerOptions: {
-    drive: {
-      key: '876543925663-7v2tf3dg8p9nugtjtcm37ks8ckub8cf4.apps.googleusercontent.com',
-      secret: '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',
+var options = {
+  'providerOptions': {
+    'drive': {
+      'key': '876543925663-7v2tf3dg8p9nugtjtcm37ks8ckub8cf4.apps.googleusercontent.com',
+      'secret': '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',
     },
-    dropbox: {
-      key: '07xw8rzny3rgkno',
-      secret: '7an6hhingzaqgif',
+    'dropbox': {
+      'key': '07xw8rzny3rgkno',
+      'secret': '7an6hhingzaqgif',
     },
-    box: {
-      key: 'rv3q3tz9cq7995uaguahjvc9m524uhso',
-      secret: 'PupcsbgEtWblcIk9TiELudGpxXB3zbvD',
+    'box': {
+      'key': 'rv3q3tz9cq7995uaguahjvc9m524uhso',
+      'secret': 'PupcsbgEtWblcIk9TiELudGpxXB3zbvD',
     },
     // you can also add options for additional providers here
   },
-  server: {
-    host: 'cuahsi-dev-1.hydroshare.org',
-    protocol: 'https',
-    implicitPath: '/companion', 
+  'server': {
+    'host': 'cuahsi-dev-1.hydroshare.org',
+    'protocol': 'https',
+    'implicitPath': '/companion', 
   },
-  filePath: '/uppy_tmp',
+  'filePath': '/uppy_tmp',
   // sendSelfEndpoint: 'cuahsi-dev-1.hydroshare.org/companion', 
-  secret: 'some-random-secret',
-  debug: true,
-  metrics: true, 
-}
+  'secret': 'some-random-secret',
+  'debug': true,
+  'metrics': true, 
+  'uploadUrls': ['https://cuahsi-dev-1.hydroshare.org'], 
+};
 
-app.use(companion.app(uppyOptions))
+console.log("after options"); 
+console.log(options);
+
+const { app: companionApp } = companion.app(options);
+app.use('/companion', companionApp);
 
 // handle 404
-app.use((req, res) => {
+app.use((err, req, res) => {
   return res.status(404).json({ message: 'Not Found' })
-})
+});
 
 // handle server errors
 app.use((err, req, res) => {
   console.error('\x1b[31m', err.stack, '\x1b[0m')
   res.status(err.status || 500).json({ message: err.message, error: err })
-})
+});
 
-companion.socket(app.listen(3020))
-console.log('Welcome to Companion!')
-console.log(`Listening on http://0.0.0.0:${3020}`)
+companion.socket(app.listen(3020));
+console.log('Welcome to Companion!');
+console.log(`Listening on http://0.0.0.0:${3020}`);
