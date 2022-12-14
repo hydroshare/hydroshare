8923c503e3d6289346afdd03db47c507164b3804
Devin Cowan <dcowan@cuahsi.org>
2022-12-14 14:08:16 -0500

    [#4897] dont recommend readme on Tools Resource

--- hs_core/models.py
+++ hs_core/models.py
@@ -4323,7 +4323,7 @@ class CoreMetaData(models.Model, RDF_MetaData_Mixin):
         missing_recommended_elements = []
         if not self.funding_agencies.count():
             missing_recommended_elements.append('Funding Agency')
-        if not self.resource.readme_file:
+        if not self.resource.readme_file and "CompositeResource" in self.resource.resource_type:
             missing_recommended_elements.append('Readme file containing variables, '
                                                 'abbreviations/acronyms, and non-standard file formats')
         if not self.coverages.count():
