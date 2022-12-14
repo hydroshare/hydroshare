c8c5f229b3ac2ab6372880a48bc35fb411ff185e
Devin Cowan <dcowan@cuahsi.org>
2022-12-14 11:01:07 -0500

    [#4868] pytest config modifications

--- hs_core/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Initialize hs_core module with HSCoreAppConfig."""
-default_app_config = 'hs_core.apps.HSCoreAppConfig'
--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -103,7 +103,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # https://docs.djangoproject.com/en/3.2/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
