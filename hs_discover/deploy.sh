878fa9203c239787e6b7423f96054c0c17a224d6
Devin Cowan <dcowan@cuahsi.org>
2023-03-01 10:56:20 -0500

    [#4846] cp static js during discover build

--- hs_discover/deploy.sh
+++ hs_discover/deploy.sh
@@ -5,6 +5,6 @@ rm static/js/*.map
 mkdir -p static/css
 mkdir -p static/js
 cp templates/hs_discover/css/* static/css/
-cp templates/hs_discover/js/* static/js/*
+cp templates/hs_discover/js/* static/js/
 cp templates/hs_discover/map.js static/js/map.js
 docker exec -it hydroshare python manage.py collectstatic --no-input
