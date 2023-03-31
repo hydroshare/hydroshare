46fb16300485be1ed9436c7cf7eec841d9422664
Alva Couch <acouch@cuahsi.org>
2023-03-31 16:27:15 -0400

    Add ability to measure the extent of an access change

--- hs_access_control/models/shortcut.py
+++ hs_access_control/models/shortcut.py
@@ -1,12 +1,18 @@
 from django.db.models import Q
 
-from hs_access_control.models import ResourceAccess
-from hs_access_control.models.privilege import PrivilegeCodes, UserResourcePrivilege, \
-    GroupResourcePrivilege
+from django.contrib.auth.models import User
+from hs_access_control.models.privilege import PrivilegeCodes, PrivilegeBase, \
+    UserResourcePrivilege, GroupResourcePrivilege
+from hs_access_control.models.exceptions import PolymorphismError
+from hs_core.models import BaseResource
+import hs_access_control.signals 
+from django.dispatch import receiver
+import logging
 
+logger = logging.getLogger(__name__)
 
 #############################################
-# Shortcut queries for data access
+# Shortcut query for data access
 # These queries shortcut around the typical assumptions of access control by
 # taking in keys rather than objects and shortcutting around the whole process
 # of resolving keys to objects, as used in the rest of access control.
@@ -23,6 +29,7 @@ def get_user_resource_privilege(email, short_id):
     # It returns the min of the privileges:
     # - if an email corresponds to more than one user account
     # - if a GUID somehow refers to more than one resource.
+    from hs_access_control.models import ResourceAccess
 
     # public access
     privilege = list(ResourceAccess.objects.filter(
@@ -48,3 +55,46 @@ def get_user_resource_privilege(email, short_id):
         return min(privilege)  # min of a list
     else:
         return PrivilegeCodes.NONE
+
+
+#############################################
+# Zone of effect queries for the resources affected
+# by a change in user access, group access, etc.
+#############################################
+# 1. Adding a user to a group: affects that user only, all resources
+#    that the group can see.
+# 2. Adding a user to a resource access list: affects that user/resource pair only.
+# 3. Adding or removing a resource from a group: affects that resource
+#    and all users in that group.
+# 4. Making a resource public or private: affects potentially all users.
+#    in other words, there's no situation in which a change affects
+# both multiple users and multiple resources.
+# this sends signal access_changed.
+
+def zone_of_influence(**kwargs):
+    for k in kwargs: 
+        print("{}: {}".format(k, kwargs[k]))
+    if len(kwargs) > 2:
+        raise PolymorphismError("Too many arguments")
+    if 'resource' in kwargs:
+        if 'user' in kwargs:
+            users = list([kwargs['user'].username])
+            resources = list([kwargs['resource'].short_id])
+        elif 'group' in kwargs:
+            users = list(User.objects
+                .filter(u2ugp__group=kwargs['group'])
+                .values_list('username', flat=True))
+            resources = list([kwargs['resource'].short_id])
+    elif 'user' in kwargs and 'group' in kwargs:
+        users = list([kwargs['user'].username])
+        resources = list(BaseResource.objects
+            .filter(r2grp__group=kwargs['group'])
+            .values_list('short_id', flat=True))
+    else:
+        raise PolymorphismError("Too few arguments")
+    hs_access_control.signals.access_changed.send(sender=PrivilegeBase, users=users, resources=resources)
+
+
+@receiver(hs_access_control.signals.access_changed, sender=PrivilegeBase)
+def access_changed(sender, **kwargs):
+    print("access_changed: users: {} resources: {}".format(kwargs['users'], kwargs['resources']))
