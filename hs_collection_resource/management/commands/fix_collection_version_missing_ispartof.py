cf7b831c552646d858bdeb4275238280b3c5fb2f
Devin Cowan <dcowan@cuahsi.org>
2023-05-03 13:22:30 -0400

    [#5057] add --dryrun arg for mng command

--- hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
+++ hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
@@ -6,11 +6,17 @@ from hs_core.models import BaseResource
 class Command(BaseCommand):
     help = "Add missing isPartOf relation to resources in a collection"
 
-    # Get the hasPart relations for all collections
-    # For each relation, get the resource that is part
-    # Check that the resource has a reverse relation (that it isPartOf the collection)
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--dryrun',
+            action='store_true',
+            dest='dryrun',
+            default=False,
+            help='Show what options would be edited without committing changes',
+        )
 
     def handle(self, *args, **options):
+        dry_run = options['dryrun']
         collections = BaseResource.objects.filter(resource_type="CollectionResource").all()
         for collection in collections:
             print(f"Checking collection: {collection.short_id}")
@@ -18,8 +24,12 @@ class Command(BaseCommand):
                 rel_values = [rel.value for rel in res.metadata.relations.filter(type=RelationTypes.isPartOf).all()]
                 print(f"Relations: {rel_values}")
                 if collection.get_citation() not in rel_values:
-                    print("IsPartOf relation missing from resource {res}. Creating it...")
-                    res.metadata.create_element('relation',
-                                                type=RelationTypes.isPartOf,
-                                                value=collection.get_citation())
+                    print("IsPartOf relation missing from resource {res}.")
+                    if dry_run:
+                        print("SKIPPING creating IsPartOf relation because dry_run")
+                    else:
+                        print("Creating IsPartOf relation...")
+                        res.metadata.create_element('relation',
+                                                    type=RelationTypes.isPartOf,
+                                                    value=collection.get_citation())
         print("DONE")
