ba915bb3fe0df8cf73bb4e6cdb4de8972cab259c
Devin Cowan <dcowan@cuahsi.org>
2023-05-04 10:49:32 -0400

    [#5059] improve management command output
    (cherry picked from commit a096f3ed89dc430d6bd0d221a3f04644472d23e9)

--- hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
+++ hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from hs_core.enums import RelationTypes
 from hs_core.models import BaseResource
+from hs_core.hydroshare import current_site_url
 
 
 class Command(BaseCommand):
@@ -17,14 +18,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         dry_run = options['dryrun']
+        current_site = current_site_url()
         collections = BaseResource.objects.filter(resource_type="CollectionResource").all()
         for collection in collections:
-            print(f"Checking collection: {collection.short_id}")
+            print(f"Checking collection: {collection} {current_site}/resource/{collection.short_id}")
             for res in collection.resources.all():
                 rel_values = [rel.value for rel in res.metadata.relations.filter(type=RelationTypes.isPartOf).all()]
                 print(f"Relations: {rel_values}")
                 if collection.get_citation() not in rel_values:
-                    print("IsPartOf relation missing from resource {res}.")
+                    print(f"IsPartOf relation missing from {res}, {current_site}/resource/{res.short_id}")
                     if dry_run:
                         print("SKIPPING creating IsPartOf relation because dry_run")
                     else:
@@ -32,4 +34,27 @@ class Command(BaseCommand):
                         res.metadata.create_element('relation',
                                                     type=RelationTypes.isPartOf,
                                                     value=collection.get_citation())
+        print('-' * 100)
+        print("check that all collections include hasPart metadata for contained resources")
+        for res in BaseResource.object.all():
+            print(f"Checking resource {res}, {current_site}/resource/{res.short_id}")
+            rels = res.metadata.relations.filter(type=RelationTypes.isPartOf).all()
+            if not rels:
+                print("Skipping resource, no isPartOf relations")
+                continue
+            for rel in rels:
+                col = rel.metadata.resource
+                haspart_relations = col.metadata.relations.filter(type=RelationTypes.hasPart).all()
+                citation = res.get_citation()
+                if citation not in haspart_relations:
+                    print(f"{res}, {current_site}/resource/{res.short_id} has isPart meta.")
+                    print(f"Collection {col}, {current_site}/resource/{col.short_id} is missing hasPart.")
+                    if dry_run:
+                        print("SKIPPING creating hasPart relation because dry_run")
+                    else:
+                        print("Creating hasPart relation...")
+                        col.metadata.create_element('relation',
+                                                    type=RelationTypes.hasPart,
+                                                    value=citation)
+
         print("DONE")
