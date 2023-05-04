f3ccc3235cf4116a45ba091731ad121250a81ff7
Devin Cowan <dcowan@cuahsi.org>
2023-05-04 11:56:49 -0400

    only print on issue
    (cherry picked from commit 9ac35332da45d3d1a9eb5dfeff3969a51a5d5dd6)

--- hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
+++ hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
@@ -28,10 +28,10 @@ class Command(BaseCommand):
         fixed = 0
         for collection in collections:
             n = n + 1
-            print(f"{n}/{col_count}: Checking collection: {collection} {current_site}/resource/{collection.short_id}")
             for res in collection.resources.all():
                 rel_values = [rel.value for rel in res.metadata.relations.filter(type=RelationTypes.isPartOf).all()]
                 if collection.get_citation() not in rel_values:
+                    print(f"{n}/{col_count}: Collection={collection} {current_site}/resource/{collection.short_id}")
                     print(f"IsPartOf relation missing from {res}, {current_site}/resource/{res.short_id}")
                     if dry_run:
                         print("SKIPPING creating IsPartOf relation because dry_run")
@@ -44,7 +44,7 @@ class Command(BaseCommand):
         print(f"{fixed} isPart relations were missing")
 
         print('-' * 100)
-        print("check that all collections include hasPart metadata for contained resources")
+        print("2. Check that all collections include hasPart metadata for contained resources")
         resources = BaseResource.objects.all()
         res_count = resources.count()
         print(f"{res_count} resources to check")
@@ -52,19 +52,17 @@ class Command(BaseCommand):
         fixed = 0
         for res in resources:
             i = i + 1
-            print(f"{i}/{res_count}: Checking resource {res}, {current_site}/resource/{res.short_id}")
             rels = None
             if res.metadata:
                 rels = res.metadata.relations.filter(type=RelationTypes.isPartOf).all()
             if not rels:
-                print("Skipping resource, no isPartOf relations")
                 continue
             for rel in rels:
                 col = rel.metadata.resource
                 haspart_relations = col.metadata.relations.filter(type=RelationTypes.hasPart).all()
                 citation = res.get_citation()
                 if citation not in haspart_relations:
-                    print(f"{res}, {current_site}/resource/{res.short_id} has isPart meta.")
+                    print(f"{i}/{res_count}:{res}, {current_site}/resource/{res.short_id} has isPart meta.")
                     print(f"Collection {col}, {current_site}/resource/{col.short_id} is missing hasPart.")
                     if dry_run:
                         print("SKIPPING creating hasPart relation because dry_run")
