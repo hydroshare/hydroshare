28ed17bb976aa711d1304b336783cb5d3437f048
Devin Cowan <dcowan@cuahsi.org>
2023-05-04 10:35:46 -0400

    [#5059] management command check hasPart

--- hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
+++ hs_collection_resource/management/commands/fix_collection_version_missing_ispartof.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from hs_core.enums import RelationTypes
-from hs_core.models import BaseResource
+from hs_core.models import BaseResource, Relation
 
 
 class Command(BaseCommand):
@@ -18,6 +18,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dry_run = options['dryrun']
         collections = BaseResource.objects.filter(resource_type="CollectionResource").all()
+        print('-' * 100)
+        print("1. Check that all resources in collections have isPartOf metadata")
         for collection in collections:
             print(f"Checking collection: {collection.short_id}")
             for res in collection.resources.all():
@@ -32,4 +34,35 @@ class Command(BaseCommand):
                         res.metadata.create_element('relation',
                                                     type=RelationTypes.isPartOf,
                                                     value=collection.get_citation())
+        print('-' * 100)
+        print("check that all collections include hasPart metadata for contained resources")
+        # for rel in Relation.objects.filter(type=RelationTypes.isPartOf).all():
+        #     col = rel.metadata.resource
+        #     print(f"Checking collection {col}, https://beta.hydroshare.org/resource/{col.short_id}")
+        #     print("This collection is pointed to by a res with isPartOf metadata. Checking that col contains hasPart metadata")
+        #     haspart_relations = col.metadata.relations.filter(type=RelationTypes.hasPart).all()
+        #     print(haspart_relations)
+        #     # Need a way to check that hasPart relations contains the resources with isPart metadata
+
+        for res in BaseResource.object.all():
+            print(f"Checking resource {res}, https://beta.hydroshare.org/resource/{res.short_id}")
+            rels = res.metadata.relations.filter(type=RelationTypes.isPartOf).all()
+            if not rels:
+                print("Skipping resource, no isPartOf relations")
+                continue
+            for rel in rels:
+                col = rel.metadata.resource
+                haspart_relations = col.metadata.relations.filter(type=RelationTypes.hasPart).all()
+                citation = res.get_citation()
+                if citation not in haspart_relations:
+                    print(f"{res}, https://beta.hydroshare.org/resource/{res.short_id} has isPart meta.")
+                    print(f"Collection {col}, https://beta.hydroshare.org/resource/{col.short_id} is missing hasPart.")
+                    if dry_run:
+                        print("SKIPPING creating hasPart relation because dry_run")
+                    else:
+                        print("Creating hasPart relation...")
+                        col.metadata.create_element('relation',
+                                                    type=RelationTypes.hasPart,
+                                                    value=citation)
+
         print("DONE")
