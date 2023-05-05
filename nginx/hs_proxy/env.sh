6da657d632b9227abefd6975d7f21550f8b6d529
Devin Cowan <dcowan@cuahsi.org>
2023-05-05 15:04:21 -0400

    redirect public metrics to caddy host

--- nginx/hs_proxy/env.sh
+++ nginx/hs_proxy/env.sh
@@ -8,6 +8,8 @@ export CURRENT_FILE="${WORKING_DIR}/current-vm"
 export RUNNING_FILE="${WORKING_DIR}/running-vm"
 export HOME_DIR=${PWD}
 export CURRENT_VM=`cat $CURRENT_FILE 2>/dev/null`
+export EXTERNAL_METRICS_HOST='public.cuahsi.org'
+export INTERNAL_METRICS_HOST='metrics-hs-01.edc.renci.org:8080'
 export RUNNING_VM=`cat nginx/config-files/hs-nginx.conf | grep -m 1 proxy_pass | cut -f3 -d'/' | cut -f1 -d';'`
 export BACKUP_VM=`cat $WORKER_FILE | grep -v $RUNNING_VM | head -1`
 echo -n $RUNNING_VM > $RUNNING_FILE
