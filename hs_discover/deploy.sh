aace34b4c989c4db2379e018b3cf5f46df29b701
Devin Cowan <dcowan@cuahsi.org>
2023-06-26 11:48:24 -0400

    Revert "Merge branch '4846-chunk-vendors' into 3996-map-clusters"
    This reverts commit ecf5102ff8985329f4b57a6e316a52bf17157403, reversing
    changes made to 1719ceb24705a4468f467fb3ec78a36aad5d0ce1.

--- hs_discover/deploy.sh
+++ hs_discover/deploy.sh
@@ -4,7 +4,12 @@ rm static/js/*.js
 rm static/js/*.map
 mkdir -p static/css
 mkdir -p static/js
-cp templates/hs_discover/css/* static/css/
-cp templates/hs_discover/js/* static/js/
+cp templates/hs_discover/css/app*.css static/css/app.css
+cp templates/hs_discover/css/chunk-vendors*.css static/css/chunk-vendors.css
+cp templates/hs_discover/css/*.css static/css
+cp templates/hs_discover/js/app*.js static/js/app.js
+cp templates/hs_discover/js/*.js static/js
 cp templates/hs_discover/map.js static/js/map.js
+cp templates/hs_discover/js/*.map static/js/
+cp templates/hs_discover/js/chunk-vendors*.js static/js/chunk-vendors.js
 docker exec -it hydroshare python manage.py collectstatic --no-input
