b630c1d42b2f24b39ab3bc33182ac815f18bfec5
Devin Cowan <dcowan@cuahsi.org>
2023-09-12 16:16:25 -0400

    [#5157] add sunset/deprecation headers

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -434,6 +434,7 @@ MIDDLEWARE = (
     "mezzanine.core.middleware.FetchFromCacheMiddleware",
     "hs_core.robots.RobotFilter",
     "hs_tracking.middleware.Tracking",
+    "hs_core.middleware.SunsetMiddleware",
 )
 
 # security settings
