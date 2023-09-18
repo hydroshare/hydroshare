57c0706a73c8d8f48dd4b2cc346456003a2133fc
Devin Cowan <dcowan@cuahsi.org>
2023-09-18 13:43:38 -0400

    [#5157] redirect admin login to oidc

--- hydroshare/urls.py
+++ hydroshare/urls.py
@@ -96,6 +96,7 @@ urlpatterns = i18n_patterns(
         theme.create_irods_account,
         name="create_irods_account",
     ),
+    url(r"^admin/login/$", RedirectView.as_view(url='/oidc/authenticate'), name="admin_login"),
     url(r"^accounts/login/$", RedirectView.as_view(url='/oidc/authenticate'), name="login"),
     url(r"^landingPage/$", theme.landingPage, name="landing_page"),
     url(r"^home/$", theme.dashboard, name="dashboard"),
