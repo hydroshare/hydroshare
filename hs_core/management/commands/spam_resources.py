fc4aa0ae01f8a6c05d29c364e501ef3d745946d5
Devin Cowan <dcowan@cuahsi.org>
2023-10-02 14:19:25 -0400

    print stats about matched strings

--- hs_core/management/commands/spam_resources.py
+++ hs_core/management/commands/spam_resources.py
@@ -59,15 +59,18 @@ class Command(BaseCommand):
         current_resource = 0
         shadow_banned_resources = []
         allowlisted = []
+        match_stats = {}
         print("Iterating over resources...")
         for resource in resources.iterator():
             current_resource += 1
             res_info = site_url + resource.absolute_url
             match = resource.spam_patterns
             if match:
+                match_text = match.group(0)
+                match_stats[match_text] = match_stats.get(match, 0) + 1
                 if resource.metadata:
                     try:
-                        res_info += f"\n{resource.metadata.title.value}\nmatched on '{match.group(0)}'"
+                        res_info += f"\n{resource.metadata.title.value}\nmatched on '{match_text}'"
                     except AttributeError:
                         pass
                 if resource.spam_allowlisted:
@@ -79,6 +82,12 @@ class Command(BaseCommand):
                 print(f"Resource has spam patterns: {res_info}")
                 print(f"Total thus far with spam patterns: {len(shadow_banned_resources)}")
                 print("*" * 100)
+
+        if match_stats:
+            print("Aggregate count of matched strings across resources:")
+            for k, v in match_stats.items():
+                print(f"{k}: {v}")
+            print("*" * 100)
 
         if allowlisted:
             print(f"List of {len(allowlisted)} resources that have been allowlisted:")
