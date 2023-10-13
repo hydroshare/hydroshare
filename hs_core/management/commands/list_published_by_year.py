16628ebf2485c442065efadbe459394830651d56
Devin Cowan <dcowan@cuahsi.org>
2023-10-13 11:39:58 -0400

    update resources published command

--- hs_core/management/commands/list_published_by_year.py
+++ hs_core/management/commands/list_published_by_year.py
@@ -10,6 +10,7 @@ from django.db.models import F
 from datetime import timedelta
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
+import csv as csv_module
 
 
 class Command(BaseCommand):
@@ -37,38 +38,66 @@ class Command(BaseCommand):
             help='limit to resources owned by specific user'
         )
 
+        parser.add_argument(
+            '--csv',
+            action='store_true',  # True for presence, False for absence
+            dest='csv',  # value is options['set_quota_holder']
+            help='export as csv'
+        )
+
     def handle(self, *args, **options):
         days = options['days']
         resources = BaseResource.objects.filter(raccess__published=True)
         owner = options['owned_by']
         res_type = options['type']
+        csv = options['csv']
+        year = options['year']
+        file_name = ''
 
         if owner is not None:
             try:
                 owner = User.objects.get(username=owner)
                 resources.filter(r2urp__user=owner,
                                  r2urp__privilege=PrivilegeCodes.OWNER)
+                if csv:
+                    file_name += f"_owner={owner}"
             except ObjectDoesNotExist:
                 print(f"User matching {owner} not found")
 
         if res_type is not None:
             if res_type in ["CompositeResource", "CollectionResource"]:
                 resources.filter(resource_type=res_type)
+                file_name += f"_restype={res_type}"
             else:
                 print(f"Type {res_type} is not supported. Must be 'CompositeResource' or 'CollectionResource'")
 
+        if csv:
+            file_name += f"_run={timezone.now()}"
+            if year:
+                file_name += f"_year={year}"
+
         resources = resources.order_by(F('updated').asc(nulls_first=True))
+        site_url = hydroshare.utils.current_site_url()
+
+        if csv:
+            csvfile = open(f'published_resources{file_name}.csv', 'w')
+            writer = csv_module.writer(csvfile)
+            headers = ['Url', 'Doi', 'Title', 'Type', 'Publication Date', 'Funding Agencies -->']
+            writer.writerow(headers)
 
         for resource in resources:
             pub_date = self.get_publication_date(resource)
-            if options['year']:
-                if pub_date.year != int(options['year']):
+            if year:
+                if pub_date.year != int(year):
                     continue
             if days:
                 cuttoff_time = timezone.now() - timedelta(days)
                 if not pub_date >= cuttoff_time:
                     continue
-            self.print_resource(resource, pub_date)
+            if csv:
+                self.write_csv(resource, pub_date, site_url, csvfile)
+            else:
+                self.print_resource(resource, pub_date, site_url)
 
     def get_publication_date(self, resource):
         published_date = resource.metadata.dates.filter(type="published").first()
@@ -76,8 +105,7 @@ class Command(BaseCommand):
             print(f"Publication date not found for {resource.short_id}")
         return published_date.start_date
 
-    def print_resource(self, res, pub_date):
-        site_url = hydroshare.utils.current_site_url()
+    def print_resource(self, res, pub_date, site_url):
         res_url = site_url + res.absolute_url
         funding_agencies = res.metadata.funding_agencies.all()
         print("\n")
@@ -116,3 +144,40 @@ class Command(BaseCommand):
                     print("No award number")
         else:
             print("Resource has no funding information")
+
+    def write_csv(self, res, pub_date, site_url, csvfile):
+        row = []
+        writer = csvfile.writer(csvfile)
+        res_url = site_url + res.absolute_url
+        funding_agencies = res.metadata.funding_agencies.all()
+        row.append(f"{res_url}")
+        if res.doi:
+            row.append(res.doi)
+        else:
+            row.append("")
+        row.append(res.metadata.title.value)
+        row.append(f"{res.resource_type}")
+        if pub_date:
+            row.append(f"Published on {pub_date}")
+        else:
+            row.append("")
+
+        if funding_agencies:
+            for f in funding_agencies:
+                if f.agency_name:
+                    row.append(f.agency_name)
+                else:
+                    row.append("")
+                if f.agency_url:
+                    row.append(f.agency_url)
+                else:
+                    row.append("")
+                if f.award_title:
+                    row.append(f.award_title)
+                else:
+                    row.append("")
+                if f.award_number:
+                    row.append(f.award_number)
+                else:
+                    row.append("")
+        writer.writerow(row)
