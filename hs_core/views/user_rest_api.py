f24c41e51333a08805ab3cad7c64e73881de9ca0
Scott Black <sblack@cuahsi.org>
2023-11-08 08:56:22 -0700

    revert userinfo organization to original state

--- hs_core/views/user_rest_api.py
+++ hs_core/views/user_rest_api.py
@@ -50,8 +50,8 @@ class UserInfo(APIView):
         user_profile = UserProfile.objects.filter(user=user).first()
         if user_profile.title:
             user_info['title'] = user_profile.title
-        user_info['organization'] = user_profile.organization
         if user_profile.organization:
+            user_info['organization'] = user_profile.organization
             user_info['organizations'] = [org for org in user_profile.organization.split(";")]
         if user_profile.state and user_profile.state.strip() and user_profile.state != 'Unspecified':
             user_info['state'] = user_profile.state.strip()
