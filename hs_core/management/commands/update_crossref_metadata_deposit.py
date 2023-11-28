33736a412271bc5a5626159331d8544e294fee8f
pkdash <pkdash_reena@hotmail.com>
2023-11-28 10:00:55 -0500

    [#5225] new management command to update crossref metadata deposit

---
+++ hs_core/management/commands/update_crossref_metadata_deposit.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist
+
+from hs_core.hydroshare.utils import get_resource_by_shortkey
+from hs_core.hydroshare import deposit_res_metadata_with_crossref
+
+
+class Command(BaseCommand):
+    help = "Update crossref metadata deposit for a published resource"
+
+    def add_arguments(self, parser):
+
+        # ID of a published resource for which crossref metadata needs to be updated
+        parser.add_argument('resource_id', type=str, help=('Required. The existing id (short_id) of'
+                                                           ' the published resource'))
+
+    def handle(self, *args, **options):
+
+        if not options['resource_id']:
+            raise CommandError('resource_id argument is required')
+        res_id = options['resource_id']
+        try:
+            resource = get_resource_by_shortkey(res_id, or_404=False)
+        except ObjectDoesNotExist:
+            raise CommandError("No Resource found for id {}".format(res_id))
+        if not resource.raccess.published:
+            raise CommandError("Resource is not a published resource")
+
+        response = deposit_res_metadata_with_crossref(resource)
+        if not response.status_code == 200:
+            err_msg = (f"Received a {response.status_code} from Crossref while depositing "
+                       f"metadata for res id {resource.short_id}")
+            raise CommandError(err_msg)
+        print(f"Successfully deposited metadata to crossref for res id {resource.short_id}")
