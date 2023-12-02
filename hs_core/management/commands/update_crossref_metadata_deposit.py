05f250b2f73c1d348efd4a46acaf82ac2e17e2d6
pkdash <pkdash_reena@hotmail.com>
2023-12-02 17:59:05 -0500

    [#5225] fixing management command for crossref deposit update

--- hs_core/management/commands/update_crossref_metadata_deposit.py
+++ hs_core/management/commands/update_crossref_metadata_deposit.py
@@ -2,8 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
 
 from hs_core.hydroshare.utils import get_resource_by_shortkey
-from hs_core.hydroshare import deposit_res_metadata_with_crossref
-
+from hs_core.hydroshare import deposit_res_metadata_with_crossref, get_resource_doi
+from hs_core.enums import CrossRefSubmissionStatus
 
 class Command(BaseCommand):
     help = "Update crossref metadata deposit for a published resource"
@@ -25,10 +25,15 @@ class Command(BaseCommand):
             raise CommandError("No Resource found for id {}".format(res_id))
         if not resource.raccess.published:
             raise CommandError("Resource is not a published resource")
+        if CrossRefSubmissionStatus.PENDING in resource.doi:
+            raise CommandError("Resource has a pending crossref deposit request. Please try again later.")
 
         response = deposit_res_metadata_with_crossref(resource)
         if not response.status_code == 200:
             err_msg = (f"Received a {response.status_code} from Crossref while depositing "
                        f"metadata for res id {resource.short_id}")
             raise CommandError(err_msg)
+        else:
+            resource.doi = get_resource_doi(resource.short_id, flag=CrossRefSubmissionStatus.PENDING)
+            resource.save()
         print(f"Successfully deposited metadata to crossref for res id {resource.short_id}")
