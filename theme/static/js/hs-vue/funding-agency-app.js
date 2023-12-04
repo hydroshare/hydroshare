78291b920364f814aa62fda9a42c0de25a58df8f
Devin Cowan <dcowan@cuahsi.org>
2023-12-04 15:20:17 -0500

    import resource funding agencies

--- theme/static/js/hs-vue/funding-agency-app.js
+++ theme/static/js/hs-vue/funding-agency-app.js
@@ -1,57 +1,57 @@
 let fundingAgenciesApp = new Vue({
-    el: '#add-agency-wrapper',
+    el: '#add-agency-app',
     delimiters: ['${', '}'],
     components: {
         VueBootstrapTypeahead
     },
     data: {
-        newKeyword: '',
-        resMode: RESOURCE_MODE,
-        resKeywords: RES_KEYWORDS, // global constant defined in parent template of subject template
+        newAgency: '',
+        fundingAgencies: RES_FUNDING_AGENCIES, // global constant defined in parent template of subject template
         showIsDuplicate: false,
         error: '',
     },
     mounted() {
+        console.log(this.fundingAgencies)
         // Until Django / VueJS cleaned up across RLP wait for VueJS state to finalize before trying to display dynamic elements
-        document.getElementById('subj-inline-error').style.display = "block";
-        document.getElementById('lst-tags').style.display = "block";
+        document.getElementById('agency-inline-error').style.display = "block";
+        document.getElementById('agency-tags').style.display = "block";
     },
     methods: {
-        addKeyword: function (resIdShort) {
+        addAgency: function (resIdShort) {
             this.showIsDuplicate = false;  // Reset
             
-            // Remove any empty keywords from the list
-            let newKeywords = this.newKeyword.split(",");
-            newKeywords = newKeywords.filter(function(a) {
+            // Remove any empty funders from the list
+            let newAgencies = this.newAgency.split(",");
+            newAgencies = newAgencies.filter(function(a) {
                return a.trim() !== "";
             });
 
             // Update to cleaned up string
-            this.newKeyword = newKeywords.join(",").trim();
+            this.newAgency = newAgencies.join(",").trim();
 
-            if (this.newKeyword.trim() === "") {
+            if (this.newAgency.trim() === "") {
                 return; // Empty string detected
-            }else if (this.newKeyword.length > 100) {
-                this.error = "Your keyword is too long. Ensure it has at most 100 characters.";
+            }else if (this.newAgency.length > 100) {
+                this.error = "Your funder is too long. Ensure it has at most 100 characters.";
                 return;
             }
 
-            let newVal =  (this.resKeywords.join(",").length ? this.resKeywords.join(",") + ",": "") + this.newKeyword;
+            let newVal =  (this.fundingAgencies.join(",").length ? this.fundingAgencies.join(",") + ",": "") + this.newAgency;
             this.error = "";
             let vue = this;
             $.post("/hsapi/_internal/" + resIdShort + "/subject/add-metadata/", {value: newVal}, function (resp) {
                 if (resp.status === "success") {
-                    // Append new keywords to our data array
-                    let newKeywordsArray = this.newKeyword.trim().split(",");
+                    // Append new funders to our data array
+                    let newAgenciesArray = this.newAgency.trim().split(",");
 
-                    for (let i = 0; i < newKeywordsArray.length; i++) {
-                        if ($.inArray(newKeywordsArray[i].trim(), this.resKeywords) >= 0) {
+                    for (let i = 0; i < newAgenciesArray.length; i++) {
+                        if ($.inArray(newAgenciesArray[i].trim(), this.fundingAgencies) >= 0) {
                             this.showIsDuplicate = true;
                         }
                         else {
-                            this.resKeywords.push(newKeywordsArray[i].trim());
-                            this.$refs.newKeyword.inputValue = "";  // open source bug https://github.com/alexurquhart/vue-bootstrap-typeahead/issues/19
-                            this.$data.newKeyword = ""
+                            this.fundingAgencies.push(newAgenciesArray[i].trim());
+                            this.$refs.newAgency.inputValue = "";  // open source bug https://github.com/alexurquhart/vue-bootstrap-typeahead/issues/19
+                            this.$data.newAgency = ""
                         }
                     }
                 }
@@ -61,9 +61,9 @@ let fundingAgenciesApp = new Vue({
                 showCompletedMessage(resp);
             }.bind(this), "json");
         },
-        removeKeyword: function (resIdShort, keywordName) {
-            let newVal = this.resKeywords.slice(); // Get a copy
-            newVal.splice($.inArray(keywordName, this.resKeywords), 1);   // Remove the keyword
+        removeAgency: function (resIdShort, agencyName) {
+            let newVal = this.fundingAgencies.slice(); // Get a copy
+            newVal.splice($.inArray(agencyName, this.fundingAgencies), 1);   // Remove the funder
 
             let vue = this;
             vue.error = "";
@@ -71,17 +71,17 @@ let fundingAgenciesApp = new Vue({
             $.post("/hsapi/_internal/" + resIdShort + "/subject/add-metadata/",
                 {value: newVal.join(",")}, function (resp) {
                     if (resp.status === "success") {
-                        vue.resKeywords = newVal;
+                        vue.fundingAgencies = newVal;
                         if (!newVal.length) {
-                            // If no keywords, the metadata is no longer sufficient to make the resource public
+                            // If no funders, the metadata is no longer sufficient to make the resource public
                             manageAccessApp.onMetadataInsufficient();
                         }
                     }
                     else {
-                        if (vue.resKeywords.length){
+                        if (vue.fundingAgencies.length){
                             // vue.error = resp.message;
                             // Response message is not very user-friendly so:
-                            vue.error = "There was a problem removing your keyword.";
+                            vue.error = "There was a problem removing your funder.";
                         }
                     }
                 }.bind(this), "json");
