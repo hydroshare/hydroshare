37cc8a201e7a708ddc83d9f1585559cc2bd22172
Devin Cowan <dcowan@cuahsi.org>
2023-12-04 16:00:38 -0500

    Query crossref api

--- theme/static/js/hs-vue/funding-agency-app.js
+++ theme/static/js/hs-vue/funding-agency-app.js
@@ -1,94 +1,69 @@
+let debounce = function(func, wait, immediate) {
+    let timeout, result;
+    return function() {
+      let context = this, args = arguments;
+      let later = function() {
+        timeout = null;
+        if (!immediate) result = func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) result = func.apply(context, args);
+      return result;
+    };
+}
+
 let fundingAgenciesApp = new Vue({
-    el: '#add-agency-app',
+    el: '#add-funding-agency',
     delimiters: ['${', '}'],
     components: {
         VueBootstrapTypeahead
     },
     data: {
         newAgency: '',
-        fundingAgencies: RES_FUNDING_AGENCIES, // global constant defined in parent template of subject template
+        fundingAgencies: RES_FUNDING_AGENCIES,
+        fundingAgencyNames: [],
+        crossrefFunders: [],
+        CROSSREF_API_URL: 'https://api.crossref.org/funders?query=:query',
         showIsDuplicate: false,
         error: '',
     },
-    mounted() {
-        console.log(this.fundingAgencies)
-        // Until Django / VueJS cleaned up across RLP wait for VueJS state to finalize before trying to display dynamic elements
-        document.getElementById('agency-inline-error').style.display = "block";
-        document.getElementById('agency-tags').style.display = "block";
+    mounted(){
+        this.fundingAgencyNames = this.fundingAgencies.map(a => a.agency_name);
+        this.fundingAgencyUrls = this.fundingAgencies.map(a => a.agency_url);
+        console.log(this.fundingAgencyUrls)
     },
     methods: {
-        addAgency: function (resIdShort) {
+        getCrossrefFunders: async function(query) {
+            // TODO:
+            // https://api.crossref.org/swagger-ui/index.html#/Funders/get_funders
+            // avoid rate limits
+            const res = await fetch(this.CROSSREF_API_URL.replace(':query', query))
+            const result = await res.json()
+            this.crossrefFunders = result.message.items
+        },
+        checkAgency: function () {
             this.showIsDuplicate = false;  // Reset
-            
-            // Remove any empty funders from the list
-            let newAgencies = this.newAgency.split(",");
-            newAgencies = newAgencies.filter(function(a) {
-               return a.trim() !== "";
-            });
-
-            // Update to cleaned up string
-            this.newAgency = newAgencies.join(",").trim();
 
             if (this.newAgency.trim() === "") {
                 return; // Empty string detected
-            }else if (this.newAgency.length > 100) {
+            }else if (this.newAgency.length > 250) {
                 this.error = "Your funder is too long. Ensure it has at most 100 characters.";
                 return;
             }
 
-            let newVal =  (this.fundingAgencies.join(",").length ? this.fundingAgencies.join(",") + ",": "") + this.newAgency;
             this.error = "";
-            let vue = this;
-            $.post("/hsapi/_internal/" + resIdShort + "/subject/add-metadata/", {value: newVal}, function (resp) {
-                if (resp.status === "success") {
-                    // Append new funders to our data array
-                    let newAgenciesArray = this.newAgency.trim().split(",");
-
-                    for (let i = 0; i < newAgenciesArray.length; i++) {
-                        if ($.inArray(newAgenciesArray[i].trim(), this.fundingAgencies) >= 0) {
-                            this.showIsDuplicate = true;
-                        }
-                        else {
-                            this.fundingAgencies.push(newAgenciesArray[i].trim());
-                            this.$refs.newAgency.inputValue = "";  // open source bug https://github.com/alexurquhart/vue-bootstrap-typeahead/issues/19
-                            this.$data.newAgency = ""
-                        }
-                    }
-                }
-                else {
-                    vue.error = resp.message.slice(0, -1);
-                }
-                showCompletedMessage(resp);
-            }.bind(this), "json");
+            if ($.inArray(this.newAgency, this.fundingAgencyNames) >= 0) {
+                this.showIsDuplicate = true;
+                this.error = "Duplicate";
+            }
+            else {
+                this.fundingAgencies.push(this.newAgency);
+            }
         },
-        removeAgency: function (resIdShort, agencyName) {
-            let newVal = this.fundingAgencies.slice(); // Get a copy
-            newVal.splice($.inArray(agencyName, this.fundingAgencies), 1);   // Remove the funder
-
-            let vue = this;
-            vue.error = "";
-
-            $.post("/hsapi/_internal/" + resIdShort + "/subject/add-metadata/",
-                {value: newVal.join(",")}, function (resp) {
-                    if (resp.status === "success") {
-                        vue.fundingAgencies = newVal;
-                        if (!newVal.length) {
-                            // If no funders, the metadata is no longer sufficient to make the resource public
-                            manageAccessApp.onMetadataInsufficient();
-                        }
-                    }
-                    else {
-                        if (vue.fundingAgencies.length){
-                            // vue.error = resp.message;
-                            // Response message is not very user-friendly so:
-                            vue.error = "There was a problem removing your funder.";
-                        }
-                    }
-                }.bind(this), "json");
-        },
-        safeJS: function (input) {
-            input.replace("\\", "\\\\")
-            return encodeURIComponent(input)
-        }
-    }
+    },
+    watch: {
+        newAgency: debounce(function(funder) { this.getCrossrefFunders(funder) }, 500)
+      }
 });
