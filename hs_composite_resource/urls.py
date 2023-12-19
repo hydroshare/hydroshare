da4df8732d14e596ec2bb8f3f1f64a657eec9f11
pkdash <pkdash_reena@hotmail.com>
2023-12-18 22:29:41 -0500

    [#5236] view functions to generate list of files to update

--- hs_composite_resource/urls.py
+++ hs_composite_resource/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
     url(r'^_internal/(?P<resource_id>[0-9a-f]+)/(?P<coverage_type>[A-z]+)/update-coverage/$',
         views.update_resource_coverage, name="update_resource_coverage"),
     url(r'^_internal/(?P<resource_id>[0-9a-f]+)/(?P<coverage_type>[A-z]+)/delete-coverage/$',
-        views.delete_resource_coverage, name="delete_resource_coverage")
+        views.delete_resource_coverage, name="delete_resource_coverage"),
+    url(r'^_internal/(?P<resource_id>[0-9a-f]+)/aggregation-files-to-sync/$',
+        views.check_aggregation_files_to_sync, name="aggregation_files_to_sync"),
 ]
