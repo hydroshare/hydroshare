fe6108751a98a9b08e1dd35067224295c530f9cc
Devin Cowan <dcowan@cuahsi.org>
2023-12-19 17:15:24 -0500

    [#5225] filter out chars from crossref query

--- theme/static/js/hs-vue/funding-agency-app.js
+++ theme/static/js/hs-vue/funding-agency-app.js
@@ -26,7 +26,9 @@ let fundingAgenciesApp = new Vue({
     startedEditing: {}, // store the funder that we started editing
     deleteUrl: "", // Django endpoint to call for deleting a funder
     currentlyDeleting: {}, // store the funder that we are deleting
-    crossreffApiDown: false
+    crossreffApiDown: false, // if we are having trouble reaching the crossref api
+    removeCharsFromQuery: ["."], // characters to be removed from search
+    filteredWords: []
   },
   mounted() {
     if (this.selfAccessLevel === "owner" && this.resPublished) {
@@ -80,12 +82,23 @@ let fundingAgenciesApp = new Vue({
       try {
         let words = funderName.split(" ");
         words = words.map((w) => encodeURIComponent(w));
+        this.filteredWords = []
+        words = words.filter((word)=>{
+          for (let char of this.removeCharsFromQuery){
+            if (word.includes(char)){
+              this.filteredWords.push(word)
+              return false
+            }
+          }
+          return true
+        })
         let query = words.join("+");
         query = `${query}&mailto=help@cuahsi.org`;
         // https://api.crossref.org/swagger-ui/index.html#/Funders/get_funders
         const res = await fetch(this.CROSSREF_API_URL.replace(":query", query));
         const result = await res.json();
         const funders = result.message.items;
+        this.crossreffApiDown = false
         return funders;
       } catch (e) {
         console.error(`Error querying Crossref API: ${e}`);
