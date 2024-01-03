8ca487ebb65fc5b106f3bc872e9e23e2e57f8012
Devin Cowan <dcowan@cuahsi.org>
2024-01-03 10:40:42 -0500

    [#5293][#5294] add publication tests

--- hs_core/tests/api/native/test_publish_resource.py
+++ hs_core/tests/api/native/test_publish_resource.py
@@ -1,11 +1,14 @@
 
-import unittest
+import tempfile
+import os
 
 from django.contrib.auth.models import Group
 from django.test import TestCase
 
 from hs_core import hydroshare
 from hs_core.testing import MockIRODSTestCaseMixin
+from hs_core.views.utils import get_default_admin_user
+from django.core.exceptions import ValidationError
 
 
 class TestPublishResource(MockIRODSTestCaseMixin, TestCase):
@@ -29,9 +32,25 @@ class TestPublishResource(MockIRODSTestCaseMixin, TestCase):
             'Test Resource'
         )
 
-    # TODO: This test needs to be enabled once the publish_resource() function is updated to register resource DOI
-    # with crossref. At that point the call to crossref needs to be mocked here in this test (IMPORTANT)
-    @unittest.skip
+        self.tmp_dir = tempfile.mkdtemp()
+        file_one = os.path.join(self.tmp_dir, "test1.txt")
+
+        file_one_write = open(file_one, "w")
+        file_one_write.write("Putting something inside")
+        file_one_write.close()
+
+        # open files for read and upload
+        self.file_one = open(file_one, "rb")
+
+        self.complete_res = hydroshare.create_resource(
+            'CompositeResource',
+            self.user,
+            'My Test Resource ' * 10,
+            files=(self.file_one,),
+            keywords=('a', 'b', 'c'),
+        )
+        self.complete_res.metadata.create_element("description", abstract="new abstract for the resource " * 10)
+
     def test_publish_resource(self):
         # check status prior to publishing the resource
         self.assertFalse(
@@ -52,9 +71,12 @@ class TestPublishResource(MockIRODSTestCaseMixin, TestCase):
         # there should not be published date type metadata element
         self.assertFalse(self.res.metadata.dates.filter(type='published').exists())
 
+        admin_user = get_default_admin_user()
+        hydroshare.submit_resource_for_review(pk=self.complete_res.short_id, user=admin_user)
+
         # publish resource - this is the api we are testing
-        hydroshare.publish_resource(self.res.short_id)
-        self.pub_res = hydroshare.get_resource_by_shortkey(self.res.short_id)
+        hydroshare.publish_resource(user=admin_user, pk=self.complete_res.short_id)
+        self.pub_res = hydroshare.get_resource_by_shortkey(self.complete_res.short_id)
 
         # test publish state
         self.assertTrue(
@@ -76,3 +98,81 @@ class TestPublishResource(MockIRODSTestCaseMixin, TestCase):
 
         # there should now published date type metadata element
         self.assertTrue(self.pub_res.metadata.dates.filter(type='published').exists())
+
+    def test_only_admin_can_publish_resource(self):
+        # check status prior to publishing the resource
+        self.assertFalse(
+            self.complete_res.raccess.published,
+            msg='The resource is published'
+        )
+
+        self.assertFalse(
+            self.complete_res.raccess.immutable,
+            msg='The resource is frozen'
+        )
+
+        self.assertFalse(
+            self.complete_res.doi,
+            msg='doi is assigned'
+        )
+
+        # there should not be published date type metadata element
+        self.assertFalse(self.res.metadata.dates.filter(type='published').exists())
+
+        admin_user = get_default_admin_user()
+        hydroshare.submit_resource_for_review(pk=self.complete_res.short_id, user=admin_user)
+
+        with self.assertRaises(ValidationError):
+            hydroshare.publish_resource(user=self.user, pk=self.complete_res.short_id)
+
+        # publish with admin user should be successful
+        hydroshare.publish_resource(user=admin_user, pk=self.complete_res.short_id)
+
+    def test_incomplete_resource_cant_publish(self):
+        # check status prior to publishing the resource
+        self.assertFalse(
+            self.complete_res.raccess.published,
+            msg='The resource is published'
+        )
+
+        self.assertFalse(
+            self.complete_res.raccess.immutable,
+            msg='The resource is frozen'
+        )
+
+        self.assertFalse(
+            self.complete_res.doi,
+            msg='doi is assigned'
+        )
+
+        self.assertFalse(
+            self.res.raccess.published,
+            msg='The resource is published'
+        )
+
+        self.assertFalse(
+            self.res.raccess.immutable,
+            msg='The resource is frozen'
+        )
+
+        self.assertFalse(
+            self.res.doi,
+            msg='doi is assigned'
+        )
+
+        # there should not be published date type metadata element
+        self.assertFalse(self.res.metadata.dates.filter(type='published').exists())
+
+        admin_user = get_default_admin_user()
+        with self.assertRaises(ValidationError):
+            hydroshare.submit_resource_for_review(pk=self.res.short_id, user=admin_user)
+
+        # submit with complete res should be successful
+        hydroshare.submit_resource_for_review(pk=self.complete_res.short_id, user=admin_user)
+
+    def test_last_updated(self):
+        admin_user = get_default_admin_user()
+        hydroshare.submit_resource_for_review(pk=self.complete_res.short_id, user=self.user)
+        hydroshare.publish_resource(user=admin_user, pk=self.complete_res.short_id)
+
+        self.assertEqual(self.complete_res.last_changed_by, self.user)
