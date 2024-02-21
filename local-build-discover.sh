cb0fc316efe0d20084c7b7dacdc5b309c7314059
hydrocheck <qa@cuahsi.org>
2024-02-21 21:25:51 +0000

    Shift the node build work to the Hydroshare image build (multi-stage Docker build)

--- local-build-discover.sh
+++ local-build-discover.sh
@@ -62,67 +62,6 @@ function getImageID() {
     docker $DOCKER_PARAM images | grep $1 | tr -s ' ' | cut -f3 -d' '
 }
 
-##nodejs build for discovery
-
-node_build() {
-
-HS_PATH=`pwd`
-#### Set version pin variable ####
-#n_ver="15.0.0"
-n_ver="14.14.0"
-
-echo '####################################################################################################'
-echo "Starting Node Build .... "
-echo '####################################################################################################'
-
-### Create Directory structure outside to maintain correct permissions
-cd hs_discover
-rm -rf static templates
-mkdir static templates
-mkdir templates/hs_discover
-mkdir static/js
-mkdir static/css
-
-# Start Docker container and Run build
-docker run -i -v $HS_PATH:/hydroshare --name=nodejs --user=$HS_UID:$HS_GID node:$n_ver /bin/bash << eof
-
-cd hydroshare
-cd hs_discover
-npm install
-npm run build
-mkdir -p static/js
-mkdir -p static/css
-cp -rp templates/hs_discover/js static/
-cp -rp templates/hs_discover/css static/
-cp -p templates/hs_discover/map.js static/js/
-echo "----------------js--------------------"
-ls -l static/js
-echo "--------------------------------------"
-echo "----------------css-------------------"
-ls -l static/css
-echo "--------------------------------------"
-cd static/
-cd ..
-eof
-
-echo "Node Build completed ..."
-echo
-echo "Removing node container"
-docker container rm nodejs
-cd $HS_PATH
-sleep 1
-
-}
-
-
-echo
-echo '########################################################################################################################'
-echo " Building Node for Discovery"
-echo '########################################################################################################################'
-echo
-
-node_build
-
 echo
 echo '########################################################################################################################'
 echo " Migrating data"
