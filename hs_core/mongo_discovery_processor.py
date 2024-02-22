831a10009226a9bf1ff5ba6c8070a3b1c7dfadb0
Scott Black <sblack@cuahsi.org>
2024-02-22 06:51:40 -0700

    only export resource ids for discoverable resources

--- hs_core/mongo_discovery_processor.py
+++ hs_core/mongo_discovery_processor.py
@@ -1,9 +1,8 @@
 import logging
+import datetime
 import hs_access_control.signals
 
 from django.conf import settings
-from hs_core.atlas_mongo import _HydroshareResourceMetadata
-from hs_rest_api2.metadata import resource_metadata
 from pymongo.mongo_client import MongoClient
 from celery import shared_task
 from hs_core.hydroshare.utils import get_resource_by_shortkey
@@ -20,18 +19,12 @@ db = client[getattr(settings, "MONGO_DISCOVERY_DATABASE", "hydroshare_beta")]
 
 @shared_task
 def update_mongo(resource_id: str):
-    res = get_resource_by_shortkey(resource_id)
-    res_json = resource_metadata(res)
-    res_metadata = _HydroshareResourceMetadata(**res_json.dict())
-    discovery_record = res_metadata.to_catalog_dataset()
-    db.discovery.update_one({"url": str(res_json.url)}, {"$set": discovery_record.dict(by_alias=True)}, upsert=True)
+    db.discovery.update_one({"resource_id": resource_id}, {"$set": {"resource_id": resource_id, "updated": datetime.datetime.now().timestamp()}}, upsert=True)
 
 
 @shared_task
 def remove_mongo(resource_id: str):
-    res = get_resource_by_shortkey(resource_id)
-    res_json = resource_metadata(res)
-    db.discovery.delete_one({"url": str(res_json.url)})
+    db.discovery.delete_one({"resource_id": resource_id})
 
 
 @receiver(hs_access_control.signals.access_changed, sender=PrivilegeBase)
