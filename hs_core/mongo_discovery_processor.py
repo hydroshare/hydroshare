2ca8d0c011021407850df6ee800f68ea371f1e54
Scott Black <sblack@cuahsi.org>
2024-03-25 15:25:22 -0600

    include update_time in collection

--- hs_core/mongo_discovery_processor.py
+++ hs_core/mongo_discovery_processor.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from pymongo.mongo_client import MongoClient
 from celery import shared_task
-
+from datetime import datetime
 
 client = MongoClient(getattr(settings, "MONGO_DISCOVERY_URL", ""), uuidRepresentation="standard")
 db = client[getattr(settings, "MONGO_DISCOVERY_DATABASE", "hydroshare_beta")]
@@ -9,8 +9,8 @@ db = client[getattr(settings, "MONGO_DISCOVERY_DATABASE", "hydroshare_beta")]
 
 @shared_task
 def update_mongo(resource_id: str):
-    db.discovery.update_one({"resource_id": resource_id}, {"$set": {"resource_id": resource_id}}, upsert=True)
+    db.discovery_ids.update_one({"resource_id": resource_id}, {"$set": {"resource_id": resource_id, "update_time": datetime.now()}}, upsert=True)
 
 @shared_task
 def remove_mongo(resource_id: str):
-    db.discover_ids_delete_one({"resource_id": resource_id})
+    db.discover_ids.delete_one({"resource_id": resource_id})
