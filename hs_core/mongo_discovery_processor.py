021183976d366ead4fdc8cd2df531a5eb8113422
Scott Black <sblack@cuahsi.org>
2024-03-25 17:53:07 -0600

    pep8

--- hs_core/mongo_discovery_processor.py
+++ hs_core/mongo_discovery_processor.py
@@ -9,7 +9,7 @@ db = client[getattr(settings, "MONGO_DISCOVERY_DATABASE", "hydroshare_beta")]
 
 @shared_task
 def update_mongo(resource_id: str):
-    db.discovery_ids.update_one({"resource_id": resource_id}, {"$set": {"resource_id": resource_id, 
+    db.discovery_ids.update_one({"resource_id": resource_id}, {"$set": {"resource_id": resource_id,
                                                                         "update_time": datetime.now()}}, upsert=True)
 
 
