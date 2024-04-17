0e8ef7be172856e2f6c1011c571916a5abd696db
Scott Black <sblack@cuahsi.org>
2024-04-17 09:26:14 -0600

    [#5432] - add mongo settings to settings.py

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -862,6 +862,10 @@ MODEL_PROGRAM_META_SCHEMA_TEMPLATE_PATH = (
 
 BULK_UPDATE_CREATE_BATCH_SIZE = 1000
 
+MONGO_DISCOVERY_URL = None # overwrite in local_settings.py
+MONGO_DISCOVERY_DATABASE = None # overwrite in local_settings.py
+
+
 if ENABLE_OIDC_AUTHENTICATION:
     # The order of the authentication classes is important. The OIDC authentication class
     # see this issue: https://github.com/encode/django-rest-framework/issues/5865
