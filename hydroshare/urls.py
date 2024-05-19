b743ab525ac9ff3166749c6b0077a3f08b812701
pkdash <pkdash_reena@hotmail.com>
2024-05-18 22:31:24 -0400

    [#5316] fixing url paths

--- hydroshare/urls.py
+++ hydroshare/urls.py
@@ -74,13 +74,13 @@ urlpatterns += i18n_patterns(
         theme.update_user_profile,
         name="update_profile",
     ),
-    path(r'^act-on-quota-request/(?P<quota_request_id>[0-9]+)/(?P<action>[a-z]+)/$',
+    re_path(r"^act-on-quota-request/(?P<quota_request_id>[0-9]+)/(?P<action>[a-z]+)/$",
          theme.act_on_quota_request, name='act_on_quota_request_noauth'),
-    re_path(r'^act-on-quota-request/(?P<quota_request_id>[0-9]+)/(?P<action>[a-z]+)/'
-            r'(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',
-            theme.act_on_quota_request, name='act_on_quota_request'),
-    path(r'^quota-request/$', theme.quota_request, name='quota_request'),
-    path(r"^update_password/$", theme.update_user_password, name="update_password"),
+    re_path(
+        r'^act-on-quota-request/(?P<quota_request_id>[0-9]+)/(?P<action>[a-z]+)/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',
+        theme.act_on_quota_request, name='act_on_quota_request'),
+    path("quota-request/", theme.quota_request, name='quota_request'),
+    path("update_password/", theme.update_user_password, name="update_password"),
     re_path(
         r"^resend_verification_email/(?P<email>.*)/",
         theme.resend_verification_email,
