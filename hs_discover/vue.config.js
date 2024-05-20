42f78fbd8458c1644ea9dcb0748cfe9a5c93c0c1
Devin Cowan <dcowan@cuahsi.org>
2024-05-20 16:29:01 -0400

    Update vue.config.js with publicPath and crossorigin options

--- hs_discover/vue.config.js
+++ hs_discover/vue.config.js
@@ -6,5 +6,9 @@ module.exports = {
   // Here profiding the ability to override the publicPath via an environment variable
   // Example export BUCKET_URL_PUBLIC_PATH=http://my-public-bucket && npm run build
   // This should be the same as the STATIC_URL in the Django settings
+  // https://cli.vuejs.org/config/#publicpath
   publicPath: process.env.BUCKET_URL_PUBLIC_PATH ? process.env.BUCKET_URL_PUBLIC_PATH : "/static/static",
+  // https://cli.vuejs.org/config/#crossorigin
+  // TODO: figure out how to get crossorigin to work with the Django static files
+  crossorigin: "anonymous",
 };
