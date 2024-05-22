865bd4b465978b7a05e196084546574d5a4523fb
Devin Cowan <dcowan@cuahsi.org>
2024-05-22 14:57:49 -0400

    Revert "use BASE_URL"
    This reverts commit 4bb7c44e7f6f838c2db7f63a536d2ec474d7ed15.

--- hs_discover/vue.config.js
+++ hs_discover/vue.config.js
@@ -4,10 +4,10 @@ module.exports = {
   },
   outputDir: "templates/hs_discover/",
   // Here profiding the ability to override the publicPath via an environment variable
-  // Example export BASE_URL=http://my-public-bucket && npm run build
+  // Example export BUCKET_URL_PUBLIC_PATH=http://my-public-bucket && npm run build
   // This should be the same as the STATIC_URL in the Django settings
   // https://cli.vuejs.org/config/#publicpath
-  publicPath: process.env.BASE_URL ? process.env.BASE_URL : "/static/static",
+  publicPath: process.env.BUCKET_URL_PUBLIC_PATH ? process.env.BUCKET_URL_PUBLIC_PATH : "/static/static",
   // https://cli.vuejs.org/config/#crossorigin
   // TODO: figure out how to get crossorigin to work with the Django static files
   crossorigin: "anonymous",
