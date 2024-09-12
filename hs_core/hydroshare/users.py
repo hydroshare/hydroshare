d41c2b53fed8f6e6d6e377c35089eb468627f452
Scott Black <sblack@cuahsi.org>
2024-09-12 07:42:35 -0600

    replace import

--- hs_core/hydroshare/users.py
+++ hs_core/hydroshare/users.py
@@ -7,6 +7,7 @@ from django.core import exceptions
 from django.core.exceptions import PermissionDenied, ObjectDoesNotExist, ValidationError
 from django.core.files import File
 from django.core.files.uploadedfile import UploadedFile
+from django.db.models import Q
 
 from hs_core.models import (
     BaseResource,
