e6d65ba2bd767a35edfe3774566dafd04c10a53a
sblack-usu <sblack@cuahsi.org>
2024-09-30 14:17:37 -0600

    pep8

--- hs_rest_api/resources/quota_holder.py
+++ hs_rest_api/resources/quota_holder.py
@@ -5,6 +5,5 @@ from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
 
 @api_view(['GET'])
 def get_quota_holder_bucket(request, resource_id):
-    res, _, _ = authorize(request, resource_id,
-                                 needed_permission=ACTION_TO_AUTHORIZE.VIEW_RESOURCE)
+    res, _, _ = authorize(request, resource_id, needed_permission=ACTION_TO_AUTHORIZE.VIEW_RESOURCE)
     return HttpResponse(res.quota_holder.userprofile.bucket_name)
