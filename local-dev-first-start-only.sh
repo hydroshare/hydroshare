c68b8c485a10024f99c69d0377fbdb552511673d
Devin Cowan <dcowan@cuahsi.org>
2024-10-02 10:09:53 -0400

    Update Dockerfile to include latest static files from hs-discover

--- local-dev-first-start-only.sh
+++ local-dev-first-start-only.sh
@@ -78,7 +78,7 @@ mkdir static/js
 mkdir static/css
 
 # Start Docker container
-docker run -i --name=discover hydroshare/hs_discover:f6a4a78 /bin/bash << eof
+docker run -i --name=discover hydroshare/hs_discover:fbe15a5 /bin/bash << eof
 
 cd /dist
 echo "----------------js--------------------"
