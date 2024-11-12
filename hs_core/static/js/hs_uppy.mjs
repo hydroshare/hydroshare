0e878790615b63c773c223ee0fa747e38ab900c3
Devin Cowan <dcowan@cuahsi.org>
2024-11-12 18:48:41 -0500

    remove nginx service

--- hs_core/static/js/hs_uppy.mjs
+++ hs_core/static/js/hs_uppy.mjs
@@ -137,13 +137,9 @@ let uppy = new Uppy({
       $("#hsDropzone").toggleClass("glow-blue", false);
     },
   });
-  // this works for gdrive from companion
-  // let UPPY_UPLOAD_ENDPOINT = 'http://hydroshare:8000/hsapi/tus/'
-  // this works for direct upload without companion
-  // let UPPY_UPLOAD_ENDPOINT = 'http://localhost:8000/hsapi/tus/'
-  let UPPY_UPLOAD_ENDPOINT = 'https://localhost/hsapi/tus/'
   uppy
   .use(Tus, {
+    // TODO: this endpoint needs to be set based on the origin vs local dev
     endpoint: `http://host.docker.internal:8000/hsapi/tus/`,
     // TODO: add some documentation for testing gdrive connection locally
     // above works for direct upload without companion
