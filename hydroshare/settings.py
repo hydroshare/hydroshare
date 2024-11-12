d966a24ab581a316654e9c198323a8e9f3274fda
Devin Cowan <dcowan@cuahsi.org>
2024-11-12 14:30:34 -0500

    Update COMPANION_URL in hs_core/models.py and hydroshare/settings.py

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -457,7 +457,7 @@ TUS_EXISTING_FILE = 'error'  # Other options are: 'overwrite',  'error', 'rename
 
 # the url for the uppy companion server
 # https://uppy.io/docs/companion/
-COMPANION_URL = 'https://companion.cuahsi.org/'
+COMPANION_URL = 'https://companion.hydroshare.org/'
 
 SWAGGER_SETTINGS = {
     "DEFAULT_GENERATOR_CLASS": "hs_rest_api2.serializers.NestedSchemaGenerator"
