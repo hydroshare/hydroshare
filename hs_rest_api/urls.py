1d6a0ec1585b041219674d87a514c9504f5f5160
sblack-usu <sblack@cuahsi.org>
2024-11-19 11:57:44 -0700

    fix errors in merge conflict resolution

--- hs_rest_api/urls.py
+++ hs_rest_api/urls.py
@@ -12,7 +12,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-<<<<<<< HEAD
 from hs_core import views as core_views
 from hs_core.views.resource_folder_hierarchy import (
     data_store_add_reference_public, data_store_edit_reference_url_public,
@@ -21,8 +20,6 @@ from hs_dictionary import views as dict_views
 from hs_file_types import views as file_type_views
 from .resources.quota_holder import get_quota_holder_bucket
 
-=======
->>>>>>> origin/develop
 from .discovery import DiscoverSearchView
 from .views.resource_share import ShareResourceGroup, ShareResourceUser
 
