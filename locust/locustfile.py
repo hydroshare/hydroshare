0f0161cf578ba3c4320b7a13e4342d8872c90681
Devin Cowan <dcowan@cuahsi.org>
2025-01-09 12:51:46 -0500

    Enhance Locust load testing setup by installing dependencies and adding resource management tasks

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,5 +1,5 @@
 # import time
-from locust import HttpUser, task  # , between
+from locust import HttpUser, task, tag  # , between
 from hsclient import HydroShare
 import os
 import glob
@@ -19,98 +19,6 @@ class HSUser(HttpUser):
     # wait_time = between(1, 2)
     number_of_resources = 1
     resources = {}
-
-    # @task
-    # def home(self):
-    #     self.client.get("/home", verify=False)
-
-    # @task
-    # def discover(self):
-    #     self.client.get("/search", verify=False)
-
-    # @task
-    # def delete(self):
-    #     if self.resources:
-    #         res_key = random.choice(list(self.resources.keys()))
-    #         res = self.resources.pop(res_key)
-    #         print(f"deleted {res}")
-    #         res.delete()
-
-    # @task
-    # def my_resources(self):
-    #     self.client.get("/my-resources/?f=owned&f=discovered&f=favorites&f=shared", verify=False)
-
-    # @task
-    # def create(self):
-    #     with self.client.get("/create", catch_response=True) as response:
-    #         try:
-    #             new_res = self.hs.create()
-    #             resIdentifier = new_res.resource_id
-    #             self.resources[resIdentifier] = new_res
-    #             logging.info(f"created {resIdentifier}")
-    #             response.success()
-    #         except Exception as e:
-    #             logging.error(f"Error creating resource")
-    #             # mark as a locust failure
-    #             response.failure(f"Error creating resource: {e}")
-
-    @task
-    def download(self):
-        if not self.resources:
-            return
-        with self.client.get("/download", catch_response=True) as response:
-            res_key = random.choice(list(self.resources.keys()))
-            try:
-                self.resources[res_key].download()
-                logging.info(f"downloaded {res_key}")
-                response.success()
-            except Exception as e:
-                logging.error(f"Error downloading resource: {e}")
-                # mark as a locust failure
-                response.failure(f"Error downloading resource: {e}")
-
-    @task
-    def copy(self):
-        if not self.resources:
-            return
-        with self.client.get("/copy", catch_response=True) as response:
-            res_key = random.choice(list(self.resources.keys()))
-            try:
-                self.resources[res_key].copy()
-                logging.info(f"copied {res_key}")
-                response.success()
-            except Exception as e:
-                logging.error(f"Error copying resource: {e}")
-                # mark as a locust failure
-                response.failure(f"Error copying resource: {e}")
-
-    @task
-    def add_files(self):
-        if not self.resources:
-            return
-        with self.client.get("/add_files", catch_response=True) as response:
-            res_key = random.choice(list(self.resources.keys()))
-            try:
-                self.resources[res_key].file_upload("locustfile.py")
-                logging.info(f"uploaded file to {res_key}")
-                response.success()
-            except Exception as e:
-                logging.error(f"Error adding files to resource: {e}")
-                # mark as a locust failure
-                response.failure(f"Error adding files to resource: {e}")
-
-    def create_resource(self):
-        try:
-            new_res = self.hs.create()
-            resIdentifier = new_res.resource_id
-            self.resources[resIdentifier] = new_res
-            logging.info(f"created {resIdentifier}")
-        except Exception:
-            logging.error("Error creating resource")
-            raise
-
-    # def get_resources(self):
-        # http://localhost:8000/hsapi/resource/?owner=asdf&edit_permission=false&published=false&include_obsolete=false
 
     def on_start(self):
         hs = HydroShare(username=USERNAME, password=PASSWORD, host=HOST, port=PORT, protocol=PROTOCOL)
@@ -142,3 +50,101 @@ class HSUser(HttpUser):
         for f in files:
             logging.info(f"Cleanup file {f}")
             os.remove(f)
+
+    def create_resource(self):
+        try:
+            new_res = self.hs.create()
+            resIdentifier = new_res.resource_id
+            self.resources[resIdentifier] = new_res
+            logging.info(f"created {resIdentifier}")
+        except Exception:
+            logging.error("Error creating resource")
+            raise
+
+    @task
+    def home(self):
+        self.client.get("/home", verify=False)
+
+    @task
+    def discover(self):
+        self.client.get("/search", verify=False)
+
+    @task
+    def my_resources(self):
+        self.client.get("/my-resources/?f=owned&f=discovered&f=favorites&f=shared", verify=False)
+
+    @task
+    def get_resources(self):
+        # http://localhost:8000/hsapi/resource/?owner=asdf&edit_permission=false&published=false&include_obsolete=false
+        pass
+
+    # @task
+    # def create(self):
+    #     with self.client.get("/create", catch_response=True) as response:
+    #         try:
+    #             new_res = self.hs.create()
+    #             resIdentifier = new_res.resource_id
+    #             self.resources[resIdentifier] = new_res
+    #             logging.info(f"created {resIdentifier}")
+    #             response.success()
+    #         except Exception as e:
+    #             logging.error(f"Error creating resource")
+    #             # mark as a locust failure
+    #             response.failure(f"Error creating resource: {e}")
+
+    @task
+    @tag("async")
+    def download(self):
+        if not self.resources:
+            return
+        with self.client.get("/download", catch_response=True) as response:
+            res_key = random.choice(list(self.resources.keys()))
+            try:
+                self.resources[res_key].download()
+                logging.info(f"downloaded {res_key}")
+                response.success()
+            except Exception as e:
+                logging.error(f"Error downloading resource: {e}")
+                # mark as a locust failure
+                response.failure(f"Error downloading resource: {e}")
+
+    @task
+    @tag("async")
+    def copy(self):
+        if not self.resources:
+            return
+        with self.client.get("/copy", catch_response=True) as response:
+            res_key = random.choice(list(self.resources.keys()))
+            try:
+                self.resources[res_key].copy()
+                logging.info(f"copied {res_key}")
+                response.success()
+            except Exception as e:
+                logging.error(f"Error copying resource: {e}")
+                # mark as a locust failure
+                response.failure(f"Error copying resource: {e}")
+
+    @task
+    @tag("async")
+    def add_files(self):
+        if not self.resources:
+            return
+        with self.client.get("/add_files", catch_response=True) as response:
+            res_key = random.choice(list(self.resources.keys()))
+            try:
+                self.resources[res_key].file_upload("locustfile.py")
+                logging.info(f"uploaded file to {res_key}")
+                response.success()
+            except Exception as e:
+                logging.error(f"Error adding files to resource: {e}")
+                # mark as a locust failure
+                response.failure(f"Error adding files to resource: {e}")
+
+    @task
+    @tag("async")
+    def delete(self):
+        if self.resources:
+            res_key = random.choice(list(self.resources.keys()))
+            res = self.resources.pop(res_key)
+            print(f"deleted {res}")
+            res.delete()
