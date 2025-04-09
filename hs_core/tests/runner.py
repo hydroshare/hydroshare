c68092f9e2637133b4c760dd561986d67dabda28
Devin Cowan <dcowan@cuahsi.org>
2025-04-09 10:57:32 -0400

    Implement custom XML test runner and update test execution in Jenkins

--- hs_core/tests/runner.py
+++ hs_core/tests/runner.py
@@ -1,6 +1,7 @@
 
 from django.test.runner import DiscoverRunner
 from django.conf import settings
+from xmlrunner.extra.djangotestrunner import XMLTestRunner
 
 from celery import current_app
 
@@ -46,3 +47,11 @@ class CustomTestSuiteRunner(DiscoverRunner):
                            and (not app.startswith('django.') and not app.startswith('mezzanine.'))]
 
         return super(CustomTestSuiteRunner, self).run_tests(test_labels, extra_tests, **kwargs)
+
+
+class CustomXmlRunner(CustomTestSuiteRunner, XMLTestRunner):
+    """
+    Custom test runner that uses XMLTestRunner to output results in XML format.
+    This is useful for CI/CD pipelines and other automated testing environments.
+    """
+    pass
