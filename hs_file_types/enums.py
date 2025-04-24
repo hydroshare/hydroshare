e82f28920047ccc7af7e31f5b98e7085fe64d62e
pkdash <pabitra.dash@usu.edu>
2025-04-24 14:30:13 -0400

    [#5875] adding metadata json file name pattern to meta file path enum class

--- hs_file_types/enums.py
+++ hs_file_types/enums.py
@@ -5,3 +5,5 @@ class AggregationMetaFilePath(str, enum.Enum):
     RESMAP_FILE_ENDSWITH = "_resmap.xml"
     METADATA_FILE_ENDSWITH = "_meta.xml"
     SCHEMA_JSON_FILE_ENDSWITH = "_schema.json"
+    METADATA_JSON_FILE_NAME = "hs_user_metadata.json"
+    METADATA_JSON_FILE_ENDSWITH = ".hs_user_metadata.json"
