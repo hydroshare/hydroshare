c79d95835341db63ed1913c7a26e4b197092baa5
sblack-usu <sblack@cuahsi.org>
2025-05-09 14:25:27 -0600

    add quota migration script

---
+++ theme/management/commands/migrate_quota_to_minio.py
@@ -0,0 +1,28 @@
+import subprocess
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ValidationError
+
+from theme.models import UserQuota
+
+
+def _convert_unit(unit):
+    if len(unit) == 2:
+        return f'{unit[0]}i{unit[1]}'
+    return unit
+
+class Command(BaseCommand):
+    """
+    Migrate quota to minio
+    """
+    help = "Migrate quota to minio"
+
+    def handle(self, *args, **options):
+        for quota in UserQuota.objects.all():
+            try:
+                subprocess.run(
+                    ["mc", "quota", "set", f"{quota.zone}/{quota.user.userprofile.bucket_name}",
+                    "--size", f"{quota.allocated_value}{_convert_unit(quota.unit)}"],
+                    check=True,
+                )
+            except subprocess.CalledProcessError as e:
+                raise ValidationError(f"Error setting quota: {e}")
