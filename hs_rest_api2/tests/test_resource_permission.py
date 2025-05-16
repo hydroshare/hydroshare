733c6232a82a82c217ebd35ad18fc39b3f90e0c8
pkdash <pabitra.dash@usu.edu>
2025-05-16 17:21:13 -0400

    [#5908] adding comments to test cases

--- hs_rest_api2/tests/test_resource_permission.py
+++ hs_rest_api2/tests/test_resource_permission.py
@@ -20,28 +20,40 @@ class TestResourcePermission(HSRESTTestCase):
         )
 
         # Create test groups
-        self.edit_group = self.user.uaccess.create_group(
-            title='edit_group',
+        # a group that will have edit permission on the resource
+        self.res_edit_group = self.user.uaccess.create_group(
+            title='res_edit_group',
             description='Group for resource edit permission testing'
         )
-        self.view_group = self.user.uaccess.create_group(
-            title='view_group',
+        # a group that will have view permission on the resource
+        self.res_view_group = self.user.uaccess.create_group(
+            title='res_view_group',
             description='Group for resource view permission testing'
         )
 
         # Create users
-        self.edit_user = User.objects.create_user(
-            username='edituser',
-            email='edituser@example.com',
-            password='edituser'
+        # a user that will have edit permission on the resource
+        self.res_edit_user = User.objects.create_user(
+            username='res-edit-user',
+            email='res-edit-user@example.com',
+            password='res-edit-user'
         )
 
-        self.view_user = User.objects.create_user(
-            username='viewuser',
-            email='viewuser@example.com',
-            password='viewuser'
+        # a user that will have view permission on the resource
+        self.res_view_user = User.objects.create_user(
+            username='res-view-user',
+            email='res-view-user@example.com',
+            password='res-view-user'
         )
 
+        # a user that will have either view or edit permission on the resource
+        self.res_view_or_edit_user = User.objects.create_user(
+            username='res-view-or-edit-user',
+            email='res-view-or-edit-user@example.com',
+            password='res-view-or-edit-user'
+        )
+
+        # a user that will have no permission on the resource
         self.no_perm_user = User.objects.create_user(
             username='nopermuser',
             email='nopermuser@example.com',
@@ -49,45 +61,59 @@ class TestResourcePermission(HSRESTTestCase):
         )
 
         # Create group users
-        self.edit_user_via_group = User.objects.create_user(
-            username='groupedituser',
-            email='groupedituser@example.com',
-            password='groupedituser'
+        # a user that will have edit permission on the resource via group membership
+        self.res_edit_user_via_group = User.objects.create_user(
+            username='res-edit-user-via-group',
+            email='res-edit-user-via-group@example.com',
+            password='res-edit-user-via-group'
         )
-        self.view_user_via_group = User.objects.create_user(
-            username='groupviewuser',
-            email='groupviewuser@example.com',
-            password='groupviewuser'
+        # a user that will have view permission on the resource via group membership
+        self.res_view_user_via_group = User.objects.create_user(
+            username='res-view-user-via-group',
+            email='res-view-user-via-group@example.com',
+            password='res-view-user-via-group'
         )
 
         # Add users to groups using proper access control
+        # user res_edit_user_via_group has view permission (PrivilegeCodes.VIEW) on res_edit_group (won't be
+        # able to edit group membership)
         self.user.uaccess.share_group_with_user(
-            self.edit_group,
-            self.edit_user_via_group,
-            PrivilegeCodes.VIEW  # edit_user_via_group has view permission on edit_group
-        )
-        self.user.uaccess.share_group_with_user(
-            self.view_group,
-            self.view_user_via_group,
-            PrivilegeCodes.CHANGE  # view_user_via_group has edit permission on view_group
+            self.res_edit_group,
+            self.res_edit_user_via_group,
+            PrivilegeCodes.VIEW
         )
 
-        # Set individual permissions
-        self.user.uaccess.share_resource_with_user(
-            self.res, self.edit_user, PrivilegeCodes.CHANGE)
-
-        self.user.uaccess.share_resource_with_user(
-            self.res, self.view_user, PrivilegeCodes.VIEW)
-
-        # Set group permissions
-        self.user.uaccess.share_resource_with_group(
-            self.res,
-            self.edit_group,
+        # user res_view_user_via_group has edit permission (PrivilegeCodes.CHANGE) on res_view_group (will be
+        # able to edit group membership)
+        self.user.uaccess.share_group_with_user(
+            self.res_view_group,
+            self.res_view_user_via_group,
             PrivilegeCodes.CHANGE
         )
+
+        # Set individual permissions on the resource
+        # grant res_edit_user edit permission on the resource
+        self.user.uaccess.share_resource_with_user(
+            self.res, self.res_edit_user, PrivilegeCodes.CHANGE)
+
+        # grant res_view_user view permission on the resource
+        self.user.uaccess.share_resource_with_user(
+            self.res, self.res_view_user, PrivilegeCodes.VIEW)
+
+        # Set group permissions on the resource
+        # grant res_edit_group edit permission on the resource - members of res_edit_group have edit
+        # permission on the resource
         self.user.uaccess.share_resource_with_group(
             self.res,
-            self.view_group,
+            self.res_edit_group,
+            PrivilegeCodes.CHANGE
+        )
+
+        # grant res_view_group view permission on the resource - members of res_view_group have view
+        # permission on the resource
+        self.user.uaccess.share_resource_with_group(
+            self.res,
+            self.res_view_group,
             PrivilegeCodes.VIEW
         )
 
@@ -95,32 +121,32 @@ class TestResourcePermission(HSRESTTestCase):
         self.url = "/hsapi2/resource/{}/permission/json/".format(self.res.short_id)
 
     def test_get_permission_owner(self):
-        # Test owner permission
+        # Test owner permission on the resource
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_json = json.loads(response.content.decode())
         self.assertEqual(response_json['permission'], 'owner')
 
     def test_get_permission_edit(self):
-        # Test edit permission
+        # Test edit permission on the resource
         self.client.logout()
-        self.client.login(username='edituser', password='edituser')
+        self.client.login(username='res-edit-user', password='res-edit-user')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_json = json.loads(response.content.decode())
         self.assertEqual(response_json['permission'], 'edit')
 
     def test_get_permission_view(self):
-        # Test view permission
+        # Test view permission on the resource
         self.client.logout()
-        self.client.login(username='viewuser', password='viewuser')
+        self.client.login(username='res-view-user', password='res-view-user')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_json = json.loads(response.content.decode())
         self.assertEqual(response_json['permission'], 'view')
 
     def test_get_permission_none(self):
-        # Test no permission
+        # Test no permission on the resource
         self.client.logout()
         self.client.login(username='nopermuser', password='nopermuser')
         response = self.client.get(self.url)
@@ -137,7 +163,7 @@ class TestResourcePermission(HSRESTTestCase):
         self.assertEqual(response_json['permission'], 'none')
 
     def test_get_permission_anonymous_user(self):
-        # Test no permission
+        # Test no permission on the resource
         self.client.logout()
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -145,18 +171,18 @@ class TestResourcePermission(HSRESTTestCase):
         self.assertEqual(response_json['permission'], 'none')
 
     def test_get_permission_group_edit(self):
-        # Test edit permission through group membership
+        # Test edit permission on the resource through group membership
         self.client.logout()
-        self.client.login(username='groupedituser', password='groupedituser')
+        self.client.login(username='res-edit-user-via-group', password='res-edit-user-via-group')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_json = json.loads(response.content.decode())
         self.assertEqual(response_json['permission'], 'edit')
 
     def test_get_permission_group_view(self):
-        # Test view permission through group membership
+        # Test view permission on the resource through group membership
         self.client.logout()
-        self.client.login(username='groupviewuser', password='groupviewuser')
+        self.client.login(username='res-view-user-via-group', password='res-view-user-via-group')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_json = json.loads(response.content.decode())
@@ -164,16 +190,30 @@ class TestResourcePermission(HSRESTTestCase):
 
     def test_get_permission_highest_privilege(self):
         # Test user gets highest privilege when they have both direct and group permissions
-        self.user.uaccess.share_group_with_user(
-            self.edit_group,
-            self.view_user,
-            PrivilegeCodes.CHANGE  # view_user has edit permission on edit_group
-        )
 
+        # grant res_view_or_edit_user view permission on the resource
+        self.user.uaccess.share_resource_with_user(
+            self.res, self.res_view_or_edit_user, PrivilegeCodes.VIEW)
+
+        # test res_view_or_edit_user has view permission on the resource
         self.client.logout()
-        self.client.login(username='viewuser', password='viewuser')
+        self.client.login(username='res-view-or-edit-user', password='res-view-or-edit-user')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_json = json.loads(response.content.decode())
-        # view_user has both edit (via group) and view (direct) permission on the resource
+        self.assertEqual(response_json['permission'], 'view')
+
+        # make res_view_or_edit_user a member of res_edit_group - with membership edit permission
+        self.user.uaccess.share_group_with_user(
+            self.res_edit_group,
+            self.res_view_or_edit_user,
+            PrivilegeCodes.CHANGE
+        )
+
+        # test res_view_or_edit_user has edit permission on the resource via group membership
+        self.client.logout()
+        self.client.login(username='res-view-or-edit-user', password='res-view-or-edit-user')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_json = json.loads(response.content.decode())
         self.assertEqual(response_json['permission'], 'edit')
