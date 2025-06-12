7c29edf1069f5ed874d3d0ba7e17b36241d22e8d
Devin Cowan <dcowan@cuahsi.org>
2025-06-12 10:29:57 -0400

    Update TUS_FILE_NAME_FORMAT options to include 'increment'

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -461,7 +461,7 @@ INSTALLED_APPS = (
     'django_tus',
 )
 
-TUS_FILE_NAME_FORMAT = 'keep'  # Other options are: 'random-suffix', 'random', 'keep'
+TUS_FILE_NAME_FORMAT = 'keep'  # Other options are: 'random-suffix', 'random', 'increment'
 TUS_EXISTING_FILE = 'error'  # Other options are: 'overwrite',  'error', 'rename'
 
 # the url for the uppy companion server
