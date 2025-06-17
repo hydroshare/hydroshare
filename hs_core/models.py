c2afcd9fbbb7c63a2b3a97792232bb57765283b8
MSDrao <msandesh0055@gmail.com>
2025-06-17 13:34:25 -0500

    removed trailing white spaces

--- hs_core/models.py
+++ hs_core/models.py
@@ -3998,9 +3998,9 @@ class BaseResource(Page, AbstractResource):
                     return ''
                 else:
                     logger.error(
-                        f"Failed to get funder_id for '{funder_name}'. "
+                        f"Failed to get funder_ƒid for '{funder_name}'. "
                         f"Status code: {response.status_code} for resource id: {self.short_id}"
-                    )                  
+                    )
                     return ''
             except requests.RequestException as e:
                 logger.error(
