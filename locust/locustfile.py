15913040fd3cff950a2e150cdd6a01592929166a
Devin Cowan <dcowan@cuahsi.org>
2025-07-23 17:17:46 -0400

    Increase number of resources created in HSUser and update file upload tasks

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -32,7 +32,7 @@ if LOCUST_HOST:
 
 class HSUser(HttpUser):
     # wait_time = between(1, 2)
-    number_of_resources = 1
+    number_of_resources = 5
     resources = {}
     randname = 0
 
@@ -42,6 +42,12 @@ class HSUser(HttpUser):
         self.client.get("/accounts/login/", verify=False)
         self.hs = hs
         self.resources = {}
+
+        # create 5 resources
+        for _ in range(self.number_of_resources):
+            new_res = self.hs.create()
+            resIdentifier = new_res.resource_id
+            self.resources[resIdentifier] = new_res
 
     def on_stop(self):
         for res in self.resources.values():
@@ -53,36 +59,36 @@ class HSUser(HttpUser):
             logging.info(f"Cleanup file {f}")
             os.remove(f)
 
-    @task
-    @tag('get')
-    def home(self):
-        self.client.get("/home", verify=False)
+    # @task
+    # @tag('get')
+    # def home(self):
+    #     self.client.get("/home", verify=False)
 
-    @task
-    @tag('get')
-    def discover(self):
-        self.client.get("/search", verify=False)
+    # @task
+    # @tag('get')
+    # def discover(self):
+    #     self.client.get("/search", verify=False)
 
-    @task
-    @tag('get')
-    def my_resources(self):
-        self.client.get("/my-resources/?f=owned&f=discovered&f=favorites&f=shared", verify=False)
+    # @task
+    # @tag('get')
+    # def my_resources(self):
+    #     self.client.get("/my-resources/?f=owned&f=discovered&f=favorites&f=shared", verify=False)
 
-    @task
-    @tag('get')
-    def get_resources(self):
-        url = f"/hsapi/resource/?owner={USERNAME}&edit_permission=false&published=false&include_obsolete=false"
-        with self.client.get(url, verify=False, catch_response=True) as response:
-            if response.status_code == 200:
-                # get the resources from the json
-                resources = response.json().get('results')
-                for res in resources:
-                    resIdentifier = res.get('resource_id')
-                    self.resources[resIdentifier] = self.hs.resource(resIdentifier)
-                response.success()
-            else:
-                response.failure("Failed to get resources")
-        pass
+    # @task
+    # @tag('get')
+    # def get_resources(self):
+    #     url = f"/hsapi/resource/?owner={USERNAME}&edit_permission=false&published=false&include_obsolete=false"
+    #     with self.client.get(url, verify=False, catch_response=True) as response:
+    #         if response.status_code == 200:
+    #             # get the resources from the json
+    #             resources = response.json().get('results')
+    #             for res in resources:
+    #                 resIdentifier = res.get('resource_id')
+    #                 self.resources[resIdentifier] = self.hs.resource(resIdentifier)
+    #             response.success()
+    #         else:
+    #             response.failure("Failed to get resources")
+    #     pass
 
     @task
     @tag('post')
@@ -99,47 +105,47 @@ class HSUser(HttpUser):
                 # mark as a locust failure
                 response.failure(f"Error creating resource: {e}")
 
-    @task
-    @tag("async")
-    @tag('post')
-    def download(self):
-        if not self.resources:
-            return
-        with self.client.post("/download", catch_response=True) as response:
-            res_key = random.choice(list(self.resources.keys()))
-            try:
-                self.resources[res_key].download()
-                logging.info(f"downloaded {res_key}")
-                response.success()
-            except Exception as e:
-                logging.error(f"Error downloading resource: {e}")
-                # mark as a locust failure
-                response.failure(f"Error downloading resource: {e}")
+    # @task
+    # @tag("async")
+    # @tag('post')
+    # def download(self):
+    #     if not self.resources:
+    #         return
+    #     with self.client.post("/download", catch_response=True) as response:
+    #         res_key = random.choice(list(self.resources.keys()))
+    #         try:
+    #             self.resources[res_key].download()
+    #             logging.info(f"downloaded {res_key}")
+    #             response.success()
+    #         except Exception as e:
+    #             logging.error(f"Error downloading resource: {e}")
+    #             # mark as a locust failure
+    #             response.failure(f"Error downloading resource: {e}")
+
+    # @task
+    # @tag("async")
+    # @tag('post')
+    # def copy(self):
+    #     if not self.resources:
+    #         return
+    #     with self.client.post("/copy", catch_response=True) as response:
+    #         res_key = random.choice(list(self.resources.keys()))
+    #         try:
+    #             self.resources[res_key].copy()
+    #             logging.info(f"copied {res_key}")
+    #             response.success()
+    #         except Exception as e:
+    #             logging.error(f"Error copying resource: {e}")
+    #             # mark as a locust failure
+    #             response.failure(f"Error copying resource: {e}")
 
     @task
     @tag("async")
     @tag('post')
-    def copy(self):
+    def add_small_file(self):
         if not self.resources:
             return
-        with self.client.post("/copy", catch_response=True) as response:
-            res_key = random.choice(list(self.resources.keys()))
-            try:
-                self.resources[res_key].copy()
-                logging.info(f"copied {res_key}")
-                response.success()
-            except Exception as e:
-                logging.error(f"Error copying resource: {e}")
-                # mark as a locust failure
-                response.failure(f"Error copying resource: {e}")
-
-    @task
-    @tag("async")
-    @tag('post')
-    def add_files(self):
-        if not self.resources:
-            return
-        with self.client.post("/add_files", catch_response=True) as response:
+        with self.client.post("/add_small_file", catch_response=True) as response:
             res_key = random.choice(list(self.resources.keys()))
             # generate a random file name
             filename = f"{self.randname}-locustfile.py"
@@ -161,17 +167,69 @@ class HSUser(HttpUser):
     @task
     @tag("async")
     @tag('post')
-    def delete(self):
+    def add_1gb_file(self):
         if not self.resources:
             return
-        with self.client.post("/delete_res", catch_response=True) as response:
+        with self.client.post("/add_1gb_file", catch_response=True) as response:
+            res_key = random.choice(list(self.resources.keys()))
+            # generate a random file name
+            filename = f"{self.randname}-locustfile.py"
+            self.randname = self.randname + 1
+            # create a 1GB file locally
+            with open(filename, "wb") as f:
+                f.seek(1024 * 1024 * 1024 - 1)
+                f.write(b"\0")
             try:
-                res_key = random.choice(list(self.resources.keys()))
-                res = self.resources.pop(res_key)
-                res.delete()
-                print(f"deleted {res}")
+                self.resources[res_key].file_upload(filename)
+                logging.info(f"uploaded file to {res_key}")
                 response.success()
             except Exception as e:
-                logging.error(f"Error deleting resource: {e}")
+                logging.error(f"Error adding files to resource: {e}")
                 # mark as a locust failure
-                response.failure(f"Error deleting resource: {e}")
+                response.failure(f"Error adding files to resource: {e}")
+            # cleanup the file
+            os.remove(filename)
+
+    @task
+    @tag("async")
+    @tag('post')
+    def add_2gb_file(self):
+        if not self.resources:
+            return
+        with self.client.post("/add_2gb_file", catch_response=True) as response:
+            res_key = random.choice(list(self.resources.keys()))
+            # generate a random file name
+            filename = f"{self.randname}-locustfile.py"
+            self.randname = self.randname + 1
+            # create a 2GB file locally
+            with open(filename, "wb") as f:
+                f.seek(2 * 1024 * 1024 * 1024 - 1)
+                f.write(b"\0")
+            try:
+                self.resources[res_key].file_upload(filename)
+                logging.info(f"uploaded file to {res_key}")
+                response.success()
+            except Exception as e:
+                logging.error(f"Error adding files to resource: {e}")
+                # mark as a locust failure
+                response.failure(f"Error adding files to resource: {e}")
+            # cleanup the file
+            os.remove(filename)
+
+    # @task
+    # @tag("async")
+    # @tag('post')
+    # def delete(self):
+    #     if not self.resources:
+    #         return
+    #     with self.client.post("/delete_res", catch_response=True) as response:
+    #         try:
+    #             res_key = random.choice(list(self.resources.keys()))
+    #             res = self.resources.pop(res_key)
+    #             res.delete()
+    #             print(f"deleted {res}")
+    #             response.success()
+    #         except Exception as e:
+    #             logging.error(f"Error deleting resource: {e}")
+    #             # mark as a locust failure
+    #             response.failure(f"Error deleting resource: {e}")
