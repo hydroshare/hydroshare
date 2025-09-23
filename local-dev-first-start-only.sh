db803cc5f55ab80380b06639e08f7c2044073b9e
Devin Cowan <dcowan@cuahsi.org>
2025-09-22 20:44:55 -0400

    add discovery-atlas hostname to local development setup and update container configurations

--- local-dev-first-start-only.sh
+++ local-dev-first-start-only.sh
@@ -166,9 +166,9 @@ else
 fi
 
 DOCKER_COMPOSER_YAML_FILE='local-dev.yml'
-HYDROSHARE_CONTAINERS=(hydroshare defaultworker rabbitmq postgis companion redis nginx minio micro-auth pgbouncer)
+HYDROSHARE_CONTAINERS=(hydroshare defaultworker rabbitmq postgis companion redis nginx minio micro-auth pgbouncer discovery-atlas)
 HYDROSHARE_VOLUMES=(hydroshare_postgis_data_vol hydroshare_rabbitmq_data_vol hydroshare_share_vol hydroshare_temp_vol hydroshare_minio_data_vol hydroshare_redis_data_vol hydroshare_companion_vol)
-HYDROSHARE_IMAGES=(hydroshare-defaultworker hydroshare-hydroshare postgis/postgis rabbitmq nginx redis transloadit/companion minio/minio edoburu/pgbouncer hydroshare/micro-auth)
+HYDROSHARE_IMAGES=(hydroshare-defaultworker hydroshare-hydroshare postgis/postgis rabbitmq nginx redis transloadit/companion minio/minio edoburu/pgbouncer hydroshare/micro-auth hydroshare-discovery-atlas)
 
 NODE_CONTAINER_RUNNING=`docker ps -a | grep nodejs`
 
