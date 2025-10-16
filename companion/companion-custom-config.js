b3c097a35edab0f2a69b4b1b65146a77f7cc8af4
Devin Cowan <dcowan@cuahsi.org>
2025-10-16 18:29:28 -0400

    Enhance S3 bucket configuration to support dynamic retrieval from request headers and update local development settings for Companion to use http instead of SSL

--- companion/companion-custom-config.js
+++ companion/companion-custom-config.js
@@ -5,9 +5,13 @@ import { defaultOptions, getMaskableSecrets, validateConfig } from './default-co
 
 const bucket = ({ filename, metadata, req }) => {
   // https://uppy.io/docs/companion/#s3bucket-companion_aws_bucket
-  console.log('dynamically setting bucket for filename:', filename, 'metadata:', metadata);
   // Use dynamic bucket from metadata or fall back to env var
-  const bucket = metadata?.bucket_name || process.env.COMPANION_AWS_BUCKET;
+  let bucket = metadata?.bucket_name || process.env.COMPANION_AWS_BUCKET;
+
+  // if the bucket is still not set, get it from the req headers
+  if (!bucket && req && req.headers['s3-bucket']) {
+    bucket = req.headers['s3-bucket'];
+  }
   
   if (!bucket) {
     throw new Error('No bucket specified and COMPANION_AWS_BUCKET not set');
@@ -17,7 +21,6 @@ const bucket = ({ filename, metadata, req }) => {
 
 // https://uppy.io/docs/companion/#s3getkey-filename-metadata-req-
 const getKey = ({ filename, metadata, req }) => {
-  console.log('dynamically setting key for filename:', filename, 'metadata:', metadata);
   // Use dynamic key from metadata or default to filename
   const key = metadata?.dynamic_key || filename;
 
