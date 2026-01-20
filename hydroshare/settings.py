d847a7bcd5248960ac718e9ac9f91fa4bdb9d79c
Devin Cowan <dcowan@cuahsi.org>
2026-01-20 08:47:04 -0500

    Add TODO comment to remove SOLR settings after redpanda integration

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -429,7 +429,7 @@ INSTALLED_APPS = (
     "crispy_forms",
     "crispy_bootstrap3",
     "mezzanine.accounts",
-    "haystack",
+    "haystack",  # TODO: remove haystack after redpanda is in the stack
     "rest_framework",
     "robots",
     "sorl.thumbnail",
@@ -691,6 +691,7 @@ REST_FRAMEWORK = {
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.NamespaceVersioning",
 }
 
+# TODO remove these settings once redpanda is in the stack
 SOLR_HOST = os.environ.get("SOLR_PORT_8983_TCP_ADDR", "localhost")
 SOLR_PORT = "8983"
 HAYSTACK_CONNECTIONS = {
