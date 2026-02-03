25c5977e1bccc91aae3e3d3b36ac9a490b57505a
sblack-usu <sblack@cuahsi.org>
2026-02-02 17:42:02 -0700

    add access token creation

--- hs_rest_api/views/service_account_minio.py
+++ hs_rest_api/views/service_account_minio.py
@@ -1,9 +1,9 @@
-import requests
 import logging
 from django_s3.utils import bucket_and_name
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from rest_framework import status
 from hs_core.models import get_user
 from django.conf import settings
@@ -22,39 +22,22 @@ class MinIOServiceAccounts(APIView):
             return Response({"detail": "Authentication credentials were not provided."},
                             status=status.HTTP_401_UNAUTHORIZED)
         user = get_user(request)
-        # using bucketname as the usnername since it is character safe and derived from the username
-        response = requests.post(micro_auth_service_url, json={"username": user.userprofile.bucket_name})
-        return Response(response.json(), status=response.status_code)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        print(token.key)
+        response_json = {"access_key": user.username, "secret_key": token.key}
+        return Response(response_json, status=201)
 
     @swagger_auto_schema(operation_description="Lists service accounts for the user")
     def get(self, request):
-        if not request.user.is_authenticated:
-            return Response({"detail": "Authentication credentials were not provided."},
-                            status=status.HTTP_401_UNAUTHORIZED)
-        user = get_user(request)
-        # using bucketname as the username since it is character safe and derived from the username
-        response = requests.get(micro_auth_service_url + user.userprofile.bucket_name)
-        if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
-        return Response({"detail": "Failed to retrieve service accounts."}, status=response.status_code)
+        raise NotImplementedError("Listing service accounts is not implemented yet.")
 
 
 class MinIOServiceAccountsDelete(APIView):
 
     @swagger_auto_schema(operation_description="Delete a service account for the user")
     def delete(self, request, service_account_key):
-        if not request.user.is_authenticated:
-            return Response({"detail": "Authentication credentials were not provided."},
-                            status=status.HTTP_401_UNAUTHORIZED)
-        user = get_user(request)
-        # using bucketname as the usnername since it is character safe and derived from the username
-        response = requests.get(micro_auth_service_url + user.userprofile.bucket_name)
-        response_json = response.json()
-        service_account_keys = [sa['access_key'] for sa in response_json['service_accounts']]
-        if service_account_key not in service_account_keys:
-            return Response({"detail": "Service account key not found."}, status=status.HTTP_404_NOT_FOUND)
-        response = requests.delete(micro_auth_service_url + service_account_key)
-        return Response(status=response.status_code)
+        raise NotImplementedError("Deleting service accounts is not implemented yet.")
 
 
 class MinIOResourceBucketAndPrefix(APIView):
