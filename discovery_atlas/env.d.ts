1c7a1984a2c7e78029905c83191d123c8bf3cfd3
sblack-usu <sblack@cuahsi.org>
2026-02-20 13:06:09 -0700

    rename vite static url environment variable

--- discovery_atlas/env.d.ts
+++ discovery_atlas/env.d.ts
@@ -1,5 +1,5 @@
 declare const VITE_APP_NAME: string;
 declare const VITE_APP_BASE: string;
-declare const VITE_STATIC_URL: string;
+declare const VITE_APP_STATIC_URL: string;
 declare const VITE_APP_URL: string;
 declare const VITE_APP_API_URL: string;
