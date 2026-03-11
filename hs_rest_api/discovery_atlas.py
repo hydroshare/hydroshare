cb757658a401e79556dc8604c9eed26aae97b99f
sblack-usu <sblack@cuahsi.org>
2026-03-10 18:25:08 -0600

    commit unmerged default sorting when no term in discovery

--- hs_rest_api/discovery_atlas.py
+++ hs_rest_api/discovery_atlas.py
@@ -271,7 +271,10 @@ class SearchQuery(BaseModel):
         order = 1 if self.order == "asc" else -1
 
         # These sorts can occur inside the $search stage
-        if self.sortBy == "name":
+        if not self.term and not self.has_filters:
+            # override search to most recently modified if no search criteria is provided
+            search_stage["$search"]['sort'] = {"dateModified": -1}
+        elif self.sortBy == "name":
             search_stage["$search"]['sort'] = {"name": order}
         elif self.sortBy == "dateCreated":
             search_stage["$search"]['sort'] = {"dateCreated": order}
