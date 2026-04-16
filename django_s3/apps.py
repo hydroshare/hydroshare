918d7924aea5cc8e34a470d60169b107fdb03d01
sblack-usu <sblack@cuahsi.org>
2026-04-16 14:29:20 -0600

    restore bucket event set on bucket creation

--- django_s3/apps.py
+++ django_s3/apps.py
@@ -1,3 +1,4 @@
+import subprocess
 from django.apps import AppConfig
 from .settings import get_resource_s3_zones_config
 
@@ -15,3 +16,5 @@ class DjangoS3AppConfig(AppConfig):
                 istorage.connection(zone_name).meta.client.head_bucket(Bucket=bucket_name)
             except Exception:
                 istorage.connection(zone_name).create_bucket(Bucket=bucket_name)
+                subprocess.run(["mc", "event", "add", f"{zone_name}/{bucket_name}",
+                            "arn:minio:sqs::RESOURCEFILE:kafka", "--event", "put,delete"], check=True)
