7264c874fd4f9c1f487a330b6f056cb63e5f083b
sblack-usu <sblack@cuahsi.org>
2026-04-16 14:40:37 -0600

    flake8

--- django_s3/apps.py
+++ django_s3/apps.py
@@ -17,4 +17,4 @@ class DjangoS3AppConfig(AppConfig):
             except Exception:
                 istorage.connection(zone_name).create_bucket(Bucket=bucket_name)
                 subprocess.run(["mc", "event", "add", f"{zone_name}/{bucket_name}",
-                            "arn:minio:sqs::RESOURCEFILE:kafka", "--event", "put,delete"], check=True)
+                                "arn:minio:sqs::RESOURCEFILE:kafka", "--event", "put,delete"], check=True)
