b5eed01981fd041a51608bde4ba81ee16f0fc4e1
pkdash <pabitra.dash@usu.edu>
2026-04-23 18:16:48 -0400

    new helper function for metadata object determination with user metadata file

--- hs_extract/hsextract/content_types/__init__.py
+++ hs_extract/hsextract/content_types/__init__.py
@@ -1,3 +1,6 @@
+import os
+from typing import Union
+
 from hsextract.content_types.raster.models import RasterMetadataObject
 from hsextract.content_types.singlefile.models import SingleFileMetadataObject
 from hsextract.content_types.fileset.models import FileSetMetadataObject
@@ -5,6 +8,7 @@ from hsextract.content_types.timeseries.models import TimeSeriesMetadataObject
 from hsextract.content_types.netcdf.models import NetCDFMetadataObject
 from hsextract.content_types.feature.models import FeatureMetadataObject
 from hsextract.content_types.models import BaseMetadataObject
+
 
 metadata_classes = [
     RasterMetadataObject,
@@ -16,10 +20,42 @@ metadata_classes = [
     FileSetMetadataObject
 ]
 
+ReturnMetaObjectType = Union[
+    RasterMetadataObject,
+    TimeSeriesMetadataObject,
+    NetCDFMetadataObject,
+    FeatureMetadataObject,
+    SingleFileMetadataObject,
+    FileSetMetadataObject,
+    BaseMetadataObject
+]
 
-def determine_metadata_object(file_object_path: str, file_updated: bool,
-                              file_user_meta: bool = False) -> BaseMetadataObject:
+def determine_metadata_object(file_object_path: str, file_updated: bool) -> ReturnMetaObjectType:
     for metadata_class in metadata_classes:
         if metadata_class.is_content_type(file_object_path):
-            return metadata_class(file_object_path, file_updated, file_user_meta)
+            return metadata_class(file_object_path, file_updated)
     return BaseMetadataObject(file_object_path, file_updated)
+
+
+def determine_metadata_object_from_user_metadata(
+        user_metadata_file_path: str, file_updated: bool
+) -> ReturnMetaObjectType:
+    if not user_metadata_file_path.endswith("user_metadata.json"):
+        return BaseMetadataObject(user_metadata_file_path, file_updated)
+
+    bucket_path, relative_user_meta_path = user_metadata_file_path.split("/.hsmetadata/", 1)
+    is_fileset_user_metadata = False
+    if user_metadata_file_path.endswith("/user_metadata.json"):
+        is_fileset_user_metadata = True
+        # fileset user metadata
+        relative_content_path = relative_user_meta_path[:-len("/user_metadata.json")]
+    else:
+        # all other content type user metadata
+        relative_content_path = relative_user_meta_path[:-len(".user_metadata.json")]
+    content_path = os.path.join(bucket_path, "data", "contents", relative_content_path)
+    if not is_fileset_user_metadata:
+        # check content path exists, if not return BaseMetadataObject (unknown content type)
+        from hsextract.utils.s3 import exists
+        if not exists(content_path):
+            return BaseMetadataObject(content_path, file_updated)
+    return determine_metadata_object(content_path, file_updated)
